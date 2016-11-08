334ab45e1ed46522ecaf8fd31a3d31ff076b2528
bcurran3 <bcurran3@yahoo.com>
2016-11-08 02:59:09 -0800

    Add files via upload

---
+++ irfanview/tools/chocolateyUninstall.ps1
@@ -0,0 +1,23 @@
+﻿$packageName = 'irfanview'
+$softwareName = "$packageName *"
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)
+
+$scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ahkFile = Join-Path $scriptPath "chocolateyUninstall.ahk"
+$ahkExe = 'AutoHotKey'
+$ahkRun = "$Env:Temp\$(Get-Random).ahk"
+Copy-Item $ahkFile "$ahkRun" -Force
+Start-Process $ahkExe $ahkRun
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+$key | ForEach-Object {
+  Uninstall-ChocolateyPackage -PackageName $packageName `
+                              -FileType $installerType `
+                              -SilentArgs $($silentArgs) `
+                              -File $($_.UninstallString.Replace('"','')) `
+                              -ValidExitCodes $validExitCodes
+}
+
+Remove-Item "$ahkRun" -Force
