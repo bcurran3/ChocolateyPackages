ec7ddefc90016bcd87df7e9f8655ba8acb83d779
bcurran3 <bcurran3@yahoo.com>
2016-11-09 02:42:01 -0800

    v20.0.25 update

--- take-command/tools/chocolateyinstall.ps1
+++ take-command/tools/chocolateyinstall.ps1
@@ -2,6 +2,7 @@
 $packageName= 'take-command'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://jpsoft.com/downloads/v20/tcmd.exe'
+$checksum   = 'D0ED94DB96C65EB726ABB5A3BE31B10817755326198921EE2DE6A6F1B582B964'
 
 
 $packageArgs = @{
@@ -9,7 +10,7 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  checksum      = 'FE9D14B2FE6732A1E909986F1EFABDED5615CA4769849D63CA61F4AF1B4F4318'
+  checksum      = $checksum 
   checksumType  = 'sha256'
   softwareName  = 'Take Command*'
   silentArgs   = '/PASSIVE'
