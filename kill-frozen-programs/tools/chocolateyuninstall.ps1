2a17187296fe06ec3f207fd8b4e1517ceb4b48d4
bcurran3 <bcurran3@yahoo.com>
2016-11-10 02:57:33 -0800

    Add files via upload

---
+++ kill-frozen-programs/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName= 'kill-frozen-programs'
+$IconName   = 'Kill Frozen Programs'
+
+remove-item "$env:Public\Desktop\$IconName.lnk" -Force -ErrorAction 'SilentlyContinue'
+remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$IconName.lnk" -Force -ErrorAction 'SilentlyContinue'
