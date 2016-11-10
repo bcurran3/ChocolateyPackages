b5c8ff59c1d2ab05325b5db78229ab6849c36275
bcurran3 <bcurran3@yahoo.com>
2016-11-10 02:32:56 -0800

    Add files via upload

---
+++ hardware-identify/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName= 'hardware-identify'
+$softwareName = 'Tweaking.com - Hardware Identify*'
+$installerType = 'EXE'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.tweaking.com/files/setups/tweaking.com_hardware_identify_setup.exe' 
+$checksum   = '7A242007EF5A46E9CF003EBA91143FEBA25F4B888CEF13170DECA5465E7ED6E2'
+$silentArgs = '/S'
+$validExitCodes= @(0)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  validExitCodes= $validExitCodes
+  silentArgs    = $silentArgs
+  softwareName  = $softwareName
+  checksum      = $checksum
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs  
+  
