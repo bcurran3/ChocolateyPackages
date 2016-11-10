3485dc2d5ff8e2136526808e14cf3f1ce2253468
bcurran3 <bcurran3@yahoo.com>
2016-11-10 02:57:24 -0800

    Add files via upload

---
+++ change-dns-servers/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName= 'change-dns-servers'
+$IconName   = 'Change DNS Servers'
+
+remove-item "$env:Public\Desktop\$IconName.lnk" -Force -ErrorAction 'SilentlyContinue'
+remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$IconName.lnk" -Force -ErrorAction 'SilentlyContinue'
