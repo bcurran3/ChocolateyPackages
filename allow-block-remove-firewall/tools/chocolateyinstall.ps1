a940ddb78742a022c2e435955b0f9689653a0189
bcurran3 <bcurran3@yahoo.com>
2016-11-10 02:33:15 -0800

    Add files via upload

---
+++ allow-block-remove-firewall/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName= 'allow-block-remove-firewall'
+$installerType = 'ZIP'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.tweaking.com/files/setups/Tweaking.com-Right-Click_Allow_Block_or_Remove-Windows_Firewall.zip' 
+$checksum   = '73E92E56E0D6E4C6D5DB49706D671587FB957FEB6EE4DC7100339959569834FA'
+
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = $installerType
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+
+  
