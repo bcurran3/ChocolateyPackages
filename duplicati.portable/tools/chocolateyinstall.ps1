bc6eccbe8003ca112c2283d577f8f160b3b57ade
bcurran3 <bcurran3@yahoo.com>
2016-11-13 15:48:51 -0800

    Add files via upload

--- duplicati.portable/tools/chocolateyinstall.ps1
+++ duplicati.portable/tools/chocolateyinstall.ps1
@@ -1,22 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName   = 'duplicati.portable'
-$installerType = 'zip'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url           = 'http://updates.duplicati.com/experimental/duplicati-2.0.1.30_experimental_2016-11-03.zip' 
 $checksum      = '6822850407C48511E879DFD546EA73F6534A42BFAD1675D8F16DF8D07059B1D7'
-$silentArgs    = '/quiet /qn /norestart'
-$validExitCodes= @(0, 3010, 1641)
+$silentArgs    = ''
+$validExitCodes= @(0)
+$fileLocation  = "$env:ChocolateyInstall\lib\3dmark\tools\3dmark-setup.exe"
 
 $packageArgs = @{
   packageName   = $packageName
-  fileType      = $installerType
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP' 
   url           = $url
-  validExitCodes= $validExitCodes
-  silentArgs    = $silentArgs
-  softwareName  = 'Duplicati 2*'
-  checksum      = $checksum 
-  checksumType  = 'sha256' 
+  checksum      = $checksum
+  checksumType  = 'sha256'
 }
 
-Install-ChocolateyZipPackage @packageArgs  
+Install-ChocolateyZipPackage @packageArgs 
+
   
