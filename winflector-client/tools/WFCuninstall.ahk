de4939d8d86f6654c7181b61a3d707d0fcd890f6
bcurran3 <bcurran3@yahoo.com>
2016-11-12 17:45:52 -0800

    Add files via upload

---
+++ winflector-client/tools/WFCuninstall.ahk
@@ -0,0 +1,11 @@
+#NoEnv 
+#NoTrayIcon
+SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+ 
+WinWaitActive, Uninstall,, 60
+WinActivate
+BlockInput On
+Send !y
+BlockInput Off
+
---
+++ winflector-client/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'winflector-client'
+$softwareName = 'Windows Driver Package - Era software*'
+$installerType = 'EXE' 
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+$validExitCodes = @(0, 3010, 1605, 1614, 1641)
+$file = "$env:appdata\Winflector\uninst\unins000.exe"
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path $toolsDir "WFCuninstall.ahk"
+Start-Process $ahkExe $ahkFile
+
+ Uninstall-ChocolateyPackage -PackageName $packageName `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes `
+                                -File "$file"
+ 
