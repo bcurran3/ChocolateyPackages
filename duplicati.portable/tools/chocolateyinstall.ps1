4e9d726712cbb436cc44babe3dc24d6ee775fcde
bcurran3 <bcurran3@yahoo.com>
2016-11-13 14:45:44 -0800

    Add files via upload

---
+++ duplicati.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName   = 'duplicati.portable'
+$installerType = 'zip'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url           = 'http://updates.duplicati.com/experimental/duplicati-2.0.1.30_experimental_2016-11-03.zip' 
+$checksum      = '6822850407C48511E879DFD546EA73F6534A42BFAD1675D8F16DF8D07059B1D7'
+$silentArgs    = '/quiet /qn /norestart'
+$validExitCodes= @(0, 3010, 1641)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  validExitCodes= $validExitCodes
+  silentArgs    = $silentArgs
+  softwareName  = 'Duplicati 2*'
+  checksum      = $checksum 
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyZipPackage @packageArgs  
+  
