a25bd56be450118501d6673be75a341acbc2a65e
bcurran3 <bcurran3@yahoo.com>
2016-11-14 03:15:16 -0800

    Add files via upload

---
+++ ad-tidy-free/tools/chocolateyinstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName   = 'ad-tidy-free' 
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url           = 'http://www.cjwdev.co.uk/Software/ADTidy/ADTidyInstaller.zip'
+$checksum      = '5E32C0FF305F5432AEA62210C60D601620038E0596248583E1118F61F585D45A'
+$silentArgs    = '/quiet /norestart'
+$validExitCodes= @(0)
+$fileLocation  = "$env:ChocolateyInstall\lib\$packageName\tools\ADTidyFreeInstaller.exe"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs 
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  file          = $fileLocation
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = 'AD Tidy Free Edition*'
+}
+ 
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item "$env:ChocolateyInstall\lib\$packageName\tools\ADTidyFreeInstaller.exe"
