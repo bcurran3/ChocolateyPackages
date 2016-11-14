d332d5e2893f5d32fc9907520f3c378aa3d5e734
bcurran3 <bcurran3@yahoo.com>
2016-11-14 03:51:50 -0800

    Add files via upload

---
+++ googleearth/tools/chocolateyinstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName   = 'googleearth'
+$installerType = 'EXE'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url           = 'https://dl.google.com/earth/client/GE7/release_7_1_7/googleearth-win-bundle-7.1.7.2602.exe' 
+$checksum      = '677483CCCC9C905C982821B9C997C38A9BF6D266FD14048F9CB0A80BAC239B04'
+$silentArgs    = ''
+$validExitCodes= @(0)
+$ahkExe        = 'AutoHotKey'
+$ahkFile       = Join-Path $toolsDir "GoogleEarthInstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE' 
+  url           = $url
+  softwareName  = 'Google Earth*' 
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+}
+
+Install-ChocolateyPackage @packageArgs 
+  
