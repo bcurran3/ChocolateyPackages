ab380c0fdb33ecfa9ca4e0452c04ca272da04820
bcurran3 <bcurran3@yahoo.com>
2016-11-23 00:31:51 -0800

    Add files via upload

--- go-contact-sync-mod/tools/chocolateyinstall.ps1
+++ go-contact-sync-mod/tools/chocolateyinstall.ps1
@@ -1,7 +1,8 @@
 ﻿$packageName= 'go-contact-sync-mod'
 $installerType = 'msi'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://iweb.dl.sourceforge.net/project/googlesyncmod/Releases/3.10.9/SetupGCSM-3.10.9.msi' 
+$url        = 'http://pilotfiber.dl.sourceforge.net/project/googlesyncmod/Releases/3.10.13/SetupGCSM-3.10.13.msi' 
+$checksum   = 'E00F98E9D0F5D644DE4C0F981AE51A2BA95ADC91BB1DDEC0CCE75449AD26554F'
 $silentArgs = '/quiet /qn /norestart'
 $validExitCodes= @(0, 3010, 1641)
 
@@ -13,7 +14,7 @@ $packageArgs = @{
   url           = $url
   silentArgs    = $silentArgs
   softwareName  = 'Go Contact Sync Mod*' 
-  checksum      = '2F2544A37548371E4B397224C9CFA8429E5D3DA909CFEB6AA24BD376CFCE9278'
+  checksum      = $checksum
   checksumType  = 'sha256'  
   }
   
