d42a6ef979369cd5ddec8f785c9a97a96d0a38c4
bcurran3 <bcurran3@yahoo.com>
2016-11-25 14:02:18 -0800

    Add files via upload

---
+++ slickrss-chrome/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,11 @@
+$bits = Get-ProcessorBits
+$packageName = 'slickrss-chrome'
+$extensionID = 'ealjoljnibpdkocmldliaoojpgdkcdob'
+if ($bits -eq 64)
+{
+Remove-Item 'HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID' -Force -ErrorAction SilentlyContinue
+}
+else
+{
+Remove-Item 'HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID' -Force -ErrorAction SilentlyContinue
+}
