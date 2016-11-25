4e0af6b26a56520dfdeb2b7f40dbab76e97cf247
bcurran3 <bcurran3@yahoo.com>
2016-11-25 02:51:16 -0800

    Add files via upload

---
+++ killprocess/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'killprocess'
+
+remove-item "$env:Public\Desktop\KillProcess.lnk" -Force -ErrorAction 'SilentlyContinue'
+remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\KillProcess.lnk" -Force -ErrorAction 'SilentlyContinue'
+
