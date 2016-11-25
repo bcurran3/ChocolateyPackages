a13e5a5bf5e2ae170fa5fe841d0d2c44eabe3b0e
bcurran3 <bcurran3@yahoo.com>
2016-11-25 14:39:30 -0800

    cleaned up, fixed uninstall, added key to show chocolatey package name and installed by chocolatey, checked for pre-existence

--- _chrome_extension_template_powershell_only/tools/ChocolateyInstall.ps1
+++ _chrome_extension_template_powershell_only/tools/ChocolateyInstall.ps1
@@ -1,14 +1,17 @@
 ﻿$bits = Get-ProcessorBits
+$packageName = '-chrome'
 $extensionID = ''
-$packageName = ''
-
 if ($bits -eq 64)
-{
-New-Item -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" -Value “default value”
-New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID\" -Name "update_url" -Value "https://clients2.google.com/service/update2/crx"
-}
-else
-{
-New-Item -Path "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID" -Value “default value”
-New-ItemProperty -Path "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID\" -Name "update_url" -Value "https://clients2.google.com/service/update2/crx"
-}
+   {
+    if (Test-Path -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID")
+       {
+       Write-Host "Extension already installed." -foreground "magenta" –backgroundcolor "blue"
+       } else {
+         New-Item -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" 
+         New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID\" -Name "update_url" -Value "https://clients2.google.com/service/update2/crx"
+         New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID\" -Name "$packageName" -Value "installed with Chocolatey"		 
+         }
+    } else {
+      New-Item -Path "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID" 
+      New-ItemProperty -Path "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID\" -Name "update_url" -Value "https://clients2.google.com/service/update2/crx"
+      }
--- _chrome_extension_template_powershell_only/tools/ChocolateyUninstall.ps1
+++ _chrome_extension_template_powershell_only/tools/ChocolateyUninstall.ps1
@@ -1,12 +1,9 @@
 $bits = Get-ProcessorBits
+$packageName = '-chrome'
 $extensionID = ''
-$packageName = ''
 
-if ($bits -eq 64)
-{
-Remove-Item 'HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID' -Force -ErrorAction SilentlyContinue
-}
-else
-{
-Remove-Item 'HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID' -Force -ErrorAction SilentlyContinue
+if ($bits -eq 64) {
+    Remove-Item "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue
+   }else{
+    Remove-Item "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue
 }
