6eb45d6bfea8ad68a3a354c9c2f532c6673c5a90
bcurran3 <bcurran3@yahoo.com>
2016-11-25 02:28:47 -0800

    Add files via upload

---
+++ disk-wipe/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'disk-wipe'
+
+remove-item "$env:Public\Desktop\DiskWipe.lnk" -Force -ErrorAction 'SilentlyContinue'
+remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\DiskWipe.lnk" -Force -ErrorAction 'SilentlyContinue'
+
