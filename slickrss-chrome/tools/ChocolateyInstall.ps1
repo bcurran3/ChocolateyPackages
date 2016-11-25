ad9784316fb91fdb9c121c63afc006dbdd620230
bcurran3 <bcurran3@yahoo.com>
2016-11-25 14:02:18 -0800

    Add files via upload

---
+++ slickrss-chrome/tools/ChocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$bits = Get-ProcessorBits
+$packageName = 'slickrss-chrome'
+$extensionID = 'ealjoljnibpdkocmldliaoojpgdkcdob'
+if ($bits -eq 64)
+{
+New-Item -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" -Value “default value”
+New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID\" -Name "update_url" -Value "https://clients2.google.com/service/update2/crx"
+}
+else
+{
+New-Item -Path "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID" -Value “default value”
+New-ItemProperty -Path "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID\" -Name "update_url" -Value "https://clients2.google.com/service/update2/crx"
+}
