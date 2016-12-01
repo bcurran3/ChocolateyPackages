79c3f28ba03b2b4aa59a7e693bf44938fb0916ad
bcurran3 <bcurran3@yahoo.com>
2016-12-01 15:43:07 -0800

    Add files via upload

--- irfanviewplugins/tools/chocolateyInstall.ps1
+++ irfanviewplugins/tools/chocolateyInstall.ps1
@@ -1,22 +1,15 @@
-﻿$packageName = 'irfanviewplugins'
-$installerType = 'exe'
-$urlArray = @('https://www.fosshub.com/IrfanView.html/irfanview_plugins_442_setup.exe','https://www.fosshub.com/IrfanView.html/irfanview_plugins_x64_442_setup.exe')
-$url = $urlArray[0]
-$checksum = '18494995dcad42c7227faebdb7b8c09935ff3ad7a57effc5ca3cf16ea3972311'
-$checksumType = 'sha256'
-$url64 = $urlArray[1]
-$checksum64 = '35cbcede45773e324d203fb7d72789570e9460ba4ef50d282084ab00b2332f21'
-$checksumType64 = 'sha256'
+﻿# https://www.fosshub.com/IrfanView.html/irfanview_plugins_442_setup.exe
+$packageName    = 'irfanviewplugins'
+$installerType  = 'exe'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = "$toolsDir\irfanview_plugins_442_setup.exe"
+$checksum       = '18494995dcad42c7227faebdb7b8c09935ff3ad7a57effc5ca3cf16ea3972311'
+$url64          = "$toolsDir\irfanview_plugins_x64_442_setup.exe"
+$checksum64     = '35cbcede45773e324d203fb7d72789570e9460ba4ef50d282084ab00b2332f21'
 $validExitCodes = @(0)
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$silentArgs = "/silent"
+$silentArgs     = "/silent"
 
-if (!(Get-Command Get-UrlFromFosshub -ErrorAction SilentlyContinue)) {
-  Import-Module "$($toolsDir)\Get-UrlFromFosshub.ps1"
-}
-$url = Get-UrlFromFosshub $url
-$url64 = Get-UrlFromFosshub $url64
-						  
+					  
 $packageArgs = @{
   packageName   = $packageName
   fileType      = $installerType
@@ -25,9 +18,12 @@ $packageArgs = @{
   silentArgs    = $silentArgs
   softwareName  = ''
   checksum      = $checksum
-  checksumType  = $checksumType
+  checksumType  = 'sha256'
   Checksum64    = $checksum64
-  ChecksumType64= $checksumType64
+  ChecksumType64= 'sha256'
 }
 
-Install-ChocolateyPackage @packageArgs  						  
+Install-ChocolateyPackage @packageArgs  
+
+Remove-Item $url 
+Remove-Item $url64							  
