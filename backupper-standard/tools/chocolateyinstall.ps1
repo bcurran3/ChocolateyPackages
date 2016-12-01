3acf05e7825f5531d9c6b15600b51902941a4ccb
bcurran3 <bcurran3@yahoo.com>
2016-12-01 00:52:15 -0800

    Add files via upload

---
+++ backupper-standard/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName   = 'backupper-standard'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url           = 'http://www.aomeisoftware.com/download/adb/BackupperFull.exe'
+$checksum      = 'A5AB8076AAF953C7E136D4D479523FA2E2B28E5E951F31A656802ED1A56BD3A5'
+$silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = $installerType
+  url           = $url
+  silentArgs    = $silentArgs
+  softwareName  = 'AOMEI Backupper Standard*' 
+  checksum      = $checksum 
+  checksumType  = 'sha256'  
+  }
+Install-ChocolateyPackage @packageArgs
+
