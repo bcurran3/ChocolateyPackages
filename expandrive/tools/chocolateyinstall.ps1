73f6f4d58939491a547de7306cb6ccc37eb4a6c3
bcurran3 <bcurran3@yahoo.com>
2016-12-10 13:25:02 -0800

    Add files via upload

--- expandrive/tools/chocolateyinstall.ps1
+++ expandrive/tools/chocolateyinstall.ps1
@@ -1,33 +1,20 @@
-﻿$ErrorActionPreference = 'Stop'
-$packageName   = 'expandrive' 
-$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$packageName   = 'expandrive' 
 $url           = 'https://expandrive.s3.amazonaws.com/expandrive_win/v5-4-2_published_2016-11-22_at_17_05_41/ExpanDriveInstaller.msi' 
 $checksum      = 'EE321782F1E27AF1C852BF61D044384824E0AAA6EEE999D9DE4386E811101B6D'
-$UnzippedMSI   = Join-Path $toolsDir 'ExpanDriveInstaller.msi'
 $installerType = 'MSI'
-$silentArgs    = '/passive /norestart ACCEPT=YES'
+$silentArgs = '/quiet /qn /norestart'
 $validExitCodes= @(0, 3010, 1641)
 
 $packageArgs = @{
   packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'ZIP' 
+  fileType      = $installerType
   url           = $url
-  checksum      = $checksum
-  checksumType  = 'sha256'
-}
-
-Install-ChocolateyZipPackage @packageArgs
-
-$packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'msi'
-  file          = $UnzippedMSI
-  silentArgs    = $silentArgs
   validExitCodes= $validExitCodes
-  softwareName  = 'Arq*'   
+  silentArgs    = $silentArgs
+  softwareName  = 'Expandrive*'
+  checksum      = $checksum
+  checksumType  = 'sha256' 
 }
 
-Install-ChocolateyInstallPackage @packageArgs
-
+Install-ChocolateyPackage @packageArgs  
 
