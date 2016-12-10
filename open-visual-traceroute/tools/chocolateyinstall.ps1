6742a6ad09ef629791578cf30c90fab373f390f7
bcurran3 <bcurran3@yahoo.com>
2016-12-10 13:25:12 -0800

    Add files via upload

---
+++ open-visual-traceroute/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName= 'open-visual-traceroute' 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://superb-dca2.dl.sourceforge.net/project/openvisualtrace/1.6.4/OpenVisualTraceroute1.6.4.exe'
+$checksum   = '00D8F43D7A4651ECCBEBDB503C79B5685F0CC34DDEC4F7F26DFCAE6AA6C872A8'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+  softwareName  = 'Open Visual Traceroute*' 
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  }
+Install-ChocolateyPackage @packageArgs
+
