f2d762b614ed16f9f8649656a69b1f0fdfc91271
bcurran3 <bcurran3@yahoo.com>
2016-12-12 04:28:02 -0800

    Add files via upload

--- open-visual-traceroute/tools/chocolateyinstall.ps1
+++ open-visual-traceroute/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName= 'open-visual-traceroute' 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://superb-dca2.dl.sourceforge.net/project/openvisualtrace/1.6.4/OpenVisualTraceroute1.6.4.exe'
+$url        = 'http://pilotfiber.dl.sourceforge.net/project/openvisualtrace/1.6.4/OpenVisualTraceroute1.6.4.exe'
 $checksum   = '00D8F43D7A4651ECCBEBDB503C79B5685F0CC34DDEC4F7F26DFCAE6AA6C872A8'
 
 $packageArgs = @{
