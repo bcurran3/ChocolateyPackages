6ce5f661030ed37067fae139bc099e36a5a4326f
bcurran3 <bcurran3@yahoo.com>
2016-12-24 10:43:01 -0800

    Add files via upload

--- dell-update/tools/chocolateyinstall.ps1
+++ dell-update/tools/chocolateyinstall.ps1
@@ -1,16 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName= 'dell-update' 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://downloads.dell.com/FOLDER03738438M/1/Application_75FF5_WN32_1.9.5.0_A10.EXE'
+$url        = 'https://downloads.dell.com/FOLDER03951264M/2/Application_TRR3F_WN32_1.9.7.0_A00.EXE'
+$checksum   = 'CB5867FE403AE6B8E902EEA444B4BF0B186BC6C7FBC84C171734EAC7255B2796'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE' 
   url           = $url
-  checksum      = '57fe03d54dba7ad3594dd2296d61758310b545c1e8d5a67cc88b38ebb1b4f585'
+  checksum      = $checksum
   checksumType  = 'sha256'
-  silentArgs   = '/S' 
+  silentArgs    = '/S' 
   softwareName  = 'Dell Update*' 
 }
 
