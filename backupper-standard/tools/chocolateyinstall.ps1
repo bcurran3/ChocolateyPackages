5815df4586988206f6cd923b774f6ae80f1fb393
bcurran3 <bcurran3@yahoo.com>
2016-12-24 10:41:50 -0800

    Add files via upload

--- backupper-standard/tools/chocolateyinstall.ps1
+++ backupper-standard/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
 $url           = 'http://www.aomeisoftware.com/download/adb/BackupperFull.exe'
-$checksum      = 'A5AB8076AAF953C7E136D4D479523FA2E2B28E5E951F31A656802ED1A56BD3A5'
+$checksum      = 'B17302A623EAE14ACF58A88614B7127B6A959DE6FEF2315B805ECA35EF1D4FBF'
 $silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 
 $packageArgs = @{
