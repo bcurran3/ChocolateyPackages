451ee33ca673982aa2f68f9f6f872b70eab019a4
bcurran3 <bcurran3@yahoo.com>
2017-01-14 05:07:36 -0800

    Add files via upload

--- winflector/tools/WinflectorInstall.ahk
+++ winflector/tools/WinflectorInstall.ahk
@@ -3,6 +3,7 @@
 SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
 SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
  
+;BlockInput On 
 WinWaitActive, Setup,Would you like to enable Winflector server in Windows Firewall?, 60 
 WinActivate
 Send !y
@@ -11,9 +12,12 @@ WinWaitActive,Windows Security,, 30
 WinActivate
 Send !i
  
+WinWaitActive, Setup,Winflector printer driver has not been installed properly, 10
+WinActivate
+Send {Enter}
+ 
 WinWaitActive, Setup - Winflector,, 30
 WinActivate
-BlockInput On
 Send !n
 Send !a
 Send !n
@@ -21,5 +25,5 @@ Send !n
 Send !n
 Send !n
 Send !i
-BlockInput Off
+;BlockInput Off
 
--- winflector/tools/chocolateyinstall.ps1
+++ winflector/tools/chocolateyinstall.ps1
@@ -3,13 +3,13 @@ $packageName= 'winflector'
 $softwareName = 'Winflector*'
 $installerType = 'EXE'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.winflector.com/store/free-version/index/id/250' 
-$checksum   = '0C2CFDC7B43DC17AA2582F8A624A23BF52BD12980684606DA32CD86DE330AECF'
+$url        = 'http://www.winflector.com/store/free-version/index/id/264' 
+$checksum   = '59BD7C509653B189B117C818126AA60DCC21F09A68F706DDE263CCD369FAE707'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes= @(0, 3010, 1641)
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$ahkExe = 'AutoHotKey'
-$ahkFile = Join-Path $toolsDir "WinflectorInstall.ahk"
+$ahkExe     = 'AutoHotKey'
+$ahkFile    = Join-Path $toolsDir "WinflectorInstall.ahk"
+
 Start-Process $ahkExe $ahkFile
 
 $packageArgs = @{
