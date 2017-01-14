8081ec2c94a3663be336d9fcc359483db50b029a
bcurran3 <bcurran3@yahoo.com>
2017-01-14 05:05:28 -0800

    Add files via upload

---
+++ choco-upgrade-all-at-startup/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$chocoCmd = Get-Command -Name 'choco' -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Select-Object -ExpandProperty Source
+if ($chocoCmd -eq $null) { break }
+
+# Settings for the scheduled task
+$taskAction = New-ScheduledTaskAction –Execute $chocoCmd -Argument 'upgrade all -y'
+$taskTrigger = New-ScheduledTaskTrigger -AtStartup
+$taskUserPrincipal = New-ScheduledTaskPrincipal -UserId 'SYSTEM'
+$taskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8
+
+# Set up the task, and register it
+$task = New-ScheduledTask -Action $taskAction -Principal $taskUserPrincipal -Trigger $taskTrigger -Settings $taskSettings
+Register-ScheduledTask -TaskName 'Run a Choco Upgrade All at Startup' -InputObject $task -Force
