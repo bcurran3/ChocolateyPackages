6bba55f282d6b54c2d02527c2d133237d6a7ce2f
bcurran3 <bcurran3@yahoo.com>
2017-01-14 05:05:28 -0800

    Add files via upload

---
+++ vlc-skin-editor/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿#http://mirror.os6.org/videolan/skin-editor/0.8.5/VLCSkinEditor_w32.zip
+$packageName           = 'vlc-skin-editor' 
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url                   = "$toolsDir\VLCSkinEditor_w32.zip"
+$checksum               = 'E918340A1CE5AE8924B2C1BBB018C0AD2E384FF6FCF40E12FC334F11EC34CA84'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP'
+  url           = $url
+  checksum      = $checksum 
+  checksumType  = 'sha256'  
+  }
+  
+Install-ChocolateyZIPPackage @packageArgs
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\VLC Skin Editor.lnk" -targetPath "$env:ChocolateyInstall\lib\$packageName\tools\VLCSkinEditor.exe"
+Remove-Item $url
---
+++ vlc-skins/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿#https://www.videolan.org/vlc/download-skins2-go.php?url=vlc-skins.zip
+$packageName    = 'vlc-skins'
+$installerType  = 'zip'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = "$toolsDir\vlc-skins.zip"
+$checksum       = 'AF5A3ABE17BBB67FB3B07D0300A88C403AF66B9A2917AA4F3B2321892E56AB2C'
+$checksumType   = 'sha256'
+$unzipLocation  = ${env:ProgramFiles} + '\VideoLAN\VLC\skins'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $unzipLocation
+  fileType      = $installerType
+  url           = $url
+  checksum      = $checksum
+  checksumType  = $checksumType
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Remove-Item $url 
+				  
