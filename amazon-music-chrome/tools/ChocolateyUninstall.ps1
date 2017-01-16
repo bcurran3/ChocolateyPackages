7c99343d76cf56dcf8e69a63153db4262d14588c
bcurran3 <bcurran3@yahoo.com>
2017-01-15 18:31:37 -0800

    Add files via upload

---
+++ amazon-music-chrome/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,9 @@
+$bits = Get-ProcessorBits
+$packageName = 'amazon-music-chrome'
+$extensionID = 'cfkfgcfgfpgmkogcnibdjcckkpdiajgp'
+
+if ($bits -eq 64) {
+    Remove-Item "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue | out-null
+   }else{
+    Remove-Item "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue | out-null
+}
