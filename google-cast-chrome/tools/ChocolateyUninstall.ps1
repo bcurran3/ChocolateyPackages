2b283b7dc113fe1fd53b964aaaf0a7296e5b8533
bcurran3 <bcurran3@yahoo.com>
2017-01-15 18:32:15 -0800

    Add files via upload

--- google-cast-chrome/tools/ChocolateyUninstall.ps1
+++ google-cast-chrome/tools/ChocolateyUninstall.ps1
@@ -3,7 +3,7 @@ $packageName = 'google-cast-chrome'
 $extensionID = 'boadgeojelhgndaghljhdicfkmllpafd'
 
 if ($bits -eq 64) {
-    Remove-Item "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue
+    Remove-Item "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue | out-null
    }else{
-    Remove-Item "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue
+    Remove-Item "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue | out-null
 }
--- open-screenshot-chrome/tools/ChocolateyUninstall.ps1
+++ open-screenshot-chrome/tools/ChocolateyUninstall.ps1
@@ -3,7 +3,7 @@ $packageName = 'open-screenshot-chrome'
 $extensionID = 'akgpcdalpfphjmfifkmfbpdmgdmeeaeo'
 
 if ($bits -eq 64) {
-    Remove-Item "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue
+    Remove-Item "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue | out-null
    }else{
-    Remove-Item "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue
+    Remove-Item "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue | out-null
 }
