f7284167110d9c313be3aa0be1c3f0d105233d96
bcurran3 <bcurran3@yahoo.com>
2017-01-21 18:31:45 -0800

    Add files via upload

--- duplicati/tools/chocolateyinstall.ps1
+++ duplicati/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $packageName   = 'duplicati'
 $installerType = 'msi'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url           = 'https://github.com/duplicati/duplicati/releases/download/v2.0.1.38-2.0.1.38_canary_2016-12-27/duplicati-2.0.1.38_canary_2016-12-27-x86.msi' 
-$checksum      = '41B09A5AD62DCA01B3F480108AC22A2671BB618F39683ED2BC91376B71E8A85D'
-$url64         = 'https://github.com/duplicati/duplicati/releases/download/v2.0.1.38-2.0.1.38_canary_2016-12-27/duplicati-2.0.1.38_canary_2016-12-27-x64.msi' 
-$checksum64    = '9ACC7F1A1C84F019B9EBD0C01351F348F938C1D9F357A2BC25F8D15E4003CF9A'
+$url           = 'https://github.com/duplicati/duplicati/releases/download/v2.0.1.42-2.0.1.42_canary_2017-01-16/duplicati-2.0.1.42_canary_2017-01-16-x86.msi' 
+$checksum      = '0040D58F2A713702DD23916EA7F516984FDCCE35A288F680F7CF1E758E35FCCE'
+$url64         = 'https://github.com/duplicati/duplicati/releases/download/v2.0.1.42-2.0.1.42_canary_2017-01-16/duplicati-2.0.1.42_canary_2017-01-16-x64.msi' 
+$checksum64    = '6F225A99EC7ABB65EDA36C3A3C2EE79FDF2A70C24FBC75770BD4F7E94A5E42FD'
 $silentArgs    = '/quiet /qn /norestart'
 $validExitCodes= @(0, 3010, 1641)
 
