f7a9e536ebf6c309d597062b5733b7204ee95a95
bcurran3 <bcurran3@yahoo.com>
2017-02-14 06:46:04 -0800

    Add files via upload

--- dokany/tools/chocolateyinstall.ps1
+++ dokany/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
-﻿$packageName= 'dokany'
-$installerType = 'msi'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/dokan-dev/dokany/releases/download/v1.0.1/Dokan_x86.msi' 
-$checksum   = '5B24B1FB5F9B65D76DF6ABDA2F3B9E56C1165B0FC94CA027C26044EF82081AAF'
-$url64      = 'https://github.com/dokan-dev/dokany/releases/download/v1.0.1/Dokan_x64.msi' 
-$checksum64 = '65FF528C1B3A3D4A47325870D8C87469FF768DAED1270852DF6217FF71F5B050'
-$silentArgs = '/passive /norestart'
-$validExitCodes= @(0, 3010, 1641)
+﻿$packageName    = 'dokany'
+$installerType  = 'msi'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://github.com/dokan-dev/dokany/releases/download/v1.0.2/Dokan_x86.msi' 
+$checksum       = '0B374923D138C7B24C01289DFD786A8B477913A32C97D18E6CF563FB083E7D5B'
+$url64          = 'https://github.com/dokan-dev/dokany/releases/download/v1.0.2/Dokan_x64.msi' 
+$checksum64     = '1D777D35498198FCF2C008A237387F38130018C6B1D1B6B3810F3587D88BF1BA'
+$silentArgs     = '/passive /norestart'
+$validExitCodes = @(0, 3010, 1641)
 
 $packageArgs = @{
   packageName   = $packageName
