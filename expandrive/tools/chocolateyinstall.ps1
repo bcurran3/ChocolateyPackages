62c5a07e49f2acbfc38b6d81f209e99ff086e3be
bcurran3 <bcurran3@yahoo.com>
2017-02-14 06:47:33 -0800

    Add files via upload

--- expandrive/tools/chocolateyinstall.ps1
+++ expandrive/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName   = 'expandrive' 
-$url           = 'https://expandrive.s3.amazonaws.com/expandrive_win/v5-4-4_published_2017-01-18_at_21_45_42/ExpanDriveInstaller.msi' 
-$checksum      = '8C05399E922D7FE2505856E38838C3D3B78D5BF7840B44A4EEADB608849FBAAB'
+$url           = 'https://expandrive.s3.amazonaws.com/expandrive_win/v5-4-5_published_2017-02-03_at_21_38_25/ExpanDriveInstaller.msi' 
+$checksum      = '1A80A948D9E3D1E9683BC179E95A07EE67908E5FEA1D65EB4770B2A59233668A'
 $installerType = 'MSI'
 $silentArgs = '/quiet /qn /norestart'
 $validExitCodes= @(0, 3010, 1641)
