d044aeb7803f983e117f6f2c3710c53524c9136a
bcurran3 <bcurran3@yahoo.com>
2017-02-16 22:01:24 -0800

    Add files via upload

---
+++ openhab-addons/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'openhab-addons' 
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://github.com/openhab/openhab2-addons/archive/2.0.0.beta2.zip'
+$checksum     = 'C827AFAEA681F6D211391609567190149AD51143BFCE7FD5809D8C4492312E83'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyZipPackage @packageArgs
