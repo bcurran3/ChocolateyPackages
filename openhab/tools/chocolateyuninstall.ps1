6d30071bf604e8f54ee1c5a1e8df2489c5797455
bcurran3 <bcurran3@yahoo.com>
2017-02-16 22:00:19 -0800

    Add files via upload

---
+++ openhab/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName= 'openhab'
+$ExeFile      = 'start.bat'
+$ShortcutName = 'openHAB'
+
+remove-item "$env:Public\Desktop\$ShortcutName.lnk" -Force -ErrorAction 'SilentlyContinue'
+remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName.lnk" -Force -ErrorAction 'SilentlyContinue'
