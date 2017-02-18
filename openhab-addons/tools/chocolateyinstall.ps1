11225aa236cb5a1cb90b6f27a8da82ef344ca5ee
bcurran3 <bcurran3@yahoo.com>
2017-02-17 21:27:50 -0800

    Add files via upload

--- openhab-addons/tools/chocolateyinstall.ps1
+++ openhab-addons/tools/chocolateyinstall.ps1
@@ -1,16 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName  = 'openhab-addons' 
-$toolsDir     = '$env:ChocolateyInstall\lib\openhab\tools'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$openHABdir   = "$env:ChocolateyInstall\lib\openhab\tools"
 $url          = 'https://github.com/openhab/openhab2-addons/archive/2.0.0.beta2.zip'
 $checksum     = 'C827AFAEA681F6D211391609567190149AD51143BFCE7FD5809D8C4492312E83'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = 'ZIP' 
+  fileType      = 'ZIP'   
   url           = $url
   checksum      = $checksum
-  checksumType  = 'sha256' 
+  checksumType  = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
+xcopy $toolsDir\openhab2-addons-2.0.0.beta2\*.* $openHABdir /s /y
+Remove-Item $toolsDir\openhab2-addons-2.0.0.beta2 -recurse -force
