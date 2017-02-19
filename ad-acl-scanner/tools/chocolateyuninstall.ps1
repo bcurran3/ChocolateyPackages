4354f7e6334111089b92218ce0c6ba391ce5937a
bcurran3 <bcurran3@yahoo.com>
2017-02-19 04:02:11 -0800

    Add files via upload

--- ad-acl-scanner/tools/chocolateyuninstall.ps1
+++ ad-acl-scanner/tools/chocolateyuninstall.ps1
@@ -1,8 +1,5 @@
 ﻿$packageName  = 'ad-acl-scanner'
-$fileName     = 'ADACLScan.ps1'
 $shortcutName = 'AD ACL Scanner' 
-$FileFullpath = Join-Path $ToolsDir $fileName
 
-remove-item "$env:Public\Desktop\$shortcutName.lnk" -Force -ErrorAction 'SilentlyContinue'
-remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk" -Force -ErrorAction 'SilentlyContinue'
+remove-item "$env:USERPROFILE\Desktop\$shortcutName.lnk" -Force -ErrorAction 'SilentlyContinue'
 
