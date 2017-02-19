b5ece4af1769f6df88950c94ea1d18dab272926c
bcurran3 <bcurran3@yahoo.com>
2017-02-19 05:28:36 -0800

    Add files via upload

---
+++ vnc-connect/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName    = 'vnc-connect'
+$installerType  = 'exe'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://www.realvnc.com/download/file/vnc.files/VNC-6.0.2-Windows.exe'
+$checksum       = '8778472143F863F319DE02F34C343D2B9779A2833C50F3B627079AB31AC2B860'
+$silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0, 3010, 1641)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  validExitCodes= $validExitCodes
+  silentArgs    = $silentArgs
+  softwareName  = 'AllWemo*'
+  checksum      = $checksum
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs  
+  
