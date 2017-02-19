cf06cf8bbf20f78f56e92b221cca7bad63d51549
bcurran3 <bcurran3@yahoo.com>
2017-02-19 05:49:38 -0800

    Add files via upload

---
+++ vnc-viewer/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName    = 'vnc-viewer'
+$installerType  = 'exe'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-6.0.2-Windows-64bit.exe'
+$checksum       = '8195890C66CCF93DFC00DC465C4EF10200B0305D2A5BDBBBBEDBAA9702444BFB'
+$silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0, 3010, 1641)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  validExitCodes= $validExitCodes
+  silentArgs    = $silentArgs
+  softwareName  = 'AllWemo*'
+  checksum      = $checksum
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs  
+  
