b79e15d660b65f67783516465c4e7a1d8e986bb4
bcurran3 <bcurran3@yahoo.com>
2017-02-19 03:34:13 -0800

    Add files via upload

---
+++ ad-acl-scanner/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'ad-acl-scanner' 
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=adaclscan&DownloadId=1635180&FileTime=131309703369400000&Build=21031'
+$checksum     = '8FCC040FA75E0593372C3F4397F26F0A1B7418A8B69491C08F565F5C566BA6E1'
+$fileName     = 'ADACLScan.ps1'
+$shortcutName = 'AD ACL Scanner' 
+$FileFullpath = Join-Path $ToolsDir $fileName
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = ''
+  url           = $url
+  FileFullPath  = $FileFullpath
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName.lnk" -targetPath $FileFullpath -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk" -targetPath $FileFullpath
