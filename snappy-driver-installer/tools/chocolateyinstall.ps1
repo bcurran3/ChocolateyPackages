9931b2f1d40344d5f044b7aa2ed0d2ce3bd55804
bcurran3 <bcurran3@yahoo.com>
2017-02-19 04:54:04 -0800

    Add files via upload

---
+++ snappy-driver-installer/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName     = 'snappy-driver-installer' 
+$toolsDir        = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url             = 'http://www.sdi-tool.org/releases/SDI_R535.zip' 
+$checksum        = '08D439A3A94095BF78C27F461D1DA00E71892AC94E51667A597699A3534C4A39'
+$fileName32      = 'SDI_R535.exe'
+$fileName64      = 'SDI_x64_R535.exe'
+$FileFullpath32  = Join-Path $ToolsDir $fileName32
+$FileFullpath64  = Join-Path $ToolsDir $fileName64
+$shortcutName    = 'Snappy Driver Installer' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+if ($bits -eq 64)
+    {
+   Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName.lnk" -targetPath $FileFullpath64 -WorkingDirectory "$toolsDir"
+   Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk" -targetPath $FileFullpath64
+    } else {
+   Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName.lnk" -targetPath $FileFullpath32 -WorkingDirectory "$toolsDir"
+   Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk" -targetPath $FileFullpath32
+    }
