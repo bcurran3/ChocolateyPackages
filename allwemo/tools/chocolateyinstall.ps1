d7d8e08d3028e6615755500fe8dad9b5b0ba1ba0
bcurran3 <bcurran3@yahoo.com>
2017-02-20 16:01:29 -0800

    Add files via upload

--- allwemo/tools/chocolateyinstall.ps1
+++ allwemo/tools/chocolateyinstall.ps1
@@ -1,9 +1,10 @@
 ﻿#http://www.allware.com.mx/Doc.aspx?D=177&dn=AllWemo_2_14_setup.zip
+#NOTE: Installed software reports v2.13 in Control Panel, not v2.14
 $packageName    = 'allwemo'
 $installerType  = 'msi'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = "$toolsDir\AllWemo.msi" 
-$checksum       = '2B3558D9B74C3D51B3AE716C00C89A174755F14A3A0876FD33B04417E1D50917'
+$checksum       = '466FC0B3474E978B6C515C9A44F30DCC627D25FE0E5799AE12B84B70CDDCA2F2'
 $silentArgs     = '/quiet /qn /norestart'
 $validExitCodes = @(0, 3010, 1641)
 
@@ -19,4 +20,3 @@ $packageArgs = @{
 }
 
 Install-ChocolateyPackage @packageArgs  
-  
