2055af2f72ae3f80cf6593921124695b21db39e9
bcurran3 <bcurran3@yahoo.com>
2017-02-20 18:58:18 -0800

    Add files via upload

---
+++ visual-bcd-editor_0901/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName   = 'visual-bcd-editor'
+$installerType = 'MSI'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url           = 'https://www.boyans.net/VisualBcd5/VisualBcd_0.9.0.1.msi' 
+$checksum      = 'B0A4AE5697F042B0E46FF9E42E7643C711106D2D3F246757F9CD29751C630004'
+$silentArgs    = '/quiet /qn /norestart'
+$validExitCodes= @(0, 3010, 1641)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  validExitCodes= $validExitCodes
+  silentArgs    = $silentArgs
+  softwareName  = 'Visual BCD'
+  checksum      = $checksum
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs  
+  
