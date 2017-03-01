99ad86e84a3adec1fc399eba18a0b1f07fdd6e8b
bcurran3 <bcurran3@yahoo.com>
2017-03-01 03:33:41 -0800

    Add files via upload

---
+++ readynas-remote/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'readynas-remote' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'http://remote.readynas.com/ReadyNASRemoteSetup.exe'
+$checksum    = '81952B2568A0C21254D39DC4FF07AF4F00010DEDA8CA959505E30D28DE7CD13C'
+$silentArgs  = '/S' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'  
+  silentArgs    = $silentArgs
+  softwareName  = 'ReadyNAS Remote' 
+  }
+Install-ChocolateyPackage @packageArgs
---
+++ readyshare-vault-software-utility/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'readyshare-vault-software-utility' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'http://www.downloads.netgear.com/files/GDC/R7000/ReadySHAREVault-install-v1.0.50.500.zip'
+$checksum    = '26658D4C6CB3C42360305AEE8D00E0FFACA6964BD7D7CE0FE28B37E85E153071'
+$silentArgs  = '/S' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'  
+  silentArgs    = $silentArgs
+  softwareName  = 'ReadyCLOUD' 
+  }
+Install-ChocolateyPackage @packageArgs
