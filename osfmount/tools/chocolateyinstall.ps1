bdd64774cb88751f0f5ae8118f879992a22c5b1d
bcurran3 <bcurran3@yahoo.com>
2017-03-01 03:33:41 -0800

    Add files via upload

---
+++ osfmount/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName= 'osfmount' 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.osforensics.com/downloads/osfmount.exe'
+$checksum   = 'EBA56C38791DFA34CDAD7632B27FA2E852B5637ACD93F02C04BA8DCBADDB0A36'
+$url64      = 'http://www.osforensics.com/downloads/osfmount_x64.exe'
+$checksum64 = 'B41134466D1D059FCD02F2A8AF82543A0332994FF798DC5187827D6F61C2ACAC'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'  
+  url64bit      = $url64
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+  silentArgs    = $silentArgs
+  softwareName  = 'OSFMount*' 
+  }
+Install-ChocolateyPackage @packageArgs
---
+++ readyshare-printer-utility/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'readyshare-printer-utility' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'http://www.downloads.netgear.com/files/GDC/R6300/ReadySharePrinter_setup_v1.36.zip'
+$checksum    = '4C7187D49CD302120EC7EB0A4928E920A57691DBE0651C2862225DBEBF5DDB4A'
+$silentArgs  = '/S' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'  
+  silentArgs    = $silentArgs
+  softwareName  = '' 
+  }
+Install-ChocolateyPackage @packageArgs
