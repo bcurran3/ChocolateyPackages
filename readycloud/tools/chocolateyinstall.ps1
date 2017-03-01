58fa79d32ad41a6422cfed66910e665a8c962e02
bcurran3 <bcurran3@yahoo.com>
2017-03-01 03:33:41 -0800

    Add files via upload

---
+++ readycloud/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'readycloud' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'http://readycloud.netgear.com/client/install/ReadyCloudSetup.exe'
+$checksum    = '49B573C525538F9653A25673982EA2040FA79041FE8C10D9E025B5508D84ADDD'
+$silentArgs = '/S' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'  
+  silentArgs    = $silentArgs
+  softwareName  = 'ReadyCLOUD' 
+  }
+Install-ChocolateyPackage @packageArgs
