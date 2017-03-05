3fd372b3fa8d899fdc216e1adc3c9a446d87aeb0
bcurran3 <bcurran3@yahoo.com>
2017-03-04 23:41:35 -0800

    Add files via upload

---
+++ choco-upgrade-all-at/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$PassedParameter = $env:chocolateyPackageParameters
+
+if ($PassedParameter) {
+    $RunTime = $PassedParameter
+	} else {
+	  Write-Host "No time specified, defaulting to midnight."
+	  $RunTime         = '00:00'
+	  }
+
+Write-Host "Now configured to run choco upgrade all at $Runtime every day."
+SchTasks /Create /SC DAILY /TN "choco upgrade all at" /TR "el choco upgrade all" /ST $RunTime
