eb3404573d0c611c1f6d65e2ad7e61b8f32ea65a
bcurran3 <bcurran3@yahoo.com>
2017-03-24 10:47:05 -0700

    Add files via upload

--- gpu-z/tools/chocolateyinstall.ps1
+++ gpu-z/tools/chocolateyinstall.ps1
@@ -1,27 +1,24 @@
-﻿$ErrorActionPreference = 'Stop'
-$packageName      ='gpu-z'
-#http://us2-dl.techpowerup.com/files/bPHdSWiAQa4191OO9onywQ/1487242612/GPU-Z.1.17.0.exe
-$url              = "$toolsDir\GPU-Z.1.17.0.exe"
-$checksum         = '7C94B376E592745185D2EA47491A219936F108CB1990818F815F7457AC3F4F26'
-$exeFile          = "GPU-Z.1.17.0.exe"
+﻿#http://us2-dl.techpowerup.com/files/eCkLKAGjgHgutFGViLjHAg/1490377969/GPU-Z.1.18.0.exe
+
+$packageName      = 'gpu-z'
+$toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url              = "$toolsDir\GPU-Z.1.18.0.exe"
+$checksum         = '73AA057194D35FF17025B3B5018D2DFEBE48C9E3E31CC4587AD8988FA6C4B881'
 $scriptDir        = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$installerPackage = Join-Path $scriptDir $exeFile
+$exeFile          = "GPU-Z.1.18.0.exe"
+$installerPackage = Join-Path $scriptDir $ExeFile
 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'EXE'
   url           = $url
-  validExitCodes= ''
-  silentArgs    = ''
-  FileFullPath  = $installerPackage
+  FileFullPath  = $installerPackage  
   softwareName  = 'GPU*'
   checksum      = $checksum
   checksumType  = 'sha256'
 }
 
-Install-ChocolateyPackage @packageArgs 	
+Get-ChocolateyWebFile @packageArgs
 
 Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\GPU-Z.lnk" -targetPath "$env:ChocolateyInstall\lib\gpu-z\tools\$exeFile"
 Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\GPU-Z.lnk" -targetPath "$env:ChocolateyInstall\lib\$packageName\tools\$exeFile"
-
-Remove-Item $url 
