e597141a22ec283b7d9ed5af5e476bb88fd572ae
bcurran3 <bcurran3@yahoo.com>
2017-04-25 13:32:11 -0700

    Add files via upload

---
+++ performance-monitor/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$packageName    = 'performance-monitor' 
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'http://www.hexagora.com/download/PerfMon.zip'
+$checksum       = '75D30013EF5FF769020B3198228A2A5E6E4653173A3E7F194E2B94ED1E67B27C'
+$silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)
+$fileName       = 'PerfMon4x.exe'
+$fileLocation   = Join-Path $toolsDir $fileName
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs 
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  file           = $fileLocation
+  silentArgs     = $silentArgs
+  validExitCodes = $validExitCodes
+  softwareName   = 'Performance Monitor 4*'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
