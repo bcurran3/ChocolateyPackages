bcc015894ec850d7ba83960b23d9d8addb132176
bcurran3 <bcurran3@yahoo.com>
2017-04-25 13:32:11 -0700

    Add files via upload

---
+++ intel-graphics-driver_15.45.16.4627/tools/chocolateyInstall.ps1
@@ -0,0 +1,34 @@
+﻿$packageName = 'intel-graphics-driver' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = ''
+$checksum    = ''
+$url64       = 'https://downloadmirror.intel.com/26669/eng/win64_154516.4627.exe'
+$checksum64  = '2BFE439F637C71AD1C894E0424BAE6382EAE4D75EAAB582A072B13E78847AD6E'
+$silentArgs  = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+$bits = Get-ProcessorBits
+$ahkExe      = 'AutoHotKey'
+$ahkFile     = Join-Path $toolsDir "IGDInstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'  
+  url64bit      = $url64
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+  silentArgs    = $silentArgs
+  softwareName  = 'Intel® Graphics Driver' 
+  }
+
+if ($bits -eq 32)
+  {
+   Write-Host "This version is only for 64 bit OSes." -foreground "red" –backgroundcolor "blue"
+   throw
+  } else {  
+    Install-ChocolateyPackage @packageArgs
+   }
---
+++ onenote/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$packageName    = 'onenote'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://c2rsetup.officeapps.live.com/c2r/download.aspx?productReleaseID=OneNoteFreeRetail&platform=x86&language=en-US&version=O16GA&Source=O16ONF' 
+$checksum       = '04DA8A36108F736597727B8DABF0290D8164E654ED2F5DD18F92FED0A3E282DA'
+$url64          = 'https://c2rsetup.officeapps.live.com/c2r/download.aspx?productReleaseID=OneNoteFreeRetail&platform=x86&language=en-US&version=O16GA&Source=O16ONF'
+$checksum64     = '699DA27776D2C586A6FA2FE045D07FC2FD81A66A9EFCD8C98082EBD576BE33B8'
+$silentArgs     = ''
+$validExitCodes = @(0)
+$ahkExe         = 'AutoHotKey'
+$ahkFile        = Join-Path $toolsDir "OneNoteInstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE' 
+  url           = $url
+  softwareName  = 'Microsoft OneNote Hom and Student 2016*' 
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+}
+
+Install-ChocolateyPackage @packageArgs 
