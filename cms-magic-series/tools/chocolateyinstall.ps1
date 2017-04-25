2030c45eae614a9b8f3c42bf2676bb4abc04a14e
bcurran3 <bcurran3@yahoo.com>
2017-04-25 13:33:34 -0700

    Add files via upload

--- cms-magic-series/tools/chocolateyinstall.ps1
+++ cms-magic-series/tools/chocolateyinstall.ps1
@@ -1,12 +1,13 @@
-﻿$packageName   = 'cms-magic-series'
-$installerType = 'msi'
-$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url           = 'http://download.eyemaxdvr.com/XVST%20MAGIC%20SERIES/CMS/CMS-2.0.1.8C-win32.msi' 
-$checksum      = 'DF60C984A122A61AFDDCB12B76A0E6C56691D3F63406C15790B2C48D9F6D1386'
-$url64         = 'http://download.eyemaxdvr.com/XVST%20MAGIC%20SERIES/CMS/CMS-2.0.1.8C-win64.msi' 
-$checksum64    = '951D3605EF24993387275CBA3A2C8E2B3A81C8CB4C76359D312A365E1D4A1754'
-$silentArgs    = '/quiet /qn /norestart'
-$validExitCodes= @(0, 3010, 1641)
+﻿$packageName    = 'cms-magic-series'
+$installerType  = 'MSI'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'http://download.eyemaxdvr.com/XVST%20MAGIC%20SERIES/CMS/CMS-IPT-2.2.3.2027-win32.msi' 
+$checksum       = 'A095DE7B93F2826A21AAFE272FA5A5DEEA822E2033FD241D62FFEC7545DD689C'
+$url64          = 'http://download.eyemaxdvr.com/XVST%20MAGIC%20SERIES/CMS/CMS-IPT-2.2.3.2027-win64.msi' 
+$checksum64     = '55CBE78A6EAFBEA7A5B61C0D6A8E1A7A55D1821DBF411A8DAB49FB4744762F0A'
+$silentArgs     = '/quiet /qn /norestart'
+$validExitCodes = @(0, 3010, 1641)
+$fileLocation   = "$env:ChocolateyInstall\lib\cms-magic-series\tools\CMS#unix-IPT#2.2.1.2025-win32.msi"
 
 $packageArgs = @{
   packageName   = $packageName
@@ -17,10 +18,9 @@ $packageArgs = @{
   silentArgs    = $silentArgs
   softwareName  = 'CMS'
   checksum      = $checksum
-  checksumType  = 'sha256' 
+  checksumType  = 'sha256'
   checksum64    = $checksum64
   checksumType64= 'sha256'
 }
-
+ 
 Install-ChocolateyPackage @packageArgs  
-  
