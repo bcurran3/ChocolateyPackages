2628e9a1f65af639b3ecde71fa586509bb9ecc60
bcurran3 <bcurran3@yahoo.com>
2017-05-12 02:30:48 -0700

    Add files via upload

---
+++ exchange-edb-viewer/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName = 'exchange-edb-viewer' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'http://www.nucleustechnologies.com/downloads/exchange-edb-viewer.exe'
+$checksum    = '465BC1041EE2C5082F2E26A38B1B61C587423B4D2F032B5BE38CE7D574ADE3ED'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+  softwareName  = 'Kernel Exchange EDB Viewer*' 
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  }
+  
+Install-ChocolateyPackage @packageArgs
+
---
+++ pst-merger/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'pst-merger' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'http://d.4team.biz/files/pstmerger_setup.exe'
+$checksum    = 'FDEB0680222F70C0F5C1749FA3601EAD66692267AEBA07FA0ACAE23433096018'
+$ahkExe      = 'AutoHotKey'
+$ahkFile     = Join-Path $toolsDir "PSTMergerInstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url
+  silentArgs    = '' 
+  softwareName  = '4Team PST Merger*' 
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  }
+  
+Install-ChocolateyPackage @packageArgs
+
