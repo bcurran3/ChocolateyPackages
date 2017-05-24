4f8b048545bdac858f8258511f17cb8b701949e7
bcurran3 <bcurran3@yahoo.com>
2017-05-24 03:28:46 -0700

    Add files via upload

---
+++ resilio-sync-home/tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿$packageName    = 'resilio-sync-home'
+$installerType  = 'exe'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://download-cdn.resilio.com/stable/windows/Resilio-Sync.exe'
+$checksum       = 'E47591893CAA2B57846B2D60F55779483B3597B91E2147135ACD7C3027C5D028'
+$url64          = 'https://download-cdn.resilio.com/stable/windows64/Resilio-Sync_x64.exe'
+$checksum64     = '0168CE102BC478B71C3F6CDC8C473E931840CBFE90F60E3C7F66264C66097416'
+$silentArgs     = '/S'
+$validExitCodes = @(0, 1)
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = $installerType
+  url            = $url
+  url64          = $url64
+  validExitCodes = $validExitCodes
+  silentArgs     = $silentArgs
+  softwareName   = 'Resilio Sync'
+  checksum       = $checksum
+  checksum64     = $checksum64
+  checksumType   = 'sha256' 
+  checksumType64 = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs
+
+Start-Sleep -s 5
+  
+if((get-process "Resilio Sync" -ea SilentlyContinue) -eq $Null){ 
+    Write-Host "Resilio Sync currently NOT running." 
+  }else{ 
+    Write-Host "Stopping Resilio Sync process..."
+    Stop-Process -processname "Resilio Sync"
+	}
