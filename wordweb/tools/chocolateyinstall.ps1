91f2069276a9af736fd7088b98b50897199667fb
bcurran3 <bcurran3@yahoo.com>
2017-05-24 02:10:23 -0700

    Add files via upload

---
+++ wordweb/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName    = 'wordweb-free'
+$installerType  = 'exe'
+$url            = 'http://uk.wordwebsoftware.com/downloads/wordweb8.exe'
+$checksum       = 'BD93E4F745B65C9FE395FC646B26E835D10A6EA7063DEC14DD7B188D32AE3C53'
+$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$silentArgs     = '/s'
+$validExitCodes = @(0)
+$ahkExe         = 'AutoHotKey'
+$ahkFile        = Join-Path $toolsDir "WWFInstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = 'WordWeb'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
