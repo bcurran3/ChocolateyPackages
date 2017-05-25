dd22b0f8dc4c73eb31ab80c21d0f29e199a4879c
bcurran3 <bcurran3@yahoo.com>
2017-05-24 22:13:03 -0700

    Add files via upload

---
+++ streambaby/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,11 @@
+﻿$packageName = 'streambaby'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$currentINI  = '$toolsDir\streambaby-0.53\streambaby.ini'
+
+If (Test-Path $currentINI)
+    {
+     Write-Host "Backing up current streambaby.ini" -foreground "magenta" –backgroundcolor "blue"
+     Copy-Item $currentINI "$env:ChocolateyInstall\lib-bkp"
+    }Else{
+      Write-Host "No current streambaby.ini found." -foreground "magenta" –backgroundcolor "blue"
+     }
--- streambaby/tools/chocolateyuninstall.ps1
+++ streambaby/tools/chocolateyuninstall.ps1
@@ -1,4 +1,5 @@
 ﻿$packageName = 'streambaby'
 
 remove-item "$env:Public\Desktop\$packageName.lnk" -Force -ErrorAction 'SilentlyContinue'
+remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$packageName.lnk" -Force -ErrorAction 'SilentlyContinue'
 
