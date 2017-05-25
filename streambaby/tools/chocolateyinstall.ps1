0fee9a7ee2c2ba05a6196a6c35a3b625f850c8a8
bcurran3 <bcurran3@yahoo.com>
2017-05-24 22:13:03 -0700

    Add files via upload

--- streambaby/tools/chocolateyinstall.ps1
+++ streambaby/tools/chocolateyinstall.ps1
@@ -1,15 +1,30 @@
-﻿$packageName= 'streambaby'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://downloads.sourceforge.net/project/streambaby/streambaby-0.53.zip'
+﻿$packageName = 'streambaby'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'https://cytranet.dl.sourceforge.net/project/streambaby/streambaby-0.54.zip'
+$checksum    = '934106829C63B1EBED70C80B15CFB66A2E9B145385060F706E818876168055C0'
+$oldINI      = 'env:ChocolateyInstall\lib-bkp\streambaby.ini'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'ZIP' 
   url           = $url
-  checksum      = 'B5A6782EE631C952A9944311ACA17C5D0B6F2B2144D19863AD0827CAA8A25C79'
+  checksum      = $checksum
   checksumType  = 'sha256' 
 }
 
 Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$packageName.lnk" -targetPath "$env:ChocolateyInstall\lib\$packageName\tools\streambaby-0.53\streambaby.bat" -iconLocation "$env:ChocolateyInstall\lib\$packageName\tools\streambaby.ico"
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\streambaby.lnk" -targetPath "$toolsDir\streambaby-0.54\streambaby.bat" -iconLocation "$toolsDir\streambaby.ico"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\streambaby.lnk" -targetPath "$toolsDir\streambaby-0.54\streambaby.bat" -iconLocation "$toolsDir\streambaby.ico"
+
+$WhoAmI=whoami
+icacls.exe $toolsDir /grant $WhoAmI":"'(OI)(CI)'F /T | out-null
+
+If (Test-Path $oldINI)
+    {
+     Write-Host "Restoring previous streambaby.ini" -foreground "magenta" –backgroundcolor "blue"
+     Copy-Item $oldINI "$toolsDir\streambaby-0.54\"
+    }Else{
+	 Write-Host "No previous streambaby.ini found." -foreground "magenta" –backgroundcolor "blue"
+   }
