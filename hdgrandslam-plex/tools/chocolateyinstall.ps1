53335c096b5e1c861778e45ab599080b014b3421
bcurran3 <bcurran3@yahoo.com>
2017-05-26 01:02:57 -0700

    Add files via upload

--- hdgrandslam-plex/tools/chocolateyinstall.ps1
+++ hdgrandslam-plex/tools/chocolateyinstall.ps1
@@ -1,18 +1,35 @@
 ﻿$packageName      = 'hdgrandslam-plex' 
 $BundleName       = 'HDGrandSlam.bundle'
+$BundleVer        = '1.6.2'
 $url              = 'https://github.com/jumpmanjay/HDGrandSlam.bundle/archive/v1.6.2.zip'
 $checksum         = 'D79FD30991832022DBEBE1FCD4EAA0FB210D92A5038623F6381B70C7CC8B31B6'
-$LocalAppDataPath = Get-ItemProperty -path "registry::hkey_current_user\software\Plex, Inc.\Plex Media Server" | select-object -expandproperty LocalAppDataPath
+$LocalAppDataPath = Get-ItemProperty -path "registry::hkey_current_user\software\Plex, Inc.\Plex Media Server" | select-object -expandproperty LocalAppDataPath | out-null
 
 if ($LocalAppDataPath -eq $null) {
-   Write-Host "Plex Media Server has not been configured yet!" -ForegroundColor red -BackgroundColor blue
+   Write-Host "Plex Media Server is not installed or has not been configured yet!" -ForegroundColor red -BackgroundColor blue
    Write-Host "Creating default directories..." -ForegroundColor green -BackgroundColor blue
-   New-Item "$env:LOCALAPPDATA\Plex" -Type Directory -force
-   New-Item "$env:LOCALAPPDATA\Plex\Plex Media Server" -Type Directory -force
-   New-Item "$env:LOCALAPPDATA\Plex\Plex Media Server\Plug-ins" -Type Directory -force
+   New-Item "$env:LOCALAPPDATA\Plex" -Type Directory -force | out-null
+   New-Item "$env:LOCALAPPDATA\Plex\Plex Media Server" -Type Directory -force | out-null
+   New-Item "$env:LOCALAPPDATA\Plex\Plex Media Server\Plug-ins" -Type Directory -force | out-null
    $LocalAppDataPath = "$env:LOCALAPPDATA\Plex"
   }
-$UnZipDir         = Join-Path -Path $LocalAppDataPath -ChildPath "Plex Media Server\Plug-ins"
+  
+$UnZipDir = Join-Path -Path $LocalAppDataPath -ChildPath "Plex Media Server\Plug-ins"
+
+$strFileName="$LocalAppDataPath\Plex Media Server\Plug-ins\$BundleName.old"
+If (Test-Path $strFileName){
+  Write-Host "Removing .old version (2 versions old)." -ForegroundColor green -BackgroundColor blue
+  Remove-Item "$UnZipDir\$BundleName.old" -recurse | out-null
+}Else{
+}
+
+$strFileName="$LocalAppDataPath\Plex Media Server\Plug-ins\$BundleName"
+If (Test-Path $strFileName){
+  Write-Host "Renaming previously installed version .old." -ForegroundColor green -BackgroundColor blue
+  Rename-Item "$UnZipDir\$BundleName" "$BundleName.old" | out-null
+}Else{
+  Write-Host "No previous version exists." -ForegroundColor green -BackgroundColor blue
+}
 
 $packageArgs = @{
   packageName   = $packageName
@@ -25,21 +42,4 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 
-$strFileName="$LocalAppDataPath\Plex Media Server\Plug-ins\$BundleName.old"
-If (Test-Path $strFileName){
-  Write-Host "Removing previous .old version." -ForegroundColor green -BackgroundColor blue
-  Remove-Item "$UnZipDir\$BundleName.old" -recurse
-}Else{
-  Write-Host ".old version does not exist." -ForegroundColor green -BackgroundColor blue
-}
-
-$strFileName="$LocalAppDataPath\Plex Media Server\Plug-ins\$BundleName"
-If (Test-Path $strFileName){
-  Write-Host "Renaming previous version .old." -ForegroundColor green -BackgroundColor blue
-  Rename-Item "$UnZipDir\$BundleName" "$BundleName.old"
-}Else{
-  Write-Host "No previous version exists." -ForegroundColor green -BackgroundColor blue
-}
-
-Rename-Item "$UnZipDir\$BundleName-1.6.2" $BundleName
-Write-Host "You can ignore Only an exit code of non-zero will fail... messages." -ForegroundColor green -BackgroundColor blue
+Rename-Item "$UnZipDir\$BundleName-$Bundlever" $BundleName | out-null
--- lan-messenger/tools/chocolateyinstall.ps1
+++ lan-messenger/tools/chocolateyinstall.ps1
@@ -1,15 +1,14 @@
-﻿$ErrorActionPreference = 'Stop'
-$packageName = 'lan-messenger' 
-$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://github.com/lanmessenger/lanmessenger/releases/download/v1.2.37/lmc-1.2.37-win32.exe'
-$checksum    = '4E2170F960CADA658C9F0417E071CDB03251BC294AACEC25C92939531597091B'
+﻿$packageName= 'lan-messenger' 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://downloads.sourceforge.net/project/lanmsngr/1.2.35/lmc-1.2.35-win32.exe'
+$checksum   = 'EAB7C8C3D7C9C82BC7E6860E78D82049DBFEA74943ED59CB1145B9DCB7124386'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url
-  silentArgs    = '/S' 
+  silentArgs   = '/S' 
   softwareName  = 'LAN Messenger*' 
   checksum      = $checksum
   checksumType  = 'sha256'  
