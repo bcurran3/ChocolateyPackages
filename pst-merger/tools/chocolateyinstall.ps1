675ab2926da294061f8d5592639c03c106c31950
bcurran3 <bcurran3@yahoo.com>
2017-06-03 00:47:01 -0700

    Add files via upload

--- pst-merger/tools/chocolateyinstall.ps1
+++ pst-merger/tools/chocolateyinstall.ps1
@@ -1,21 +1,23 @@
-﻿$packageName = 'pst-merger' 
-$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'http://d.4team.biz/files/pstmerger_setup.exe'
-$checksum    = 'FDEB0680222F70C0F5C1749FA3601EAD66692267AEBA07FA0ACAE23433096018'
-$ahkExe      = 'AutoHotKey'
-$ahkFile     = Join-Path $toolsDir "PSTMergerInstall.ahk"
+﻿$packageName    = 'pst-merger' 
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'http://d.4team.biz/files/pstmerger_setup.exe'
+$checksum       = 'FDEB0680222F70C0F5C1749FA3601EAD66692267AEBA07FA0ACAE23433096018'
+$validExitCodes = @(0,2)
+$ahkExe         = 'AutoHotKey'
+$ahkFile        = Join-Path $toolsDir "PSTMergerInstall.ahk"
 
 Start-Process $ahkExe $ahkFile
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe' 
-  url           = $url
-  silentArgs    = '' 
-  softwareName  = '4Team PST Merger*' 
-  checksum      = $checksum
-  checksumType  = 'sha256'
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe' 
+  url            = $url
+  silentArgs     = '' 
+  validExitCodes = $validExitCodes
+  softwareName   = '4Team PST Merger*' 
+  checksum       = $checksum
+  checksumType   = 'sha256'
   }
   
 Install-ChocolateyPackage @packageArgs
