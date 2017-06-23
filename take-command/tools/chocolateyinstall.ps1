96607119122f2e989527118d45a0728c69842085
bcurran3 <bcurran3@yahoo.com>
2017-06-23 12:29:29 -0700

    Add files via upload

--- take-command/tools/chocolateyinstall.ps1
+++ take-command/tools/chocolateyinstall.ps1
@@ -1,20 +1,20 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName= 'take-command'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://jpsoft.com/downloads/v20/tcmd.exe'
-$checksum   = '4DDB891DDFEE474D4FCA94F292A542997995568D43F31F3F90B7623044773909'
+$packageName = 'take-command'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'https://jpsoft.com/downloads/v21/tcmd.exe'
+$checksum    = '0C48B18249DFB5267C6508A7AD37F4DCBA87E40640A8931E810B2959DA98AB7E'
 
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
-  checksum      = $checksum 
-  checksumType  = 'sha256'
-  softwareName  = 'Take Command*'
-  silentArgs   = '/PASSIVE'
-  validExitCodes= @(0) 
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = $url
+  checksum       = $checksum 
+  checksumType   = 'sha256'
+  softwareName   = 'Take Command*'
+  silentArgs     = '/PASSIVE'
+  validExitCodes = @(0) 
 }
 
 Install-ChocolateyPackage @packageArgs 
