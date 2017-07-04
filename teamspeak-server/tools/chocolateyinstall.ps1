373e5dbcd02bf8b6440a4903743df5d8e5b679bd
bcurran3 <bcurran3@yahoo.com>
2017-07-04 02:56:26 -0700

    Add files via upload

---
+++ teamspeak-server/tools/chocolateyinstall.ps1
@@ -0,0 +1,35 @@
+﻿$packageName  = 'teamspeak-server' 
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'http://dl.4players.de/ts/releases/3.0.13.6/teamspeak3-server_win32-3.0.13.6.zip'
+$checksum     = 'F5ACF2960685992258A6701CE0CD98AA223BAB009321527042E54FD1543B7776'
+$url64        = 'http://dl.4players.de/ts/releases/3.0.13.6/teamspeak3-server_win64-3.0.13.6.zip'
+$checksum64   = 'C7EEB1937B0BCE0B99E7C7E20DE030A4B71ADCAF09750481801CFA361433522F'
+$shortcutName = 'TeamSpeak Server.lnk'
+$exe          = 'ts3server.exe'
+
+if ((Get-OSArchitectureWidth -eq 64) -and ($env:chocolateyForceX86 -ne $true))
+    {
+     $workingDir = 'teamspeak3-server_win64'
+    } else {
+     $workingDir = 'teamspeak3-server_win32'
+    }
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  url64          = $url64 
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'  
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath "$toolsDir\$workingDir\$exe" -WorkingDirectory "$toolsDir\$workingDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$toolsDir\$workingDir\$exe" -WorkingDirectory "$toolsDir\$workingDir"
+
+$WhoAmI=whoami
+icacls.exe $toolsDir /grant $WhoAmI":"'(OI)(CI)'F /T | out-null
