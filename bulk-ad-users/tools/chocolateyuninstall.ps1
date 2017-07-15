f68647917cbd99037d243a5eedb5c476c2ee99d2
bcurran3 <bcurran3@yahoo.com>
2017-07-15 15:40:26 -0700

    Add files via upload

---
+++ bulk-ad-users/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName  = 'bulk-ad-users' 
+$shortcutName = 'Bulk AD Users.lnk'
+
+remove-item "$env:Public\Desktop\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
+remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
+
