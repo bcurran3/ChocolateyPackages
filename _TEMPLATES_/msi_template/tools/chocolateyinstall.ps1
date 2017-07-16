63ee27a6671522f37355ea5f96c94ed3df694dc9
bcurran3 <bcurran3@yahoo.com>
2017-07-15 17:58:50 -0700

    Add files via upload

---
+++ _TEMPLATES_/msi_template/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName    = ''
+$installerType  = 'msi'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = '' 
+$checksum       = ''
+$silentArgs     = ''
+$validExitCodes = @(0, 3010, 1641)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  validExitCodes= $validExitCodes
+  silentArgs    = $silentArgs
+  softwareName  = ''
+  checksum      = $checksum
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs  
+  
