0c1093ea1c8e2a503daf8bee72856edd6b652596
bcurran3 <bcurran3@yahoo.com>
2017-07-15 17:58:50 -0700

    Add files via upload

---
+++ _TEMPLATES_/ZIP_portable_template/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName  = '' 
+$shortcutName = '.lnk'
+
+remove-item "$env:Public\Desktop\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
+remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
+
