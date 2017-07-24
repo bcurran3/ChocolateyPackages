96f5d0affd6ea73207bf4f52156f2443eac4a9f0
bcurran3 <bcurran3@yahoo.com>
2017-07-24 03:28:04 -0700

    Add files via upload

--- network-inventory-advisor/tools/chocolateyinstall.ps1
+++ network-inventory-advisor/tools/chocolateyinstall.ps1
@@ -1,20 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName= 'network-inventory-advisor'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://dl.network-inventory-advisor.com/network_inventory_advisor.exe'
+$packageName    = 'network-inventory-advisor'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'http://dl.network-inventory-advisor.com/network_inventory_advisor.exe'
+$checksum       = '45671B1A3AF668FA302C1055DA73C9FA99F1922D9CF618B20DFAE43CC8726FC1'
 $validExitCodes = @(0, 3010, 1641)
-$SilentArgs = '/S'
+$SilentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
-  validExitCodes= $validExitCodes
-  silentArgs    = $SilentArgs
-  softwareName  = 'Network Inventory Advisor*'
-  checksum      = 'FF9B850A3763094709C3FF362F5DC273B999552FA71F291C499BB10AF25816F0'
-  checksumType  = 'sha256'   
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url            = $url
+  validExitCodes = $validExitCodes
+  silentArgs     = $SilentArgs
+  softwareName   = 'Network Inventory Advisor*'
+  checksum       = $checksum
+  checksumType   = 'sha256'   
 }
 
 Install-ChocolateyPackage @packageArgs
--- opendns-updater/tools/chocolateyinstall.ps1
+++ opendns-updater/tools/chocolateyinstall.ps1
@@ -1,8 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName= 'opendns-updater'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.opendns.com/wp-content/uploads/2014/01/OpenDNS-Updater-2.2.1.exe' 
-$silentArgs = '/S' 
+$packageName    = 'opendns-updater'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://s3-us-west-1.amazonaws.com/opendns-downloads/OpenDNS-Updater-2.2.1.exe' 
+$checksum       = 'F806B50836914731B1D5A5611B4F497191D097C58A728DD8129B29C7FB66FA25'
+$silentArgs     = '/S' 
 $validExitCodes = @(0) 
 
 $packageArgs = @{
@@ -13,7 +14,7 @@ $packageArgs = @{
   silentArgs    = $silentArgs 
   validExitCodes= $validExitCodes 
   softwareName  = 'OpenDNS Updater 2.2.1*'
-  checksum      = 'F806B50836914731B1D5A5611B4F497191D097C58A728DD8129B29C7FB66FA25'
+  checksum      = $checksum
   checksumType  = 'sha256'  
 }
 
