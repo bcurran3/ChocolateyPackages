0c0d81323da7737f91b1f71eeeab95d621e7d355
bcurran3 <bcurran3@yahoo.com>
2017-08-07 21:46:06 -0700

    Add files via upload

--- hppark/tools/chocolateyinstall.ps1
+++ hppark/tools/chocolateyinstall.ps1
@@ -2,6 +2,8 @@
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'http://ftp.hp.com/pub/softlib/software13/COL40860/ds-99433-18/park-v1.8.2.zip'
 $checksum     = '6D67845DEA872317BF065F05C05289EE2E076664322B16A0D566465399297B76'
+$shortcutName = 'HP PARK.lnk'
+$folder       = 'park-v1.8.2'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -13,3 +15,5 @@ $packageArgs = @{
 }
 
 Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath "$toolsDir\$folder"
