d14ce19d1b172b4ff6c07c36df9947f8ff6aab43
bcurran3 <bcurran3@yahoo.com>
2017-08-07 22:04:14 -0700

    Add files via upload

--- gpu-z/tools/chocolateyinstall.ps1
+++ gpu-z/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
-﻿#http://us2-dl.techpowerup.com/files/zqWQL0Zwp-N0t9JLcvZ-jg/1495567080/GPU-Z.2.1.0.exe
+﻿#http://us2-dl.techpowerup.com/files/FgPOUfUxTgURn3uKd9sALQ/1502168858/GPU-Z.2.2.0.exe
 
 $packageName      = 'gpu-z'
 $toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$exeFile          = "GPU-Z.2.1.0.exe"
-$checksum         = '5DFE5CA1602B7161FF3EEC13A8B52C9B0EFFB67233353E3C60CE0A3011147B3E'
+$exeFile          = "GPU-Z.2.2.0.exe"
+$checksum         = '81BEA70BF1308D508E87929D2EB32718012DA277191F8A1D61347E50EBE73F6F'
 $url              = "$toolsDir\$exeFile"
 
 Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\GPU-Z.lnk" -targetPath $url
