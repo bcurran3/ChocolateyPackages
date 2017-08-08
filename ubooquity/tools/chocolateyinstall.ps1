b33e2224256b0331e8978830d747d0e5de1c031d
bcurran3 <bcurran3@yahoo.com>
2017-08-08 03:46:48 -0700

    Add files via upload

--- ubooquity/tools/chocolateyinstall.ps1
+++ ubooquity/tools/chocolateyinstall.ps1
@@ -1,18 +1,21 @@
-﻿$packageName = 'ubooquity' 
-$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'http://vaemendis.net/ubooquity/downloads/Ubooquity-2.0.2.zip'
-$checksum    = '814650CCC44F624BE006ED6129140FBA4F064691E3A68BFE7138FDB49FC94AD4'
+﻿$packageName  = 'ubooquity' 
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'http://vaemendis.net/ubooquity/downloads/Ubooquity-2.1.0.zip'
+$checksum     = '19892106AF6EC84932A41658ED9B5F0B91B661C520982969A97FBE904548594E'
+$checksumType = 'sha256' 
+$fileType     = 'ZIP'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = 'ZIP' 
+  fileType      = $fileType
   url           = $url
   checksum      = $checksum
-  checksumType  = 'sha256'   
+  checksumType  = $checksumType
 }
 
 Install-ChocolateyZipPackage @packageArgs
+
 Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\Ubooquity.lnk" -targetPath "$env:ChocolateyInstall\lib\ubooquity\tools\Ubooquity.jar" -WorkingDirectory "$env:ChocolateyInstall\lib\ubooquity\tools"
 Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Ubooquity.lnk" -targetPath "$env:ChocolateyInstall\lib\ubooquity\tools\Ubooquity.jar" -WorkingDirectory "$env:ChocolateyInstall\lib\ubooquity\tools"
 
