460c24fc7ef5c9b6a2e1b6ef373f2994960901a1
bcurran3 <bcurran3@yahoo.com>
2017-08-08 03:27:54 -0700

    Add files via upload

--- arq/tools/chocolateyinstall.ps1
+++ arq/tools/chocolateyinstall.ps1
@@ -1,7 +1,8 @@
 ﻿$packageName   = 'arq' 
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url           = 'https://www.arqbackup.com/download/Arq.exe' 
-$checksum      = '40AA24EB407882168C62524472E1176B8ADE5C5418A450A7FE1B4FFBF630C053'
+$checksum      = '83DF54F1F322F4D6B37E429ADD4ECADC3A29FB07C88346D4EDC00645654D1D86'
+$checksumType  = 'sha256'
 $UnzippedMSI   = Join-Path $toolsDir 'ArqSetup.msi'
 $installerType = 'MSI'
 $silentArgs    = '/passive ACCEPT=YES'
@@ -13,7 +14,7 @@ $packageArgs = @{
   fileType      = 'ZIP' 
   url           = $url
   checksum      = $checksum
-  checksumType  = 'sha256'
+  checksumType  = $checksumType
 }
 
 Install-ChocolateyZipPackage @packageArgs
