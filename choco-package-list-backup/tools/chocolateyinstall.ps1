2846c7bd8a7373ad89606b2086b6a548f8f2406a
bcurran3 <bcurran3@yahoo.com>
2017-08-08 22:11:11 -0700

    Add files via upload

--- choco-package-list-backup/tools/chocolateyinstall.ps1
+++ choco-package-list-backup/tools/chocolateyinstall.ps1
@@ -1,11 +1,9 @@
 ﻿$packageName  = 'choco-package-list-backup' 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$script        = '$toolsDir\choco-package-list-backup.ps1'
+$script       = "choco-package-list-backup.ps1"
 
-$WPSDir = (Test-Path $Env:USERPROFILE\Documents\WindowsPowerShell)
-if ($WPSDir -eq "True")
-   {
-    Move-Item $script $Env:USERPROFILE\Documents\WindowsPowerShell | out-null
-   } else{
-    throw
-	}
+Move-Item "$toolsDir\$script" $env:ChocolateyInstall\bin -Force -ErrorAction 'SilentlyContinue'
+Write-Host "Edit $env:ChocolateyInstall\bin\$script to customize your backup(s)." -foreground "magenta" –backgroundcolor "blue"
+Write-Host "From PowerShell, type CHOCO-PACKAGE-LIST-BACKUP.PS1 to backup your Chocolatey packages list." -foreground "magenta" –backgroundcolor "blue"
+Write-Host "From Command Prompt, type POWERSHELL CHOCO-PACKAGE-LIST-BACKUP.PS1 to backup your Chocolatey packages list." -foreground "magenta" –backgroundcolor "blue"
+
