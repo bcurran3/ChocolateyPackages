9b154ab11792c209eff99ef11779f4ac827df7ad
bcurran3 <bcurran3@yahoo.com>
2017-08-09 03:46:36 -0700

    Add files via upload

--- choco-package-list-backup/tools/ChocolateyUninstall.ps1
+++ choco-package-list-backup/tools/ChocolateyUninstall.ps1
@@ -1,5 +1,4 @@
 $packageName  = 'choco-package-list-backup' 
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $script       = "choco-package-list-backup.ps1"
 
 Remove-Item $env:ChocolateyInstall\bin\$script -Force -ErrorAction 'SilentlyContinue'
