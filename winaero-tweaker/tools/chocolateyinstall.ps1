589fd32afec2cbb988943fa145fc45a1521ffb8f
bcurran3 <bcurran3@yahoo.com>
2017-08-20 03:17:55 -0700

    Add files via upload

--- winaero-tweaker/tools/chocolateyinstall.ps1
+++ winaero-tweaker/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 $packageName    = 'winaero-tweaker' 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'http://winaero.com/e107_files/downloads/winaerotweaker.zip'
-$checksum       = 'E12E88F52086DD2C14A35D9369F85CAEE3898D9886E19DBECDEAB32110CC58EE' 
+$checksum       = 'EE6D06B9DB646C69D993C4D897B8D682CA2489B11E1A41201A9E014C65D07C82'
 $checksumType   = 'sha256'
-$UnzippedEXE    = Join-Path $toolsDir 'WinaeroTweaker-0.7.0.4-setup.exe'
+$UnzippedEXE    = Join-Path $toolsDir 'WinaeroTweaker-0.8.0.0-setup.exe'
 $installerType  = 'EXE'
 $silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0)
