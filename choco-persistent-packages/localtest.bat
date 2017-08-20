74911e727a692bb89d6d965b6b088a8e30d2c088
bcurran3 <bcurran3@yahoo.com>
2017-08-19 23:21:28 -0700

    Add files via upload

--- choco-persistent-packages/localtest.bat
+++ choco-persistent-packages/localtest.bat
@@ -1,5 +1,5 @@
 @echo off
 cpack
-cinst choco-upgrade-all-at -s S:\ChocolateyPackages\choco-persistent-packages
-rem cinst choco-upgrade-all-at -s S:\ChocolateyPackages\choco-persistent-packages --params "'/Time:%1'" -force
+rem cinst choco-persistent-packages -s S:\ChocolateyPackages\choco-persistent-packages 
+cinst choco-persistent-packages -s S:\ChocolateyPackages\choco-persistent-packages --params "'/Time:%1'" -force
 rem %1 will fail on install if null
--- choco-persistent-packages/tools/chocolateyuninstall.ps1
+++ choco-persistent-packages/tools/chocolateyuninstall.ps1
@@ -1,3 +1,5 @@
-﻿$packageName = 'choco-persistent-packages'
+﻿#$env:ChocolateyInstall\config\persistentpackages.config purposely not deleted as not to lose config on a re-installation
+$packageName = 'choco-persistent-packages'
 
 SchTasks /Delete /TN “choco-persistent-packages” /F
+
