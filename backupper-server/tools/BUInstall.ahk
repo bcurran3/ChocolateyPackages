a7e3693a6f9a022ceaa07a2355052f76b9c8a926
bcurran3 <bcurran3@yahoo.com>
2017-08-31 19:49:13 -0700

    Add files via upload

--- backupper-server/tools/BUInstall.ahk
+++ backupper-server/tools/BUInstall.ahk
@@ -3,13 +3,9 @@
 SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
 SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
 
-;BlockInput On 
-;WinWaitActive, Setup - AOMEI Backupper, , 60
-;WinActivate
-;Send !{f4}
 Sleep, 60000
 Run, %comspec% /c taskkill /IM "ABServer.exe" /F
 Run, %comspec% /c taskkill /IM "ABServer.tmp" /F
-;BlockInput Off
+
 
 
--- backupper-server/tools/chocolateyinstall.ps1
+++ backupper-server/tools/chocolateyinstall.ps1
@@ -1,12 +1,20 @@
-﻿$packageName   = 'backupper-server'
-$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$installerType = 'EXE'
-$url           = 'http://www.aomeisoftware.com/download/adb/ABServer.exe'
-$checksum      = '814EF78059AA36C6A52DA179B1F73EB5EFD3932C1ABFC03E7B97D33B52E1A84F'
-$silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+﻿$packageName    = 'backupper-server'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installerType  = 'EXE'
+$url            = 'http://www.aomeisoftware.com/download/adb/ABServer.exe'
+$checksum       = 'C9369853EB6F138E800575D60A62AFE6C1DD513744D6CA3B452C53A365B43925'
+$silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0,1)
 $ahkExe         = 'AutoHotKey'
 $ahkFile        = Join-Path $toolsDir "BUInstall.ahk"
+$ServerOS       = (Get-WmiObject -class Win32_OperatingSystem).Caption
+$Answer         = $ServerOS -match "Server"
+
+if ($Answer -ne "True")
+   {
+    Write-Host "This package is for Windows Server OSes only. Aborting." -foreground red -backgroundcolor blue
+	throw
+   }
 
 Start-Process $ahkExe $ahkFile
 
