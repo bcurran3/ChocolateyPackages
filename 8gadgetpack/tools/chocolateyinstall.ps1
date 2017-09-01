0b8b137778437cc6dee35017fff66bf8bb980fa4
bcurran3 <bcurran3@yahoo.com>
2017-08-31 21:24:41 -0700

    Add files via upload

---
+++ 8gadgetpack/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName    = '8gadgetpack'
+$installerType  = 'msi'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'http://8gadgetpack.net/dl230/8GadgetPackSetup.msi' 
+$checksum       = '814655C82CAA5F3B2993022C07C824E260194F003371E3866B2C60C130F5784E'
+$silentArgs     = '/quiet /qn /norestart'
+$validExitCodes = @(0, 3010, 1641)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  validExitCodes= $validExitCodes
+  silentArgs    = $silentArgs
+  softwareName  = '8GadgetPack'
+  checksum      = $checksum
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs  
+  
