7e46199ac1dab415aa6699287d94df55abd609c5
bcurran3 <bcurran3@yahoo.com>
2017-09-01 03:10:06 -0700

    Add files via upload

--- plexmediaplayer/tools/chocolateyinstall.ps1
+++ plexmediaplayer/tools/chocolateyinstall.ps1
@@ -4,6 +4,7 @@ $installerType = 'exe'
 $url           = 'https://downloads.plex.tv/plexmediaplayer/2.1.0.74-8e24a8f4/PlexMediaPlayer-2.1.0.74-8e24a8f4-windows-x64.exe'
 $checksum      = '214314E6ACB72858EC781BD20061EAB4C349A898D3FBD02839343BA3EB140F09'
 $silentArgs    = '/S'
+$bits          = Get-ProcessorBits
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,5 +16,12 @@ $packageArgs = @{
   checksum      = $checksum
   checksumType  = 'sha256'  
   }
-Install-ChocolateyPackage @packageArgs
+
+if ($bits -eq 32)
+  {
+   Write-Host "This program only supports 64 bit OSes." -foreground "red" –backgroundcolor "blue"
+   throw
+  } else {  
+    Install-ChocolateyPackage @packageArgs
+   }  
 
