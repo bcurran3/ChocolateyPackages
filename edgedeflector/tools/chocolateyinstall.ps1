c5dc5b8e5c6c1fcbcb5cd8b1b0209e514087612b
bcurran3 <bcurran3@yahoo.com>
2017-09-02 09:12:49 -0700

    Add files via upload

--- edgedeflector/tools/chocolateyinstall.ps1
+++ edgedeflector/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = 'edgedeflector'
 $installerType  = 'exe'
-$url            = 'https://github.com/da2x/EdgeDeflector/releases/download/v1.0.1.0/EdgeDeflector_install.exe' 
-$checksum       = 'BF01F9051FB6B233997370C97FA70F4D406D31154EE90EC54EEE9B1C803219EB'
+$url            = 'https://github.com/da2x/EdgeDeflector/releases/download/v1.1.1.0/EdgeDeflector_install.exe' 
+$checksum       = '2C12A85769C68468721A9A096DDE95A065DEDAC5266BEF22A65FE5721369FC11'
 $silentArgs     = '/S'
 $validExitCodes = @(0)
 
