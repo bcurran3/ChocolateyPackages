ec3a0271b1f3fdf3ed970941d3d34aa828488fe9
bcurran3 <bcurran3@yahoo.com>
2017-09-02 07:42:30 -0700

    Add files via upload

--- allway-sync/tools/chocolateyuninstall.ps1
+++ allway-sync/tools/chocolateyuninstall.ps1
@@ -1,15 +1,18 @@
-$processor = Get-WmiObject Win32_Processor
-$is64bit = $processor.AddressWidth -eq 64
-if ($is64bit) {
-$unpath = "${Env:ProgramFiles}\Allway Sync\unins000.exe"
-} else {
-$unpath = "${Env:ProgramFiles (x86)}\Allway Sync\unins000.exe"
-}
-$packageName = 'allway-sync'
-$softwareName = 'Allway Sync*'
-$installerType = 'EXE' 
-$silentArgs = '/VERYSILENT'
+$packageName    = 'allway-sync'
+$softwareName   = 'Allway Sync*'
+$installerType  = 'EXE' 
+$silentArgs     = '/VERYSILENT'
 $validExitCodes = @(0, 3010, 1605, 1614, 1641)
+$processor      = Get-WmiObject Win32_Processor
+$bits           = Get-ProcessorBits
+
+if ($bits -eq 64)
+  {
+   $unpath = "${Env:ProgramFiles}\Allway Sync\unins000.exe" 
+  } else {
+   $unpath = "${Env:ProgramFiles (x86)}\Allway Sync\unins000.exe"
+  }
+
 
 Uninstall-ChocolateyPackage $packageName $installerType `
   $silentArgs $unpath -validExitCodes $validExitCodes
