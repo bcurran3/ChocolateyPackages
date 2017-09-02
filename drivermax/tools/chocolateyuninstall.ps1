1b96c11e3ef63e00269e7318c61f7962537ce127
bcurran3 <bcurran3@yahoo.com>
2017-09-02 09:35:17 -0700

    Add files via upload

--- drivermax/tools/chocolateyuninstall.ps1
+++ drivermax/tools/chocolateyuninstall.ps1
@@ -3,14 +3,14 @@ $softwareName   = 'DriverMax*'
 $installerType  = 'EXE' 
 $silentArgs     = '/VERYSILENT'
 $validExitCodes = @(0, 3010, 1605, 1614, 1641)
-$processor      = Get-WmiObject Win32_Processor
+$bits           = Get-ProcessorBits
 
-$is64bit = $processor.AddressWidth -eq 64
-if ($is64bit) {
-  $unpath = "${Env:ProgramFiles(x86)}\Innovative Solutions\DriverMax\unins000.exe"
-} else {
-  $unpath = "${Env:ProgramFiles}\Innovative Solutions\DriverMax\unins000.exe"
-}
+if ($bits -eq 64)
+  {
+   $unpath = "${Env:ProgramFiles(x86)}\Innovative Solutions\DriverMax\unins000.exe"
+  } else {
+   $unpath = "${Env:ProgramFiles}\Innovative Solutions\DriverMax\unins000.exe"
+  }
 
 if((get-process "drivermax" -ea SilentlyContinue) -eq $Null){ 
     Write-Host "drivermax currently NOT running." 
