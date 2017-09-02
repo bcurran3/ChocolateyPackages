b28279316319d6eb6ad14a8213bf24e7aaaf8222
bcurran3 <bcurran3@yahoo.com>
2017-09-02 08:30:20 -0700

    Add files via upload

---
+++ cms-magic-series/readme.md
@@ -0,0 +1,16 @@
+**BCURRAN3'S PACKAGE NOTES:**
+
+* This package downloads the installer via a version specific URL. It should never fail unless the version is completely removed from the vendor website.
+* This has been tested with TVST (HD-TVI) and XVST (HD-SDI) MAGIC Series DVRs. It probably will work with UVST (EX-SDI) MAGIC Series DVRs as well.
+* I personally use and endorse this program.
+
+Like my [packages](https://chocolatey.org/profiles/bcurran3)? 
+
+Find them useful?
+
+**Want to buy me a beer?**
+
+[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4ECL3UCG5CGB6)
+
+If applicable, please always consider donating to the developer or purchasing the software first - this includes Chocolatey licensed editions. 
+
--- cms-magic-series/tools/chocolateyinstall.ps1
+++ cms-magic-series/tools/chocolateyinstall.ps1
@@ -1,13 +1,11 @@
 ﻿$packageName    = 'cms-magic-series'
 $installerType  = 'MSI'
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'http://download.eyemaxdvr.com/XVST%20MAGIC%20SERIES/CMS/CMS-IPT-2.2.3.2027-win32.msi' 
-$checksum       = 'A095DE7B93F2826A21AAFE272FA5A5DEEA822E2033FD241D62FFEC7545DD689C'
-$url64          = 'http://download.eyemaxdvr.com/XVST%20MAGIC%20SERIES/CMS/CMS-IPT-2.2.3.2027-win64.msi' 
-$checksum64     = '55CBE78A6EAFBEA7A5B61C0D6A8E1A7A55D1821DBF411A8DAB49FB4744762F0A'
+$url            = 'http://download.eyemaxdvr.com/XVST%20MAGIC%20SERIES/CMS/CMS-IPT-2.2.4.2028-win32.msi' 
+$checksum       = 'BE9FC352EACB0A9FCF8D90DE0304500AE4E631FA62C502F867DCF9379955262D'
+$url64          = 'http://download.eyemaxdvr.com/XVST%20MAGIC%20SERIES/CMS/CMS-IPT-2.2.4.2028-win64.msi' 
+$checksum64     = 'E733FC55F94B8C7ECF65590653BD4D5C10B2EB6481FAF302A24C104AE94509E6'
 $silentArgs     = '/quiet /qn /norestart'
 $validExitCodes = @(0, 3010, 1641)
-$fileLocation   = "$env:ChocolateyInstall\lib\cms-magic-series\tools\CMS#unix-IPT#2.2.1.2025-win32.msi"
 
 $packageArgs = @{
   packageName   = $packageName
