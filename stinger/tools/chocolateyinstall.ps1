d4a406de944390238d473e3997a8c384e868fd18
bcurran3 <bcurran3@yahoo.com>
2017-09-19 01:42:29 -0700

    Add files via upload

--- stinger/tools/chocolateyinstall.ps1
+++ stinger/tools/chocolateyinstall.ps1
@@ -2,14 +2,15 @@
 $packageName      = 'stinger' 
 $toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url              = 'http://downloadcenter.mcafee.com/products/mcafee-avert/stinger/stinger32.exe'
-$checksum         = 'E9DBAEBA8868087D9471D1A2234907DD8A9E4589CA52D6C544CCA5D3CC32BF29'
+$checksum         = '95E3A73A1BD9F7CEAF01FD9F637E0D66A1423D083D60EE1B66FB11D5133CBF93'
 $url64            = 'http://downloadcenter.mcafee.com/products/mcafee-avert/stinger/stinger64.exe'
-$checksum64       = '28F87A4A8017EC742F774C238D495F76096DBC29948CDDCAA406E51708CBD327'
+$checksum64       = '53D1082DD0EA98E663457201D8F701EC0402471EA1BDE3601F0380E33C92FF0C'
 $ExeFile32        = 'stinger32.exe'
 $ExeFile64        = 'stinger64.exe'
 $ShortcutName     = 'McAfee Stinger'
+$bits             = Get-ProcessorBits
 
-if (Get-OSArchitectureWidth -eq 64)
+if ($bits -eq 64)
    {
     $installerPackage = Join-Path $toolsDir $ExeFile64
    } else {
@@ -31,7 +32,7 @@ $packageArgs = @{
 
 Get-ChocolateyWebFile @packageArgs
 
-if (Get-OSArchitectureWidth -eq 64)
+if ($bits -eq 64)
     {
      Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$ShortcutName.lnk" -targetPath "$toolsDir\$ExeFile64" -WorkingDirectory "$toolsDir\$packageName"
      Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\McAfee Stinger.lnk" -targetPath "$toolsDir\$ExeFile64" -WorkingDirectory "$toolsDir\$packageName"
