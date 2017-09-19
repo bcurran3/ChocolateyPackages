cb894d0991a89ff4677a1931d0c6ab7ff179bd2c
bcurran3 <bcurran3@yahoo.com>
2017-09-19 03:11:49 -0700

    Add files via upload

---
+++ autosaver/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName  = 'autosaver' 
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'http://www.door2windows.com/download-freeware/#AutoSaver'
+$checksum     = '859EF88B1B94C7B93524E6CB8157BB10C1F5B9BF047147EC9CB2C2B84CA46695'
+$shortcutName = 'AutoSaver.lnk'
+$exe          = 'AutoSaver.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath "$toolsDir\$exe" -WorkingDirectory $toolsDir
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$toolsDir\$exe" -WorkingDirectory $toolsDir
