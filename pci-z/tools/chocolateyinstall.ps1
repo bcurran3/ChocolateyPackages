58c82f3d85bc3a5b6664f4872fb1d5fe60a79123
bcurran3 <bcurran3@yahoo.com>
2017-09-22 06:46:59 -0700

    Add files via upload

--- pci-z/tools/chocolateyinstall.ps1
+++ pci-z/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageName = 'pci-z' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url         = 'https://www.pci-z.com/current/PCI-Z.zip'
-$checksum    = '7E848A81C6CDCC5E47A54317BC27A2007F04E4AC4A0CB6481935DD2CD0192869'
-$url64       = 'https://www.pci-z.com/current/PCI-Zx64.zip'
-$checksum64  = '5F2F9D05C87B667EBB9036A216F2574D91CE3BB9AC5DA1167A1AF0942820AB84'
-$bits = Get-ProcessorBits
+$checksum    = '10CF2525408761B9BE84EA4449D54C6B8234B4A055E598E15D9E9611727DFC07'
+$url64       = 'https://www.pci-z.com/current/x64/PCI-Z.zip'
+$checksum64  = '217E3FEFF786086BEC79B16D1CC9A463E78811426BFE055190AB6BDD570802B8'
+
 
 
 $packageArgs = @{
@@ -21,11 +21,5 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 
-if ($bits -eq 64)
-   {
-    Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\PCI-Z.lnk" -targetPath "$toolsDir\PCI-Zx64.exe" -WorkingDirectory "$toolsDir"
-    Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\PCI-Z.lnk" -targetPath "$toolsDir\PCI-Zx64.exe"    
-   } else {
-    Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\PCI-Z.lnk" -targetPath "$toolsDir\PCI-Z.exe" -WorkingDirectory "$toolsDir"
-    Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\PCI-Z.lnk" -targetPath "$toolsDir\PCI-Z.exe"       
-    }
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\PCI-Z.lnk" -targetPath "$toolsDir\PCI-Z.exe" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\PCI-Z.lnk" -targetPath "$toolsDir\PCI-Z.exe"       
