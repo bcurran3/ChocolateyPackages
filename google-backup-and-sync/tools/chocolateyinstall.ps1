6c86e37d2c6026c3a54007d0e457d7e8433540be
bcurran3 <bcurran3@yahoo.com>
2017-09-24 03:09:03 -0700

    Add files via upload

--- google-backup-and-sync/tools/chocolateyinstall.ps1
+++ google-backup-and-sync/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = 'google-backup-and-sync' 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://dl.google.com/tag/s/appguid%3D%7B3C122445-AECE-4309-90B7-85A6AEF42AC0%7D%26iid%3D%7B9648D435-67BA-D2A7-54D2-1E0B5656BF03%7D%26ap%3Duploader%26appname%3DBackup%2520and%2520Sync%26needsadmin%3Dtrue/drive/installbackupandsync.exe'
-$checksum       = '60D56C4BBB7D5C86DC631E03A21B3F5FB97503ECA094B0F7D2776511D20E56BD'
+$url            = 'https://dl.google.com/tag/s/appguid%3D%7B3C122445-AECE-4309-90B7-85A6AEF42AC0%7D%26iid%3D%7B9648D435-67BA-D2A7-54D2-1E0B5656BF03%7D%26ap%3Duploader-photos%26appname%3DBackup%2520and%2520Sync%26needsadmin%3Dtrue/drive/installbackupandsync.exe'
+$checksum       = '2C2EA0D23D37AB0A5511D4F926EF961493CB55210FBDDC5899607FD9788B7854'
 $validExitCodes = @(0)
 $ahkExe         = 'AutoHotKey'
 $ahkFile        = Join-Path $toolsDir "GBASInstall.ahk"
