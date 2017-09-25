b32d1338acf7948f102283a948f4d559fe75512b
bcurran3 <bcurran3@yahoo.com>
2017-09-25 15:24:04 -0700

    Add files via upload

--- vnc-connect/tools/chocolateyinstall.ps1
+++ vnc-connect/tools/chocolateyinstall.ps1
@@ -1,21 +1,42 @@
 ﻿$packageName    = 'vnc-connect'
 $installerType  = 'exe'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.2.0-Windows.exe'
-$checksum       = 'D763B26FD9EC4C376040BF20B68B95DEFA2D5841FC7B698D0C3AE10735577508'
-$silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url            = 'https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.2.0-Windows-msi.zip'
+$checksum       = '0aa243c2817ee3d4adfdaae3a0ed56344aa9517b89645e0049587956bc79bc96'
+$silentArgs     = "/quiet /qn /norestart"
 $validExitCodes = @(0, 3010, 1641)
+$bits           = Get-ProcessorBits
+$extractDir     = "$toolsDir\extracted"
+
+New-Item $extractDir -type directory -force
 
 $packageArgs = @{
   packageName   = $packageName
-  fileType      = $installerType
+  unzipLocation = $extractDir
+  fileType      = 'ZIP' 
   url           = $url
-  validExitCodes= $validExitCodes
-  silentArgs    = $silentArgs
-  softwareName  = 'VNC *'
   checksum      = $checksum
-  checksumType  = 'sha256' 
+  checksumType  = 'sha256'
 }
 
-Install-ChocolateyPackage @packageArgs  
-  
+Install-ChocolateyZipPackage @packageArgs 
+
+if ($bits -eq 64)
+   {
+    $Installer = "$extractDir\VNC-Server-6.2.0-Windows-en-64bit.msi"
+   } else {
+    $Installer = "$extractDir\VNC-Server-6.2.0-Windows-en-32bit.msi"
+   }
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'MSI'
+  file          = $Installer
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = 'VNC *'
+}
+ 
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item $extractDir -recurse -force | out-null
