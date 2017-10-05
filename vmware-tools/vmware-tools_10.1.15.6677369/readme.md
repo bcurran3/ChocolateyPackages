843f993bac0ec8e00584613c66d69e525a7a88e9
bcurran3 <bcurran3@yahoo.com>
2017-10-05 02:22:52 -0700

    Add files via upload

---
+++ vmware-tools/vmware-tools_10.1.15.6677369/readme.md
@@ -0,0 +1,15 @@
+**BCURRAN3'S PACKAGE NOTES:**
+
+* This package downloads the installer via a version specific URL. It should never fail unless the version is completely removed from the vendor website.
+* "REBOOT=R" argument implemented as of this package version to supress reboots.
+* I personally use and endorse this program.
+
+Like my [packages](https://chocolatey.org/profiles/bcurran3)? 
+
+Find them useful?
+
+**Want to buy me a beer?**
+
+[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4ECL3UCG5CGB6)
+
+If applicable, please always consider donating to the developer or purchasing the software first - this includes Chocolatey licensed editions. 
---
+++ vmware-tools/vmware-tools_10.1.15.6677369/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName   = 'vmware-tools'
+$installerType = 'exe'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url           = 'https://packages.vmware.com/tools/releases/10.1.15/windows/x86/VMware-tools-10.1.15-6677369-i386.exe' 
+$checksum      = '7E3355C387E312741D86B409170A6A174D0E75F019FE39CC6C32D11C5C3821E7'
+$url64         = 'https://packages.vmware.com/tools/releases/10.1.15/windows/x64/VMware-tools-10.1.15-6677369-x86_64.exe' 
+$checksum64    = '81139F8391FA74ED987244B04943D3E9E678951C63A36A01B97C8FEB1A0CB842'
+$silentArgs    = '/S /v /qn REBOOT=R'
+$validExitCodes= @(0)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  url64bit      = $url64
+  validExitCodes= $validExitCodes
+  silentArgs    = $silentArgs
+  softwareName  = 'VMware Tools'
+  checksum      = $checksum 
+  checksumType  = 'sha256' 
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs  
+  
