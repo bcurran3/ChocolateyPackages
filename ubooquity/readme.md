3fc20ecf842ff8d458f4bf5d46a469da41c84d0b
bcurran3 <bcurran3@yahoo.com>
2017-10-05 03:05:06 -0700

    Add files via upload

--- ubooquity/readme.md
+++ ubooquity/readme.md
@@ -1,6 +1,7 @@
 **BCURRAN3'S PACKAGE NOTES:**
 
 * This package downloads the .ZIP file via a version specific URL. It should never fail unless the version is completely removed from the vendor website.
+* If Ubooquity is running, upgrades will fail. You must manually quit it and upgrde. I don't know how to script stopping a specific Java program without stopping all running Java programs. If you know how, feel free to tell me!
 * I personally use and endorse this program.
 
 Like my [packages](https://chocolatey.org/profiles/bcurran3)? 
--- ubooquity/tools/chocolateyinstall.ps1
+++ ubooquity/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName  = 'ubooquity' 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'http://vaemendis.net/ubooquity/downloads/Ubooquity-2.1.0.zip'
-$checksum     = '19892106AF6EC84932A41658ED9B5F0B91B661C520982969A97FBE904548594E'
+$url          = 'https://vaemendis.net/ubooquity/downloads/Ubooquity-2.1.1.zip'
+$checksum     = 'D7D454A99B3DB9C9F333F71F7C2A8977F3E0EA0B9B079E6370495C335AE0F600'
 $checksumType = 'sha256' 
 $fileType     = 'ZIP'
 
@@ -16,8 +16,8 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 
-Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\Ubooquity.lnk" -targetPath "$env:ChocolateyInstall\lib\ubooquity\tools\Ubooquity.jar" -WorkingDirectory "$env:ChocolateyInstall\lib\ubooquity\tools"
-Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Ubooquity.lnk" -targetPath "$env:ChocolateyInstall\lib\ubooquity\tools\Ubooquity.jar" -WorkingDirectory "$env:ChocolateyInstall\lib\ubooquity\tools"
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\Ubooquity.lnk" -targetPath "$toolsDir\Ubooquity.jar" -WorkingDirectory $toolsDir -IconLocation "$toolsDir\ubooquity.ico"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Ubooquity.lnk" -targetPath "$toolsDir\Ubooquity.jar" -WorkingDirectory $toolsDir -IconLocation "$toolsDir\ubooquity.ico"
 
 $WhoAmI=whoami
 icacls.exe $toolsDir /grant $WhoAmI":"'(OI)(CI)'F /T | out-null
