f037d21f73727b62ad391ca022eb8e8b059fd6c8
bcurran3 <bcurran3@yahoo.com>
2017-10-05 03:58:35 -0700

    Add files via upload

--- intel-driver-update-utility/tools/chocolateyinstall.ps1
+++ intel-driver-update-utility/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = 'intel-driver-update-utility'
 $installerType  = 'exe'
-$url            = 'https://downloadmirror.intel.com/24345/a08/Intel%20Driver%20Update%20Utility%20Installer.exe'
-$checksum       = '5EFA70CF6E2BCD90A74EF91671967A3668C62175D14FA8F34CACCEE170025F2D'
+$url            = 'https://downloadmirror.intel.com/24345/a08/Intel%20Driver%20and%20Support%20Assistant%20Installer.exe'
+$checksum       = '3F1ECFDC887F65AF50BAE9B0D1C5D58099DDDC33DE01032F4D0AE2F9A412C0F7'
 $toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $silentArgs     = '/q /norestart'
 $validExitCodes = @(0)
