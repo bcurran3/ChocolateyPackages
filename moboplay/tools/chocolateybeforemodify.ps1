2d6048f8ca58248b0b14c28e79dd9f018bd5553d
bcurran3 <bcurran3@yahoo.com>
2017-10-07 04:34:39 -0700

    Add files via upload

---
+++ moboplay/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'moboplay'
+
+if((get-process "iMoboPlay" -ea SilentlyContinue) -eq $Null){ 
+    Write-Host "MoboPlay for iOS not running. (Good!)" -foreground magenta
+  }else{ 
+    Write-Host "Stopping MoboPlay for iOS..." -foreground magenta
+    Stop-Process -processname "iMoboPlay"
+	}
+	
+if((get-process "aMoboPlay" -ea SilentlyContinue) -eq $Null){ 
+    Write-Host "MoboPlay for Android not running. (Good!)" -foreground magenta
+  }else{ 
+    Write-Host "Stopping MoboPlay for Android..." -foreground magenta
+    Stop-Process -processname "aMoboPlay"
+	}	
