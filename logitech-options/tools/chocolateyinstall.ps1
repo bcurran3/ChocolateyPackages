d375bd4f163e6310e012963f1fbc365e84d7d60c
bcurran3 <bcurran3@yahoo.com>
2017-10-11 02:00:42 -0700

    Add files via upload

--- logitech-options/tools/chocolateyinstall.ps1
+++ logitech-options/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName   = 'logitech-options'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url           = 'https://download01.logi.com/web/ftp/pub/techsupport/options/Options_6.50.60.exe'
-$checksum      = 'C25506BAFEB00AB96E362C468BF263D5E419A02F80A593A190DDA78815495513'
+$url           = 'https://download01.logi.com/web/ftp/pub/techsupport/options/Options_6.70.1197.exe'
+$checksum      = '63FD5F943BBD338DB8F4868E0BDE2CE97742E46C23BB35F9FF83A6544272719C'
 $silentArgs    = '/S'
 
 $packageArgs = @{
