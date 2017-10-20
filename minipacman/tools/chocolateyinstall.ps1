2114bc0b6836bc7421a18ca50863248642b3e022
bcurran3 <bcurran3@yahoo.com>
2017-10-20 05:26:30 -0700

    Add files via upload

---
+++ minipacman/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'minipacman' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\minpacman.zip"
+$checksum    = '23824112EEBAFAD331477B5586A0062F76D72F785CCD191AE15A13CD016CB546'
+$Shortcut    = 'Pacman (mini).lnk'
+$ProgramEXE  = 'minpacman.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  }
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$Shortcut" -targetPath "$toolsDir\$ProgramEXE" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\$ProgramEXE"    
+
+Remove-Item $url | out-null
