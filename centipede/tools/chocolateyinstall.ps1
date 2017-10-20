2b8b4b632a5670dd33dcc8c62495126939d7f354
bcurran3 <bcurran3@yahoo.com>
2017-10-20 05:28:49 -0700

    Add files via upload

---
+++ centipede/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'centipede' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\centipede.zip"
+$checksum    = 'FD9DF178F85EFBD7A9594C25514AF1C0C0940D261778998ACA975EF78C999CE6'
+$Shortcut    = 'Centipede.lnk'
+$ProgramEXE  = 'centipede.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  }
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$Shortcut" -targetPath "$toolsDir\$ProgramEXE" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\$ProgramEXE"    
+Remove-Item $url | out-null
---
+++ lunarlander/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'lunarlander' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\lander.zip"
+$checksum    = '5B6B8032DA8AEB3B390404462FB261DDAED8628049C90095B1D48CE54D8DDE9F'
+$Shortcut    = 'Lunar Lander.lnk'
+$ProgramEXE  = 'lander.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  }
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$Shortcut" -targetPath "$toolsDir\$ProgramEXE" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\$ProgramEXE"    
+
+Remove-Item $url | out-null
