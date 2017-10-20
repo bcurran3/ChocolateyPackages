a2dc0f1a971e3dd410be3653e773c7427d976038
bcurran3 <bcurran3@yahoo.com>
2017-10-20 05:28:49 -0700

    Add files via upload

---
+++ donkeykongjr/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'donkeykongjr' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\kongjr.zip"
+$checksum    = '0A709062F147DB1E8CAB11E553F51008896C615C981C32ABEF256B551FCF9FC0'
+$Shortcut    = 'Donkey Kong Jr.lnk'
+$ProgramEXE  = 'kongjr.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  }
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$Shortcut" -targetPath "$toolsDir\$ProgramEXE" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\$ProgramEXE"    
+Remove-Item $url | out-null
