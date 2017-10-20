3034972c2aec5b2a1c502339d71c2be35626feb5
bcurran3 <bcurran3@yahoo.com>
2017-10-20 05:28:49 -0700

    Add files via upload

---
+++ asteroids/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'asteroids' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\asteroids.zip"
+$checksum    = 'CB994A1700249149D713B1833C6B83A0D8B3716265E9453FD8328894D55E34EB'
+$Shortcut    = 'Asteroids.lnk'
+$ProgramEXE  = 'asteroids.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  }
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$Shortcut" -targetPath "$toolsDir\$ProgramEXE" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\$ProgramEXE"    
+
+Remove-Item $url | out-null
