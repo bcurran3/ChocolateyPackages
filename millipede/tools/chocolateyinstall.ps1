d827232313ff7de8cdfbfba0a6a1789616e23afb
bcurran3 <bcurran3@yahoo.com>
2017-10-20 05:26:30 -0700

    Add files via upload

---
+++ millipede/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'millipede' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\millipede.zip"
+$checksum    = '751C4EFFC6656075C828BE3601145D8B04898EC0BC52F1A14C3E49D782E26544'
+$Shortcut    = 'Millipede.lnk'
+$ProgramEXE  = 'millipede.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  }
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$Shortcut" -targetPath "$toolsDir\$ProgramEXE" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\$ProgramEXE"    
+
+Remove-Item $url | out-null
---
+++ misslecommand/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'misslecommand' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\command.zip"
+$checksum    = 'FD2088E42284F06A03D0606E545517D7FDD4129AE8644E02A0BC3D2BA0B22873'
+$Shortcut    = 'Missle Command.lnk'
+$ProgramEXE  = 'command.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  }
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$Shortcut" -targetPath "$toolsDir\$ProgramEXE" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\$ProgramEXE"    
+
+Remove-Item $url | out-null
