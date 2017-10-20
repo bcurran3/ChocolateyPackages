d080de2c261defb1de51c5c417530e224bcd7872
bcurran3 <bcurran3@yahoo.com>
2017-10-20 05:24:47 -0700

    Add files via upload

---
+++ superpacman/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'superpacman' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\superpacman.zip"
+$checksum    = '3E9643578F98FA19354BDE5C6560837621B43905F2F673F596666B666097702F'
+$Shortcut    = 'Super Pacman.lnk'
+$ProgramEXE  = 'superpacman.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  }
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$Shortcut" -targetPath "$toolsDir\$ProgramEXE" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\$ProgramEXE"    
+
+Remove-Item $url | out-null
