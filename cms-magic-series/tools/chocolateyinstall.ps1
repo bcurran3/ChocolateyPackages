8d797b4ff7b85eb61af864ac99ad31cfc1b0392e
bcurran3 <bcurran3@yahoo.com>
2017-10-29 03:49:15 -0700

    Add files via upload

--- cms-magic-series/tools/chocolateyinstall.ps1
+++ cms-magic-series/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName    = 'cms-magic-series'
 $installerType  = 'MSI'
-$url            = 'http://download.eyemaxdvr.com/XVST%20MAGIC%20SERIES/CMS/CMS-IPT-2.2.4.2028-win32.msi' 
-$checksum       = 'BE9FC352EACB0A9FCF8D90DE0304500AE4E631FA62C502F867DCF9379955262D'
-$url64          = 'http://download.eyemaxdvr.com/XVST%20MAGIC%20SERIES/CMS/CMS-IPT-2.2.4.2028-win64.msi' 
-$checksum64     = 'E733FC55F94B8C7ECF65590653BD4D5C10B2EB6481FAF302A24C104AE94509E6'
+$url            = 'http://download.eyemaxdvr.com/XVST%20MAGIC%20SERIES/CMS/CMS-IPT-2.2.5.2029-win32.msi' 
+$checksum       = '317EDF3D8E632DF2C4786EB0003EC6A3C95100E33D9B521E0078BD858D7D07D5'
+$url64          = 'http://download.eyemaxdvr.com/XVST%20MAGIC%20SERIES/CMS/CMS-IPT-2.2.5.2029-win64.msi' 
+$checksum64     = '18D6DFFB8615352F732C96772326100499A33CC7FCC5EE9386C4A890F83248F3'
 $silentArgs     = '/quiet /qn /norestart'
 $validExitCodes = @(0, 3010, 1641)
 
