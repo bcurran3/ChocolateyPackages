e0439be4387934ec8a398bbd258758a290529565
bcurran3 <bcurran3@yahoo.com>
2017-11-04 15:42:03 -0700

    Add files via upload

---
+++ betterhash/tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName    = 'betterhash'
+$softwareName   = 'BetterHash'
+
+if((get-process "BetterHash" -ea SilentlyContinue) -eq $Null){ 
+    Write-Host "BetterHash currently NOT running." 
+  }else{ 
+    Write-Host "Stopping BetterHash process..."
+    Stop-Process -processname "BetterHash"
+  }
+
