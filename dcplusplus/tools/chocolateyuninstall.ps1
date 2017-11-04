427a977a4594d36652ee9391de75dd17f41c3a37
bcurran3 <bcurran3@yahoo.com>
2017-11-04 15:12:59 -0700

    Add files via upload

---
+++ dcplusplus/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'dcplusplus' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ahkExe      = 'AutoHotKey'
+$ahkFile     = Join-Path $toolsDir "DCPPuninstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+
