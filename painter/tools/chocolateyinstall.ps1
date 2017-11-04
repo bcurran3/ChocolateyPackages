ac09b57e61fe6f1760ccec2f36f8df35af5014fc
bcurran3 <bcurran3@yahoo.com>
2017-11-04 02:22:56 -0700

    Add files via upload

---
+++ painter/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'painter' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\specypainter.zip"
+$checksum    = '71412961283069E1A1DD959DDD905E4F4D9ACDAFD37CD384E956E1ACC4D8C73C'
+$Shortcut    = 'Painter.lnk'
+$ProgramEXE  = 'painter.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  }
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$Shortcut" -targetPath "$toolsDir\$ProgramEXE" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\$ProgramEXE"    
+Remove-Item $url | out-null
---
+++ snake/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'snake' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\snake.zip"
+$checksum    = 'E2C8FC917020F07FCE0B4720CCA5CD3A1372FFF628901DE6FCCB0597D86DB3A8'
+$Shortcut    = 'Snake.lnk'
+$ProgramEXE  = 'snake.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  }
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$Shortcut" -targetPath "$toolsDir\$ProgramEXE" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\$ProgramEXE"    
+Remove-Item $url | out-null
