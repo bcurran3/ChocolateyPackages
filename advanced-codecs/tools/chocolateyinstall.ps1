2731fecdb68065522e136caaad9c861d4c5f3060
bcurran3 <bcurran3@yahoo.com>
2017-11-04 02:49:23 -0700

    Add files via upload

--- advanced-codecs/tools/chocolateyinstall.ps1
+++ advanced-codecs/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿#http://www.majorgeeks.com/mg/getmirror/win7codecs,1.html
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName    = 'advanced-codecs' 
-$url            = "$toolsDir\ADVANCED_Codecs_v835.exe"
-$checksum       = 'E6E92138E09EB7018FF54C57A7DDFC748B68A5F9CA8730F1EA59BC64CE910162'
+$url            = "$toolsDir\ADVANCED_Codecs_v840.exe"
+$checksum       = '9446881D7FA14515ADA5BA6B7D7AC6EE3479CE47F87F01762E9DDF797A95413B'
 $installerType  = 'EXE'
 $silentArgs     = '/S /v/qn'
 $validExitCodes = @(0, 3010, 1641)
