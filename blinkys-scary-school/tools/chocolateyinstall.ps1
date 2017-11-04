03ba66ffc7667733253d379ab5ee2f43db14688a
bcurran3 <bcurran3@yahoo.com>
2017-11-04 02:22:56 -0700

    Add files via upload

---
+++ blinkys-scary-school/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'blinkys-scary-school' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\blinky.zip"
+$checksum    = 'BD89D5F042F55B30A52E3CF9B3929E987528DEB00523ED9BD622F69F90EFA0B9'
+$Shortcut    = 'Blinkys Scary School.lnk'
+$ProgramEXE  = 'blinky.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  }
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$Shortcut" -targetPath "$toolsDir\$ProgramEXE" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\$ProgramEXE"    
+Remove-Item $url | out-null
---
+++ blitz/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'blitz' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\blitz.zip"
+$checksum    = '4EB8203AB1821F1685330873304AC38886930F7D4338FAC140E37E4449236CA0'
+$Shortcut    = 'Blitz.lnk'
+$ProgramEXE  = 'blitz.exe'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  }
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$Shortcut" -targetPath "$toolsDir\$ProgramEXE" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$Shortcut" -targetPath "$toolsDir\$ProgramEXE"    
+Remove-Item $url | out-null
