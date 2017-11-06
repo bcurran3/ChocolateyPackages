65dcb50d2e42c29bf939448a783606afd1b41e5c
bcurran3 <bcurran3@yahoo.com>
2017-11-06 03:28:12 -0800

    Add files via upload

---
+++ choco-cleaner/readme.md
@@ -0,0 +1,24 @@
+**BCURRAN3'S PACKAGE NOTES:**
+
+* A BCURRAN3 original!
+* This may NOT work on Windows 7 / Windows Server 2012. I've added PowerShell v4 as a dependency in hopes it will work.
+* Read the INSTALL EXAMPLES again before use!
+* I personally use and endorse this script/package/program/task/whatever-you-want-to-call-it.
+
+ROADMAP:
+* Add options for weekly and monthly instead of only daily.
+* Cancel task to stop after xxx minutes in event of a non-silent package getting "stuck"
+* Possibly drop the leading 0 required for times less than < 10:00 AM
+
+Like my [packages](https://chocolatey.org/profiles/bcurran3)? 
+
+Find them useful?
+
+**Want to buy me a beer?**
+
+[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4ECL3UCG5CGB6)
+
+If applicable, please always consider donating to the developer or purchasing the software first - this includes Chocolatey licensed editions. 
+
+
+
---
+++ choco-cleaner/tools/choco-cleaner.ps1
@@ -0,0 +1,38 @@
+# choco-cleaner.ps1 v0.0.1 by Bill Curran
+# LICENSE: GNU GPL v3 - https://www.gnu.org/licenses/gpl.html
+# Open to suggestions - open a GitHub issue please if you have a suggestion/request.
+#Remove-Item -path $env:chocolateyinstall\.chocolatey\* -recurse -whatif
+Remove-Item -path $env:chocolateyinstall\logs\* -recurse -exclude chocolatey.log,choco.summary.log -whatif
+Remove-Item -path $env:chocolateyinstall\* -recurse -include *.msi,*.zip -whatif
+Remove-Item -path $env:chocolateyinstall\* -recurse -include *.msi.ignore,*.zip.txt,*.exe.txt -whatif
+Remove-Item -path $env:chocolateyinstall\* -recurse -include credits.txt,license.txt,*.license.txt,verification.txt,readme.txt,*.md -whatif
+Remove-Item -path $env:chocolateyinstall\bin\_processed.txt -whatif
+Remove-Item -path $env:chocolateyinstall\* -recurse -include *.old -whatif
+#Remove-Item -path $env:chocolateyinstall\* -recurse -include *.backup -whatif
+#Remove-Item -path $env:chocolateyinstall\* -recurse -include *.txt -whatif
+#
+#Remove-Item -path $env:chocolateyinstall\lib-bad\* -recurse -whatif
+#Remove-Item -path $env:chocolateyinstall\lib-bkp\* -recurse -whatif
+#
+#Remove-Item -path $env:tmp\chocolatey\* -recurse -whatif
+#Remove-Item -path $env:tmp\temp\chocolatey\* -recurse -whatif
+exit
+
+#######################################################################################################################
+You can comment out anything above you donot want to delete such as log files with a "#" at the front of the line.
+
+LEGEND:
+* log files that are not the most current one
+* msi and zip files that are left over from packages that forgot to delete them post install
+* msi.ignore files created to ignore the file for shimming, but we just deleted the msi files
+* zip.txt and exe.txt files are list of files that were extracted from an archive file
+* license.txt and *.license.txt are license files that you can read on the web
+* credits.txt are credits files you can read on the web
+* license.txt and verification.txt files are included when packges have binaries, you can read them on the web
+* programname.license.txt files are license information for Chocolatey tools
+* readme.txt files you can read on the web
+* md files are usually markdown read me's that you can read on the web
+* old files are Chocolatey files that have been replaced by newer ones (exe's and dll's mostly)
+* _processed.txt - I have no idea what made this file
+* backup files are 
+#######################################################################################################################
