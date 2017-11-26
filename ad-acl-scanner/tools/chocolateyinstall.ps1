072432c72dfe48478a3f592c914d23f84b93011f
bcurran3 <bcurran3@yahoo.com>
2017-11-26 00:00:17 -0800

    Add files via upload

--- ad-acl-scanner/tools/chocolateyinstall.ps1
+++ ad-acl-scanner/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $packageName  = 'ad-acl-scanner' 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://raw.githubusercontent.com/canix1/ADACLScanner/master/ADACLScan.ps1'
-$checksum     = '96716DF59F6C1FBF2146A8D55793B5D3F64E6E9D8CC9CDAA222077D1FE2DC58B'
+$checksum     = '7BADE8A248F461325535C1240741AC57133D41EBA1557681A16B9EFE60158F11'
 $fileName     = 'ADACLScan.ps1'
 $shortcutName = 'AD ACL Scanner' 
 $FileFullpath = Join-Path $ToolsDir $fileName
