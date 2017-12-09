ad02350a4b7d984a1db99d17a6b81e65348c039d
bcurran3 <bcurran3@yahoo.com>
2017-12-09 04:13:47 -0800

    Add files via upload

--- drivermax/tools/chocolateyinstall.ps1
+++ drivermax/tools/chocolateyinstall.ps1
@@ -1,13 +1,15 @@
 ﻿$packageName = 'drivermax' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url         = 'http://www.drivermax.com/soft/dmx/drivermax.exe'
-$checksum    = '8759200632F18DEE39FCA671F69B2F11886DBF756D49FBFFABC75AC6294A22CE'
+$checksum    = '0CD497165E1D4BB749927677B70E8F3277F0823F455DEF496A13D9DB12C4CC58'
+$validExitCodes = @(0, 1)
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url
+  validExitCodes= $validExitCodes  
   silentArgs    = '/SP- /VERYSILENT /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS' 
   softwareName  = 'DriverMax*' 
   checksum      = $checksum
