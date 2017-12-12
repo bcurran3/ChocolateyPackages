53e633bc2e9b7d3d1ec38184ce9e72d2a9dea1ad
bcurran3 <bcurran3@yahoo.com>
2017-12-12 05:50:37 -0800

    Add files via upload

--- ultimate-settings-panel/tools/chocolateyInstall.ps1
+++ ultimate-settings-panel/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName    = 'ultimate-settings-panel'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://downloads.sourceforge.net/project/ultimatesettingspanelpro/v5.7/UltimateSettingsPanel.zip'
-$checksum       = '5C1B5A98C40E1A73306799E09B8B0BD55CF1B2242ACB45717E2A4F7364DA8C0C'
+$url            = 'https://downloads.sourceforge.net/project/ultimatesettingspanelpro/v5.8/UltimateSettingsPanel.zip'
+$checksum       = 'FA0A1C329F072B7B18A4D0F43A8B89A1E9C702E7DE767BB371ABE56D3B3FA4D6'
 $silentArgs     = '/quiet /qn /norestart'
 $validExitCodes = @(0)
 $osBits         = Get-ProcessorBits
@@ -19,11 +19,11 @@ Install-ChocolateyZipPackage @packageArgs
 
   if ($osBits -eq 64) 
     {
-    $UnzippedEXE  = Join-Path $toolsDir 'Installers\x64\Ultimate Settings Panel x64.msi'
+     $UnzippedEXE  = Join-Path $toolsDir 'x64\Ultimate Settings Panel x64.msi'
     }
   else
     {
-    $UnzippedEXE  = Join-Path $toolsDir 'Installers\x86\Ultimate Settings Panel.msi'
+     $UnzippedEXE  = Join-Path $toolsDir 'x86\Ultimate Settings Panel.msi'
     }
 
 $packageArgs = @{
@@ -36,4 +36,8 @@ $packageArgs = @{
 }
 
 Install-ChocolateyInstallPackage @packageArgs	
+
+Remove-Item $toolsDir\x64 -Force -Recurse -ErrorAction SilentlyContinue
+Remove-Item $toolsDir\x86 -Force -Recurse -ErrorAction SilentlyContinue
+Remove-Item $toolsDir\*.url -Force -ErrorAction SilentlyContinue
 
