575a399f9743760ad065c28f84d47c4424f95d1d
bcurran3 <bcurran3@yahoo.com>
2018-01-01 14:36:46 -0800

    Add files via upload

---
+++ readycloud/tools/chocolateyuninstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'readycloud' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ahkExe      = 'AutoHotKey'
+$ahkFile     = "$toolsDir\ReadyCLOUDuninstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+
+Start-Sleep -s 20
+
