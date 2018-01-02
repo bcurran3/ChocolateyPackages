06bf1524188d0acd720b73a96dbccd81583e1891
bcurran3 <bcurran3@yahoo.com>
2018-01-01 20:17:57 -0800

    Add files via upload

---
+++ lan-speed-test.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName  = 'lan-speed-test.portable'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'http://www.totusoft.com/files/LAN_SpeedTestPortable.zip'
+$checksum     = 'E1731799ED0B162BC043DADD87A81392657FBBABAAA879B7BD11996FC48C32F3'
+$shortcutName = 'LAN Speed Test.lnk'
+$portableEXE  = "$toolsDir\LAN_SpeedTestPortable.exe";
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath $portableEXE -WorkingDirectory $toolsDir
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath $portableEXE -WorkingDirectory $toolsDir
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lan-speed-test.portable/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+$packageName  = 'lan-speed-test.portable'
+$shortcutName = 'LAN Speed Test.lnk'
+
+remove-item "$env:Public\Desktop\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
+remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
