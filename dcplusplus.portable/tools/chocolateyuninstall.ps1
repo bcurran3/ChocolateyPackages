11dab82b125b515777009b7710178755f97ac64f
bcurran3 <bcurran3@yahoo.com>
2018-01-05 13:39:45 -0800

    Add files via upload

---
+++ dcplusplus.portable/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+$packageName  = 'dcplusplus.portable'
+$shortcutName = 'DC++.lnk'
+
+remove-item "$env:Public\Desktop\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
+remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
