1c3d13d3265cf4b25e6eab8c94d7ac806151ae46
bcurran3 <bcurran3@yahoo.com>
2018-01-05 18:28:32 -0800

    Add files via upload

--- advanced-codecs/tools/chocolateyuninstall.ps1
+++ advanced-codecs/tools/chocolateyuninstall.ps1
@@ -1,28 +1,5 @@
 ﻿$packageName    = 'advanced-codecs'
 $softwareName   = 'Shark007 ADVANCED Codecs*'
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$installerType  = 'EXE' 
-$silentArgs     = '/SILENT'
-$silentArgs2    = 'uninstall'
-$validExitCodes = @(0)
-$unpath         = "${Env:ProgramFiles}\Shark007\unins000.exe"
-$bits           = Get-ProcessorBits
-$ahkExe         = 'AutoHotKey'
-$ahkFile        = Join-Path $toolsDir "ACUninstall.ahk"
 
-if ($bits -eq 64)
-  {
-   $unpath2 = "${Env:ProgramFiles(x86)}\Shark007\Advanced\Tools\Settings32.exe"  
-  } else {
-   $unpath2 = "${Env:ProgramFiles}\Shark007\Advanced\Tools\Settings32.exe"
-  }
-
-Start-Process $ahkExe $ahkFile
-
-If (Test-Path $unpath){
-Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -File $unpath -validExitCodes $validExitCodes
-}Else{
-}
-
-Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs2 -File $unpath2 -validExitCodes $validExitCodes
-
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/x {8C0CAA7A-3272-4991-A808-2C7559DE3409} /qn"
+Start-Process -FilePath "$env:programfiles\Shark007\unins000.exe" -ArgumentList "/SILENT"
