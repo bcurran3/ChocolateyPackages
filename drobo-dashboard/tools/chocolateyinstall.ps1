a47fd0d81e9623d698307a977f705a117fd809f3
bcurran3 <bcurran3@yahoo.com>
2018-01-05 17:14:21 -0800

    Add files via upload

--- drobo-dashboard/tools/chocolateyinstall.ps1
+++ drobo-dashboard/tools/chocolateyinstall.ps1
@@ -1,10 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = 'drobo-dashboard' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'http://files.drobo.com/webrelease/dashboard/Drobo-Dashboard-3.1.1.exe'
-$checksum    = '371A568C3B78F5A02DFBB2C336B69E87B7167BEE82D01A05806F40225DE7574A'
+$url         = 'http://files.drobo.com/webrelease/dashboard/Drobo-Dashboard-3.1.5.exe'
+$checksum    = 'B6101E134A9448E02A4B0FB4B632734682ABA030F2FF1D074A8741A296258E16'
 $ahkExe      = 'AutoHotKey'
 $ahkFile     = Join-Path $toolsDir "DDinstall.ahk"
+
+$Installed = (Test-Path "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{863885B3-7C05-421C-8817-568712778745}")
+If ($Installed -eq "True")
+    {
+	Write-Host Found previous version of Drobo Dashboard. -ForegroundColor red -BackgroundColor blue
+	Write-Host Attempting to uninstall it... -ForegroundColor red -BackgroundColor blue
+	Start-Process -FilePath "msiexec.exe" -ArgumentList "/x {863885B3-7C05-421C-8817-568712778745} /qn"
+	Start-Sleep -s 10
+    }
 
 Start-Process $ahkExe $ahkFile
 
