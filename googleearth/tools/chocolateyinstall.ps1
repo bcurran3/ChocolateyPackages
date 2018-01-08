c60190d9e06164fad49c4411fa3aa1269ecfec34
bcurran3 <bcurran3@yahoo.com>
2018-01-08 13:28:43 -0800

    Add files via upload

--- googleearth/tools/chocolateyinstall.ps1
+++ googleearth/tools/chocolateyinstall.ps1
@@ -4,12 +4,8 @@ $installerType = 'EXE'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url           = 'https://dl.google.com/earth/client/GE7/release_7_1_8/googleearth-win-7.1.8.3036.exe' 
 $checksum      = 'B64FFC6F31E3E42F154E7D19DFA301376DE987403A9C50B575DAC04022E06576'
-$silentArgs    = ''
+$silentArgs    = 'OMAHA=1'
 $validExitCodes= @(0)
-$ahkExe        = 'AutoHotKey'
-$ahkFile       = Join-Path $toolsDir "GoogleEarthInstall.ahk"
-
-Start-Process $ahkExe $ahkFile
 
 $packageArgs = @{
   packageName   = $packageName
@@ -23,13 +19,3 @@ $packageArgs = @{
 }
 
 Install-ChocolateyPackage @packageArgs 
-  
-Start-Sleep -s 20
-
-if((get-process "googleearth" -ea SilentlyContinue) -eq $Null){ 
-    Write-Host "googleearth currently NOT running." 
-  }else{ 
-    Write-Host "Stopping googleearth process..."
-    Stop-Process -processname "googleearth" -force
-  }
- 
