330f603dae6debe8cf5b23aa4ebac9e26edbc610
bcurran3 <bcurran3@yahoo.com>
2018-01-18 22:25:36 -0800

    Add files via upload

--- bzflag/tools/chocolateyinstall.ps1
+++ bzflag/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$packageName    = 'bzflag'
 $installerType  = 'exe'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://download.bzflag.org/bzflag/windows/2.4.10/bzflag-2.4.10.exe' 
+$url            = 'https://download.bzflag.org/bzflag/windows/2.4.10/bzflag-2.4.10.exe'
 $checksum       = 'A33119ABACF6B896E523C777BBF918D042D8618BE9DBF0369644FB49009F2ECF'
 $silentArgs     = '/S'
 $validExitCodes = @(0)
-
+ 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = $installerType
@@ -13,9 +13,9 @@ $packageArgs = @{
   validExitCodes= $validExitCodes
   silentArgs    = $silentArgs
   softwareName  = 'BZFlag*'
-  checksum      = $checksum 
-  checksumType  = 'sha256' 
+  checksum      = $checksum
+  checksumType  = 'sha256'
 }
-
+ 
 Install-ChocolateyPackage @packageArgs  
   
