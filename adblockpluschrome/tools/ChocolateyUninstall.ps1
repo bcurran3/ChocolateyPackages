2289bf433e168a9cbc7921b58d56c76b924d01ee
bcurran3 <bcurran3@yahoo.com>
2018-01-18 22:21:39 -0800

    Add files via upload

--- adblockpluschrome/tools/ChocolateyUninstall.ps1
+++ adblockpluschrome/tools/ChocolateyUninstall.ps1
@@ -1,9 +1,9 @@
 $bits = Get-ProcessorBits
-if ($bits -eq 64)
-{
-Remove-Item 'HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\cfhdojbkjhnklbpkdaibdccddilifddb' -Force -ErrorAction SilentlyContinue
-}
-else
-{
-Remove-Item 'HKLM:\SOFTWARE\Google\Chrome\Extensions\cfhdojbkjhnklbpkdaibdccddilifddb' -Force -ErrorAction SilentlyContinue
+$packageName = 'adblockpluschrome'
+$extensionID = 'cfhdojbkjhnklbpkdaibdccddilifddb'
+
+if ($bits -eq 64) {
+    Remove-Item "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue
+   }else{
+    Remove-Item "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue
 }
