dc65f8de14526e42af2971ce4752fe28e5868470
bcurran3 <bcurran3@yahoo.com>
2018-01-18 22:27:48 -0800

    Add files via upload

---
+++ choco-package-list-backup/tools/choco-package-list-backup-manual.bat
@@ -0,0 +1,20 @@
+@echo off
+net session >nul 2>&1
+if %errorLevel% == 0 goto RunScript
+  else (
+   color 4e
+   cls
+   echo.
+   echo                  ********************************************
+   echo                  *  You must choose "Run as administrator"  *
+   echo                  ********************************************
+   echo.
+   timeout 5
+   goto end
+ )
+
+:RunScript
+powershell -NoProfile -ExecutionPolicy Bypass -Command "\ProgramData\chocolatey\bin\choco-package-list-backup.ps1"
+timeout 5
+:end
+
--- choco-package-list-backup/tools/chocolateyinstall.ps1
+++ choco-package-list-backup/tools/chocolateyinstall.ps1
@@ -6,4 +6,4 @@ Move-Item "$toolsDir\$script" $env:ChocolateyInstall\bin -Force -ErrorAction 'Si
 Write-Host "Edit $env:ChocolateyInstall\bin\$script to customize your backup(s)." -foreground "magenta" –backgroundcolor "blue"
 Write-Host "From PowerShell, type CHOCO-PACKAGE-LIST-BACKUP.PS1 to backup your Chocolatey packages list." -foreground "magenta" –backgroundcolor "blue"
 Write-Host "From Command Prompt, type POWERSHELL CHOCO-PACKAGE-LIST-BACKUP.PS1 to backup your Chocolatey packages list." -foreground "magenta" –backgroundcolor "blue"
-
+Write-Host "From Windows, double click choco-package-list-backup-manual.bat in \ProgramData\chocolatey\lib\choco-package-list-backup\tools" -foreground "magenta" –backgroundcolor "blue"
