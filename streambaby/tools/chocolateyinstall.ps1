8c0216b55de7d5fe454c4398049cb5f217bcfd80
bcurran3 <bcurran3@yahoo.com>
2018-01-19 02:11:19 -0800

    Add files via upload

--- streambaby/tools/chocolateyinstall.ps1
+++ streambaby/tools/chocolateyinstall.ps1
@@ -1,8 +1,10 @@
-﻿$packageName = 'streambaby'
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'streambaby'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url         = 'https://cytranet.dl.sourceforge.net/project/streambaby/streambaby-0.54.zip'
 $checksum    = '934106829C63B1EBED70C80B15CFB66A2E9B145385060F706E818876168055C0'
-$oldINI      = 'env:ChocolateyInstall\lib-bkp\streambaby.ini'
+$oldINI      = '$toolsDir\streambaby-0.53\streambaby.ini'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -18,9 +20,6 @@ Install-ChocolateyZipPackage @packageArgs
 Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\streambaby.lnk" -targetPath "$toolsDir\streambaby-0.54\streambaby.bat" -iconLocation "$toolsDir\streambaby.ico"
 Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\streambaby.lnk" -targetPath "$toolsDir\streambaby-0.54\streambaby.bat" -iconLocation "$toolsDir\streambaby.ico"
 
-$WhoAmI=whoami
-icacls.exe $toolsDir /grant $WhoAmI":"'(OI)(CI)'F /T | out-null
-
 If (Test-Path $oldINI)
     {
      Write-Host "Restoring previous streambaby.ini" -foreground "magenta" –backgroundcolor "blue"
@@ -28,3 +27,6 @@ If (Test-Path $oldINI)
     }Else{
 	 Write-Host "No previous streambaby.ini found." -foreground "magenta" –backgroundcolor "blue"
    }
+   
+$WhoAmI=whoami
+icacls.exe $toolsDir /grant $WhoAmI":"'(OI)(CI)'F /T | out-null
