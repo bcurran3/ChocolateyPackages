85ccbe809542d01382f138d5ac33546fa6710fcd
bcurran3 <bcurran3@yahoo.com>
2018-01-28 04:34:57 -0800

    Add files via upload

--- intel-dsa/tools/chocolateyinstall.ps1
+++ intel-dsa/tools/chocolateyinstall.ps1
@@ -1,10 +1,14 @@
 ﻿$packageName    = 'intel-dsa'
 $installerType  = 'exe'
 $url            = 'https://downloadmirror.intel.com/24345/a08/Intel%20Driver%20and%20Support%20Assistant%20Installer.exe'
-$checksum       = '3EED30FFF567C995140A0DABDACF8960C4B7E9BCDA7F956B6479BFB60A903397'
+$checksum       = 'F6938A3275E2A343CC883261BC94E154EA40D19E81D9E8D7AD31E907CD4F50FF'
 $toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $silentArgs     = '/S'
 $validExitCodes = @(0)
+$ahkExe         = 'AutoHotKey'
+$ahkFile        = Join-Path $toolsDir "DSAinstall.ahk"
+
+Start-Process $ahkExe $ahkFile
 
 $packageArgs = @{
   packageName   = $packageName
@@ -12,18 +16,9 @@ $packageArgs = @{
   url           = $url
   silentArgs    = $silentArgs
   validExitCodes= $validExitCodes
-  softwareName  = 'Intel® Driver Update Utility*'
+  softwareName  = 'Intel® Driver & Support Assistant'
   checksum      = $checksum
   checksumType  = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
-
-Start-Sleep -s 20
-
-if((get-process "SurConsent" -ea SilentlyContinue) -eq $Null){ 
-    Write-Host "SurConsent currently NOT running." 
-  }else{ 
-    Write-Host "Stopping SurConsent process..."
-    Stop-Process -processname "SurConsent" -force
-  }
