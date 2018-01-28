17beb55e71f57ecd5f64840368267c902ebc65d4
bcurran3 <bcurran3@yahoo.com>
2018-01-28 04:03:27 -0800

    Add files via upload

--- dsinternals-psmodule/tools/chocolateyinstall.ps1
+++ dsinternals-psmodule/tools/chocolateyinstall.ps1
@@ -1,4 +1,6 @@
-﻿$packageName = 'dsinternals-psmodule'
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'dsinternals-psmodule'
 
+Get-PackageProvider -Name NuGet -Force
 Install-Module -Name DSInternals -RequiredVersion 2.22 -AllowClobber -Force
+Import-Module -Name DSInternals
