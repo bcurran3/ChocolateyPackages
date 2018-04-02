180f6db2f144e91eb0a2f900126f97ee49f35372
bcurran3 <bcurran3@yahoo.com>
2018-04-02 15:27:57 -0700

    Add files via upload

--- getdataback-simple/tools/chocolateyinstall.ps1
+++ getdataback-simple/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = 'getdataback-simple'
 $installerType  = 'exe'
 $url            = 'https://www.runtime.org/gdbsimsetup.exe'
-$checksum       = 'A7C32A5AFACF7D13D98321502E4964E7A357B6C2BF6E97B40CC69AC240896DD6'
+$checksum       = '54836F889CF154CDCC98CE7F2DEB39D3F87E6E479BFFB01F21C1479AB0203B81'
 $silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $validExitCodes = @(0)
