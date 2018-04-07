ce7165c6aa0e009022a970d38cfa855b3c0b3f9f
bcurran3 <bcurran3@yahoo.com>
2018-04-06 23:55:10 -0700

    Add files via upload

--- google-play-music-manager/tools/chocolateyinstall.ps1
+++ google-play-music-manager/tools/chocolateyinstall.ps1
@@ -1,7 +1,8 @@
 ﻿$packageName    = 'google-play-music-manager' 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://dl.google.com/androidjumper/win/musicmanagerinstaller.exe'
-$checksum       = 'A267CA8DD5CC654C23771C89C26AE3CC713F9C6F82110A977037D801E5C40B62'
+#$url            = 'https://dl.google.com/androidjumper/win/musicmanagerinstaller.exe'
+$url            = 'https://dl.google.com/tag/s/appguid%3D%7BEA25377A-280A-4539-8FED-21FACE57594F%7D%26iid%3D%7B4902BC13-D0C8-BD14-94C9-7CE1F3EAB86D%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DMusic%2520Manager%26needsadmin%3Dfalse/androidjumper/win/musicmanagerinstaller.exe'
+$checksum       = '157A9CB59E40A98E1CEBDF7F4797177900FFA4A9124BA4DC7F7032AD89A126DD'
 $validExitCodes = @(0)
 $ahkExe         = 'AutoHotKey'
 $ahkFile        = Join-Path $toolsDir "GPMMinstall.ahk"
