eaa780cf713ce0d9d75b65de69f481cdefb103d1
bcurran3 <bcurran3@yahoo.com>
2018-04-07 00:23:31 -0700

    Add files via upload

--- hp-universal-print-driver-ps/tools/chocolateyinstall.ps1
+++ hp-universal-print-driver-ps/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName    = 'hp-universal-print-driver-ps' 
-$url            = 'http://ftp.hp.com/pub/softlib/software13/COL40842/ds-99375-14/upd-ps-x32-6.5.0.22695.exe'
-$checksum       = '782DA57EAC8465F3B120FBFCBE0705F34C43054C9F596675866EE2EF09DE92EB'
-$url64          = 'http://ftp.hp.com/pub/softlib/software13/COL40842/ds-99376-14/upd-ps-x64-6.5.0.22695.exe'
-$checksum64     = 'B0F5F82F9E1074ECC041DEDC75607223C68C740772BE800313A6837F75B66326'
+$url            = 'http://ftp.hp.com/pub/softlib/software13/COL40842/ds-99375-19/upd-ps-x32-6.6.0.23029.exe'
+$checksum       = '435A1DB19A0D89A118DC0F64D353674B28692A9FC5CAF46600DE411C52E90AF2'
+$url64          = 'http://ftp.hp.com/pub/softlib/software13/COL40842/ds-99376-19/upd-ps-x64-6.6.0.23029.exe'
+$checksum64     = 'C3D116088D2C99FFC78CAA862D62861571A26E45AE37CAB8956B75CD066F65B0'
 $installerType  = 'ZIP'
 $silentArgs     = '/dm /nd /npf /q /h'
 $validExitCodes = @(0, 3010, 1641)
