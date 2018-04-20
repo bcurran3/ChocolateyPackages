8e5e3295f6eee374ec2b1f0271da654e378fcaa4
bcurran3 <bcurran3@yahoo.com>
2018-04-19 22:46:43 -0700

    Add files via upload

--- intel-dsa/tools/chocolateyinstall.ps1
+++ intel-dsa/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = 'intel-dsa'
 $installerType  = 'exe'
 $url            = 'https://downloadmirror.intel.com/24345/a08/Intel%20Driver%20and%20Support%20Assistant%20Installer.exe'
-$checksum       = '879DA6C6E8A7CA7892BA1B615EA6255CF41156DCD5EAF95C5AB9F0EFE522250C'
+$checksum       = '25D4FD8EAE3D67A1D6B1A7E7841D3B2D84E2FBFEA2329CA5C6A7B22355F77A6E'
 $toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $silentArgs     = '/S'
 $validExitCodes = @(0)
