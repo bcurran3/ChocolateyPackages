91c8e637b3dd2c3295617f4d83e4e5a8d461a85b
bcurran3 <bcurran3@yahoo.com>
2018-04-20 22:04:28 -0700

    Add files via upload

--- dell-update/tools/chocolateyinstall.ps1
+++ dell-update/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
-﻿$packageName= 'dell-update' 
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://downloads.dell.com/FOLDER04792274M/1/Dell-Update-Application_P0KKN_WIN_1.11.1.0_A00.EXE'
-$checksum   = '209919261607280C89C2F8B2970076C54C319E80379A3A096749355FFAA84454'
+﻿$packageName = 'dell-update' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'https://downloads.dell.com/FOLDER04915494M/1/Dell-Update-Application_V3DT0_WIN_2.1.3.0_A00.EXE'
+$checksum    = 'FAD20C76782650917917E2F950C5E3ECCABAF3E86779559FF05FF4785111A1BC'
 
 $packageArgs = @{
   packageName   = $packageName
---
+++ intel-graphics-driver/intel-graphics-driver_15.65.5.4982/tools/IGDInstall.ahk
@@ -0,0 +1,24 @@
+#NoEnv 
+SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+
+WinWaitActive, , This setup program will install the following components , 1200
+WinActivate
+Send {space}
+Send !n
+WinWaitActive, , License Agreement , 90
+Send !y
+Send !n
+Sleep, 30000
+WinWaitActive, , Setup Progress , 120
+Send !n
+WinWaitActive, , Setup Is Complete , 90
+Send {down}
+Send !f
+If WinExist("Error")
+  {
+   Send {space}
+  }
+
+
+
