911a757b6a6affc3ed7f513753e9f12bf34bc530
bcurran3 <bcurran3@yahoo.com>
2018-04-21 23:24:27 -0700

    Add files via upload

--- choco-upgrade-all-at/tools/chocolateyinstall.ps1
+++ choco-upgrade-all-at/tools/chocolateyinstall.ps1
@@ -1,25 +1,61 @@
 $ErrorActionPreference = 'Stop'
 $packageName = 'choco-upgrade-all-at'
 $pp          = Get-PackageParameters
-$GotTask     = (get-scheduledtask -TaskName "choco upgrade all at" -ErrorAction SilentlyContinue) 
 
+# delete old task name if it exists from previous versions
+$GotTask = (get-scheduledtask -TaskName "choco upgrade all at" -ErrorAction SilentlyContinue)
 if ($GotTask -ne $null){
-   Write-Host
-   Write-Host Existing "'choco upgrade all at'" scheduled task found. Keeping existing scheduled task. -foreground magenta 
-   Write-Host Upgrading choco-upgrade-all-at Chocolatey package files only. -foreground magenta 
-   exit
+    SchTasks /DELETE /TN "choco upgrade all at" /F
    }
 
+$GotTask = (get-scheduledtask -TaskName choco-upgrade-all-at -ErrorAction SilentlyContinue)
+if ($GotTask -ne $null){
+   Write-Host
+   Write-Host Existing choco-upgrade-all-at scheduled task found. Keeping existing scheduled task. -foreground magenta -background blue
+   Write-Host Upgrading choco-upgrade-all-at Chocolatey package files only. -foreground magenta -background blue
+   Write-Host If you want to change the task runtime, uninstall and reinstall the package. -foreground magenta -background blue
+   exit
+   }
+   
+Write-Host "" 
+Write-Host "choco-upgrade-all-at Summary:" -foreground magenta
+
 if ($pp["Time"] -eq $null -or $pp["Time"] -eq ''){
-     $RunTime = "00:00"
-     Write-Host No time specified, defaulting to MIDNIGHT. -foreground magenta 
+     Write-Host " * TIME NOT specified, defaulting to 02:00." -foreground magenta
+	 $RunTime = "02:00"
      } else {
-       $RunTime = $pp["Time"]
-	   }
+	   $RunTime = $pp["Time"]
+	   Write-Host " * TIME specified as $RunTime." -foreground magenta
+      } 
 
-SchTasks /Create /SC DAILY /RU SYSTEM /RL HIGHEST /TN "choco upgrade all at" /TR "choco upgrade all -y" /ST $RunTime /F
-Write-Host Now configured to run choco upgrade all at $RunTime every day. -foreground magenta 
-
-# Reference for possible future use:
-# https://technet.microsoft.com/en-us/library/jj649816(v=wps.630).aspx
-
+if (($pp["DAILY"] -eq $null -or $pp["DAILY"] -eq '') -and ($pp["WEEKLY"] -eq $null -or $pp["WEEKLY"] -eq '')){
+     Write-Host " * Defaulting to DAILY running of ""choco upgrade all -y"" at $RunTime." -foreground magenta -background blue
+     SchTasks /CREATE /SC DAILY /RU SYSTEM /RL HIGHEST /TN choco-upgrade-all-at /TR "choco upgrade all -y" /ST $RunTime /F
+	 SchTasks /query /tn "choco-upgrade-all-at"
+	 exit
+    }
+		  
+if ($pp["DAILY"] -eq $null -or $pp["DAILY"] -eq ''){
+       Write-Host " * DAILY NOT specified." -foreground magenta
+     } else {
+	    Write-Host " * DAILY specified." -foreground magenta
+	    SchTasks /CREATE /SC DAILY /RU SYSTEM /RL HIGHEST /TN choco-upgrade-all-at /TR "choco upgrade all -y" /ST $RunTime /F
+		SchTasks /query /tn "choco-upgrade-all-at"
+	    exit
+	   }  
+		  
+if ($pp["WEEKLY"] -eq $null -or $pp["WEEKLY"] -eq ''){
+     Write-Host " * WEEKLY NOT specified." -foreground magenta
+     } else {
+	     Write-Host " * WEEKLY specified." -foreground magenta
+         if ($pp["DAY"] -eq $null -or $pp["DAY"] -eq ''){
+            Write-Host " * DAY NOT specified, defaulting to SUNDAY." -foreground magenta
+            SchTasks /CREATE /SC WEEKLY /D SUN /RU SYSTEM /RL HIGHEST /TN choco-upgrade-all-at /TR "choco upgrade all -y" /ST $RunTime /F
+          } else {
+		    $RunDay = $pp["DAY"]
+            Write-Host " * DAY specified as $RunDay." -foreground magenta
+		    SchTasks /CREATE /SC WEEKLY /D $pp["DAY"] /RU SYSTEM /RL HIGHEST /TN choco-upgrade-all-at /TR "choco upgrade all -y" /ST $RunTime /F
+	      }
+		 SchTasks /query /tn "choco-upgrade-all-at"
+		}
+		
