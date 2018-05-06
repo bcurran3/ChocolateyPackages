c3be0d0d209d8492ea9fd210b9c6a49a32112903
bcurran3 <bcurran3@yahoo.com>
2018-05-05 23:48:58 -0700

    Add files via upload

---
+++ 8x8virtualoffice/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName    = '8x8virtualoffice'
+$installerType  = 'msi'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://support.8x8.com/us/downloads/get/2109' 
+$checksum       = 'A562F76262986FFF42760BF8F4AA7181A4EF1057D5EDDB3062C6FDE529D0886E'
+$silentArgs     = '/quiet /norestart'
+$validExitCodes = @(0, 3010, 1641)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  validExitCodes= $validExitCodes
+  silentArgs    = $silentArgs
+  softwareName  = ''
+  checksum      = $checksum
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs  
+  
