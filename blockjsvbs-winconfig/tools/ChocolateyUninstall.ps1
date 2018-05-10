9abbec97c8a07d4864f94dfbd402e58bd3eef86a
bcurran3 <bcurran3@yahoo.com>
2018-05-10 03:29:15 -0700

    Add files via upload

---
+++ blockjsvbs-winconfig/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'blockjsvbs-winconfig'
+
+cmd.exe /c assoc .js=JSFile
+cmd.exe /c assoc .jse=JSEFile
+cmd.exe /c assoc .vbs=VBSFile
+cmd.exe /c assoc .vbe=VBEFile
+Write-Host .js, .jse, .vbs, and .vbs script files now re-associated back to default script engines. -foreground magenta
