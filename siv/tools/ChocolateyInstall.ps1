149f12bc5e8db135e05091f35425aa5ea0e08866
bcurran3 <bcurran3@yahoo.com>
2018-05-14 18:53:26 -0700

    Add files via upload

---
+++ siv/tools/ChocolateyInstall.ps1
@@ -0,0 +1,28 @@
+﻿$packageName = 'siv' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\siv.zip"
+$checksum    = '5338B225B736BA0613AF9FA08C85B45C490ABA889B1D4386500C44E925411324'
+$bits        = Get-ProcessorBits
+
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  }
+
+Install-ChocolateyZipPackage @packageArgs
+
+if ($bits -eq 64)
+   {
+    Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\SIV.lnk" -targetPath "$toolsDir\SIV64X.exe" -WorkingDirectory "$toolsDir"
+    Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\SIV.lnk" -targetPath "$toolsDir\SIV64X.exe"    
+     } else {
+    Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\SIV.lnk" -targetPath "$toolsDir\SIV32X.exe" -WorkingDirectory "$toolsDir"
+    Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\SIV.lnk" -targetPath "$toolsDir\SIV32X.exe"       
+    }
+
+Remove-Item $url | out-null
