a5adcd3bd100899ee3a5af9ce8630760a8aeb157
bcurran3 <bcurran3@yahoo.com>
2018-05-17 03:27:12 -0700

    Add files via upload

---
+++ backupper-standard/tools/ChocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName   = 'backupper-standard'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'exe'
+$url           = 'http://www.aomeisoftware.com/download/adb/BackupperFull.exe'
+$checksum      = '08A26ED007F5BFAE3E1F46802380FA88A4F3C99F4261612871AD6C6F06A2FE32'
+$silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = $installerType
+  url           = $url
+  silentArgs    = $silentArgs
+  softwareName  = 'AOMEI Backupper Standard*' 
+  checksum      = $checksum 
+  checksumType  = 'sha256'  
+  }
+Install-ChocolateyPackage @packageArgs
+
---
+++ gptgen/tools/ChocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'gptgen' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'https://download.sourceforge.net/project/gptfdisk/gptfdisk/1.0.1/gdisk-binaries/gdisk-windows-1.0.1.zip'
+$checksum    = '12CB72C557515DB3471D4A564858D230BE6ADCD24023DE0ED2BF680A05498B27'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
