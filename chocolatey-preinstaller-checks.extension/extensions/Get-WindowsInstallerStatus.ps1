f5d74c01f092c6ea52fe6017560242486256e4ed
bcurran3 <bcurran3@yahoo.com>
2018-05-24 04:49:17 -0700

    Add files via upload

---
+++ chocolatey-preinstaller-checks.extension/extensions/Get-WindowsInstallerStatus.ps1
@@ -0,0 +1,16 @@
+function Get-WindowsInstallerStatus{
+$msiexecInstances = @(Get-Process -ea silentlycontinue msiexec).count
+# From my observations, msiexec sticks around after a .msi is run. Running a .msi causes 2-3 occurrences of msiexec.
+
+if ($msiexecInstances -gt 1)
+   {
+    while ($msiexecInstances -gt 1)
+	{
+     Write-Host "  ** WARNING: Windows Installer IS currently running. Pausing 30 seconds..." -foreground red
+	 Start-Sleep -seconds 30
+     $msiexecInstances = @(Get-Process -ea silentlycontinue msiexec).count
+	}
+   }
+Write-Host "  ** Windows Installer IS NOT currently running" -foreground green
+}
+
---
+++ chocolatey-preinstaller-checks.extension/extensions/Get-chocoStatus.ps1
@@ -0,0 +1,19 @@
+function Get-chocoStatus($AllowedChocos){
+$chocoInstances = @(Get-Process -ea silentlycontinue choco).count
+# running choco got you here and is one instance!
+# install causes one instance
+# uninstall seems to always cause two instances
+
+if ($chocoInstances -gt $AllowedChocos)
+    {
+     while ($chocoInstances -gt $AllowedChocos)
+     {
+      Write-Host "  ** WARNING: Found multiple instances of choco.exe running. Pausing 30 seconds..." -foreground red
+	  Start-Sleep -seconds 30
+      $chocoInstances = @(Get-Process -ea silentlycontinue choco).count
+     }
+	} 
+Write-Host "  ** choco.exe IS NOT running multiple instances." -foreground green
+}
+
+
