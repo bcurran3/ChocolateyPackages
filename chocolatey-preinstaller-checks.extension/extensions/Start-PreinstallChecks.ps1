f78deec74e4055eb1251001a8309cb1f1cad08f4
bcurran3 <bcurran3@yahoo.com>
2018-05-24 04:49:17 -0700

    Add files via upload

---
+++ chocolatey-preinstaller-checks.extension/extensions/Start-PreinstallChecks.ps1
@@ -0,0 +1,14 @@
+function Start-PreInstallChecks{
+
+Write-Host "PRE-INSTALLATION CHECKS:" -foreground magenta
+if (Get-PendingRebootStatus) 
+   {
+     Write-Host "  ** WARNING: Pending reboot found." -foreground red 
+   } else {
+     Write-Host "  ** Pending reboot NOT found." -foreground green
+	}
+Get-WindowsInstallerStatus
+Get-chocoStatus 1
+Remove-Item alias:\Install-ChocolateyInstallPackage
+Install-ChocolateyInstallPackage @args
+}
