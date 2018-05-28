d65deded8de96740d07a5d897cf7a39deb3925f7
bcurran3 <bcurran3@yahoo.com>
2018-05-28 15:32:26 -0700

    Add files via upload

--- chocolatey-preinstaller-checks.extension/extensions/Get-WindowsInstallerStatus.ps1
+++ chocolatey-preinstaller-checks.extension/extensions/Get-WindowsInstallerStatus.ps1
@@ -6,11 +6,11 @@ if ($msiexecInstances -gt 1)
    {
     while ($msiexecInstances -gt 1)
 	{
-     Write-Host "  ** WARNING: Windows Installer IS currently running. Pausing 30 seconds..." -foreground red
+     Write-Host "  * WARNING: Windows Installer IS currently running. Pausing 30 seconds..." -foreground red
 	 Start-Sleep -seconds 30
      $msiexecInstances = @(Get-Process -ea silentlycontinue msiexec).count
 	}
    }
-Write-Host "  ** Windows Installer IS NOT currently running" -foreground green
+Write-Host "  * Windows Installer IS NOT currently running" -foreground green
 }
 
--- chocolatey-preinstaller-checks.extension/extensions/Get-chocoStatus.ps1
+++ chocolatey-preinstaller-checks.extension/extensions/Get-chocoStatus.ps1
@@ -8,12 +8,12 @@ if ($chocoInstances -gt $AllowedChocos)
     {
      while ($chocoInstances -gt $AllowedChocos)
      {
-      Write-Host "  ** WARNING: Found multiple instances of choco.exe running. Pausing 30 seconds..." -foreground red
+      Write-Host "  * WARNING: Found multiple instances of choco.exe running. Pausing 30 seconds..." -foreground red
 	  Start-Sleep -seconds 30
       $chocoInstances = @(Get-Process -ea silentlycontinue choco).count
      }
 	} 
-Write-Host "  ** choco.exe IS NOT running multiple instances." -foreground green
+Write-Host "  * choco.exe IS NOT running multiple instances." -foreground green
 }
 
 
