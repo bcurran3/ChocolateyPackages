e755ce7a56c0d88201b477550f029069d5c7a22d
bcurran3 <bcurran3@yahoo.com>
2018-05-28 15:59:29 -0700

    Delete Get-IsWin81.ps1

--- chocolateyinstall-helpers.extension/extensions/Get-IsWin81.ps1
+++
@@ -1,5 +0,0 @@
-function Get-IsWin81{
-$IsWin8=[Environment]::OSVersion.Version.Major
-$IsWin81=[Environment]::OSVersion.Version.Minor
-if ($IsWin8 -eq "8" -and $IsWin81 -eq "1") {return $true} else {return $false}
-}  
