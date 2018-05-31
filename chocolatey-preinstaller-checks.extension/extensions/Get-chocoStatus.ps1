e24c40c2f4011aaf378a741a6bf049202e86bcea
bcurran3 <bcurran3@yahoo.com>
2018-05-30 23:22:10 -0700

    Add files via upload

--- chocolatey-preinstaller-checks.extension/extensions/Get-chocoStatus.ps1
+++ chocolatey-preinstaller-checks.extension/extensions/Get-chocoStatus.ps1
@@ -1,8 +1,16 @@
 function Get-chocoStatus($AllowedChocos){
 $chocoInstances = @(Get-Process -ea silentlycontinue choco).count
+$CheckLicense = "$env:ChocolateyInstall\license\chocolatey.license.xml"
 # running choco got you here and is one instance!
 # install causes one instance
 # uninstall seems to always cause two instances
+# choco-agent reports as choco.exe per gep13?? Unknown, can't test. 
+
+if((get-process "choco-agent" -ea SilentlyContinue) -eq $Null){
+    } else {
+    Write-Host "  * choco-agent.exe found running, can't effectively check for multiple instances at this time. (Sorry!) " -foreground yellow
+	break
+  }
 
 if ($chocoInstances -gt $AllowedChocos)
     {
@@ -15,5 +23,4 @@ if ($chocoInstances -gt $AllowedChocos)
 	} 
 Write-Host "  * choco.exe IS NOT running multiple instances." -foreground green
 }
-
 
