4e61624711072a2651a017b949904d5c2a6836ba
bcurran3 <bcurran3@yahoo.com>
2018-06-05 02:56:46 -0700

    Add files via upload

---
+++ advanced-codecs/tools/ChocolateyInstall.ps1
@@ -0,0 +1,33 @@
+﻿#http://www.majorgeeks.com/mg/getmirror/win7codecs,1.html
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName    = 'advanced-codecs' 
+$url            = "$toolsDir\ADVANCED_Codecs_v1015.exe"
+$checksum       = '562C9A9E21B23E3ABD71C9C51D4DAAEC3F4EFE6FCBB8691FFBD440F4ECCF93FF'
+$installerType  = 'EXE'
+$silentArgs     = '/S /v/qn'
+$validExitCodes = @(0, 3010, 1641)
+$softwareName   = 'Shark007 ADVANCED Codecs*'
+$ahkExe         = 'AutoHotKey'
+$ahkFile        = Join-Path $toolsDir "ACinstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  validExitCodes= $validExitCodes
+  silentArgs    = $silentArgs
+  softwareName  = $softwareName
+  checksum      = $checksum
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs  
+
+Start-Sleep -s 20
+
+Start-CheckandStop "Settings32"  
+Start-CheckandStop "Settings64"  
+
+Remove-Item $url
