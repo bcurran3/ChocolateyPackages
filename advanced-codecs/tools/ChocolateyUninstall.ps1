82eba3f0e0e96e5458e6d34f18596af89c7c161a
bcurran3 <bcurran3@yahoo.com>
2018-06-05 02:56:46 -0700

    Add files via upload

---
+++ advanced-codecs/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName    = 'advanced-codecs'
+$softwareName   = 'Shark007 ADVANCED Codecs*'
+
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/x {8C0CAA7A-3272-4991-A808-2C7559DE3409} /qn"
+Start-Process -FilePath "$env:programfiles\Shark007\unins000.exe" -ArgumentList "/SILENT"
