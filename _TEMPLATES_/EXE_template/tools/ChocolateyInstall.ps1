5c5433596274bfadcdc8b24a6a43c86b619dbf69
bcurran3 <bcurran3@yahoo.com>
2018-06-05 02:52:50 -0700

    Add files via upload

---
+++ _TEMPLATES_/EXE_template/tools/ChocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$packageName    = ''
+$installerType  = 'exe'
+$url            = ''
+$checksum       = ''
+$silentArgs     = '/S'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$validExitCodes = @(0,1)
+$softwareName   = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir  
+  fileType      = $installerType
+  url           = $url
+  validExitCodes= $validExitCodes
+  silentArgs    = $silentArgs
+  softwareName  = $softwareName
+  checksum      = $checksum
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs
+
