87fa0e1b8ee38020988b906a4ae76d8e412f8a1c
bcurran3 <bcurran3@yahoo.com>
2018-06-05 02:52:50 -0700

    Add files via upload

---
+++ _TEMPLATES_/EXE_portable_template/tools/ChocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = '' 
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = ''
+$checksum     = ''
+$FileFullPath = "$toolsDir\.exe"
+$shortcutName = '.lnk'
+$portableEXE  = '.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  url           = $url
+  FileFullPath  = $FileFullPath
+  softwareName  = ''
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+ 
+Get-ChocolateyWebFile @packageArgs
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath "$toolsDir\$portableEXE" -WorkingDirectory "$toolsDir\"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$toolsDir\$portableEXE" -WorkingDirectory "$toolsDir\"
--- _TEMPLATES_/chrome_extension_template/tools/ChocolateyInstall.ps1
+++ _TEMPLATES_/chrome_extension_template/tools/ChocolateyInstall.ps1
@@ -5,7 +5,7 @@ if ($bits -eq 64)
    {
     if (Test-Path -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID")
        {
-       Write-Host "Extension already installed." -foreground "magenta" –backgroundcolor "blue"
+       Write-Host "Extension already installed." -foreground magenta
        } else {
          New-Item -Force -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" | out-null
          New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID\" -Name "update_url" -Value "https://clients2.google.com/service/update2/crx" | out-null
