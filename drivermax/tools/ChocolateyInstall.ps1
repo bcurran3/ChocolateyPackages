82d9a64f035d9169066f6ca07e68fcc691cb3424
bcurran3 <bcurran3@yahoo.com>
2018-06-05 02:55:52 -0700

    Add files via upload

---
+++ drivermax/tools/ChocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName    = 'drivermax' 
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://www.drivermax.com/soft/dmx/drivermax.exe'
+$checksum       = '5263DF09DBA92F5430CC084FFB87EF71C746FB335F6CC7157774B449A0562EE9'
+$validExitCodes = @(0, 1)
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url
+  validExitCodes= $validExitCodes  
+  silentArgs    = '/SP- /VERYSILENT /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS' 
+  softwareName  = 'DriverMax*' 
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  }
+  
+Install-ChocolateyPackage @packageArgs
+
---
+++ pcmark8/tools/ChocolateyInstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'pcmark8' 
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'http://akamai-dl.futuremark.com.akamaized.net/PCMark8-v2-10-901.zip'
+$checksum       = '5CA0578BCF55671C2F8A1BCEF767A88A687091099354E01901D418D4570547BA'
+$silentArgs     = '/S'
+$validExitCodes = @(0)
+$fileLocation   = "$env:ChocolateyInstall\lib\pcmark8\tools\PCMark8-setup.exe"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs 
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  file          = $fileLocation
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  softwareName  = 'PCMark*'
+}
+ 
+Install-ChocolateyInstallPackage @packageArgs
+
+Start-CheckandStop "FMSISvc"  
