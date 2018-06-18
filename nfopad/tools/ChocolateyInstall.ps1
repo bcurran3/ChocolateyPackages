f81ea9fcb0375fea6cb179034d4ad9b6a2e6eb61
bcurran3 <bcurran3@yahoo.com>
2018-06-18 01:33:54 -0700

    Add files via upload

---
+++ nfopad/tools/ChocolateyInstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'nfopad'
+$url            = 'http://www.truehumandesign.se/download/nfopad174.exe'
+$checksum       = 'CE7E7BC70C5D07269439438309D353FCACA005049566826AE429B3EC59EAD392'
+$installerType  = 'exe'
+$silentArgs     = '/S'
+$validExitCodes = @(0)
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE' 
+  url           = $url
+  softwareName  = 'NFOPad*' 
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+						  
+						  
+						  
+						  
