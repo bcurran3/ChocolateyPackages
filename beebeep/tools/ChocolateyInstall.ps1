a119dc786c2a6c3af4908083ac1a8c4f6885b1cb
bcurran3 <bcurran3@yahoo.com>
2018-07-02 21:20:37 -0700

    Add files via upload

---
+++ beebeep/tools/ChocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName  = 'beebeep' 
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://downloads.sourceforge.net/project/beebeep/Windows/beebeep-5.0.0-windows-x86.zip'
+$checksum     = '4AF823C6A39FFADFDC3C56D4B82E493E5A415603BE4943AEB700B7FA0AD4CB51'
+$ShortcutName = 'beeBEEP'
+$unzipDir     = 'beebeep-5.0.0'
+$exe          = 'beeBEEP.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'zip' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'  
+  }
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$ShortcutName.lnk" -targetPath "$toolsDir\$unzipDir\$exe" -WorkingDirectory "$toolsDir\$unzipDir"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName.lnk" -targetPath "$toolsDir\$unzipDir\$exe" -WorkingDirectory "$toolsDir\$unzipDir"
---
+++ cms-magic-series/tools/ChocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$packageName    = 'cms-magic-series'
+$installerType  = 'MSI'
+$url            = 'http://download.eyemaxdvr.com/XVST%20MAGIC%20SERIES/CMS/CMS-IPT-2.3.7.2048-win32.msi' 
+$checksum       = '1B011F121F3117B230602AD67BB896057EF5D05B57C4D818BF283CC303AAF01A'
+$url64          = 'http://download.eyemaxdvr.com/XVST%20MAGIC%20SERIES/CMS/CMS-IPT-2.3.7.2048-win64.msi' 
+$checksum64     = 'E029C05213FC647434A96B7028351571F58A78F32BCF777458DDEA88A6DEAA1C'
+$silentArgs     = '/quiet /qn /norestart'
+$validExitCodes = @(0, 3010, 1641)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  url64bit      = $url64
+  validExitCodes= $validExitCodes
+  silentArgs    = $silentArgs
+  softwareName  = 'CMS'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+}
+ 
+Install-ChocolateyPackage @packageArgs  
