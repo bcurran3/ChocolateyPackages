72962e6ca613248c953b3dbd34ef3c4fc96614fa
bcurran3 <bcurran3@yahoo.com>
2018-07-05 23:51:00 -0700

    Add files via upload

--- simple-sticky-notes/tools/ChocolateyInstall.ps1
+++ simple-sticky-notes/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = 'simple-sticky-notes'
 $installerType  = 'exe'
 $url            = 'http://www.simplestickynotes.com/setup/Setup_SimpleStickyNotes.exe'
-$checksum       = '86FAA7DEF6A6C7677C5AB36C9446A26593EE5FAC3B32239101751F2A30DAFD79'
+$checksum       = '90ACD3F1568CB332DBB774B56C5C16166AF75C4D6DF42DF9A2FD3A515576845D'
 $silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $validExitCodes = @(0,1)
