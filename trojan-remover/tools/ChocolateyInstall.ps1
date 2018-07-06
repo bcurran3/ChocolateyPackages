bbfc013fd64f67e60b2249f2fe7793a5a914fda3
bcurran3 <bcurran3@yahoo.com>
2018-07-05 23:35:03 -0700

    Add files via upload

---
+++ trojan-remover/tools/ChocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'trojan-remover'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://simplysup.co.uk/download/dl/trjsetup695.exe' 
+$checksum       = '0C19F2AE009BB056C07F05EE5E0F23F6860A8E705A2B75A238C4AD4FC8127678'
+$silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+$validExitCodes = @(0) 
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE' 
+  url           = $url
+  silentArgs    = $silentArgs  
+  validExitCodes= $validExitCodes
+  softwareName  = 'Trojan Remover*'
+  checksum      = $checksum
+  checksumType  = 'sha256'   
+}
+
+Install-ChocolateyPackage @packageArgs
