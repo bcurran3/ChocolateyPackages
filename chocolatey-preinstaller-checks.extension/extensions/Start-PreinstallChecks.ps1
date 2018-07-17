ea76af05fb6ff5b6e8ee03b3448bf313662bd5a5
bcurran3 <bcurran3@yahoo.com>
2018-07-17 04:32:50 -0700

    Add files via upload

--- chocolatey-preinstaller-checks.extension/extensions/Start-PreinstallChecks.ps1
+++ chocolatey-preinstaller-checks.extension/extensions/Start-PreinstallChecks.ps1
@@ -1,16 +1,16 @@
+# chocolatey-preinstaller-checks.extension by Bill Curran AKA BCURRAN3 - 2018 public domain
+# This function is aliased as Install-ChocolateyInstallPackage to intercept and run before Install-ChocolateyInstallPackage
+
 function Start-PreInstallChecks{
 $CheckLicense = "$env:ChocolateyInstall\license\chocolatey.license.xml"
 
 Write-Host "PRE-INSTALLATION CHECKS:" -foreground magenta
-if (Get-PendingRebootStatus) 
-   {
-     Write-Host "  * WARNING: Pending reboot found." -foreground red 
-   } else {
-     Write-Host "  * Pending reboot NOT found." -foreground green
-	}
+Get-PendingRebootStatus
 Get-WindowsInstallerStatus
-Get-chocoStatus 1
-Remove-Item alias:\Install-ChocolateyInstallPackage
+Get-chocoStatus
+
+# Remove alias for normal operations and call Install-ChocolateyInstallPackage actual
+Remove-Item alias:\Install-ChocolateyInstallPackage 
 if ($env:ChocolateyLicenseValid -eq 'true') {
     Set-Alias Install-ChocolateyInstallPackage Install-ChocolateyInstallPackageCmdlet -Force -Scope Global
    }
--- chocolatey-preinstaller-checks.extension/extensions/Start-PreuninstallChecks.ps1
+++ chocolatey-preinstaller-checks.extension/extensions/Start-PreuninstallChecks.ps1
@@ -1,14 +1,13 @@
-function Start-PreuninstallChecks{
+# chocolatey-preinstaller-checks.extension by Bill Curran AKA BCURRAN3 - 2018 public domain
+# This function is aliased as Uninstall-ChocolateyPackage to intercept and run before Uninstall-ChocolateyPackage
 
+function Start-PreuninstallChecks{
 Write-Host "PRE-UNINSTALLATION CHECKS:" -foreground magenta
-if (Get-PendingRebootStatus) 
-   {
-     Write-Host "  * WARNING: Pending reboot found." -foreground red 
-   } else {
-     Write-Host "  * Pending reboot NOT found." -foreground green
-	}
+Get-PendingRebootStatus
 Get-WindowsInstallerStatus
-Get-chocoStatus 2
+Get-chocoStatus
+
+# Remove alias for normal operations and call Uninstall-ChocolateyPackage actual
 Remove-Item alias:\Uninstall-ChocolateyPackage
 Uninstall-ChocolateyPackage @args
 }
