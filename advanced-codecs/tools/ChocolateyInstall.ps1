813c2e64f06538e8f94011a91ac3614021e67326
bcurran3 <bcurran3@yahoo.com>
2018-07-19 04:37:55 -0700

    Add files via upload

--- advanced-codecs/tools/ChocolateyInstall.ps1
+++ advanced-codecs/tools/ChocolateyInstall.ps1
@@ -1,8 +1,9 @@
 ﻿#http://www.majorgeeks.com/mg/getmirror/win7codecs,1.html
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$global:packageMaintainer = 'BCURRAN3'
 $packageName    = 'advanced-codecs' 
-$url            = "$toolsDir\ADVANCED_Codecs_v1032.exe"
-$checksum       = '612A0C57B186AD2DA2762DCD210DB76DA2C6C4B0A03A5B275FAD3CE7B3A15F4F'
+$url            = "$toolsDir\ADVANCED_Codecs_v1034.exe"
+$checksum       = 'A567BBCEBF387C345FB4BF5F3385B13FB0180C4A345FD30486D9880869F8DA54'
 $installerType  = 'EXE'
 $silentArgs     = '/S /v/qn'
 $validExitCodes = @(0, 3010, 1641)
@@ -23,7 +24,9 @@ $packageArgs = @{
   checksumType  = 'sha256' 
 }
 
+Show-Patreon "https://www.patreon.com/bcurran3"
 Install-ChocolateyPackage @packageArgs  
+Show-ToastMessage "$packageName installed." "Version $env:packageVersion."
 
 Start-Sleep -s 10
 Start-CheckandStop "Settings32"  
