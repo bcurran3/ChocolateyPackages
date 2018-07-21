28e0f447a5d59b714fa5ca1d378b7cba64fd63fa
bcurran3 <bcurran3@yahoo.com>
2018-07-21 15:33:49 -0700

    Add files via upload

---
+++ rocketdock/tools/ChocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName    = 'rocketdock'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = "$toolsDir\RocketDock-v1.3.5.exe"
+$checksum       = '43759B0C441FD4F71FE5EEB69F548CD2EB40AC0ABFA02EA3AFC44FBDDF28DC16'
+$silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE' 
+  url           = $url
+  softwareName  = 'RocketDock*' 
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+}
+
+Install-ChocolateyPackage @packageArgs 
