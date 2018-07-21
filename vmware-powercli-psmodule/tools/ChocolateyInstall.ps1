d953bb59e5569a0c81bb9d18c0598efefa81449a
bcurran3 <bcurran3@yahoo.com>
2018-07-20 20:25:41 -0700

    Add files via upload

--- vmware-powercli-psmodule/tools/ChocolateyInstall.ps1
+++ vmware-powercli-psmodule/tools/ChocolateyInstall.ps1
@@ -1,13 +1,26 @@
 $packageName = 'vmware-powercli-psmodule'
+$global:packageMaintainer = 'BCURRAN3'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$shortcutName = 'VMware.PowerCLI.lnk'
+$exe          = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
 
 $PSversion = $PSVersionTable.PSVersion.Major
 if ($PSversion -lt "5")
    {
-    Write-Host "PowerShell < v5 detected." -forground red 
-	Write-Host "If PowerShell v5 was installed as a dependency, you need to reboot before installing this package." -foreground yellow
+    Write-Warning "PowerShell < v5 detected."
+	Write-Warning "If PowerShell v5 was installed as a dependency, you need to reboot before installing this package."
 	throw
    }
 
-Get-PackageProvider -Name NuGet -Force
-Install-Module -Name VMware.PowerCLI -Scope AllUsers -RequiredVersion 10.1.0.8403314 -AllowClobber -Force
-Import-Module VMware.PowerCLI
+Show-Patreon "https://www.patreon.com/bcurran3"
+if (Get-Module -ListAvailable -Name VMware.PowerCLI -ErrorAction SilentlyContinue){
+     Update-Module "VMware.PowerCLI" -RequiredVersion "$env:packageVersion" -Force
+   } else {   
+     Get-PackageProvider -Name NuGet -Force
+     Install-Module -Name VMware.PowerCLI -Scope AllUsers -RequiredVersion $env:packageVersion -AllowClobber -Force
+     Import-Module VMware.PowerCLI
+   }
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath $exe -Arguments '-noe -c "Import-Module VMware.PowerCLI"' -WorkingDirectory "$env:SystemDrive\" -Description 'VMware.PowerCLI' -IconLocation "$toolsDir\powercli.ico" -RunAsAdmin
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath $exe -Arguments '-noe -c "Import-Module VMware.PowerCLI"' -WorkingDirectory "$env:SystemDrive\" -Description 'VMware.PowerCLI' -IconLocation "$toolsDir\powercli.ico" -RunAsAdmin
+Show-ToastMessage "$packageName installed." "Version $env:packageVersion."
