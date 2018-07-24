d012c725ecdfac73b5effa7193bcf3e4106b5c50
bcurran3 <bcurran3@yahoo.com>
2018-07-24 14:54:56 -0700

    Add files via upload

--- font-awesome-font/tools/ChocolateyInstall.ps1
+++ font-awesome-font/tools/ChocolateyInstall.ps1
@@ -3,7 +3,8 @@ $global:packageMaintainer = 'BCURRAN3'
 $url         = 'https://use.fontawesome.com/releases/v5.1.1/fontawesome-free-5.1.1-desktop.zip'
 $checksum    = '87808972A3CD7FA7EAEF7169DEA2ACA40EC4DCBFF599B1D3B2116A02EF69F739'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$destination = Join-Path $toolsDir 'fontawesome-free-5.1.1-desktop'
+$destination = "$toolsDir\fontawesome-free-5.1.1-desktop"
+$fontDir     = "$destination\otfs"
 
 $packageArgs = @{
   packageName   = $packageName
@@ -14,14 +15,10 @@ $packageArgs = @{
   checksumType  = 'sha256'  
   }
   
-Show-Patreon "https://www.patreon.com/bcurran3"  
 Install-ChocolateyZIPPackage @packageArgs
-Show-ToastMessage "$packageName installed." "Version $env:packageVersion."
  
-$shell = New-Object -ComObject Shell.Application
-$fontsFolder = $shell.Namespace(0x14)
+Install-ChocolateyFont "$fontDir\Font Awesome 5 Brands-Regular-400.otf"
+Install-ChocolateyFont "$fontDir\Font Awesome 5 Free-Regular-400.otf"
+Install-ChocolateyFont "$fontDir\Font Awesome 5 Free-Solid-900.otf"
  
-Get-ChildItem $destination -Recurse -Filter *.otf |
-ForEach-Object { $fontsFolder.CopyHere($_.FullName) }
 Remove-Item $destination -Recurse
-
