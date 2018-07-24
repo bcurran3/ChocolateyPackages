2134534e5513acdb6ad9044eb7f83990b885ccd0
bcurran3 <bcurran3@yahoo.com>
2018-07-24 02:17:37 -0700

    Add files via upload

--- chocolatey-font-helpers.extension/extensions/Install-ChocolateyFont.ps1
+++ chocolatey-font-helpers.extension/extensions/Install-ChocolateyFont.ps1
@@ -1,10 +1,11 @@
-# chocolatey-font-helpers.extension v0.0.1-pre (07-23-2018) by Bill Curran - public domain
+# chocolatey-font-helpers.extension v0.0.1 (07-24-2018) by Bill Curran - public domain
 # Install-ChocolateyFont.ps1 - uses Microsoft's Add-Font.ps1 v1.0.1 Copyright (C) 2010 Microsoft Corporation
+# Usage example: Install-ChocolateyFont "FilePath\FontName.ext"
 # https://blogs.technet.microsoft.com/deploymentguys/2010/12/04/adding-and-removing-fonts-with-windows-powershell/
 
-function Install-ChocolateyFont($FontPath)
+function Install-ChocolateyFont($FontPathAndFileName)
 {
  Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
  Set-Location "$env:ChocolateyInstall\extensions\chocolatey-font-helpers\MS"
- .\Add-Font.ps1 -Path "$FontPath"
+ .\Add-Font.ps1 -Path $FontPathAndFileName
 }
--- chocolatey-font-helpers.extension/extensions/Uninstall-ChocolateyFont.ps1
+++ chocolatey-font-helpers.extension/extensions/Uninstall-ChocolateyFont.ps1
@@ -1,10 +1,11 @@
-# chocolatey-font-helpers.extension v0.0.1-pre (07-23-2018) by Bill Curran - public domain
+# chocolatey-font-helpers.extension v0.0.1 (07-24-2018) by Bill Curran - public domain
 # Uninstall-ChocolateyFont.ps1 -uses Microsoft's Remove-Font.ps1 v1.0.1 Copyright (C) 2010 Microsoft Corporation
+# Usage example: Uninstall-ChocolateyFont "FontName.ext"
 # https://blogs.technet.microsoft.com/deploymentguys/2010/12/04/adding-and-removing-fonts-with-windows-powershell/
 
-function Uninstall-ChocolateyFont($FontPath)
+function Uninstall-ChocolateyFont($FontName)
 {
  Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
  Set-Location "$env:ChocolateyInstall\extensions\chocolatey-font-helpers\MS"
- .\Remove-Font.ps1 -Path "$FontPath"
+ .\Remove-Font.ps1 $FontName
 }
