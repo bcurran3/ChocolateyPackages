fedffb1faf4146355a7343b201d2b236722efd10
bcurran3 <bcurran3@yahoo.com>
2018-07-24 14:54:56 -0700

    Add files via upload

---
+++ font-awesome-font/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,3 @@
+Uninstall-ChocolateyFont "Font Awesome 5 Brands-Regular-400.otf"
+Uninstall-ChocolateyFont "Font Awesome 5 Free-Regular-400.otf"
+Uninstall-ChocolateyFont "Font Awesome 5 Free-Solid-900.otf"
