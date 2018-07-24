41ed94126379ab04e6c4f3293bf54692f1ac01f5
bcurran3 <bcurran3@yahoo.com>
2018-07-24 14:53:18 -0700

    Add files via upload

---
+++ airstream-futuropolis-regular-font/tools/ChocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'airstream-futuropolis-regular-font'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+#SHA256 checksum = '7BFBB3C7866D5F1ACD3EACCABF9E1F06CCC3479AF3D4E5A127F4FB03DFBA8C6B'
+
+Install-ChocolateyFont "$toolsDir\Airstream Futuropolis Regular.ttf"
+Remove-Item "$toolsDir\Airstream Futuropolis Regular.ttf"
