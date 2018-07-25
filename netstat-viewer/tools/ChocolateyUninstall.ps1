fd15b4b4f51ab5b1304134e862bf237601d32708
bcurran3 <bcurran3@yahoo.com>
2018-07-25 16:57:08 -0700

    Add files via upload

---
+++ netstat-viewer/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'netstat-viewer'
+
+#Uninstall-ChocolateyZipPackage
+remove-item "$env:Public\Desktop\Netstat Viewer.lnk" -Force -ErrorAction 'SilentlyContinue'
+remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Netstat Viewer.lnk" -Force -ErrorAction 'SilentlyContinue'
