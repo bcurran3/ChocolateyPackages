9a70df2c4d1f48a33923cbb44af780ceb64dc5e1
bcurran3 <bcurran3@yahoo.com>
2018-07-27 12:50:39 -0700

    Add files via upload

---
+++ metax/tools/ChocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$arguments = @{
+    packageName    = $env:ChocolateyPackageName
+    softwareName   = 'MetaX for Windows'
+    url            = 'https://www.danhinsley.com/downloads/MetaXSetup.msi'
+    checksum       = '186AFD435DCB49528943F68EF59E66A74CF040A0BFA23C6FCE480DE721A6DE03'
+    fileType       = 'msi'
+    checksumType   = 'sha256'
+    silentArgs     = '/quiet'
+    validExitCodes = @(0, 1641, 3010)
+}
+
+Install-ChocolateyPackage @arguments
