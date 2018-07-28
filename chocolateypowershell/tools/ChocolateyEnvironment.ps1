0d31bfcdf94a7c0f00d0a0522e66021f5fc8aba8
bcurran3 <bcurran3@yahoo.com>
2018-07-28 06:38:32 -0700

    Add files via upload

---
+++ chocolateypowershell/tools/ChocolateyEnvironment.ps1
@@ -0,0 +1,7 @@
+﻿Set-Location $env:ChocolateyInstall\helpers
+Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+Import-Module $env:ChocolateyInstall\helpers\chocolateyInstaller.psm1
+Set-Location $env:ChocolateyInstall\extensions\chocolatey-core
+Import-Module $env:ChocolateyInstall\extensions\chocolatey-core\chocolatey-core.psm1
+Set-Location $env:SystemDrive\
+function global:prompt {"[ChocolateyPS] $($ExecutionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "}
---
+++ chocolateypowershell/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName  = 'chocolateypowershell' 
+$shortcutName = 'ChocolateyPowerShell.lnk'
+
+remove-item "$env:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
+remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
+
