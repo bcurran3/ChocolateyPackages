e61e9ed626d865aab114462d5c3b1949ad3d39ff
bcurran3 <bcurran3@yahoo.com>
2018-07-28 06:38:32 -0700

    Add files via upload

---
+++ chocolateypowershell/tools/ChocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName  = 'chocolateypowershell' 
+$shortcutName = 'ChocolateyPowerShell.lnk'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$exe          = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
+
+if (!(Test-Path "$env:ChocolateyInstall\choco.ico")){Invoke-WebRequest -Uri "https://cdn.rawgit.com/chocolatey/ChocolateyGUI/develop/Source/ChocolateyGui/chocolateyicon.ico" -OutFile "$env:ChocolateyInstall\choco.ico"}
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath $exe -Arguments "-noexit -c $toolsDir\chocolateyEnvironment.ps1" -WorkingDirectory "$env:SystemDrive\" -IconLocation "$env:ChocolateyInstall\choco.ico" -RunAsAdmin
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath $exe -Arguments "-noexit -c $toolsDir\chocolateyEnvironment.ps1" -WorkingDirectory "$env:SystemDrive\" -IconLocation "$env:ChocolateyInstall\choco.ico" -RunAsAdmin
