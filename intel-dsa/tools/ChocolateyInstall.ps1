bd7c7e2f88a3ee6c1ac862bf0f567906111ef7ae
bcurran3 <bcurran3@yahoo.com>
2018-08-03 15:28:02 -0700

    Add files via upload

--- intel-dsa/tools/ChocolateyInstall.ps1
+++ intel-dsa/tools/ChocolateyInstall.ps1
@@ -1,22 +1,18 @@
-﻿$packageName    = 'intel-dsa'
-$installerType  = 'exe'
+﻿$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$packageName    = 'intel-dsa'
 $url            = 'https://downloadmirror.intel.com/24345/a08/Intel%20Driver%20and%20Support%20Assistant%20Installer.exe'
-$checksum       = 'DF6B51AD6C494E53BCEB7958650FF8ED7DC106324EC6F507381EB4494F0974AD'
-$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$silentArgs     = '-s -norestart'
-$validExitCodes = @(0)
-
-Start-WaitandStop "SurConsent"
+$checksum       = 'BFE7468612C9E2B7CB08B9CA39E269AD376D8DD545B742EAAB31319A8DA73ABA'
 
 $packageArgs = @{
   packageName   = $packageName
-  fileType      = $installerType
+  fileType      = 'exe'
   url           = $url
-  silentArgs    = $silentArgs
-  validExitCodes= $validExitCodes
+  silentArgs    = '-s -norestart'
+  validExitCodes= @(0)
   softwareName  = 'Intel® Driver & Support Assistant'
   checksum      = $checksum
   checksumType  = 'sha256'
 }
 
+Start-WaitandStop "SurConsent"
 Install-ChocolateyPackage @packageArgs
