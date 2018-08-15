42a5f4a38786582befdd4ef684cf99985c41d4a8
bcurran3 <bcurran3@yahoo.com>
2018-08-15 10:00:49 -0700

    Add files via upload

---
+++ go-contact-sync-mod/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'; 
+
+$packageName = 'go-contact-sync-mod'
+$softwareName = 'GO Contact Sync Mod*' 
+$installerType = 'MSI' 
+
+$silentArgs = '/qn /norestart'
+$validExitCodes = @(0, 3010, 1605, 1614, 1641)
+$uninstalled = $false
+try {
+  Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                            'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                            'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                   -ErrorAction:SilentlyContinue `
+  | Where-Object   { $_.DisplayName -like "$softwareName*" } `
+  | ForEach-Object { Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                                                 -FileType "$installerType" `
+                                                 -SilentArgs "$($_.PSChildName) $silentArgs" `
+                                                 -ValidExitCodes $validExitCodes }
+} catch {
+  throw $_.Exception
+}
