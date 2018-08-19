c25c9a8bd674c68cb40de44a186a6477467859c3
bcurran3 <bcurran3@yahoo.com>
2018-08-18 18:49:54 -0700

    Add files via upload

---
+++ uninstall-onedrive-win10-winconfig/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,12 @@
+﻿# This is an anti-pattern package: ChocolateyInstall UNinstalls OneDrive and ChocolateyUninstall installs OneDrive
+$packageName = 'uninstall-onedrive-win10-winconfig'
+$bits        = Get-ProcessorBits
+
+Set-ItemProperty -LiteralPath "HKLM:\Software\Policies\Microsoft\Windows\OneDrive" -Name DisableFileSyncNGSC -Value 0 -ea SilentlyContinue
+
+if ($bits -eq 64)
+   {
+	"$env:SystemRoot\SysWOW64\OneDriveSetup.exe"
+   } else {
+	"$env:SystemRoot\System32\OneDriveSetup.exe"
+   }
