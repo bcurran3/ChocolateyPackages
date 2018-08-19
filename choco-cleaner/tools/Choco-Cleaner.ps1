febd2cfe27895cdbb0c1bf4edf5370379962d20e
bcurran3 <bcurran3@yahoo.com>
2018-08-19 04:08:47 -0700

    Add files via upload

--- choco-cleaner/tools/Choco-Cleaner.ps1
+++ choco-cleaner/tools/Choco-Cleaner.ps1
@@ -1,4 +1,4 @@
-# Choco-Cleaner.ps1 Copyleft 2018 by Bill Curran 
+# Choco-Cleaner.ps1 Copyleft 2017-2018 by Bill Curran 
 # LICENSE: GNU GPL v3 - https://www.gnu.org/licenses/gpl.html
 # Open a GitHub issue at https://github.com/bcurran3/ChocolateyPackages/issues if you have suggestions for improvement.
 $CCver = "v0.0.5 (08/19/2018)"
