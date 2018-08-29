edb73ed7fbeb48d4d1d113bc15978555b3c06a1a
bcurran3 <bcurran3@yahoo.com>
2018-08-29 15:50:17 -0700

    Add files via upload

--- clash-royale/tools/ChocolateyInstall.ps1
+++ clash-royale/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $global:packageMaintainer = 'BCURRAN3'
 $url            = 'https://d1x9snl812q4nd.cloudfront.net/installer/com.supercell.clashroyale/Clash_Royale-gameslol.exe'
-$checksum       = 'BD170AA3C7DB5EE31E219D7C0A7BA5F515A773742E741F6A38BB2FF007ECEFC9'
+$checksum       = '39AFDC41554AB34A64E1EA82956753DCBA103021734BD5DA6327AFAA8370CD04'
 $ahkExe         = 'AutoHotKey'
 $ahkFile        = "$toolsDir\CRinstall.ahk"
 
@@ -17,10 +17,8 @@ $packageArgs = @{
 }
 
 Start-Process $ahkExe $ahkFile
-Show-Patreon "https://www.patreon.com/bcurran3"
 Install-ChocolateyPackage @packageArgs
 Start-sleep 2
-Show-ToastMessage "Clash Royale installed." "Version $env:packageVersion."
 Remove-Item "$env:AppData\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\GamesLOL.lnk" -ea SilentlyContinue
 Remove-Item "$env:USERPROFILE\Desktop\GamesLOL.lnk" -ea SilentlyContinue
 Remove-Item "$env:AppData\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Clash Royale.lnk" -ea SilentlyContinue
--- realtek-hd-audio-driver/tools/ChocolateyInstall.ps1
+++ realtek-hd-audio-driver/tools/ChocolateyInstall.ps1
@@ -2,9 +2,9 @@
 $packageName    = 'realtek-hd-audio-driver' 
 $global:packageMaintainer = 'BCURRAN3'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://az695102.vo.msecnd.net/rtdrivers/pc/audio/0009-64bit_Win7_Win8_Win81_Win10_R282.exe'
+$url            = 'https://az695102.vo.msecnd.net/rtdrivers/pc/audio/0009-32bit_Win7_Win8_Win81_Win10_R282.exe'
 $checksum       = '4DBEB838B2E6650D0B57F0DB6EABE0DD8445A144DC64E3B38E3DF39BE1D41DAD'
-$url64          = 'https://az695102.vo.msecnd.net/rtdrivers/pc/audio/0009-32bit_Win7_Win8_Win81_Win10_R282.exe'
+$url64          = 'https://az695102.vo.msecnd.net/rtdrivers/pc/audio/0009-64bit_Win7_Win8_Win81_Win10_R282.exe'
 $checksum64     = '09784AB2BDFE97EEB085985B0C8A9E9727E882450B7918990B057B62612B967B'
 $silentArgs     = '/S'
 $validExitCodes = @(0)
@@ -46,8 +46,6 @@ $packageArgs = @{
   softwareName  = 'Realtek*'
 }
 
-Show-Patreon "https://www.patreon.com/bcurran3"
 Install-ChocolateyInstallPackage @packageArgs
-Show-ToastMessage "Realtek High Definition Audio System Software installed." "Version $env:packageVersion."
 
 Remove-Item $extractDir -recurse| out-null
