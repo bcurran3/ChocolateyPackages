f9a49d28782b8816a7ca76d0a64a7042a818cb47
bcurran3 <bcurran3@yahoo.com>
2018-09-02 05:42:31 -0700

    Add files via upload

---
+++ chocolatey-toast-notifications.extension/extensions/chocolatey-toast-notifications-extension.psm1
@@ -0,0 +1,20 @@
+# chocolatey-toast-notifications.extension v0.0.1 by Bill Curran AKA BCURRAN3 - 2018 public domain
+
+$scriptRoot = Split-Path -Path $MyInvocation.MyCommand.Definition
+
+if (!(Test-Path "$ChocolateyInstall\extensions\chocolatey-toast-notifications\choco.ico")){
+     [System.Reflection.Assembly]::LoadWithPartialName('System.Drawing')  | Out-Null
+     [System.Drawing.Icon]::ExtractAssociatedIcon("$env:ChocolateyInstall\choco.exe").ToBitmap().Save("$env:ChocolateyInstall\extensions\chocolatey-toast-notifications\choco.ico")
+}
+
+$publicFunctions = @(
+    'Get-ChocolateySoftwareName',
+    'Install-ChocolateyInstallPackageWithToastNotification',
+	'Uninstall-ChocolateyPackageWithToastNotification'
+)
+
+Get-ChildItem -Path "$scriptRoot\*.ps1" | ForEach-Object { . $_ }
+Export-ModuleMember -Function $publicFunctions
+
+Set-Alias Install-ChocolateyInstallPackage Install-ChocolateyInstallPackageWithToastNotification -Force -Scope Global
+Set-Alias Uninstall-ChocolateyPackage Uninstall-ChocolateyPackageWithToastNotification -Force -Scope Global
