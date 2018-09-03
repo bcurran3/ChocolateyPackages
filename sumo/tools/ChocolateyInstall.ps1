04bbbcc915356c283b0c796d52fe1cbc28d8e03b
bcurran3 <bcurran3@yahoo.com>
2018-09-02 21:45:50 -0700

    Add files via upload

---
+++ sumo/tools/ChocolateyInstall.ps1
@@ -0,0 +1,20 @@
+$ErrorActionPreference = 'Stop';
+$packageName = 'sumo'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ahkExe      = 'AutoHotKey'
+$ahkFile     = "$toolsDir\SUMo_Install.ahk"
+$url         = 'http://www.kcsoftwares.com/files/sumo_lite.exe'
+$checksum    = '73AF40BC6632FA0AD478EAAD0B3EAC3E22855BB93BC0B1DB6F235C1B4B711EA8'
+
+$packageArgs = @{
+    packageName    = $packageName
+    url            = $url
+    checkum        = $checksum
+    checksumType   = 'sha256'
+    fileType       = 'EXE'
+    silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+    validExitCodes = @(0)   
+}
+
+Start-Process $ahkExe $ahkFile
+Install-ChocolateyPackage @packageArgs
