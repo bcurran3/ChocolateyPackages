38cfbfcdce9e2566643a009049e1fab9dcf777f9
bcurran3 <bcurran3@yahoo.com>
2018-09-04 11:52:13 -0700

    Add files via upload

---
+++ tux-of-math-command/tools/ChocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'tux-of-math-command'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = "$toolsDir\tuxmath-2.0.2-win32-installer.exe"
+$checksum       = '155173F1CB3258D971E8969B0C3E55EEBDB8F093D93FC342CF8DD8223AC50DA5'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  file           = $url
+  checksum       = $checksum
+  silentArgs     = '/S'
+  validExitCodes = @(0,1)
+  softwareName   = ''
+}
+ 
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $url | out-null
