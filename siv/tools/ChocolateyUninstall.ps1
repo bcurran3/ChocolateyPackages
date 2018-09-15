cdc5c1b0bb17d1c239203adb57fed2b9adc9ee69
bcurran3 <bcurran3@yahoo.com>
2018-09-15 00:24:55 -0700

    Add files via upload

--- siv/tools/ChocolateyUninstall.ps1
+++ siv/tools/ChocolateyUninstall.ps1
@@ -1,5 +1,4 @@
 ﻿$packageName = 'siv' 
 
-remove-item "$env:Public\Desktop\SIV.lnk" -Force -ErrorAction 'SilentlyContinue'
-remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\SIV.lnk" -Force -ErrorAction 'SilentlyContinue'
-
+remove-item "$env:Public\Desktop\SIV.lnk" -Force -ErrorAction SilentlyContinue
+remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\SIV.lnk" -Force -ErrorAction SilentlyContinue
