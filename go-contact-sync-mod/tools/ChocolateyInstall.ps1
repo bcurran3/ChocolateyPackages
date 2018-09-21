fbd9e2c62953232d358b80c878de3e02e6ddb7c4
bcurran3 <bcurran3@yahoo.com>
2018-09-21 03:26:46 -0700

    Add files via upload

--- go-contact-sync-mod/tools/ChocolateyInstall.ps1
+++ go-contact-sync-mod/tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'go-contact-sync-mod'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://downloads.sourceforge.net/project/googlesyncmod/Releases/3.10.16/SetupGCSM-3.10.16.msi' 
-$checksum    = '3538EE3D78AC0D3ECD0C229F9C6B687A4D0F18D9B38269761D0C7F6EAD7ECF21'
+$url         = 'https://downloads.sourceforge.net/project/googlesyncmod/Releases/3.10.17/SetupGCSM-3.10.17.msi' 
+$checksum    = 'E6C5E52D82C182A589F823CF9A3F0B326CBC8FC0E1AAAD2E2F83F965C8B0F9F5'
 
 
 $packageArgs = @{
