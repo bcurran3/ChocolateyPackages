fe5d392886bfd0e3dc22276ebea91eabb735dbaa
bcurran3 <bcurran3@yahoo.com>
2018-09-21 04:13:46 -0700

    Add files via upload

--- chocolatey-misc-helpers.extension/extensions/Show-ToastMessage.ps1
+++ chocolatey-misc-helpers.extension/extensions/Show-ToastMessage.ps1
@@ -6,9 +6,13 @@
 # Not guaranteed to work on Windows versions other than Windows 10 and Server 2016. 
 # Should do nothing on other versions; i.e. tested on Windows Server 2012 R2 - no messages, no errors.
 
-# Below shows toast messages if the Chocolatey user has installed the PowerShell module BurntToast. 
-# By installing BurntToast, Chocolatey users show a preference towards wanting toast messages and thus have opted-in for use of them.
 function Show-ToastMessage($MessageLine1, $MessageLine2){
+# 08-31-2018
+# This function has been disallowed by the Chocolatey team as "pop ups" need to be 100% opt-in.
+# Its existence is for backward compatibility, actual function NULLIFIED.
+# I argued that by having BurntToast installed, users WANT the toast messages. The Chocolatey Team still disagreed. :(
+# This might return in a different form in the future.
+return
 
 if (!(Test-Path "$ChocolateyInstall\extensions\chocolatey-misc-helpers\choco.ico")){
      [System.Reflection.Assembly]::LoadWithPartialName('System.Drawing')  | Out-Null
@@ -16,35 +20,29 @@ if (!(Test-Path "$ChocolateyInstall\extensions\chocolatey-misc-helpers\choco.ico
 }
 
 if (Get-Module -ListAvailable -Name BurntToast -ErrorAction SilentlyContinue){
-     New-BurntToastNotification -Text "Chocolatey ($env:packageName)", "$MessageLine1", "$MessageLine2" -AppLogo "$env:ChocolateyInstall\extensions\chocolatey-misc-helpers\choco.ico"
+     New-BurntToastNotification -Text "Chocolatey ($env:packageName)", "$MessageLine1", "$MessageLine2" -AppLogo "$env:ChocolateyInstall\extensions\chocolatey-misc-helpers\choco.ico" -ErrorAction SilentlyContinue
    } else {
-     return
-	}
-# 08-30-2018
-# The remainder of this function has been disallowed by the Chocolatey team as "pop ups" need to be 100% opt-in. :(
-# Its purpose was to show toast messages to Windows10/Server2016 users who don't have BurntToast installed.
-
-#     $app = '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
-#     [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
-#     $Template = [Windows.UI.Notifications.ToastTemplateType]::ToastImageAndText01
-#     #Gets the Template XML so we can manipulate the values
-#     [xml]$ToastTemplate = ([Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent($Template).GetXml())
-#     [xml]$ToastTemplate = @"
-#<toast launch="app-defined-string">
-#  <visual>
-#    <binding template="ToastGeneric">
-#      <text>Chocolatey ($env:packageName)</text>	
-#      <text>$MessageLine1</text>
-#      <text>$MessageLine2</text>
-#	  <branding>logo</branding>
-#	  <image placement="appLogoOverride" src="$env:ChocolateyInstall\extensions\chocolatey-misc-helpers\choco.ico"/>
-#    </binding>
-#  </visual>
-#</toast>
-#"@
-#     $ToastXml = New-Object -TypeName Windows.Data.Xml.Dom.XmlDocument
-#     $ToastXml.LoadXml($ToastTemplate.OuterXml)
-#     $notify = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($app) 
-#     $notify.Show($ToastXml)
-#    }
+     $app = '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
+     [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+     $Template = [Windows.UI.Notifications.ToastTemplateType]::ToastImageAndText01
+     #Gets the Template XML so we can manipulate the values
+     [xml]$ToastTemplate = ([Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent($Template).GetXml())
+     [xml]$ToastTemplate = @"
+<toast launch="app-defined-string">
+  <visual>
+    <binding template="ToastGeneric">
+      <text>Chocolatey ($env:packageName)</text>	
+      <text>$MessageLine1</text>
+      <text>$MessageLine2</text>
+	  <branding>logo</branding>
+	  <image placement="appLogoOverride" src="$env:ChocolateyInstall\extensions\chocolatey-misc-helpers\choco.ico"/>
+    </binding>
+  </visual>
+</toast>
+"@
+     $ToastXml = New-Object -TypeName Windows.Data.Xml.Dom.XmlDocument
+     $ToastXml.LoadXml($ToastTemplate.OuterXml)
+     $notify = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($app) 
+     $notify.Show($ToastXml)
+    }
 }
