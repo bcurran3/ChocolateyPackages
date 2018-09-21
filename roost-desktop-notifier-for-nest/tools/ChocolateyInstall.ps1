d6e0a65452014fc1edaf908a615487a301ff3b0f
bcurran3 <bcurran3@yahoo.com>
2018-09-21 03:27:39 -0700

    Add files via upload

---
+++ roost-desktop-notifier-for-nest/tools/ChocolateyInstall.ps1
@@ -0,0 +1,32 @@
+﻿$packageName = 'roost-desktop-notifier-for-nest' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'http://nest.richardeverett.com/notifierfornest/setup.exe' 
+$checksum    = '0C10F511A089DA138C58A42F1EB7B51BC119680D7F9DE893C3DBEFCAFCBA2BA4'
+$ahkExe      = 'AutoHotKey'
+
+Start-CheckandStop "NotifierForNest"  
+
+if (Get-IsWin10){
+    $ahkFile = "$toolsDir\RoostInstallWin10.ahk"
+   } else {
+     $ahkFile = "$toolsDir\RoostInstallpreWin10.ahk"
+    }
+ 
+Start-Process $ahkExe $ahkFile
+ 
+ $packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  silentArgs     = ''
+  validExitCodes = @(0)  
+  softwareName   = 'Roost Desktop Notifier*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
+Start-Sleep -s 30
+if ($ProcessWasRunning -eq "True") {&"$ProcessFullPath"}
+# Note related to uninstall:
+# Remove-Item C:\Users\$env:username\AppData\Local\Apps\2.0\* -recurse
--- roost-desktop-notifier-for-nest/tools/RoostInstallWin10.ahk
+++ roost-desktop-notifier-for-nest/tools/RoostInstallWin10.ahk
@@ -1,12 +1,8 @@
 #NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
-#NoTrayIcon
 SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
 SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
 
 ;WINDOWS 10 
-WinWaitActive, Application Install - Security Warning, Do you want to install this application? , 300
-WinActivate
-Send !i
-
-
-
+WinWaitActive, Application Install - Security Warning, &Install , 300
+sleep 5000
+Send !I
