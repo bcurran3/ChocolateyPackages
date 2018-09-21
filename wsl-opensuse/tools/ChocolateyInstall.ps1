a013c92fd3c8ef7657bfd94c6b4d16966054baf4
bcurran3 <bcurran3@yahoo.com>
2018-09-21 03:26:46 -0700

    Add files via upload

--- wsl-opensuse/tools/ChocolateyInstall.ps1
+++ wsl-opensuse/tools/ChocolateyInstall.ps1
@@ -1,11 +1,12 @@
-﻿$ErrorActionPreference = 'Stop'
+﻿# openSUSE-42 doesn't support being silently installed. This script kludges it.
+$ErrorActionPreference = 'Stop'
 $packageName    = 'wsl-opensuse'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://aka.ms/wsl-opensuse-42'
 $checksum       = '7E8D474A53631C4843C7F209E3530B777A40F6DA12B9C91AEA418071D76DCA02'
 $unzipLocation  = "$toolsDir\unzipped"
 
-New-Item $unzipLocation -type directory | out-null
+New-Item $unzipLocation -type directory -ErrorAction SilentlyContinue | out-null
 
 $packageArgs = @{
   packageName    = $packageName
@@ -18,6 +19,22 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 
+# open openSUSE-42 installer in a new window
 Set-Location -Path $unzipLocation
-.\openSUSE-42.exe run exit
-wslconfig /list
+Start-Process openSUSE-42.exe
+
+Write-Host "  ** When you see ""Installation successful!"" in the other window, you may close it." -foreground magenta
+Write-Host "  ** Otherwise, just wait up to 25 seconds and it will close automatically." -foreground magenta
+
+# wait for openSUSE-42 installer to finish extracting files
+while (!(Test-Path $unzipLocation\rootfs\var\tmp)){
+       Start-Sleep -Seconds 15
+      }
+
+# wait 10 more seconds to make sure the files IN rootfs\var\tmp get extracted
+Start-Sleep -Seconds 10
+
+# terminate openSUSE-42.EXE ~when it hits the "Enter new UNIX username:" prompt (meaning installation has finished)
+Start-CheckandStop "openSUSE-42"
+
+Write-Host "  ** root is the default user upon running WSL/openSUSE-42." -foreground magenta
