9becace6eef29405ae72a974fbc41bf443651927
buenyamin <buenyamin.olgun@web.de>
2018-09-22 22:31:46 +0200

    Update dell-update to version 3.0

--- dell-update/tools/chocolateyinstall.ps1
+++ dell-update/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'dell-update' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://downloads.dell.com/FOLDER04915494M/1/Dell-Update-Application_V3DT0_WIN_2.1.3.0_A00.EXE'
-$checksum    = 'FAD20C76782650917917E2F950C5E3ECCABAF3E86779559FF05FF4785111A1BC'
+$url         = 'https://downloads.dell.com/FOLDER04998700M/1/Dell-Update-Application_JD7FD_WIN_3.0.0_A00.EXE'
+$checksum    = '5EC3E784C507821197A2F23077132FCA57425F3CFCD79DE0AE1689DEF2B43E32'
 
 $packageArgs = @{
   packageName   = $packageName
