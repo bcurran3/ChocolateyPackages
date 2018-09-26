fe4df88fcc6111621ca439bd1fa3ca0f7bf112a4
bcurran3 <bcurran3@yahoo.com>
2018-09-26 04:34:55 -0700

    WSL package updates

--- wsl-debiangnulinux/tools/ChocolateyInstall.ps1
+++ wsl-debiangnulinux/tools/ChocolateyInstall.ps1
@@ -4,6 +4,7 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://aka.ms/wsl-debian-gnulinux'
 $checksum       = '02CC1CE48E2FC21331B6FAA2AF44F22FBB9E9C219EAF74202970164BE6E3D3D8'
 $unzipLocation  = "$toolsDir\unzipped"
+$exe            = 'debian.exe'
 
 New-Item $unzipLocation -type directory | out-null
 
@@ -18,6 +19,14 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 
-Set-Location -Path $unzipLocation
-debian.exe install
+$packageArgs = @{
+  packageName    = $packageName
+  softwareName   = ''
+  fileType       = 'EXE'
+  silentArgs     = 'install --root'
+  file           = "$unzipLocation\$exe"
+  validExitCodes = @(0)
+  }
+
+Install-ChocolateyInstallPackage @packageArgs
 wslconfig /list
