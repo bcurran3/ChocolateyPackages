ed7168d6b134bc0548a809270135d41bbbcf0dae
bcurran3 <bcurran3@yahoo.com>
2018-09-28 04:55:32 -0700

    LMS and various updates

---
+++ logitech-media-server/tools/ChocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$packageName= 'logitech-media-server' 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://downloads.slimdevices.com/LogitechMediaServer_v7.7.6/LogitechMediaServer-7.7.6.exe' 
+$checksum   = 'E0F23BDFF0FF256F61C77F494F813244472E0EF0DDA2278B26B579EF93E6DA37'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE' 
+  url           = $url
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+  validExitCodes= @(0) 
+  softwareName  = 'Logitech Media Server*' 
+  checksum      = $checksum
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs
