c9a0b7e2e18b4dda2bd164a0f885699b768808bb
bcurran3 <bcurran3@yahoo.com>
2018-10-01 08:51:11 -0700

    updates

--- intel-dsa/tools/chocolateyinstall.ps1
+++ intel-dsa/tools/chocolateyinstall.ps1
@@ -1,20 +1,17 @@
 ﻿$packageName    = 'intel-dsa'
-$installerType  = 'exe'
 $url            = 'https://downloadmirror.intel.com/24345/a08/Intel%20Driver%20and%20Support%20Assistant%20Installer.exe'
-$checksum       = 'A270362BFB1AC55DE6FC87F33D992F34342ACC2DC6D3D156F7485A6B18A3B487'
-$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$silentArgs     = '-s -norestart'
-$validExitCodes = @(0)
+$checksum       = '0BBD4D96830A1E8CF370FAD934C3851B542C28B2137634267279C8F7CDC21245'
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = $installerType
-  url           = $url
-  silentArgs    = $silentArgs
-  validExitCodes= $validExitCodes
-  softwareName  = 'Intel® Driver & Support Assistant'
-  checksum      = $checksum
-  checksumType  = 'sha256'
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  silentArgs     = '-s -norestart'
+  validExitCodes = @(0)
+  softwareName   = 'Intel® Driver & Support Assistant'
+  checksum       = $checksum
+  checksumType   = 'sha256'
 }
 
+Start-WaitandStop "SurConsent"
 Install-ChocolateyPackage @packageArgs
---
+++ tageditor/tools/ChocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'tageditor' 
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$bits         = Get-ProcessorBits
+$shortcutName = 'Tag Editor.lnk'
+
+if ($bits -eq 64)
+   {
+    $exeTarget = "$toolsDir\i686-w64-mingw32\bin\tageditor.exe"
+	Remove-Item "$toolsDir\x86_64-w64-mingw32\bin\tageditor.exe" #So shim doesn't get created for both
+   } else {
+    $exeTarget = "$toolsDir\x86_64-w64-mingw32\bin\tageditor.exe"
+	Remove-Item "$toolsDir\i686-w64-mingw32\bin\tageditor.exe" #So shim doesn't get created for both
+   }
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath "$exeTarget" -WorkingDirectory "$toolsDir\"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$exeTarget" -WorkingDirectory "$toolsDir\"   
