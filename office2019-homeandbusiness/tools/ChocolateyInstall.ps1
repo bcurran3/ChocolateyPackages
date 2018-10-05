9ffe033425a8ec14366103e768a70c5bb21b64df
bcurran3 <bcurran3@yahoo.com>
2018-10-04 23:05:00 -0700

    updates

--- office2019-homeandbusiness/tools/ChocolateyInstall.ps1
+++ office2019-homeandbusiness/tools/ChocolateyInstall.ps1
@@ -2,8 +2,14 @@
 $packageName  = 'office2019-homeandbusiness' 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://officecdn.microsoft.com/db/492350F6-3A01-4F97-B9C0-C7C6DDF67D60/media/en-US/HomeBusiness2019Retail.img'
-$checksum     = ''
+$checksum     = '803E93581E991102ADFDDCB4E5CAC3B60CE9E4A28ED501F11F86CB19EB003A8D'
 $FileFullpath = "$ToolsDir\HomeBusiness2019Retail.img"
+
+# Will most likely need to update for Windows Server 2019 soon
+if (!(Get-IsWin10) -or (Get-IsWinServer2016)){
+   Write-Warning " ** Office 2019 only supports Windows 10 and Windows Server 2016"
+   throw
+  }
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,3 +21,22 @@ $packageArgs = @{
 }
 
 Get-ChocolateyWebFile @packageArgs
+
+$iso = Get-Item $FileFullPath
+Mount-DiskImage -ImagePath $iso
+$isodriveLetter = (Get-DiskImage $iso | Get-Volume).DriveLetter
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  file          = "${isodriveLetter}:\Setup.exe"
+  silentArgs    = ''
+  validExitCodes= @(0)
+  softwareName  = 'Microsoft Office 2019*'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+# I still need to check for the installation to finish (AHK) and wait before running the following:
+#Dismount-DiskImage -ImagePath $iso
+#Remote-Item $FileFullPath -Force | Out-Null
--- office2019-pro/tools/ChocolateyInstall.ps1
+++ office2019-pro/tools/ChocolateyInstall.ps1
@@ -2,8 +2,14 @@
 $packageName  = 'office2019-pro' 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'https://officecdn.microsoft.com/db/492350F6-3A01-4F97-B9C0-C7C6DDF67D60/media/en-US/Professional2019Retail.img'
-$checksum     = ''
+$checksum     = '57BE67FFC089F54846F12404731778906248DF6486BD1901F5115C4FC8A626D9'
 $FileFullpath = "$ToolsDir\Professional2019Retail.img"
+
+# Will most likely need to update for Windows Server 2019 soon
+if (!(Get-IsWin10) -or (Get-IsWinServer2016)){
+   Write-Warning " ** Office 2019 only supports Windows 10 and Windows Server 2016"
+   throw
+  }
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,3 +21,22 @@ $packageArgs = @{
 }
 
 Get-ChocolateyWebFile @packageArgs
+
+$iso = Get-Item $FileFullPath
+Mount-DiskImage -ImagePath $iso
+$isodriveLetter = (Get-DiskImage $iso | Get-Volume).DriveLetter
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  file          = "${isodriveLetter}:\Setup.exe"
+  silentArgs    = ''
+  validExitCodes= @(0)
+  softwareName  = 'Microsoft Office 2019*'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+# I still need to check for the installation to finish (AHK) and wait before running the following:
+#Dismount-DiskImage -ImagePath $iso
+#Remote-Item $FileFullPath -Force | Out-Null
