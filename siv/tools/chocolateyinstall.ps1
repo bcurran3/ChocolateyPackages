1efb09903b7f6525b12042fbc8058ba78d11fe41
bcurran3 <bcurran3@yahoo.com>
2018-10-15 01:23:49 -0700

    updates

--- siv/tools/chocolateyinstall.ps1
+++ siv/tools/chocolateyinstall.ps1
@@ -1,9 +1,8 @@
 ﻿$packageName = 'siv' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url         = "$toolsDir\siv.zip"
-$checksum    = '5D14397D3DE348996C0AAB7B7A3E96C3DA0369D2BA306DAA345244E582E0F283'
+$checksum    = '9C9BA4BE3E6C4376BF32E8E0724B31459480983E6C8671F44C022DB13FB89894'
 $bits        = Get-ProcessorBits
-
 
 $packageArgs = @{
   packageName    = $packageName
@@ -13,16 +12,14 @@ $packageArgs = @{
   checksum       = $checksum
   checksumType   = 'sha256'
   }
-
 Install-ChocolateyZipPackage @packageArgs
 
 if ($bits -eq 64)
    {
     Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\SIV.lnk" -targetPath "$toolsDir\SIV64X.exe" -WorkingDirectory "$toolsDir"
     Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\SIV.lnk" -targetPath "$toolsDir\SIV64X.exe"    
-     } else {
+   } else {
     Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\SIV.lnk" -targetPath "$toolsDir\SIV32X.exe" -WorkingDirectory "$toolsDir"
     Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\SIV.lnk" -targetPath "$toolsDir\SIV32X.exe"       
-    }
-
+   }
 Remove-Item $url | out-null
---
+++ skype-disable-updates-winconfig/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'skype-disable-updates-winconfig'
+
+Set-Location -Path $env:SystemDrive\Users
+Get-ChildItem * -Include 'Skype-Setup.exe' -Recurse -Force -ea SilentlyContinue | Remove-Item | Out-Null
+Write-Warning "Skype will now update via normal methods and Chocolatey."
