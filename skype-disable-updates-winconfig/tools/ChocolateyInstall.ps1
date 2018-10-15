31a8dcac5c46889b8ca6be4c2220ca38a4bac00f
bcurran3 <bcurran3@yahoo.com>
2018-10-15 01:23:49 -0700

    updates

---
+++ skype-disable-updates-winconfig/tools/ChocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿# https://answers.microsoft.com/en-us/skype/forum/skype_windesk-skype_startms-skype_installms/how-to-turn-off-skype-automatic-updates-on-pc/cf980eed-d9be-422c-bf0f-3a3871325b26
+$packageName = 'skype-disable-updates-winconfig'
+
+Set-Location -Path $env:SystemDrive\Users
+Get-ChildItem * -Include 'Skype-Setup.exe' -Recurse -Force -ea SilentlyContinue | Remove-Item | Out-Null
+Get-ChildItem * -Include 'Skype for Desktop' -Recurse -Force -ea SilentlyContinue | ForEach {New-Item "$_\Skype-Setup.exe" -ItemType directory} | Out-Null
+Write-Warning "Skype will now only be updated by Chocolatey."
