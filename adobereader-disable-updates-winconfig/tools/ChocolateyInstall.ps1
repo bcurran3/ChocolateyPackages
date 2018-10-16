00ea56298ee3805e560bd3dd02a9fe71cf4fbf5a
bcurran3 <bcurran3@yahoo.com>
2018-10-16 09:58:27 -0700

    updates and new stuff

---
+++ adobereader-disable-updates-winconfig/tools/ChocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'adobereader-disable-updates-winconfig'
+
+SchTasks /Change /Disable /TN "Adobe Acrobat Update Task" | Out-Null
+Stop-Service "AdobeARMservice" -force | Out-Null
+Set-Service "AdobeARMservice" -StartupType Disabled | Out-Null
+
+Write-Host "  ** Adobe Reader and Acrobat Manager (ARM) disabled." -foreground magenta
+Write-Host "  ** Adobe Reader will only be updated by Chocolatey." -foreground magenta
