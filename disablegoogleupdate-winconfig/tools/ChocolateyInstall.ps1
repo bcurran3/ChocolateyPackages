3c1f49908bd33e574e6002bd1ea6f526de116a31
bcurran3 <bcurran3@yahoo.com>
2018-10-16 09:58:27 -0700

    updates and new stuff

---
+++ disablegoogleupdate-winconfig/tools/ChocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'disablegoogleupdate-winconfig'
+$GUTask1 = 'GoogleUpdateTaskMachineCore'
+$GUTask2 = 'GoogleUpdateTaskMachineUA'
+
+SchTasks /Change /Disable /TN "$GUTask1" | Out-Null
+SchTasks /Change /Disable /TN "$GUTask2" | Out-Null
+
+Stop-Service "gupdate" | Out-Null
+Stop-Service "gupdatem" | Out-Null
+Set-Service "gupdate" -StartupType Disabled | Out-Null
+Set-Service "gupdatem" -StartupType Disabled | Out-Null
+
+New-Item "HKLM:\SOFTWARE\Policies\Google" -force -ea SilentlyContinue | Out-Null
+New-Item "HKLM:\SOFTWARE\Policies\Google\update" -force -ea SilentlyContinue | Out-Null
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Google\update" -Name "updatedefault" -Value "0"-force -ea SilentlyContinue | Out-Null
+
+Write-Host "  ** Google Update disabled." -foreground magenta
+Write-Host "  ** You're Google programs will be updated by Chocolatey only." -foreground magenta
+
+# Might have to check and disable scheduled task under user profile
---
+++ geforce-experience-disable-updates-winconfig/tools/ChocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'geforce-experience-disable-updates-winconfig'
+
+SchTasks /Change /Disable /TN "NVIDIA GeForce Experience SelfUpdate_{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8}" | Out-Null
+
+Write-Host "  ** NVIDIA GeForce Experience SelfUpdate disabled." -foreground magenta
+Write-Host "  ** NVIDIA GeForce Experience will only be updated by Chocolatey." -foreground magenta
