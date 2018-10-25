b7dd57fef1fc7f93d0813e30efa2e2627b6dd309
bcurran3 <bcurran3@yahoo.com>
2018-10-24 21:22:03 -0700

    updates

--- 8x8virtualoffice/tools/chocolateyinstall.ps1
+++ 8x8virtualoffice/tools/chocolateyinstall.ps1
@@ -1,17 +1,17 @@
 ﻿$packageName    = '8x8virtualoffice'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://support.8x8.com/@api/deki/files/1947/VOD_5_7_0_20238.msi' 
-$checksum       = 'D1D52EB83AB47F79D11BC189A9980A3D919AC1B68325FF6BF208E0C7413C67C2'
+$url            = 'https://support.8x8.com/@api/deki/files/1947/VOD_5_8_0_20403.msi' 
+$checksum       = 'BF60DB11FB2663279ACFFCAFEFF35E872ED8D0CD7917C7BDE8984316CDA2C8CF'
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'MSI'
-  url           = $url
-  validExitCodes= @(0, 3010, 1641)
-  silentArgs    = '/quiet /norestart'
-  softwareName  = '8x8 - Virtual Office'
-  checksum      = $checksum
-  checksumType  = 'sha256' 
+  packageName    = $packageName
+  fileType       = 'MSI'
+  url            = $url
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/quiet /norestart'
+  softwareName   = '8x8 - Virtual Office'
+  checksum       = $checksum
+  checksumType   = 'sha256' 
 }
 
 Start-CheckandStop "8x8 - Virtual Office"
