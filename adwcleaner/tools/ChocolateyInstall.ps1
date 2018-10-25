fcbe3d4b27ff38517ad4461eeedf804c22657db8
bcurran3 <bcurran3@yahoo.com>
2018-10-25 13:18:55 -0700

    adwcleaner finished package

--- adwcleaner/tools/ChocolateyInstall.ps1
+++ adwcleaner/tools/ChocolateyInstall.ps1
@@ -1,10 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName  = 'adwcleaner' 
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://toolslib.net/downloads/finish/1-adwcleaner/'
-$checksum     = '55B7ACEB5ED0407B1D83CD07C448B65366576315A69D8842B9361B2A14240FC6'
-$shortcutName = 'AdwCleaner.lnk'
-$portableEXE  = ".exe"
+$packageName       = 'adwcleaner' 
+$toolsDir          = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$shortcutName      = 'AdwCleaner.lnk'
+$portableEXE       = "adwcleaner_$env:packageVersion.exe"
+$urlBeforeDownload = 'https://toolslib.net/downloads/finish/1-adwcleaner/1388/';
+$regexDownloadLink = '^https://download.toolslib.net/download/file/';
+$SourcePageBeforeDownload = Invoke-WebRequest -Uri $urlBeforeDownload -UseBasicParsing;
+$htmlLink          = $SourcePageBeforeDownload.Links | Where-Object href -match $regexDownloadLink;
+$url               = $htmlLink.href;
+$checksum          = 'C9CFB0A07961616D5C4779E8A187D2EAB91D4310166C07F5063F1DC13D9FF5BA'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -17,6 +21,7 @@ $packageArgs = @{
 }
  
 Get-ChocolateyWebFile @packageArgs
+Install-BinFile -Name adwcleaner -Path "$toolsDir\$portableEXE"
 
 Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath "$toolsDir\$portableEXE" -WorkingDirectory "$toolsDir\"
 Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$toolsDir\$portableEXE" -WorkingDirectory "$toolsDir\"
