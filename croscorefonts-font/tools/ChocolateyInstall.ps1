ee8315c34614e128d737ba33268b273e555f5076
bcurran3 <bcurran3@yahoo.com>
2018-11-19 13:49:48 -0800

    croscorefonts-font final

--- croscorefonts-font/tools/ChocolateyInstall.ps1
+++ croscorefonts-font/tools/ChocolateyInstall.ps1
@@ -1,5 +1,16 @@
 ﻿$packageName = 'croscorefonts-font'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyFont "$toolsDir\*.ttf"
-Remove-Item "$toolsDir\*.ttf"
+Install-ChocolateyFont "$toolsDir\Arimo-Bold.ttf"
+Install-ChocolateyFont "$toolsDir\Arimo-BoldItalic.ttf"
+Install-ChocolateyFont "$toolsDir\Arimo-Italic.ttf"
+Install-ChocolateyFont "$toolsDir\Arimo-Regular.ttf"
+Install-ChocolateyFont "$toolsDir\Cousine-Bold.ttf"
+Install-ChocolateyFont "$toolsDir\Cousine-BoldItalic.ttf"
+Install-ChocolateyFont "$toolsDir\Cousine-Italic.ttf"
+Install-ChocolateyFont "$toolsDir\Cousine-Regular.ttf"
+Install-ChocolateyFont "$toolsDir\Tinos-Bold.ttf"
+Install-ChocolateyFont "$toolsDir\Tinos-BoldItalic.ttf"
+Install-ChocolateyFont "$toolsDir\Tinos-Italic.ttf"
+Install-ChocolateyFont "$toolsDir\Tinos-Regular.ttf"
+Remove-Item "$toolsDir\*.ttf" | Out-Null
