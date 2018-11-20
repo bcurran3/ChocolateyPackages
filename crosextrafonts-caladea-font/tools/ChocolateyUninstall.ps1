a04a9591c961564a45e1d2013bbc0248f93177a7
bcurran3 <bcurran3@yahoo.com>
2018-11-19 16:31:13 -0800

    Croscore fonts

---
+++ crosextrafonts-caladea-font/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,6 @@
+$packageName = 'crosextrafonts-caladea-font'
+Uninstall-ChocolateyFont "Caladea-Bold.ttf"
+Uninstall-ChocolateyFont "Caladea-BoldItalic.ttf"
+Uninstall-ChocolateyFont "Caladea-Italic.ttf"
+Uninstall-ChocolateyFont "Caladea-Regular.ttf"
+
---
+++ crosextrafonts-carlito-font/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,6 @@
+$packageName = 'crosextrafonts-carlito-font'
+
+Uninstall-ChocolateyFont "Carlito-Bold.ttf"
+Uninstall-ChocolateyFont "Carlito-BoldItalic.ttf"
+Uninstall-ChocolateyFont "Carlito-Italic.ttf"
+Uninstall-ChocolateyFont "Carlito-Regular.ttf"
