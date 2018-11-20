cfb837d701397d89c9be731993ca94083a43ec3c
bcurran3 <bcurran3@yahoo.com>
2018-11-19 16:31:13 -0800

    Croscore fonts

---
+++ crosextrafonts-caladea-font/tools/ChocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'crosextrafonts-caladea-font'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyFont "$toolsDir\Caladea-Bold.ttf"
+Install-ChocolateyFont "$toolsDir\Caladea-BoldItalic.ttf"
+Install-ChocolateyFont "$toolsDir\Caladea-Italic.ttf"
+Install-ChocolateyFont "$toolsDir\Caladea-Regular.ttf"
+Remove-Item "$toolsDir\*.ttf" | Out-Null
---
+++ crosextrafonts-carlito-font/tools/ChocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'crosextrafonts-carlito-font'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyFont "Carlito-Bold.ttf"
+Install-ChocolateyFont "Carlito-BoldItalic.ttf"
+Install-ChocolateyFont "Carlito-Italic.ttf"
+Install-ChocolateyFont "Carlito-Regular.ttf"
+Remove-Item "$toolsDir\*.ttf" | Out-Null
