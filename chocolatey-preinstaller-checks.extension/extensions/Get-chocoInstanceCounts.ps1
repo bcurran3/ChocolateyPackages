cf6dea2a149f6882e8a581d7c956e4064ce3e56f
Michel Buczynski <Michel.Buczynski@tdsecurities.com>
2018-11-22 12:57:03 -0500

    chocolatey-preinstaller-checks.extension add chocolatey-agent support

---
+++ chocolatey-preinstaller-checks.extension/extensions/Get-chocoInstanceCounts.ps1
@@ -0,0 +1,40 @@
+# chocolatey-preinstaller-checks.extension by Bill Curran AKA BCURRAN3 - 2018 public domain
+# modification created by Michel Buczynski AKA MikeBoutch 
+# \ProgramData\chocolatey\bin\choco.exe (in path) is a shim and \ProgramData\chocolatey\choco.exe actual (not in path)
+# SelfServe mode imply a background choco.exe with chocolatey-agent as parent and
+#    a forground choco.exe with a shim as parent
+# Get the current instance choco.exe (so we a count of 1 already)
+# get all instances of choco.exe 
+#    Dont count who has chocholatey-agent.exe has parent since they a foreground pair of shim and choco.exe running
+#    Count only those that have a shim or chocolatey-management-service as parent
+
+function Get-chocoInstanceCounts{
+    
+    $chocoCommands='chocolatey', 'choco', 'cinst', 'clist',  'cpack', 'cpush', 'cup', 'cuninst', 'cver', 'chocolatey-management-service' 
+  
+    Write-verbose "Current Process: $($PID)"
+    if ($(Get-Process -Id (Get-CimInstance -Class Win32_Process -Verbose:$false -Filter "ProcessID=$($PID)").ParentProcessId).ProcessName -eq 'chocolatey-agent'){
+        Write-verbose "   Running in SelfServe (background mode)"
+    }
+  
+    Write-verbose "Searching for choco.exe process"
+    $counts=0
+    Get-Process -ea silentlycontinue -Name choco| % {
+      $_Cim=Get-CimInstance -Class Win32_Process -Verbose:$false -Filter "ProcessID=$($_.Id)"
+      $_Parent=Get-Process -Id $_Cim.ParentProcessId
+      $_ParentCim=Get-CimInstance -Class Win32_Process -Verbose:$false -Filter "ProcessID=$($_Parent.Id)"
+      Write-Verbose "$($_.Id): ``$($_Cim.CommandLine)`` --> $($_Parent.Id): ``$($_ParentCim.CommandLine)``"
+      if ($chocoCommands.contains($_Parent.ProcessName)) {
+        Write-Verbose "$($_.Id) is actual choco process"
+        $counts+=1 
+      } elseif ($_Parent.ProcessName -eq 'chocolatey-agent'){
+        Write-Verbose "$($_.Id) is Chocolatey in background mode"
+        #count only choco.exe running in foreground   
+      } else {
+        Write-Verbose "$($_.Id) is probably a shim of choco.exe"
+      }
+    }
+    Write-Verbose "choco.exe instance count is $($counts)"
+  
+    return $counts
+  }
--- chocolatey-preinstaller-checks.extension/extensions/Get-chocoStatus.ps1
+++ chocolatey-preinstaller-checks.extension/extensions/Get-chocoStatus.ps1
@@ -1,27 +1,45 @@
-# chocolatey-preinstaller-checks.extension by Bill Curran AKA BCURRAN3 - 2018 public domain
-# Supposedly choco-agent reports as choco.exe?? per gep13. Unknown, can't test but doesn't make sense.
+# chocolatey-preinstaller-checks.extension v0.0.2-pre01 by Bill Curran AKA BCURRAN3 - 2018 public domain
+# Get-chocoStatus.ps1 - checks for multiple instances of Chocolatey running and warns or aborts
+# See/Edit Chocolatey-Preinstaller-Checks.xml for options
+# If this extension stops your packages from becoming lost and unmanaged,
+# consider becoming a patron of me at https://www.patreon.com/bcurran3 :)
 
 function Get-chocoStatus{
 
-if((get-process "chocolatey-agent" -ea SilentlyContinue) -eq $Null){
-    } else {
-    Write-Host "  * chocolatey-agent.exe found running, possibly can't check for multiple instances at this time. (Sorry!) " -foreground yellow
-	break
-  }
+# Supposedly choco-agent reports as choco.exe?? per gep13. Unknown, can't test but doesn't make sense.
 
-$chocoInstances = (Get-chocoCounts)
 
-if ($chocoInstances -ge 2)
+# Import preferences
+$xml   = 'Chocolatey-Preinstaller-Checks.xml'  
+[xml]$ConfigFile = Get-Content "$env:ChocolateyInstall\extensions\chocolatey-preinstaller-checks\$xml"  
+$AbortOnMultiples= $ConfigFile.Settings.global.AbortOnMultiples
+$WaitOnMultiple  = $ConfigFile.Settings.chocoStatus.WaitOnMultiple
+$PauseSeconds    = $ConfigFile.Settings.chocoStatus.PauseSeconds
+
+$chocoInstances = (Get-chocoInstanceCounts)
+
+if ($chocoInstances -gt 2)
     {
      while ($chocoInstances -gt 1)
      {
 	  # exclude current instance from status report
-      $chocoInstances = $chocoInstances-1
-      Write-Host "  * WARNING: $chocoInstances other instance(s) of choco.exe actual found running. Pausing 30 seconds..." -foreground red
-	  Start-Sleep -seconds 30
-      $chocoInstances = (Get-chocoCounts)
-     }
-	} else {
-	  Write-Host "  * choco.exe IS NOT running multiple instances." -foreground green
+	  If ($AbortOnMultiples -eq "true")
+          {
+	       Write-Host "  * WARNING: $($chocoInstances-1) other instance(s) of choco.exe actual found running. Aborting install of $env:packageName!" -foreground red
+		   $global:CPCEAbort = "true"
+		   return
+	      }
+	  If ($WaitOnMultiple -eq "false")
+          {
+	       Write-Host "  * WARNING: $($chocoInstances-1) other instance(s) of choco.exe actual found running." -foreground red
+		   return
+	      }
+      Write-Host "  * WARNING: $($chocoInstances-1) other instance(s) of choco.exe actual found running. Pausing $PauseSeconds seconds..." -foreground red
+	  Start-Sleep -seconds $PauseSeconds
+      $chocoInstances = (Get-chocoInstanceCounts)
 	 }
+    } else {
+	  Write-Host "  * choco.exe IS NOT running multiple instances." -foreground green
+	}
 }
+
