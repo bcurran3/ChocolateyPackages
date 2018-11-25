21fe8fd9e1f159ca91a0c506751f26d06891a384
bcurran3 <bcurran3@yahoo.com>
2018-11-24 17:41:27 -0800

    choco-install-packages-from-web-winconfig first commit

---
+++ choco-install-packages-from-web-winconfig/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'choco-install-packages-from-web-winconfig'
+$pp          = Get-PackageParameters
+
+New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT -ea SilentlyContinue | Out-Null
+Remove-Item -Path "HKCR:nupkg_auto_file" -Recurse –Force 
+Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers\" -Name "$env:ChocolateyInstall\bin\cinst.exe\bin\cinst.exe" -ea SilentlyContinue
+Remove-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers\" -Name "$env:ChocolateyInstall\bin\cinst.exe\bin\cinst.exe" -ea SilentlyContinue
