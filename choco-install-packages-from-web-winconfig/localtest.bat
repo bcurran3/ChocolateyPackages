69d7bdee66f586d25fd8889623526d7613834210
bcurran3 <bcurran3@yahoo.com>
2018-11-25 00:31:31 -0800

    updates

--- choco-install-packages-from-web-winconfig/localtest.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cpack
-choco install-packages-from-web-winconfig --params "'/ALLUSERS:YES'" -s S:\ChocolateyPackagesDEV\install-packages-from-web-winconfig\install-packages-from-web-winconfig.0.0.1.nupkg -force
