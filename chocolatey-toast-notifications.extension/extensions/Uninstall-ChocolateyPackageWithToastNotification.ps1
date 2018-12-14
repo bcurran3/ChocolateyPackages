32e50e0c118d56aa7b476bd9df44c64f194def45
bcurran3 <bcurran3@yahoo.com>
2018-12-13 17:57:20 -0800

    Add files via upload

--- chocolatey-toast-notifications.extension/extensions/Uninstall-ChocolateyPackageWithToastNotification.ps1
+++ chocolatey-toast-notifications.extension/extensions/Uninstall-ChocolateyPackageWithToastNotification.ps1
@@ -14,20 +14,15 @@ Remove-Item alias:\Uninstall-ChocolateyPackage -ErrorAction SilentlyContinue
 # Execute Uninstall-ChocolateyInstallPackage actual
 Uninstall-ChocolateyPackage @args
 
-# Ensure this ONLY runs on Windows 10 - this will be removed and dealt with via a BurntToast dependency next version
-$IsWin10=[Environment]::OSVersion.Version.Major
-if (!($IsWin10 -eq "10")){return}
-
 # Get name of program being uninstalled from the .nuspec
 $chocolateySoftwareName = Get-ChocolateySoftwareName
 
 # Show toast notification
 if ($env:USERNAME -eq "$env:COMPUTERNAME$"){
     If ((Get-Service $WinRM).Status -eq 'Running') {
-        Invoke-Command -ComputerName $env:COMPUTERNAME -ScriptBlock {New-BurntToastNotification -Text "Chocolatey ($env:chocolateyPackageName)", "$chocolateySoftwareName v$env:chocolateyPackageVersion", "Uninstalled." -Button (New-BTButton -Content 'Package Webpage' -Arguments "https://chocolatey.org/packages/$env:chocolateyPackageName") -AppLogo "$env:ChocolateyInstall\extensions\chocolatey-toast-notifications\choco.ico"}
-	  }
-   } else {
-#    $chocolateyPackagePage = New-BTButton -Content 'Package Webpage' -Arguments "https://chocolatey.org/packages/$env:chocolateyPackageName"
-     New-BurntToastNotification -Text "Chocolatey ($env:chocolateyPackageName)", "$chocolateySoftwareName v$env:chocolateyPackageVersion", "Uninstalled." -Button (New-BTButton -Content 'Package Webpage' -Arguments "https://chocolatey.org/packages/$env:chocolateyPackageName") -AppLogo "$env:ChocolateyInstall\extensions\chocolatey-toast-notifications\choco.ico"
+         Invoke-Command -ComputerName $env:COMPUTERNAME -ArgumentList $env:chocolateyPackageName,$chocolateySoftwareName,$env:chocolateyPackageVersion -ScriptBlock {param([string]$chocolateyPackageName, [string]$chocolateySoftwareName, [string]$chocolateyPackageVersion) New-BurntToastNotification -Text "Chocolatey ($chocolateyPackageName)", "$chocolateySoftwareName v$chocolateyPackageVersion", "Uninstalled." -Button (New-BTButton -Content 'Package Webpage' -Arguments "https://chocolatey.org/packages/$env:chocolateyPackageName") -AppLogo "$env:ChocolateyInstall\extensions\chocolatey-toast-notifications\choco.ico"}
+        } else {
+          New-BurntToastNotification -Text "Chocolatey ($env:chocolateyPackageName)", "$chocolateySoftwareName v$env:chocolateyPackageVersion", "Uninstalled." -Button (New-BTButton -Content 'Package Webpage' -Arguments "https://chocolatey.org/packages/$env:chocolateyPackageName") -AppLogo "$env:ChocolateyInstall\extensions\chocolatey-toast-notifications\choco.ico"
+       }
    }
 }
