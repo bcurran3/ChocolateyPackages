7231931c55b142aab3763e3fcfe8a2ae4b54d4f0
bcurran3 <bcurran3@yahoo.com>
2018-12-28 21:58:55 -0800

    updates

--- angryip/tools/chocolateyinstall.ps1
+++ angryip/tools/chocolateyinstall.ps1
@@ -1,19 +1,15 @@
 $packageName   = 'angryip'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$installerType = 'exe'
-$url           = 'https://github.com/angryziber/ipscan/releases/download/3.5.2/ipscan-3.5.2-setup.exe'
-$checksum      = '1222D5AC68AB90DFCB14E3C2E2258D695DE12B27D3AADBBD94AA85A3A85D4701'
-$silentArgs    = '/S'
+$url           = "$toolsDir\ipscan-$env:ChocolateyPackageVersion-setup.exe"
 
 $packageArgs = @{
   packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = $installerType
-  url           = $url
-  silentArgs    = $silentArgs
+  fileType      = 'EXE'
+  file          = $url
+  silentArgs    = '/S'
   softwareName  = 'Angry IP Scanner*' 
-  checksum      = $checksum
-  checksumType  = 'sha256'  
   }
-Install-ChocolateyPackage @packageArgs
-
+  
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -force | Out-Null
+Remove-Item $toolsDir\*.ignore -force | Out-Null
