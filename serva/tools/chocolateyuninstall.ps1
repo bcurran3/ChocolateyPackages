2a8da9d9a86ec4656fe30ec3c5d19c549e6beb23
bcurran3 <bcurran3@yahoo.com>
2019-01-03 14:06:09 -0800

    updates

--- serva/tools/chocolateyuninstall.ps1
+++ serva/tools/chocolateyuninstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'serva'
 
-remove-item "$env:Public\Desktop\Serva.lnk" -Force -ErrorAction 'SilentlyContinue'
-remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Serva.lnk" -Force -ErrorAction 'SilentlyContinue'
+remove-item "$env:Public\Desktop\Serva.lnk" -Force -ErrorAction SilentlyContinue
+remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Serva.lnk" -Force -ErrorAction SilentlyContinue
 
