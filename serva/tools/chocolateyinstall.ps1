08265b9f129d6a7c647d211dfe9de2b8b49d99b5
bcurran3 <bcurran3@yahoo.com>
2019-01-03 14:06:09 -0800

    updates

--- serva/tools/chocolateyinstall.ps1
+++ serva/tools/chocolateyinstall.ps1
@@ -1,20 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName= 'serva' 
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://vercot.com/~serva/download/Serva_Community_32_v3.0.0.zip'
-$url64      = 'http://vercot.com/~serva/download/Serva_Community_64_v3.0.0.zip'
-
+$packageName = 'serva' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'https://www.vercot.com/~serva/download/Serva_Community_32_64_v3.2.0.zip'
+$checksum    = 'AC7C9EE0DD322A373C6CA4616435E4EA359A14C13FD069F16EEDD95073331B3A'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'ZIP' 
   url           = $url
-  url64bit      = $url64 
-  checksum      = 'E953FE63D89EE27076656C5667F7F698AC2726B75836CA9008A07A98B4B1B03B'
   checksumType  = 'sha256' 
-  checksum64    = '70DFDA58579CA5BA7B67AE1437C190AF40A5CB2936F52D1610BE15CB41F295DE'
-  checksumType64= 'sha256'  
 }
 
 Install-ChocolateyZipPackage @packageArgs
