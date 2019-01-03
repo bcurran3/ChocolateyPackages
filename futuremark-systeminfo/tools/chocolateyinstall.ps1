8642def098845f7222d2d5bd2e4ac51976834821
bcurran3 <bcurran3@yahoo.com>
2019-01-02 21:02:23 -0800

    updates

--- futuremark-systeminfo/tools/chocolateyinstall.ps1
+++ futuremark-systeminfo/tools/chocolateyinstall.ps1
@@ -1,21 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName    = 'futuremark-systeminfo'
-$installerType  = 'MSI'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://d1ejs5fxm96rib.cloudfront.net/Futuremark_SystemInfo_v51_installer.msi' 
-$checksum       = '0475AA8F9CF750E52E7F498987502FDD58A3FE9AE843A4F5A4AC63EF16558ACF'
-$silentArgs     = '/quiet /qn /norestart'
-$validExitCodes = @(0, 3010, 1641)
+$url            = 'https://s3.amazonaws.com/download-aws.futuremark.com/Futuremark_SystemInfo_5_14_693.msi' 
+$checksum       = 'D79D47D7C5DCD82D5A43546022E79DD3A893CD01D3181CDB210C8567D17C732D'
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = $installerType
-  url           = $url
-  validExitCodes= $validExitCodes
-  silentArgs    = $silentArgs
-  softwareName  = 'Futuremark SystemInfo*'
-  checksum      = $checksum
-  checksumType  = 'sha256' 
+  packageName    = $packageName
+  fileType       = 'MSI'
+  url            = $url
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/quiet /qn /norestart'
+  softwareName   = 'Futuremark SystemInfo*'
+  checksum       = $checksum
+  checksumType   = 'sha256' 
 }
 
 Install-ChocolateyPackage @packageArgs  
