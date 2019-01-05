ca94e1561ee02042600cb02fd6095fa1af518509
bcurran3 <bcurran3@yahoo.com>
2019-01-05 12:56:06 -0800

    updates

--- gpu-z/tools/chocolateyinstall.ps1
+++ gpu-z/tools/chocolateyinstall.ps1
@@ -1,7 +1,12 @@
 ﻿#https://www.techpowerup.com/download/techpowerup-gpu-z/
-$packageName      = 'gpu-z'
-$toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url              = "$toolsDir\GPU-Z.$env:packageVersion.exe"
+$packageName = 'gpu-z'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$PortableEXE = "$toolsDir\GPU-Z.$env:packageVersion.exe"
 
-Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\GPU-Z.lnk" -targetPath $url
-Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\GPU-Z.lnk" -targetPath $url
+Remove-Item $ENV:ChocolateyInstall\bin\gpu-z*.exe # delete old shims
+Remove-Item $toolsDir\*.exe -Exclude "GPU-Z.$env:packageVersion.exe" # deleted old versions kept by upgrade
+Remove-Item $toolsDir\*.ignore -Exclude "GPU-Z.$env:packageVersion.exe.ignore" # delete old .ignore files (2.18.0+)
+Write-Host "" | Out-File "$toolsDir\GPU-Z.$env:packageVersion.exe.ignore" # create .ignore file so shim isn't created
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\GPU-Z.lnk" -targetPath $PortableEXE
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\GPU-Z.lnk" -targetPath $PortableEXE
