890d1d6cc9259ff7bf154cb41d81b3d212e01515
bcurran3 <bcurran3@yahoo.com>
2019-01-05 12:56:06 -0800

    updates

--- adwcleaner/tools/ChocolateyUninstall.ps1
+++ adwcleaner/tools/ChocolateyUninstall.ps1
@@ -2,5 +2,5 @@
 $shortcutName = 'AdwCleaner.lnk'
 
 Uninstall-BinFile adwcleaner
-remove-item "$env:Public\Desktop\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
-remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
+remove-item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
+remove-item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
--- gpu-z/tools/chocolateyuninstall.ps1
+++ gpu-z/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
 $packageName='gpu-z' 
 
-remove-item "$env:Public\Desktop\GPU-Z.lnk" -Force -ErrorAction 'SilentlyContinue'
-remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\GPU-Z.lnk" -Force -ErrorAction 'SilentlyContinue'
+remove-item "$ENV:Public\Desktop\GPU-Z.lnk" -Force -ErrorAction SilentlyContinue
+remove-item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\GPU-Z.lnk" -Force -ErrorAction SilentlyContinue
