272fa2470e36749504b4c1ee3edb2fdff00715c9
bcurran3 <bcurran3@yahoo.com>
2019-01-11 21:54:20 -0800

    updates

--- solarwinds-advanced-monitoring-agent/tools/ChocolateyInstall.ps1
+++ solarwinds-advanced-monitoring-agent/tools/ChocolateyInstall.ps1
@@ -1,31 +1,29 @@
 ﻿$packageName    = 'solarwinds-advanced-monitoring-agent'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://dashboard.systemmonitor.us/data_processor.php?function=agent_download&os=windows&versiontype=0'
+$CurrentVersion = ($env:ChocolateyPackageVersion -replace '[.]','_')
+$url            = "https://rm-downloads-us.logicnow.com/AGENT_$CurrentVersion.ZIP"
 $checksum       = '7A15778D232FE3B5CF10D1E409FFFE19C0849100FEB056B808E5F81CE10D3167'
 
 $packageArgs = @{
   packageName   = $packageName
-  fileType      = 'ZIP'
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP' 
   url           = $url
-  FileFullPath  = $toolsDir
-  softwareName  = 'Advanced Monitoring Agent'
   checksum      = $checksum
   checksumType  = 'sha256'
-  Options = @{
-  Headers = @{
-  referer = "https://dashboard.systemmonitor.us/"
-  }
-  } 
 }
- 
-Get-ChocolateyWebFile @packageArgs
+
+Install-ChocolateyZipPackage @packageArgs 
 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'EXE'
   file          = "$toolsDir\AGENT_10_7_12.EXE"
-  silentArgs    = '/S'
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   softwareName  = 'Advanced Monitoring Agent'
   }
   
 Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item "$toolsDir\*.zip" | out-null
+Remove-Item "$toolsDir\*.exe" | out-null
