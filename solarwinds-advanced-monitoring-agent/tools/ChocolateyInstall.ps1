5246deaee08e6c8d34ef3a30a7c3d94c0de895a8
bcurran3 <bcurran3@yahoo.com>
2019-01-11 17:34:19 -0800

    new SolarWindowsMSP stuff

---
+++ solarwinds-advanced-monitoring-agent/tools/ChocolateyInstall.ps1
@@ -0,0 +1,31 @@
+﻿$packageName    = 'solarwinds-advanced-monitoring-agent'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://dashboard.systemmonitor.us/data_processor.php?function=agent_download&os=windows&versiontype=0'
+$checksum       = '7A15778D232FE3B5CF10D1E409FFFE19C0849100FEB056B808E5F81CE10D3167'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'ZIP'
+  url           = $url
+  FileFullPath  = $toolsDir
+  softwareName  = 'Advanced Monitoring Agent'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  Options = @{
+  Headers = @{
+  referer = "https://dashboard.systemmonitor.us/"
+  }
+  } 
+}
+ 
+Get-ChocolateyWebFile @packageArgs
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  file          = "$toolsDir\AGENT_10_7_12.EXE"
+  silentArgs    = '/S'
+  softwareName  = 'Advanced Monitoring Agent'
+  }
+  
+Install-ChocolateyInstallPackage @packageArgs
