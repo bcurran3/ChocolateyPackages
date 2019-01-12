195effdf4e9cfe17cb1ea4b235c273e183863e98
bcurran3 <bcurran3@yahoo.com>
2019-01-11 21:54:20 -0800

    updates

--- solarwinds-automation-manager/tools/ChocolateyInstall.ps1
+++ solarwinds-automation-manager/tools/ChocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName    = 'solarwinds-automation-manager'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://rm-downloads-us.logicnow.com/automation-manager/AutomationManager-2.4.0.43.exe'
+$url            = "https://rm-downloads-us.logicnow.com/automation-manager/AutomationManager-$env:ChocolateyPackageVersion.exe"
 $checksum       = '268D35CD2E2CF269C4D2DBC2276DA6DF2F2B0227A966C4581F4EEE3EDCF2F5B1'
 
 $packageArgs = @{
