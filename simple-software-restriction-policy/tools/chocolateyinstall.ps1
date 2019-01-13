66c070aad761676d78f3cef43181c5b976d38604
bcurran3 <bcurran3@yahoo.com>
2019-01-12 18:25:41 -0800

    v2.2.0.0

--- simple-software-restriction-policy/tools/chocolateyinstall.ps1
+++ simple-software-restriction-policy/tools/chocolateyinstall.ps1
@@ -1,18 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName= 'simple-software-restriction-policy'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = "$toolsDir\SoftwarePolicy220Setup.exe"
+$packageName = 'simple-software-restriction-policy'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = "$toolsDir\SoftwarePolicy220Setup.exe"
+$checksum    = 'A5D6A2D8CE5F8F80D7CAA54509B8BB30C8483002FC6D7A084391F14C89C92F0A'
  
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  checksum      = 'A5D6A2D8CE5F8F80D7CAA54509B8BB30C8483002FC6D7A084391F14C89C92F0A'
+  checksum      = $checksum
   checksumType  = 'sha256'
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   softwareName  = 'Software Policy*'
 }
  
 Install-ChocolateyPackage @packageArgs
-Remove-Item $toolsDir\SoftwarePolicy220Setup.exe
+Remove-Item $toolsDir\*.exe -Force | Out-Null
