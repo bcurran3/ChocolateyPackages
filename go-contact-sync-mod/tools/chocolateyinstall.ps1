e776452662543fd401d876cb1b4dec6b99ad3d31
bcurran3 <bcurran3@yahoo.com>
2019-01-12 17:15:14 -0800

    v3.10.22

--- go-contact-sync-mod/tools/chocolateyinstall.ps1
+++ go-contact-sync-mod/tools/chocolateyinstall.ps1
@@ -1,15 +1,15 @@
 ﻿$packageName = 'go-contact-sync-mod'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = "$toolsDir\SetupGCSM-3.10.21.msi"
+$url         = "$toolsDir\SetupGCSM-$env:ChocolateyPackageVersion.msi"
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'MSI'
-  file          = $url
-  silentArgs    = '/quiet /qn /norestart'
-  softwareName  = 'Go Contact Sync Mod*' 
+  packageName    = $packageName
+  fileType       = 'MSI'
+  file           = $url
+  silentArgs     = '/quiet /qn /norestart'
+  softwareName   = 'Go Contact Sync Mod*' 
   validExitCodes = @(0, 3010, 1641)
   }
   
 Install-ChocolateyInstallPackage @packageArgs
-Remove-Item $toolsDir\*.msi -force | Out-Null
+Remove-Item $toolsDir\*.msi -Force | Out-Null
