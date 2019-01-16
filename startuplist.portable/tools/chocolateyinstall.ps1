cdaec05a7b7b16acc3622d2996e54cd05ba8e8bd
bcurran3 <bcurran3@yahoo.com>
2019-01-15 22:22:55 -0800

    v2.02.2019015

--- startuplist.portable/tools/chocolateyinstall.ps1
+++ startuplist.portable/tools/chocolateyinstall.ps1
@@ -1,20 +1,6 @@
-﻿$ErrorActionPreference = 'Stop'
-$packageName  = 'startuplist.portable' 
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://www.merijn.nu/files/startuplist.zip'
+﻿$packageName  = 'startuplist.portable' 
 $ExeFile      = 'StartupList.exe'
 $ShortcutName = 'StartupList'
-
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'ZIP' 
-  url           = $url
-  checksum      = '49BCCD474044F25B44C39F99C53CBF2183766285AF5B22FFB9A0B510A3703B94'
-  checksumType  = 'sha256'  
-}
-
-Install-ChocolateyZipPackage @packageArgs
 
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\$ShortcutName.lnk" -targetPath "$ENV:ChocolateyInstall\lib\$packageName\tools\$ExeFile"
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName.lnk" -targetPath "$toolsDir\$ExeFile" -WorkingDirectory "$toolsDir"
