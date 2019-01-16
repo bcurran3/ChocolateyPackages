2332d4ab264039c904f80b950b618f3a5448d27f
bcurran3 <bcurran3@yahoo.com>
2019-01-15 22:33:43 -0800

    updated uninstall script

--- startuplist.portable/tools/chocolateyuninstall.ps1
+++ startuplist.portable/tools/chocolateyuninstall.ps1
@@ -1,6 +1,5 @@
 ﻿$packageName  = 'startuplist.portable'
 $ShortcutName = 'StartupList'
 
-Uninstall-ChocolateyZipPackage
-remove-item "$ENV:Public\Desktop\$ShortcutName.lnk" -Force -ErrorAction SilentlyContinue
-remove-item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName.lnk" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:Public\Desktop\$ShortcutName.lnk" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName.lnk" -Force -ErrorAction SilentlyContinue
