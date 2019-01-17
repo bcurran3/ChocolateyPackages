ffd65ccad2a424d230de6d116f0a19ca2d40becb
bcurran3 <bcurran3@yahoo.com>
2019-01-17 12:48:44 -0800

    Updates

--- vnc-viewer/tools/chocolateyuninstall.ps1
+++ vnc-viewer/tools/chocolateyuninstall.ps1
@@ -1,6 +1,5 @@
 ﻿$packageName    = 'vnc-viewer'
 $shortcutName   = 'VNC Viewer' 
 
-remove-item "$env:PUBLIC\Desktop\$shortcutName.lnk" -Force -ErrorAction 'SilentlyContinue'
-remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk" -Force -ErrorAction 'SilentlyContinue'
-
+Remove-Item "$ENV:PUBLIC\Desktop\$shortcutName.lnk" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName.lnk" -Force -ErrorAction SilentlyContinue
