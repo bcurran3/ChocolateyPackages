aa37ce0616c9d22560ab40ad9c46c74d6d342c04
bcurran3 <bcurran3@yahoo.com>
2019-01-22 17:36:29 -0800

    Add files via upload

--- wsl-fedoraremix/tools/ChocolateyInstall.ps1
+++ wsl-fedoraremix/tools/ChocolateyInstall.ps1
@@ -1,7 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName    = 'wsl-fedoraremix'
-$bits           = Get-ProcessorBits
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$bits           = Get-ProcessorBits
+$shortcutName   = 'Fedora Remix.lnk'
+$exe            = "$toolsDir\distro\fedoraremix.exe"
 
 if (!($bits -eq 64)){
     Write-Warning "  **  This package is for 64 bit versions of Windows only. Aborting..."
@@ -37,13 +39,29 @@ $packageArgs = @{
 
 Install-ChocolateyInstallPackage @packageArgs
 
-Install-BinFile -Name fedoraremix -Path "$toolsDir\distro\fedoraremix.exe"
-
-$WhoAmI=whoami
-icacls.exe "$toolsDir\distro" /grant $WhoAmI":"'(OI)(CI)'F /T | Out-Null
+Install-BinFile -Name fedoraremix -Path $exe
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\$shortcutName" -targetPath $exe -WorkingDirectory "$toolsDir\distro"
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath $exe -WorkingDirectory "$toolsDir\distro"
 
 wslconfig /list
 Write-Host "  ** To run Fedora Remix, type fedoraremix from the Command Prompt or PowerShell." -ForeGround Magenta
+
+# wait for installer to finish extracting files
+    $LoopMePlease=0
+    while (!(Test-Path "$ENV:ChocolateyInstall\lib\wsl-fedoraremix\tools\distro\rootfs\var\yp")){
+	       Write-Host "  ** Waiting for files to finish extracting..." -ForeGround Magenta
+           Start-Sleep -Seconds 10
+		   $LoopMePlease++
+# break out of the loop after 10 minutes in case of error
+		   if ($LoopMePlease -gt 60) {
+	          Write-Warning "Something went wrong with extracting the rootfs distro files."
+			  wslconfig /list
+		      break
+			 } 
+          }
+
+$WhoAmI=whoami
+icacls.exe "$toolsDir\distro" /grant $WhoAmI":"'(OI)(CI)'F /T | Out-Null
 
 Remove-Item "$toolsDir\AppxMetadata" -Recurse -Force  -EA SilentlyContinue | Out-Null
 Remove-Item "$toolsDir\*.appx" -Force -EA SilentlyContinue | Out-Null
@@ -55,4 +73,4 @@ Remove-Item "$toolsDir\distro\Assets" -Recurse -Force -EA SilentlyContinue | Out
 Remove-Item "$toolsDir\distro\*.p7x" -Force -EA SilentlyContinue | Out-Null
 Remove-Item "$toolsDir\distro\*.pri" -Force -EA SilentlyContinue | Out-Null
 Remove-Item "$toolsDir\distro\*.xml" -Force -EA SilentlyContinue | Out-Null
-if (Test-Path "$ENV:ChocolateyInstall\lib\wsl-fedoraremix\tools\distro\rootfs\var\yp"){Remove-Item "$toolsDir\distro\*.gz" -Force -EA SilentlyContinue | Out-Null}
+Remove-Item "$toolsDir\distro\*.gz" -Force -EA SilentlyContinue | Out-Null
