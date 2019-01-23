b0746fbfa8e119e3d321b47b81aa6408b90c5d5d
bcurran3 <bcurran3@yahoo.com>
2019-01-22 17:36:29 -0800

    Add files via upload

--- wsl-fedoraremix/tools/ChocolateyUninstall.ps1
+++ wsl-fedoraremix/tools/ChocolateyUninstall.ps1
@@ -1,6 +1,9 @@
 $ErrorActionPreference = 'Stop'
 $packageName = 'wsl-fedoraremix'
+$shortcutName = 'Fedora Remix.lnk'
 
 wslconfig /unregister fedoraremix
 wslconfig /list
 Uninstall-BinFile fedoraremix
+Remove-Item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
