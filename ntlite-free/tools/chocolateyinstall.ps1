f9d9bc0ff101fe4f4daf6d06dde5ea48e3790675
bcurran3 <bcurran3@yahoo.com>
2019-02-02 17:36:10 -0800

    v1.7.2.6703

--- ntlite-free/tools/chocolateyinstall.ps1
+++ ntlite-free/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@
 $packageName    = 'ntlite-free'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'http://downloads.ntlite.com/files/NTLite_setup_x86.exe' 
-$checksum       = '7DEC4474F936DEF585C70C8CAA970B81AA8C4321D4086685B9ABB39AA6AF16D9'
+$checksum       = '2CDADB362A18E3A273B4EF5970E65BCAA6426B365B56CCD269DFD4F657BCAD0F'
 $url64          = 'http://downloads.ntlite.com/files/NTLite_setup_x64.exe' 
-$checksum64     = 'F94B4BEE62EC4B1930A0642C18CB47B7B07BDEC7FF7E9CA2CF365B5EA1807180'
+$checksum64     = '2988F87FFBBD45EA5B60A2209E4FE33060C881264EACA3991B12DBC761665D48'
 
 $packageArgs = @{
   packageName   = $packageName
