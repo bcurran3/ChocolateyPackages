f766bfe63fc8ae7eb827d8b12afca796f53099eb
bcurran3 <bcurran3@yahoo.com>
2019-02-02 17:29:54 -0800

    v8.01

--- windows-iso-downloader/tools/chocolateyuninstall.ps1
+++ windows-iso-downloader/tools/chocolateyuninstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName  = 'windows-iso-downloader'
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'windows-iso-downloader'
 $ShortcutName = 'Microsoft Windows and Office ISO Download Tool.lnk'
 
-remove-item "$env:Public\Desktop\$ShortcutName" -Force -ErrorAction 'SilentlyContinue'
-remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName" -Force -ErrorAction 'SilentlyContinue'
-
+remove-item "$ENV:Public\Desktop\$ShortcutName" -Force -ErrorAction SilentlyContinue
+remove-item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName" -Force -ErrorAction SilentlyContinue
