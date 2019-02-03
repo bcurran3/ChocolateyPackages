ac26231e5fca9787240fdeaf47a4b90fe8c741c9
bcurran3 <bcurran3@yahoo.com>
2019-02-02 17:38:51 -0800

    v11.0.6

--- advanced-codecs/tools/chocolateyuninstall.ps1
+++ advanced-codecs/tools/chocolateyuninstall.ps1
@@ -1,4 +1,5 @@
-﻿$packageName    = 'advanced-codecs'
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'advanced-codecs'
 $softwareName   = 'Shark007 ADVANCED Codecs*'
 
 Start-Process -FilePath "msiexec.exe" -ArgumentList "/x {8C0CAA7A-3272-4991-A808-2C7559DE3409} /qn"
