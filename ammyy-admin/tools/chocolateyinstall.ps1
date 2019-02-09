041bbf9577cdd14e0fb145453de6d63e7e4ddf9c
bcurran3 <bcurran3@yahoo.com>
2019-02-09 12:16:30 -0800

    v3.8.0.0

--- ammyy-admin/tools/chocolateyinstall.ps1
+++ ammyy-admin/tools/chocolateyinstall.ps1
@@ -1,9 +1,10 @@
-﻿$packageName = 'ammyy-admin' 
-$checksum    = '8BAEED8D30B9BFBFF3ADDA3496DF1552AB4BED3A7092CB7B56543F9B844B0353'
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'ammyy-admin' 
 
-Start-CheckandStop "AA_v3"
+#Start-CheckandStop "AA_v3" 
+#- if running, Chocolatey can't move to lib-bkp and will fail install before getting here since this is portable
 
 Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\Ammyy Admin.lnk" -targetPath "$env:ChocolateyInstall\lib\$packageName\tools\AA_v3.exe"
 Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Ammyy Admin.lnk" -targetPath "$env:ChocolateyInstall\lib\$packageName\tools\AA_v3.exe"
 
-if ($ProcessWasRunning -eq "True") {&"$ProcessFullPath"}
+#if ($ProcessWasRunning -eq "True") {&"$ProcessFullPath"}
