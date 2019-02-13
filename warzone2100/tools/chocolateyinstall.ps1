ec8e5e8e0ff79654f88cea3810029dfc40f93754
bcurran3 <bcurran3@yahoo.com>
2019-02-13 14:44:43 -0800

    new dl URL

--- warzone2100/tools/chocolateyinstall.ps1
+++ warzone2100/tools/chocolateyinstall.ps1
@@ -1,21 +1,18 @@
-﻿$packageName    = 'warzone2100'
-$installerType  = 'exe'
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'warzone2100'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://downloads.sourceforge.net/project/warzone2100/releases/3.2.3/warzone2100-3.2.3.exe' 
+$url            = 'https://downloads.sourceforge.net/project/warzone2100/archives/unsupported/Warzone2100-3.2/3.2.3/warzone2100-3.2.3.exe' 
 $checksum       = 'D32F78545C63767D6E51D2A3BFA2C8BB46BF3238284B0471F8F939950EBF98D1'
-$silentArgs     = '/S'
-$validExitCodes = @(0)
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = $installerType
-  url           = $url
-  validExitCodes= $validExitCodes
-  silentArgs    = $silentArgs
-  softwareName  = 'Warzone 2100*'
-  checksum      = $checksum 
-  checksumType  = 'sha256' 
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  validExitCodes = @(0)
+  silentArgs     = '/S'
+  softwareName   = 'Warzone 2100*'
+  checksum       = $checksum 
+  checksumType   = 'sha256' 
 }
 
 Install-ChocolateyPackage @packageArgs  
-  
