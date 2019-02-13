182163a09247a98fbb9472fa547622dd40f1c100
bcurran3 <bcurran3@yahoo.com>
2019-02-13 13:10:32 -0800

    v.206

--- mame/tools/chocolateyuninstall.ps1
+++ mame/tools/chocolateyuninstall.ps1
@@ -1,4 +1,5 @@
-﻿$packageName= 'mame' 
+﻿$ErrorActionPreference = 'Stop'
+$packageName= 'mame' 
 
-remove-item "$ENV:Public\Desktop\MAME.lnk" -Force -ErrorAction SilentlyContinue
-remove-item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\MAME.lnk" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:Public\Desktop\MAME.lnk" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\MAME.lnk" -Force -ErrorAction SilentlyContinue
