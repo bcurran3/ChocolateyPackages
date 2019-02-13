55d25fff129340b116c2d47fff09b96564a97d51
bcurran3 <bcurran3@yahoo.com>
2019-02-13 14:25:16 -0800

    v0.38.6.0

--- clash-of-clans/tools/ChocolateyInstall.ps1
+++ clash-of-clans/tools/ChocolateyInstall.ps1
@@ -1,7 +1,8 @@
-﻿$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $global:packageMaintainer = 'BCURRAN3'
 $url            = 'https://d1x9snl812q4nd.cloudfront.net/installer/com.supercell.clashofclans/Clash_of_Clans-gameslol.exe'
-$checksum       = '48E27D9E156368AC41E0359497BACF1EC572E60713116F1A73ED5DB6499F9DD2'
+$checksum       = '09A64B5E1C498EE5B64C837E33A7CF6F68C0478DF14AD8A8DAFD9D249A44A646'
 $ahkExe         = 'AutoHotKey'
 $ahkFile        = "$toolsDir\COCinstall.ahk"
 
@@ -22,4 +23,5 @@ Start-sleep 2
 Remove-Item "$env:AppData\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\GamesLOL.lnk" -ea SilentlyContinue
 Remove-Item "$env:USERPROFILE\Desktop\GamesLOL.lnk" -ea SilentlyContinue
 Remove-Item "$env:AppData\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Clash of Clans.lnk" -ea SilentlyContinue
-
+Start-CheckAndStop "mobinotifier"
+Start-CheckAndStop "mobiplayer"
