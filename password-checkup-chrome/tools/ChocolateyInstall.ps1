71e64b9d44f95040dc4736b24a451e3d42c2f20a
bcurran3 <bcurran3@yahoo.com>
2019-02-15 10:26:48 -0800

    Password Checkup for Chrome

---
+++ password-checkup-chrome/tools/ChocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+$bits = Get-ProcessorBits
+$packageName = 'password-checkup-chrome'
+$extensionID = 'pncabnpcffmalkkjpajodfhijclecjno'
+if ($bits -eq 64)
+   {
+    if (Test-Path -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID")
+       {
+       Write-Host "Extension already installed." -Foreground Magenta
+       } else {
+         New-Item -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" | Out-Null
+         New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID\" -Name "update_url" -Value "https://clients2.google.com/service/update2/crx" | Out-Null
+         New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID\" -Name "ChocolateyPackageName" -Value "$packageName"	| Out-Null
+         }
+    } else {
+      New-Item -Path "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID" | Out-Null
+      New-ItemProperty -Path "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID\" -Name "update_url" -Value "https://clients2.google.com/service/update2/crx" | Out-Null
+    }
---
+++ password-checkup-chrome/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'
+$bits = Get-ProcessorBits
+$packageName = 'password-checkup-chrome'
+$extensionID = 'pncabnpcffmalkkjpajodfhijclecjno'
+
+if ($bits -eq 64) {
+    Remove-Item "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue | Out-Null
+   }else{
+    Remove-Item "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue | Out-Null
+}
