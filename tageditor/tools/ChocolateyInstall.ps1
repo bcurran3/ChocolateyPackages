00d7ac70a4a9e8c188f396c89485661dab157e3b
bcurran3 <bcurran3@yahoo.com>
2019-02-19 20:41:10 -0800

    v3.2.0

--- tageditor/tools/ChocolateyInstall.ps1
+++ tageditor/tools/ChocolateyInstall.ps1
@@ -7,10 +7,10 @@ $shortcutName = 'Tag Editor.lnk'
 if ($bits -eq 64)
    {
     $exeTarget = "$toolsDir\i686-w64-mingw32\bin\tageditor.exe"
-	Remove-Item "$toolsDir\x86_64-w64-mingw32\bin\tageditor.exe" #So shim doesn't get created for both
+	Remove-Item "$toolsDir\x86_64-w64-mingw32" -Recurse -Force
    } else {
     $exeTarget = "$toolsDir\x86_64-w64-mingw32\bin\tageditor.exe"
-	Remove-Item "$toolsDir\i686-w64-mingw32\bin\tageditor.exe" #So shim doesn't get created for both
+	Remove-Item "$toolsDir\i686-w64-mingw32" -Recurse -Force
    }
 
 Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath "$exeTarget" -WorkingDirectory "$toolsDir\"
