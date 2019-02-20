e798f5c1b684258820493c1a7f5681760f7d7ca2
bcurran3 <bcurran3@yahoo.com>
2019-02-19 20:19:42 -0800

    v0.87.1

--- homeassistant/tools/ChocolateyUninstall.ps1
+++ homeassistant/tools/ChocolateyUninstall.ps1
@@ -1,8 +1,8 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 $packageName  = 'homeassistant'
 $shortcutName = 'Home Assitant.lnk'
 
 pip3 uninstall homeassistant -y
 
-remove-item "$env:Public\Desktop\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
-remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
+Remove-Item "$env:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
+Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
