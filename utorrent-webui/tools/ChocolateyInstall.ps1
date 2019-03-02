702b614e796169fb5ba386485ba8cb421f935996
bcurran3 <bcurran3@yahoo.com>
2019-03-02 11:32:01 -0800

    v0.388

---
+++ utorrent-webui/tools/ChocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'utorrent-webui'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Move-Item $toolsDir\webui.zip $ENV:AppData\uTorrent -Force
