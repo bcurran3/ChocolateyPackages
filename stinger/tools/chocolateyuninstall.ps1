d6f96c75e86b63a9e4df32dff657a0b74755ab91
bcurran3 <bcurran3@yahoo.com>
2019-03-03 00:25:49 -0800

    v12.1.0.3054

--- stinger/tools/chocolateyuninstall.ps1
+++ stinger/tools/chocolateyuninstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName = 'stinger'
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'stinger'
 
 Uninstall-BinFile stinger
-remove-item "$env:Public\Desktop\McAfee Stinger.lnk" -Force -ErrorAction 'SilentlyContinue'
-remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\McAfee Stinger.lnk" -Force -ErrorAction 'SilentlyContinue'
-
+Remove-Item "$env:Public\Desktop\McAfee Stinger.lnk" -Force -ErrorAction SilentlyContinue
+Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\McAfee Stinger.lnk" -Force -ErrorAction SilentlyContinue
