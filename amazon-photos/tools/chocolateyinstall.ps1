1490fcd92fb103ae659b5d2935a9bb87cea9414b
bcurran3 <bcurran3@yahoo.com>
2019-03-06 10:49:27 -0800

    v5.6.0.102

---
+++ amazon-photos/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName   = 'amazon-photos'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url           = 'https://d29x207vrinatv.cloudfront.net/win/AmazonPhotosSetup.exe'
+$checksum      = 'E9C1C2197A39758447047AFC7C2EB219F918D13440734C1B069CAECF50DE61BA'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  validExitCodes = @(0)
+  silentArgs     = "-quiet"
+  softwareName   = 'Amazon Photos'
+  checksum       = $checksum
+  checksumType   = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs  
+Start-Sleep -s 5
+Start-WaitandStop "amazonphotos"
