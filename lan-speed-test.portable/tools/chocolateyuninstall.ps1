e7e2914f39481d512237ea2f54209360a6df268f
bcurran3 <bcurran3@yahoo.com>
2019-03-06 11:09:54 -0800

    v4.4.0

--- lan-speed-test.portable/tools/chocolateyuninstall.ps1
+++ lan-speed-test.portable/tools/chocolateyuninstall.ps1
@@ -1,5 +1,6 @@
+﻿$ErrorActionPreference = 'Stop'
 $packageName  = 'lan-speed-test.portable'
 $shortcutName = 'LAN Speed Test.lnk'
 
-remove-item "$env:Public\Desktop\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
-remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
+Remove-Item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
