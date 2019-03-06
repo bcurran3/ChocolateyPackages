c18852b1dc8516651e32df0c0bf2187272048244
bcurran3 <bcurran3@yahoo.com>
2019-03-06 12:58:06 -0800

    v8.23

---
+++ wordweb-free/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir       = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$osBitness      = Get-ProcessorBits
+$packageName    = 'wordweb-free'
+$ahkExe         = 'AutoHotKey'
+$ahkFile        = "$toolsDir\wordweb-free-uninstall.ahk"
+
+Start-Process $ahkExe $ahkFile
+
+if ($osBitness -eq '64'){
+    & "${ENV:ProgramFiles(x86)}\WordWeb\uninst.exe"
+   } else {
+     & "$ENV:ProgramFiles\WordWeb\uninst.exe"
+    }
+
+While (Get-Process "autohotkey" -EA SilentlyContinue) {Start-Sleep -Seconds 1}
