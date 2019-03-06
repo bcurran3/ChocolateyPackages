417bb22857c0658b5a608d607964a1e4816aaff2
bcurran3 <bcurran3@yahoo.com>
2019-03-06 11:38:11 -0800

    v4.5

--- simple-sticky-notes/tools/chocolateyinstall.ps1
+++ simple-sticky-notes/tools/chocolateyinstall.ps1
@@ -1,19 +1,17 @@
-﻿$packageName    = 'simple-sticky-notes'
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'simple-sticky-notes'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'http://www.simplestickynotes.com/setup/Setup_SimpleStickyNotes.exe'
-$checksum       = '7141E617C89427425ED25BDAAE1BFB1F0DDA44652221A88B27C0C2FB0F07E30D'
+$url            = "$toolsDir\Setup_SimpleStickyNotes.exe"
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir  
-  fileType      = 'EXE'
-  url           = $url
-  checksum      = $checksum
-  checksumType  = 'sha256'   
-  validExitCodes= @(0,1)
-  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  softwareName  = 'Simple Sticky Notes*'
-}
-
-Install-ChocolateyPackage @packageArgs
-
+  packageName    = $packageName
+  fileType       = 'EXE'
+  file           = $url
+  validExitCodes = @(0,1)
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  softwareName   = 'Simple Sticky Notes*'
+  }
+  
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -force | Out-Null
+Remove-Item $toolsDir\*.ignore -force | Out-Null
