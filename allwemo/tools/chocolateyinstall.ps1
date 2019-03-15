50094deb2544cb1223eee8fdc0455fd19e80f17d
bcurran3 <bcurran3@yahoo.com>
2019-03-15 02:34:12 -0700

    remove unnecessary checkum

--- allwemo/tools/chocolateyinstall.ps1
+++ allwemo/tools/chocolateyinstall.ps1
@@ -2,7 +2,6 @@
 $packageName    = 'allwemo'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = "$toolsDir\AllWemoSetup.msi" 
-$checksum       = '1A923962B66B544EE7B602CB063BCE9F97C12C29C25FEF1424F2737AF855ED68'
 
 $packageArgs = @{
   packageName    = $packageName
@@ -11,8 +10,6 @@ $packageArgs = @{
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/quiet /qn /norestart'
   softwareName   = 'AllWemo*'
-  checksum       = $checksum
-  checksumType   = 'sha256' 
 }
 
 Install-ChocolateyPackage @packageArgs
