2e9f62849fc1f502270574a7949127c5619fb194
bcurran3 <bcurran3@yahoo.com>
2019-03-18 21:31:26 -0700

    v1.03.5016

--- pst-merger/tools/chocolateyinstall.ps1
+++ pst-merger/tools/chocolateyinstall.ps1
@@ -1,10 +1,11 @@
-﻿$packageName    = 'pst-merger' 
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'pst-merger' 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'http://d.4team.biz/files/pstmerger_setup.exe'
-$checksum       = '83875C466B4B80A6ADDD2026793122CAC9213D1F7851CAB04756C77704AFD4D5'
+$checksum       = '2EBFAFC74FDB4B1943423B8FAD4447887237F084427767CC022B827212A92023'
 $validExitCodes = @(0,2)
 $ahkExe         = 'AutoHotKey'
-$ahkFile        = Join-Path $toolsDir "PSTMergerInstall.ahk"
+$ahkFile        = "$toolsDir\PSTMergerInstall.ahk"
 
 Start-Process $ahkExe $ahkFile
 
@@ -21,4 +22,3 @@ $packageArgs = @{
   }
   
 Install-ChocolateyPackage @packageArgs
-
