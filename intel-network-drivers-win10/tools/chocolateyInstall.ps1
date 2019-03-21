611bfb50115fe023afd204bf31057b4a56f0b934
bcurran3 <bcurran3@yahoo.com>
2019-03-20 19:12:16 -0700

    v10.23.5.2

--- intel-network-drivers-win10/tools/chocolateyInstall.ps1
+++ intel-network-drivers-win10/tools/chocolateyInstall.ps1
@@ -1,24 +1,23 @@
-﻿$packageName    = 'intel-network-drivers-win10' 
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'intel-network-drivers-win10' 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://downloadmirror.intel.com/25016/eng/PROWin32.exe'
-$checksum       = '16095E66C7991085129B7045C4211955D927041F9B632B88EA53FE10240309FA'
-$url64          = 'https://downloadmirror.intel.com/25016/eng/PROWinx64.exe'
-$checksum64     = '0910834F19EE4C446756BCE3FFEF92521E43B76FB60831AE9C9AFC2E6864BD67'
-$silentArgs     = '/qn'
-$validExitCodes = @(0)
 $unzipLocation  = "$toolsDir\unzippedfiles"
 $bits           = Get-ProcessorBits
+$url            = 'https://downloadmirror.intel.com/25016/eng/PROWin32.exe'
+$checksum       = 'F7CBF8EC152B31F2AC10FF4865B54F16A24C618836A381358C072ECDA0680A73'
+$url64          = 'https://downloadmirror.intel.com/25016/eng/PROWinx64.exe'
+$checksum64     = 'FEB0C221737406A4859AEBC2C8D6F676EA9CC32619E1FAE46A3298546D0B4D57'
 
-$card = wmic path win32_NetworkAdapter get name
-if ($card -match "Intel")
+
+if (Get_IsIntelVideo)
   {
-   write-host "You've got $card!" -foreground "green" –backgroundcolor "blue"
+   Write-Host "Intel network adapter found." -Foreground Magenta
   } else {
-   write-host "No Intel network adapters found. Aborting." -foreground "red" –backgroundcolor "blue"
+   Write-Host "No Intel network adapters found. Aborting." -Foreground Red
    throw
    }
 
-New-Item $unzipLocation -type directory | out-null
+New-Item $unzipLocation -Type Directory | Out-Null
 
 $packageArgs = @{
   packageName    = $packageName
@@ -45,8 +44,8 @@ $packageArgs = @{
   packageName    = $packageName
   fileType       = 'EXE'
   file           = $url
-  silentArgs     = $silentArgs
-  validExitCodes = $validExitCodes
+  silentArgs     = '/qn'
+  validExitCodes = @(0)
   softwareName   = 'Intel(R) Network Connections*'
 }
  
@@ -54,6 +53,4 @@ Install-ChocolateyInstallPackage @packageArgs
 
 Start-Sleep -s 10
 
-Remove-Item $unzipLocation -recurse | out-null
-
-
+Remove-Item $unzipLocation -Recurse | Out-Null
