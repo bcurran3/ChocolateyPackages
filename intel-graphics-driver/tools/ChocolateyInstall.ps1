9b67465c8a700e2ff76d29135dcd277089159f3e
bcurran3 <bcurran3@yahoo.com>
2019-03-25 15:40:54 -0700

    cleanup and ready for new version

--- intel-graphics-driver/tools/ChocolateyInstall.ps1
+++ intel-graphics-driver/tools/ChocolateyInstall.ps1
@@ -1,9 +1,10 @@
-﻿$packageName = 'intel-graphics-driver' 
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'intel-graphics-driver' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = 'https://downloadmirror.intel.com/28180/a08/win64_25.20.100.6323.exe'
-$checksum64  = '70D750971905632F3F476C595032B7A0FEC1C9B7D9599FE4291D961FB7D48BDC'
+$url64       = 'https://downloadmirror.intel.com/28567/a08/dch_win64_25.20.100.6577.exe'
+$checksum64  = 'FAE0769994067E99A0EFD0F55C17A4177EE06D3C0626C41BE878FE830A22EE7D'
 $ahkExe      = 'AutoHotKey'
-$ahkFile     = "$toolsDir\IGDInstall.ahk"
+$ahkFile     = "$toolsDir\intel-graphics-driver_install.ahk"
 
 if (!(Get-IsWin10)){
     Write-Warning "  ** This version is only for Windows 10."
@@ -23,7 +24,7 @@ if (!(Get-IsIntelVideo)){
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = 'exe' 
+  fileType      = 'EXE' 
   url           = ''
   checksum      = ''
   checksumType  = ''  
