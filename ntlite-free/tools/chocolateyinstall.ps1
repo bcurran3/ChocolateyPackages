f01951b90857d98ea7d5d25bcc1352023ee0921c
bcurran3 <bcurran3@yahoo.com>
2019-03-25 17:56:53 -0700

    v1.7.4.6790

--- ntlite-free/tools/chocolateyinstall.ps1
+++ ntlite-free/tools/chocolateyinstall.ps1
@@ -2,22 +2,22 @@
 $packageName    = 'ntlite-free'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'http://downloads.ntlite.com/files/NTLite_setup_x86.exe' 
-$checksum       = 'D6830366FF93499715264C11CFC3C40FD71BEE10D892BE189EE4F44C806A0695'
+$checksum       = '7FD4A7DC66A3CD13690420500101760DD2E6073DDC8897F61746D9260EA0CB3E'
 $url64          = 'http://downloads.ntlite.com/files/NTLite_setup_x64.exe' 
-$checksum64     = '9C0EA3F3897408D47C8F864089AB3F47BD525FB71DDF4F0CAD9B10B537BC6C4A'
+$checksum64     = '509A8BE21A06A7B9C9F23C6BD914B471E23461E00C1C259DF4DEEF1F812F4086'
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'EXE'
-  url           = $url
-  url64bit      = $url64
-  validExitCodes= @(0)
-  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  softwareName  = 'NTLite*'
-  checksum      = $checksum 
-  checksumType  = 'sha256' 
-  checksum64    = $checksum64
-  checksumType64= 'sha256'
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  url64bit       = $url64
+  validExitCodes = @(0)
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  softwareName   = 'NTLite*'
+  checksum       = $checksum 
+  checksumType   = 'sha256' 
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
