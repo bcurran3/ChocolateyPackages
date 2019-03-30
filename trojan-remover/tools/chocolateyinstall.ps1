8fcf16e0ea5dcf9bdbbd760ac90119112e4b86f2
bcurran3 <bcurran3@yahoo.com>
2019-03-30 13:02:57 -0700

    checksum correction

--- trojan-remover/tools/chocolateyinstall.ps1
+++ trojan-remover/tools/chocolateyinstall.ps1
@@ -2,20 +2,18 @@
 $packageName    = 'trojan-remover'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://simplysup.co.uk/download/dl/trjsetup695.exe' 
-$checksum       = '526B5C905B4EEB58457F029A5F3EFC8851E0566C457CB809C768C9A176041285'
-$silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
-$validExitCodes = @(0) 
+$checksum       = '3DEF7DE5A01E9649CE818C5CF2AA91F6CA1829DFE09128BBC2E266E5D6BE85AB'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE' 
-  url           = $url
-  silentArgs    = $silentArgs  
-  validExitCodes= $validExitCodes
-  softwareName  = 'Trojan Remover*'
-  checksum      = $checksum
-  checksumType  = 'sha256'   
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE' 
+  url            = $url
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'   
+  validExitCodes = @(0) 
+  softwareName   = 'Trojan Remover*'
+  checksum       = $checksum
+  checksumType   = 'sha256'   
 }
 
 Install-ChocolateyPackage @packageArgs
