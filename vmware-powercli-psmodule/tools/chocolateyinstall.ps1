ddd339b088599739c34e9e820ca68fd209cdd77d
bcurran3 <bcurran3@yahoo.com>
2019-03-31 13:22:45 -0700

    removed unnecessary Import-Module command and added .SkipAutoUninstaller file

--- vmware-powercli-psmodule/tools/chocolateyinstall.ps1
+++ vmware-powercli-psmodule/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $packageName = 'vmware-powercli-psmodule'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $shortcutName = 'VMware.PowerCLI.lnk'
-$exe          = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
+$exe          = "$ENV:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
 
 $PSversion = $PSVersionTable.PSVersion.Major
 if ($PSversion -lt "5")
@@ -15,12 +15,11 @@ if ($PSversion -lt "5")
 
 if (Get-Module -ListAvailable -Name VMware.PowerCLI -ErrorAction SilentlyContinue){
 # Will fail if packageVersion is a revised version not matching module version, i.e. x.x.x.0020180101
-     Update-Module "VMware.PowerCLI" -RequiredVersion "$env:packageVersion" -Force
+     Update-Module "VMware.PowerCLI" -RequiredVersion "$ENV:packageVersion" -Force
    } else {   
      Get-PackageProvider -Name NuGet -Force
-     Install-Module -Name VMware.PowerCLI -Scope AllUsers -RequiredVersion $env:packageVersion -AllowClobber -Force
-     Import-Module VMware.PowerCLI
+     Install-Module -Name VMware.PowerCLI -Scope AllUsers -RequiredVersion $ENV:packageVersion -AllowClobber -Force
    }
 
-Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath $exe -Arguments '-noe -c "Import-Module VMware.PowerCLI"' -WorkingDirectory "$env:SystemDrive\" -Description 'VMware.PowerCLI' -IconLocation "$toolsDir\powercli.ico" -RunAsAdmin
-Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath $exe -Arguments '-noe -c "Import-Module VMware.PowerCLI"' -WorkingDirectory "$env:SystemDrive\" -Description 'VMware.PowerCLI' -IconLocation "$toolsDir\powercli.ico" -RunAsAdmin
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\$shortcutName" -targetPath $exe -Arguments '-noe -c "Import-Module VMware.PowerCLI"' -WorkingDirectory "$ENV:SystemDrive\" -Description 'VMware.PowerCLI' -IconLocation "$toolsDir\powercli.ico" -RunAsAdmin
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath $exe -Arguments '-noe -c "Import-Module VMware.PowerCLI"' -WorkingDirectory "$ENV:SystemDrive\" -Description 'VMware.PowerCLI' -IconLocation "$toolsDir\powercli.ico" -RunAsAdmin
