d776ef5ba69c898559f1c08b6d430ff9673f8ba4
bcurran3 <bcurran3@yahoo.com>
2019-03-31 11:14:16 -0700

    v24.2.46.0

--- take-command/tools/chocolateyinstall.ps1
+++ take-command/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $packageName = 'take-command'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url         = 'https://jpsoft.com/downloads/v24/tcmd.exe'
-$checksum    = 'C9609570541E7023B6CB413634DEF23CD67FEC87AAD4063BB07B15C7E8DE0262'
+$checksum    = 'B4FFE2C6FEA2D4D373EBD7460357E7A742913FC2EA031ADD5FFD0794F9C44F22'
 
 
 $packageArgs = @{
@@ -13,7 +13,7 @@ $packageArgs = @{
   checksum       = $checksum 
   checksumType   = 'sha256'
   softwareName   = 'Take Command*'
-  silentArgs     = '/PASSIVE'
+  silentArgs     = '/Q'
   validExitCodes = @(0) 
 }
 
