c211887bab86fe7ee833741edc47076bae8a27c6
bcurran3 <bcurran3@yahoo.com>
2019-03-31 13:22:45 -0700

    removed unnecessary Import-Module command and added .SkipAutoUninstaller file

--- vmware-powercli-psmodule/tools/chocolateyuninstall.ps1
+++ vmware-powercli-psmodule/tools/chocolateyuninstall.ps1
@@ -4,5 +4,5 @@ $shortcutName = 'VMware.PowerCLI.lnk'
 
 Get-InstalledModule -Name "VMware.*" |  Uninstall-Module -AllVersions -Force
 
-Remove-Item "$env:Public\Desktop\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
-Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
+Remove-Item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
