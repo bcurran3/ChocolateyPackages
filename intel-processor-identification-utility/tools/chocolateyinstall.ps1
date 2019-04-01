caf3d297895b92fb918d93b6c31e4bc44f160389
bcurran3 <bcurran3@yahoo.com>
2019-04-01 11:42:50 -0700

    v6.0.0211

--- intel-processor-identification-utility/tools/chocolateyinstall.ps1
+++ intel-processor-identification-utility/tools/chocolateyinstall.ps1
@@ -1,21 +1,18 @@
-﻿$packageName    = 'intel-processor-identification-utility'
-$installerType  = 'msi'
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'intel-processor-identification-utility'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://downloadmirror.intel.com/7838/eng/pidenu47.msi' 
-$checksum       = '89883607CDFBF447FC9AE74BEBAAB57C20EF8101620D59791EBEE256B467172C'
-$silentArgs     = '/quiet /norestart'
-$validExitCodes = @(0, 3010, 1641)
+$url            = 'https://downloadmirror.intel.com/28539/a08/Intel(R)%20Processor%20Identification%20Utility.exe'
+$checksum       = '7D5F3256F8DE62019575CA99C6315C79B26F4E0C8C45C7298278BF9E4A78EE07'
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = $installerType
-  url           = $url
-  validExitCodes= $validExitCodes
-  silentArgs    = $silentArgs
-  softwareName  = 'Intel(R) Processor Identification Utility'
-  checksum      = $checksum
-  checksumType  = 'sha256' 
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/quiet /norestart'
+  softwareName   = 'Intel(R) Processor Identification Utility'
+  checksum       = $checksum
+  checksumType   = 'sha256' 
 }
 
 Install-ChocolateyPackage @packageArgs  
-  
