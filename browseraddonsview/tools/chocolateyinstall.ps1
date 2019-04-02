b16c0991c5675840f71895bcb31a287c4f6c4643
bcurran3 <bcurran3@yahoo.com>
2019-04-01 22:27:29 -0700

    v1.16

--- browseraddonsview/tools/chocolateyinstall.ps1
+++ browseraddonsview/tools/chocolateyinstall.ps1
@@ -1,25 +1,23 @@
-﻿$packageName  = 'browseraddonsview' 
+﻿$ErrorActionPreference = 'Stop'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'http://www.nirsoft.net/utils/browseraddonsview.zip'
-$checksum     = '06DEB3A5B082DF68F2AEC330AD59D10D7210440ED63519FDC0A9A1F319E6B547'
-$url64        = 'http://www.nirsoft.net/utils/browseraddonsview-x64.zip'
-$checksum64   = 'BF75AB6CF9E32EEF7B1C95EE166986B4B3D0359D2A7B7CEC844C1A6C09777E1D'
+$url          = "$toolsDir\browseraddonsview.zip"
+$url64        = "$toolsDir\browseraddonsview-x64.zip"
 $shortcutName = 'BrowserAddonsView.lnk'
 $exe          = 'BrowserAddonsView.exe'
 
 $packageArgs = @{
-  packageName    = $packageName
-  unzipLocation  = $toolsDir
-  fileType       = 'ZIP' 
-  url            = $url
-  checksum       = $checksum
-  checksumType   = 'sha256'
-  url64          = $url64
-  checksum64     = $checksum64
-  checksumType64 = 'sha256'  
-}
+  packageName    = 'browseraddonsview'
+  softwareName   = ''
+  fileType       = 'ZIP'
+  UnzipLocation  = "$toolsDir"
+  silentArgs     = ''
+  file           = $url
+  file64         = $url64
+  validExitCodes = @(0)
+  }
 
 Install-ChocolateyZipPackage @packageArgs
 
-Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath "$toolsDir\$exe" -WorkingDirectory "$toolsDir"
-Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$toolsDir\$exe"    
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\$shortcutName" -targetPath "$toolsDir\$exe" -WorkingDirectory "$toolsDir"
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$toolsDir\$exe"    
+Remove-Item $toolsDir\*.zip -Force | Out-Null
--- browseraddonsview/tools/chocolateyuninstall.ps1
+++ browseraddonsview/tools/chocolateyuninstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName  = 'browseraddonsview' 
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'browseraddonsview' 
 $shortcutName = 'BrowserAddonsView.lnk'
 
-remove-item "$env:Public\Desktop\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
-remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
-
+Remove-Item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
