8fc28ca1c84426666825460ba2308afc86af7343
bcurran3 <bcurran3@yahoo.com>
2019-04-03 14:07:28 -0700

    updated dl link, added backup+restore config on same ver upgrade

--- streambaby/tools/chocolateyuninstall.ps1
+++ streambaby/tools/chocolateyuninstall.ps1
@@ -1,4 +1,5 @@
-﻿$packageName = 'streambaby'
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'streambaby'
 
 remove-item "$env:Public\Desktop\$packageName.lnk" -Force -ErrorAction 'SilentlyContinue'
 remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$packageName.lnk" -Force -ErrorAction 'SilentlyContinue'
