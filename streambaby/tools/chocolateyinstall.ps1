b405e20e2a235b162d7971d967bfe0c62d47ec6f
bcurran3 <bcurran3@yahoo.com>
2019-04-03 14:07:28 -0700

    updated dl link, added backup+restore config on same ver upgrade

--- streambaby/tools/chocolateyinstall.ps1
+++ streambaby/tools/chocolateyinstall.ps1
@@ -1,10 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'
-
 $packageName = 'streambaby'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://cytranet.dl.sourceforge.net/project/streambaby/streambaby-0.54.zip'
+$url         = 'https://downloads.sourceforge.net/project/streambaby/streambaby-0.54.zip'
 $checksum    = '934106829C63B1EBED70C80B15CFB66A2E9B145385060F706E818876168055C0'
-$oldINI      = '$toolsDir\streambaby-0.53\streambaby.ini'
+
+if (Test-Path "$ENV:ChocolateyInstall\lib-bkp\streambaby\tools\streambaby-0.54\streambaby.ini")
+    {
+     Write-Host "  ** Backuping up previous v0.54 streambaby.ini." -Foreground Magenta
+	 Copy-Item "$ENV:ChocolateyInstall\lib-bkp\streambaby\tools\streambaby-0.54\streambaby.ini" "$ENV:Temp" -Force
+    }
 
 $packageArgs = @{
   packageName   = $packageName
@@ -17,16 +21,15 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 
-Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\streambaby.lnk" -targetPath "$toolsDir\streambaby-0.54\streambaby.bat" -iconLocation "$toolsDir\streambaby.ico"
-Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\streambaby.lnk" -targetPath "$toolsDir\streambaby-0.54\streambaby.bat" -iconLocation "$toolsDir\streambaby.ico"
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\streambaby.lnk" -targetPath "$toolsDir\streambaby-0.54\streambaby.bat" -iconLocation "$toolsDir\streambaby.ico"
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\streambaby.lnk" -targetPath "$toolsDir\streambaby-0.54\streambaby.bat" -iconLocation "$toolsDir\streambaby.ico"
 
-If (Test-Path $oldINI)
+if (Test-Path "$ENV:Temp\streambaby.ini")
     {
-     Write-Host "Restoring previous streambaby.ini" -foreground "magenta" –backgroundcolor "blue"
-     Copy-Item $oldINI "$toolsDir\streambaby-0.54\"
-    }Else{
-	 Write-Host "No previous streambaby.ini found." -foreground "magenta" –backgroundcolor "blue"
-   }
+     Write-Host "  ** Restoring previous v0.54 streambaby.ini." -Foreground Magenta
+	 Rename-Item "$toolsDir\streambaby-0.54\streambaby.ini" "streambaby.ini.default"
+     Move-Item "$ENV:Temp\streambaby.ini" "$toolsDir\streambaby-0.54\"
+    }
    
 $WhoAmI=whoami
-icacls.exe $toolsDir /grant $WhoAmI":"'(OI)(CI)'F /T | out-null
+icacls.exe $toolsDir /grant $WhoAmI":"'(OI)(CI)'F /T | Out-Null
