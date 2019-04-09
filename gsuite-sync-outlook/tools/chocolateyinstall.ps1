be56bcb7f9c8dcc463181f49ef572dfb929ad20f
bcurran3 <bcurran3@yahoo.com>
2019-04-09 00:13:51 -0700

    v4.0.19.0

--- gsuite-sync-outlook/tools/chocolateyinstall.ps1
+++ gsuite-sync-outlook/tools/chocolateyinstall.ps1
@@ -1,26 +1,25 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName   = 'gsuite-sync-outlook'
-$installerType = 'EXE'
-$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url           = 'https://dl.google.com/tag/s/appguid%3D%7BBEBCAD10-F1BC-4F92-B4A7-9E2545C809ED%7D%26iid%3D%7B763A8E04-9BD6-AD60-8318-E6C4E060C50F%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Apps%2520Sync%25E2%2584%25A2%2520for%2520Microsoft%2520Outlook%25C2%25AE%26needsadmin%3Dtrue%26appguid%3D%7B7DF3B6EE-9890-4307-BDE5-E1F3FCB09771%7D%26appname%3DGoogle%2520Apps%2520Migration%25E2%2584%25A2%2520for%2520Microsoft%2520Outlook%25C2%25AE%26needsadmin%3Dtrue/google-apps-sync/googleappssyncsetup.exe' 
-$checksum      = '569F13CA1B8951323F47236FB4F872A592465F2094006D025AAA48E6FEE991C3'
-$silentArgs    = ''
-$validExitCodes= @(0)
-$ahkExe        = 'AutoHotKey'
-$ahkFile       = Join-Path $toolsDir "GSSMO.ahk"
+$packageName    = 'gsuite-sync-outlook'
+$installerType  = 'EXE'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://dl.google.com/tag/s/appguid%3D%7BBEBCAD10-F1BC-4F92-B4A7-9E2545C809ED%7D%26iid%3D%7BE094FE53-B836-8E02-5B87-DA45F95AD839%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DG%2520Suite%2520Sync%25E2%2584%25A2%2520for%2520Microsoft%2520Outlook%25C2%25AE%26needsadmin%3Dtrue%26appguid%3D%7B7DF3B6EE-9890-4307-BDE5-E1F3FCB09771%7D%26appname%3DG%2520Suite%2520Migration%25E2%2584%25A2%2520for%2520Microsoft%2520Outlook%25C2%25AE%26needsadmin%3Dtrue/google-apps-sync/googleappssyncsetup.exe' 
+$checksum       = '569F13CA1B8951323F47236FB4F872A592465F2094006D025AAA48E6FEE991C3'
+$silentArgs     = ''
+$validExitCodes = @(0)
+$ahkExe         = 'AutoHotKey'
+$ahkFile        = "$toolsDir\GSSMO.ahk"
 
 Start-Process $ahkExe $ahkFile
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'EXE' 
-  url           = $url
-  softwareName  = 'Google Apps Sync*' 
-  checksum      = $checksum
-  checksumType  = 'sha256'
-  silentArgs    = $silentArgs
-  validExitCodes= $validExitCodes
+  packageName    = $packageName
+  fileType       = 'EXE' 
+  url            = $url
+  softwareName   = 'Google Apps Sync*' 
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  silentArgs     = $silentArgs
+  validExitCodes = $validExitCodes
 }
 
 Install-ChocolateyPackage @packageArgs 
-  
