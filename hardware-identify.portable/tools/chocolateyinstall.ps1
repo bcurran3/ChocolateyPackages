98f38365b7bda87ab4a06369ef65e2f5d59b9a5d
Bill Curran <bcurran3@yahoo.com>
2019-04-24 12:04:35 -0700

    v2.5.0

--- hardware-identify.portable/tools/chocolateyinstall.ps1
+++ hardware-identify.portable/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $packageName= 'hardware-identify.portable'
 $installerType = 'ZIP'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://www.tweaking.com/files/setups/tweaking.com_hardware_identify_portable.zip' 
-$checksum   = 'B0F94563267737E8B32FBCB1068ED56CD5E7F6B5E733C0A4BE99558CA50B4B7E'
+$checksum   = 'E9C3DED380F1B3562F6AF65221EF07D956F7D4DD152F2913CFC2E657F091455A'
 
 
 $packageArgs = @{
@@ -17,7 +17,5 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 
-Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\Hardware Identify.lnk" -targetPath "$ToolsDir\HardwareIdentify.exe"
-Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Hardware Identify.lnk" -targetPath "$ToolsDir\HardwareIdentify.exe"
-
-  
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\Hardware Identify.lnk" -targetPath "$ToolsDir\HardwareIdentify.exe"
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\Hardware Identify.lnk" -targetPath "$ToolsDir\HardwareIdentify.exe"
