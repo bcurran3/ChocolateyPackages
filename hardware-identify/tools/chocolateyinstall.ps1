290860fe07d34186ea36941428e22ca7cc19475e
Bill Curran <bcurran3@yahoo.com>
2019-04-24 11:54:04 -0700

    v2.5.0

--- hardware-identify/tools/chocolateyinstall.ps1
+++ hardware-identify/tools/chocolateyinstall.ps1
@@ -1,23 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName= 'hardware-identify'
-$softwareName = 'Tweaking.com - Hardware Identify*'
-$installerType = 'EXE'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://www.tweaking.com/files/setups/tweaking.com_hardware_identify_setup.exe' 
-$checksum   = '7A242007EF5A46E9CF003EBA91143FEBA25F4B888CEF13170DECA5465E7ED6E2'
-$silentArgs = '/S'
-$validExitCodes= @(0)
+$checksum   = '6E593062BB5EA311EDE97DD6381FD7658E12A267991CDFE501D29446B7498ECB'
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = $installerType
-  url           = $url
-  validExitCodes= $validExitCodes
-  silentArgs    = $silentArgs
-  softwareName  = $softwareName
-  checksum      = $checksum
-  checksumType  = 'sha256' 
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  validExitCodes = @(0)
+  silentArgs     = '/S'
+  softwareName   = 'Tweaking.com - HardwareIdentify *'
+  checksum       = $checksum
+  checksumType   = 'sha256' 
 }
 
 Install-ChocolateyPackage @packageArgs  
-  
