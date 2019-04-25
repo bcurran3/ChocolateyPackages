11db5dce497244d48ae479260535c9ca935dac55
Bill Curran <bcurran3@yahoo.com>
2019-04-25 11:56:46 -0700

    v3.0.1001

--- osfmount/tools/chocolateyinstall.ps1
+++ osfmount/tools/chocolateyinstall.ps1
@@ -1,11 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName= 'osfmount' 
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://www.osforensics.com/downloads/osfmount.exe'
-$checksum   = 'A7085A13E5FBDCD7BB1D34AEFE291446F01DA1413235908B3242079557C72F7B'
-$url64      = 'http://www.osforensics.com/downloads/osfmount_x64.exe'
-$checksum64 = 'B466B7E6AFD66192F6F0E2BFB738874E428E7D09980E65AFAB0B851CD684B335'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+$packageName = 'osfmount' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$bits        = Get-ProcessorBits
+$url         = 'http://www.osforensics.com/downloads/osfmount.exe'
+$checksum    = '5348838DD60607057DBD1448E5173D1C4C6280B82AFE9967A837499822FC8C5D'
+
+if ($bits -eq 32)
+   {
+	Write-Host "  ** OSFMount v3+ is 64 bit only. Please try v2. Aborting." -Foreground Red
+	throw
+   }
 
 $packageArgs = @{
   packageName   = $packageName
@@ -17,7 +21,8 @@ $packageArgs = @{
   url64bit      = $url64
   checksum64    = $checksum64
   checksumType64= 'sha256'
-  silentArgs    = $silentArgs
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
   softwareName  = 'OSFMount*' 
   }
+
 Install-ChocolateyPackage @packageArgs
