ddfa6be2c11b2b5d7b416a651c22bdfe8b46f994
Bill Curran <bcurran3@yahoo.com>
2019-04-26 10:18:20 -0700

    v17.2.0.0

--- pdq-deploy/tools/chocolateyinstall.ps1
+++ pdq-deploy/tools/chocolateyinstall.ps1
@@ -1,12 +1,10 @@
-﻿$ErrorActionPreference = 'Stop'
+﻿#https://link.pdq.com/DownloadLatest?Product=pdqdeploy
+$ErrorActionPreference = 'Stop'
 $packageName    = 'pdq-deploy'
-$softwareName   = 'PDQ Deploy*'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://aafiles.blob.core.windows.net/files/PDQDeploy_13_3_0_0_edc3b0ab-58fc-4992-b8db-29ff282bad9c.zip'
-$checksum       = '9BF64ABB1BA80265B4BB271D36C74953ABC4F4B2634F915A900B7DAE456B67D5'
-$silentArgs     = '/S'
-$validExitCodes = @(0)
-$fileLocation   = "$toolsDir\PDQDeploy.13.3.0.0.exe"
+$url            = 'https://aafiles.blob.core.windows.net/files/PDQDeploy_17_2_0_0_fef0497c-d819-47b9-b115-c8e41f9fad94.zip'
+$checksum       = '0739657360FFFCA54B706D92B6175931B1EAE3E319EDB2A68E16C2F686DD0017'
+$fileLocation   = "$toolsDir\PDQDeploy.17.2.0.0.exe"
 
 $packageArgs = @{
   packageName   = $packageName
@@ -20,13 +18,12 @@ $packageArgs = @{
 Install-ChocolateyZipPackage @packageArgs
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'EXE'
-  file          = $fileLocation
-  silentArgs    = $silentArgs
-  validExitCodes= $validExitCodes
-  softwareName  = $softwareName
+  packageName    = $packageName
+  fileType       = 'EXE'
+  file           = $fileLocation
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'PDQ Deploy*'
 }
  
 Install-ChocolateyInstallPackage @packageArgs
-  
