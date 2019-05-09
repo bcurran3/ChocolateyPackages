72e0d484f5bba991d0f886ba14684d1906b2af63
Bill Curran <bcurran3@yahoo.com>
2019-05-08 17:23:47 -0700

    v4.6.3

--- backupper-standard/tools/chocolateyinstall.ps1
+++ backupper-standard/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $packageName   = 'backupper-standard'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url           = 'https://www2.aomeisoftware.com/download/adb/BackupperFull.exe'
-$checksum      = 'F79DB3C27F9473FD3D20745AB4CF4F39B5E569B6393C8562B9FA04B64734F6CE'
+$checksum      = '7DCABDBACED24ADA789B0FC084BB3AE8889420A06BD33B6DF72E439B3C0892BF'
 
 $packageArgs = @{
   packageName   = $packageName
