3df5e64005654054cafd3232677db3b0a4cffe0a
Bill Curran <bcurran3@yahoo.com>
2019-05-21 10:14:15 -0700

    v5.6.1

--- beebeep/tools/chocolateyinstall.ps1
+++ beebeep/tools/chocolateyinstall.ps1
@@ -1,20 +1,21 @@
-﻿$packageName  = 'beebeep' 
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'beebeep'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://downloads.sourceforge.net/project/beebeep/Windows/beebeep-4.0.0.zip'
-$checksum     = '75DAC37FF7C77213EDF2FCFDFAF928B1E5FE35C3ED052581B01455A93A32D60D'
+$url          = "$toolsDir\beebeep-" + "$ENV:ChocolateyPackageVersion" + ".zip"
+$unzipDir     = "beebeep-" + "$ENV:ChocolateyPackageVersion"
 $ShortcutName = 'beeBEEP'
-$unzipDir     = 'beebeep-4.0.0'
 $exe          = 'beeBEEP.exe'
-
+ 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'zip' 
-  url           = $url
-  checksum      = $checksum
-  checksumType  = 'sha256'  
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP'
+  url            = $url
   }
-
+ 
 Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$ShortcutName.lnk" -targetPath "$toolsDir\$unzipDir\$exe" -WorkingDirectory "$toolsDir\$unzipDir"
-Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName.lnk" -targetPath "$toolsDir\$unzipDir\$exe" -WorkingDirectory "$toolsDir\$unzipDir"
+ 
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\$ShortcutName.lnk" -targetPath "$toolsDir\$unzipDir\$exe" -WorkingDirectory "$toolsDir\$unzipDir"
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName.lnk" -targetPath "$toolsDir\$unzipDir\$exe" -WorkingDirectory "$toolsDir\$unzipDir"
+ 
+Remove-Item $url | Out-Null
