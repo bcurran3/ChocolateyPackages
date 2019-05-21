293d30bf84779d3af8cc0943919faef460996552
Bill Curran <bcurran3@yahoo.com>
2019-05-21 10:14:15 -0700

    v5.6.1

--- beebeep/tools/chocolateyuninstall.ps1
+++ beebeep/tools/chocolateyuninstall.ps1
@@ -1,5 +1,6 @@
-﻿$packageName  = 'beebeep'
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'beebeep'
 $ShortcutName = 'beeBEEP'
 
-remove-item "$env:Public\Desktop\$ShortcutName.lnk" -Force -ErrorAction 'SilentlyContinue'
-remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName.lnk" -Force -ErrorAction 'SilentlyContinue'
+Remove-Item "$ENV:Public\Desktop\$ShortcutName.lnk" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName.lnk" -Force -ErrorAction SilentlyContinue
