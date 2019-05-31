5679fbede46eee6b61396aedeb888b81920a9efa
Bill Curran <bcurran3@yahoo.com>
2019-05-31 03:34:29 -0700

    1903

---
+++ dependency-windows10/dependency-windows10.1903/tools/ChocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'dependency-windows10'
+
+$IsWin10=[Environment]::OSVersion.Version.Major
+if ($IsWin10 -ne "10") 
+   {
+    Write-Warning "This package requires Windows 10."
+	throw
+   }
+
+$IsCorrectBuild=[Environment]::OSVersion.Version.Build
+if ($IsCorrectBuild -lt "18362")
+   {
+    Write-Warning "This package requires at least Windows 10 version 1903/OS build 18362.x."
+	throw
+   }
