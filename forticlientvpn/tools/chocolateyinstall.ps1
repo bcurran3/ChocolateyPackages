372c98f85e371d75a97b3b828e4434d5d9245924
Bill Curran <bcurran3@yahoo.com>
2019-05-31 03:15:19 -0700

    forticlientvpn v6.2.0780

---
+++ forticlientvpn/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName   = 'forticlientvpn'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url           = 'https://filestore.fortinet.com/forticlient/downloads/FortiClientVPNSetup_6.2.0.exe'
+$checksum      = '354E93E1B79AFE1F401F6FCDF5F0D84781C24AA30CC5443BC2C1AC87D1BA0188'
+$url64         = 'https://filestore.fortinet.com/forticlient/downloads/FortiClientVPNSetup_6.2.0_x64.exe'
+$checksum64    = '9202AF4D28A7306645A4B34DEA45EFBCFFE5C94B5DAC8333BC68185B63F88A16'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64         = $url64
+  silentArgs    = '/QUIET /NORESTART'
+  softwareName  = 'FortiClient VPN' 
+  checksum      = $checksum
+  checksum64    = $checksum64
+  checksumType  = 'sha256'
+  }
+
+Install-ChocolateyPackage @packageArgs
