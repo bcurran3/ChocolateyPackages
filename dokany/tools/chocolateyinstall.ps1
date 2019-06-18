da0154eddf14421a806605d355a476109a5d58d5
Bill Curran <bcurran3@yahoo.com>
2019-06-18 13:35:11 -0700

    1.2.2.1001

--- dokany/tools/chocolateyinstall.ps1
+++ dokany/tools/chocolateyinstall.ps1
@@ -1,9 +1,8 @@
-﻿$packageName    = 'dokany'
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'dokany'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/dokan-dev/dokany/releases/download/v1.2.1.2000/Dokan_x86.msi' 
-$checksum       = '19371DBFD65EF554C23E5C43A390686BB152F0A41F66D23FAB56213CB36EF333'
-$url64          = 'https://github.com/dokan-dev/dokany/releases/download/v1.2.1.2000/Dokan_x64.msi' 
-$checksum64     = '735FB096DB5CC1DD02A0D91951E2638CCA4F5F2F3A0AEEF9ABF270E2BD051196'
+$url            = "$toolsDir\Dokan_x86.msi" 
+$url64          = "$toolsDir\Dokan_x64.msi"
 
 $packageArgs = @{
   packageName   = $packageName
@@ -13,10 +12,8 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
   silentArgs    = '/passive /norestart'
   softwareName  = 'Dokan Library*'
-  checksum      = $checksum
-  checksumType  = 'sha256' 
-  checksum64    = $checksum64
-  checksumType64= 'sha256'
 }
 
-Install-ChocolateyPackage @packageArgs  
+Install-ChocolateyPackage @packageArgs
+Remove-Item $url -Force -EA SilentlyContinue | Out-Null
+Remove-Item $url64 -Force -EA SilentlyContinue | Out-Null
