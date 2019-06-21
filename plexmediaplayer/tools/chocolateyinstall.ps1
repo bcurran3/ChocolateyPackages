f6c2c80a58f563378d0e6cbd33fa765cbf55f0ef
aelius <git@flamingsteel.net>
2019-06-20 21:08:09 -0400

    plexmediaplayer tweak install script
    - Use chocolatey's internal bitness detection
    - Remove unused extra lines
    - Remove unneeded variables
    - Left certain parameters blank so maintainer may run update script immediately

--- plexmediaplayer/tools/chocolateyinstall.ps1
+++ plexmediaplayer/tools/chocolateyinstall.ps1
@@ -1,25 +1,14 @@
 ﻿# https://www.plex.tv/media-server-downloads/#plex-app
 $ErrorActionPreference = 'Stop'
-$packageName   = 'plexmediaplayer'
-$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$bits          = Get-ProcessorBits
-$url           = 'https://downloads.plex.tv/plexmediaplayer/2.27.0.949-542ba3ed/PlexMediaPlayer-2.27.0.949-542ba3ed-windows-x64.exe'
-$checksum      = '239EFD820E201BF51D3A6A2FE4F408030DEA5B1BF64018057E09CFDBA70B58EA'
-
-if ($bits -eq 32){
-    Write-Warning "This program only supports 64 bit OSes. Aborting..."
-    throw
-   }
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
-  silentArgs    = '/S'
-  softwareName  = 'Plex Media Player' 
-  checksum      = $checksum
-  checksumType  = 'sha256'  
- }
+  packageName    = 'plexmediaplayer'
+  fileType       = 'EXE'
+  url64          = ''
+  silentArgs     = '/S'
+  softwareName   = 'Plex Media Player' 
+  checksum64     = ''
+  checksumType64 = ''  
+}
 
 Install-ChocolateyPackage @packageArgs
