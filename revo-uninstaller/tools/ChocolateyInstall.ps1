dd76ba960e9d772b1a870523fba8bf32bf6b0e34
Bill Curran <bcurran3@yahoo.com>
2019-06-27 07:25:08 -0700

    v2.1.0.0

--- revo-uninstaller/tools/ChocolateyInstall.ps1
+++ revo-uninstaller/tools/ChocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 $packageName = 'revo-uninstaller'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url         = "$toolsDir\revosetup.exe"
