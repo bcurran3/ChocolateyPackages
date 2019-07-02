527dcb3ac90df65e07e0fc89763bb8f2975f6ea1
Bill Curran <bcurran3@yahoo.com>
2019-07-02 09:42:04 -0700

    v1.1.2

--- searchwithmybrowser/tools/chocolateyinstall.ps1
+++ searchwithmybrowser/tools/chocolateyinstall.ps1
@@ -1,19 +1,14 @@
-﻿$packageName   = 'searchwithmybrowser'
+﻿$ErrorActionPreference = 'Stop'
+$packageName   = 'searchwithmybrowser'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$installerType = 'exe'
-$url           = 'https://github.com/charlesmilette/SearchWithMyBrowser/releases/download/1.0.0/SWMB-setup.exe'
-$checksum      = '62008A92E7DC105F4D7EF8A62B312A4C1AFEA55865508CF3DBA457A4C67E5621'
-$silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 
 $packageArgs = @{
   packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = $installerType
-  url           = $url
-  silentArgs    = $silentArgs
+  fileType      = 'EXE'
+  file          = "$toolsDir\SWMB-setup.exe"
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
   softwareName  = 'SearchWithMyBrowser*' 
-  checksum      = $checksum
-  checksumType  = 'sha256'  
-  }
-Install-ChocolateyPackage @packageArgs
-
+}
+ 
+Install-ChocolateyInstallPackage @packageArgs
