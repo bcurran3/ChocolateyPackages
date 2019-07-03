a23aa2614a5b03272b4b2b4d4d3006b7898815d6
Bill Curran <bcurran3@yahoo.com>
2019-07-03 15:15:05 -0700

    v6.8.0.24296

--- hp-universal-print-driver-ps/tools/chocolateyinstall.ps1
+++ hp-universal-print-driver-ps/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName    = 'hp-universal-print-driver-ps' 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://ftp.hp.com/pub/softlib/software13/COL40842/ds-99375-21/upd-ps-x32-6.7.0.23989.exe'
-$checksum       = '5D941CA5C5BAA3FDC5F6009FC09E9C8C0F57DA4A3ABB32716437AD16A09F1E4D'
-$url64          = 'https://ftp.hp.com/pub/softlib/software13/COL40842/ds-99376-21/upd-ps-x64-6.7.0.23989.exe'
-$checksum64     = '00CEADC3C948E58A6BB7DE9AE0AA03FF7BD57EFAF87DD0106987DDF989ADC8F8'
+$url            = 'https://ftp.hp.com/pub/softlib/software13/COL40842/ds-99375-22/upd-ps-x32-6.8.0.24296.exe'
+$checksum       = '7F224595CF4EFBFE6C50B7541FCFB8841A62B304151219433E735897DB3B63D7'
+$url64          = 'https://ftp.hp.com/pub/softlib/software13/COL40842/ds-99376-22/upd-ps-x64-6.8.0.24296.exe'
+$checksum64     = '151FD3746DDD47A1B4DB9C98032F84AAB71AB2D2A957147D32C829421A22E9F6'
 $fileLocation   = "$toolsDir\unzippedfiles\install.exe"
 
 # Make sure Print Spooler service is up and running stolen from cutepdf package.
