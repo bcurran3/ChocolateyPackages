97c745e9b29c4ae6247b46ad5131d5394acb6163
Bill Curran <bcurran3@yahoo.com>
2019-07-03 15:33:27 -0700

    GetDataBack Pro v5.50

--- getdataback-simple/tools/chocolateyinstall.ps1
+++ getdataback-simple/tools/chocolateyinstall.ps1
@@ -1,22 +1,19 @@
-﻿$packageName    = 'getdataback-simple'
-$installerType  = 'exe'
-$url            = 'https://www.runtime.org/gdbsimsetup.exe'
-$checksum       = '54836F889CF154CDCC98CE7F2DEB39D3F87E6E479BFFB01F21C1479AB0203B81'
-$silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'getdataback-simple'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$validExitCodes = @(0)
-$softwareName   = 'GetDataBack Simple*'
+$url            = 'https://www.runtime.org/gdbprosetup.exe'
+$checksum       = 'B9DE4AC29615E6116377F23E6AAAA7B4C2E1A772580712721F813C76E4E80904'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir  
-  fileType      = $installerType
-  url           = $url
-  validExitCodes= $validExitCodes
-  silentArgs    = $silentArgs
-  softwareName  = $softwareName
-  checksum      = $checksum
-  checksumType  = 'sha256' 
+  packageName    = $packageName
+  unzipLocation  = $toolsDir  
+  fileType       = 'EXE'
+  url            = $url
+  validExitCodes = @(0)
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  softwareName   = 'GetDataBack*'
+  checksum       = $checksum
+  checksumType   = 'sha256' 
 }
 
 Install-ChocolateyPackage @packageArgs
