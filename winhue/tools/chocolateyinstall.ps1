fa81d5225d1e9f8c27cb9c60e11b295f9940b2a0
Bill Curran <bcurran3@yahoo.com>
2019-07-05 14:21:16 -0700

    v3.0.4718.0

--- winhue/tools/chocolateyinstall.ps1
+++ winhue/tools/chocolateyinstall.ps1
@@ -1,19 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName    = 'winhue'
-$installerType  = 'EXE'
-$url            = 'https://github.com/Hyrules/WinHue/releases/download/3.0.4334.0/WinHue.3.0.4334.0.Setup.exe' 
-$checksum       = '01DBC8735A8B854258CEEFA1229D27BD46901669FE0857B114E8090FC56B6A2A'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'EXE' 
-  url           = $url
-  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  validExitCodes= @(0)
-  softwareName  = 'WinHue*'
-  checksum      = $checksum
-  checksumType  = 'sha256'   
+  packageName    = $packageName
+  fileType       = 'EXE' 
+  file           = "$toolsDir\WinHue."+$ENV:ChocolateyPackageVersion+".Setup.exe"
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+  softwareName   = 'WinHue*'
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
   
