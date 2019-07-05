0607b325fa77528443820b7ef1c74b25af638603
Bill Curran <bcurran3@yahoo.com>
2019-07-05 09:44:08 -0700

    v1.0.0.23 updated and embedded

--- tiny-pxe-server/tools/chocolateyuninstall.ps1
+++ tiny-pxe-server/tools/chocolateyuninstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName  = 'tiny-pxe-server' 
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'tiny-pxe-server' 
 $shortcutName = 'TinyPXE Server.lnk'
 
-remove-item "$env:Public\Desktop\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
-remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction 'SilentlyContinue'
-
+Remove-Item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
