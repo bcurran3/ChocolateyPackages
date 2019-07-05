4e7a64dcf1627e2ac09098b03a66109032993389
Bill Curran <bcurran3@yahoo.com>
2019-07-05 08:40:51 -0700

    v3.4.7.0

--- xmedia-recode/tools/ChocolateyInstall.ps1
+++ xmedia-recode/tools/ChocolateyInstall.ps1
@@ -3,13 +3,18 @@ $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName   = 'xmedia-recode'
 $TodaysVersion = ($ENV:ChocolateyPackageVersion -replace '[.]','')
 $url           = "$toolsDir\XMediaRecode"+$TodaysVersion+"_setup.exe"
-
+$url64         = "$toolsDir\XMediaRecode"+$TodaysVersion+"_x64_setup.exe"
+ 
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'EXE'
   file           = $url
+  file64         = $url64
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
 }
-
+ 
 Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item $url | Out-Null
+Remove-Item $url64 | Out-Null
