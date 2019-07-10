e211f75d8e2380f83cab81f0282d790fece9fa50
Bill Curran <bcurran3@yahoo.com>
2019-07-10 11:57:45 -0700

    v1.0

---
+++ windows-sandbox/tools/ChocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'windows-sandbox'
+$OSBuild = Get-WinVerBuild
+$OSFound = (Get-WinName) + " " + (Get-WinVerMajor) + "." + (Get-WinVerMinor) + "." + (Get-WinVerBuild)
+Write-Host "  ** OS Found: $OSFound" -Foreground Magenta
+
+if ((Get-IsWinPro) -or (Get-IsWinEnt)) {
+    Enable-WindowsOptionalFeature -Online -FeatureName  Containers-DisposableClientVM -NoRestart |Out-Null
+    if (Get-PendingReboot) {
+	   Write-Warning "A reboot is required to use Windows Sandbox."
+	   }
+	} else {
+	  Write-Host "  ** Windows Sandbox requires Windows 10 Pro or Windows 10 Enterprise."
+	  throw
+	  }
