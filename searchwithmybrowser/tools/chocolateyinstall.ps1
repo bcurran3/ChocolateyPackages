e637841047f303eb5ea68820f4d63de3e6a65555
Bill Curran <bcurran3@yahoo.com>
2019-07-11 11:20:09 -0700

    removed unnecessary dependency

--- searchwithmybrowser/tools/chocolateyinstall.ps1
+++ searchwithmybrowser/tools/chocolateyinstall.ps1
@@ -3,12 +3,18 @@ $packageName   = 'searchwithmybrowser'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'EXE'
-  file          = "$toolsDir\SWMB-setup.exe"
-  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  validExitCodes= @(0)
-  softwareName  = 'SearchWithMyBrowser*' 
+  packageName    = $packageName
+  fileType       = 'EXE'
+  file           = "$toolsDir\SWMB-setup.exe"
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+  softwareName   = 'SearchWithMyBrowser*' 
 }
- 
-Install-ChocolateyInstallPackage @packageArgs
+
+New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT -ea SilentlyContinue | Out-Null
+if (Test-Path -Path "HKCR:microsoft-edge") {
+    Install-ChocolateyInstallPackage @packageArgs
+	} else {
+	  Write-Host "  ** Microsoft Edge not found or unsupported version of Windows" -ForeGround Red
+	  throw
+	  }
