af2fb586938c1d6a25a9fe1fed0760f00a0d6230
bcurran3 <bcurran3@yahoo.com>
2019-07-21 11:38:18 -0700

    v7.14.70

--- logitech-options/tools/chocolateyinstall.ps1
+++ logitech-options/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $ahkExe   = 'AutoHotKey'
 $ahkFile  = "$toolsDir\logitech-options_install.ahk"
-$url      = 'https://download01.logi.com/web/ftp/pub/techsupport/options/Options_7.12.43.exe'
-$checksum = 'CC96E05BF0731AC149E59DE4F776BC0E16DE02BD2169359C36ABEB7427460D0F'
+$url      = 'https://download01.logi.com/web/ftp/pub/techsupport/options/Options_7.14.70.exe'
+$checksum = '4279CBA62DA562A5952A0745E5AB9FE43BDAAF961BF3A08A8C1C95F32DF661FB'
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
