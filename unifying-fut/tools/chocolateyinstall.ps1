78273570163a6c8466d1fc27669984b6ee9a1fd1
Bill Curran <bcurran3@yahoo.com>
2019-08-01 08:51:22 -0700

    initial push, in progress

---
+++ unifying-fut/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ahkExe   = 'AutoHotKey'
+$ahkFile  = "$toolsDir\unifying-fut_install.ahk"
+$url      = 'https://aws13-customer-care-assets.s3.amazonaws.com/Software/SecureDFU/Win/SecureDFU_48.exe'
+$checksum = 'D0E7DF9F19AC7DD2BA3976995ED2461C5DC296D0E5DCB65C17DAD46AACAD44C0'
+
+$packageArgs = @{
+    packageName    = $ENV:ChocolateyPackageName
+    url            = $url
+    checkum        = $checksum
+    checksumType   = 'sha256'
+    fileType       = 'EXE'
+    silentArgs     = ''
+    validExitCodes = @(0)   
+}
+
+Start-Process $ahkExe $ahkFile
+Install-ChocolateyPackage @packageArgs
