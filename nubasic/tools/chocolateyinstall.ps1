751d7e8cf439c59dd0fe0dad94b3ad4aa8eb53a2
bcurran3 <bcurran3@yahoo.com>
2019-08-09 23:10:52 -0700

    hopefully good

--- nubasic/tools/chocolateyinstall.ps1
+++ nubasic/tools/chocolateyinstall.ps1
@@ -1,28 +1,23 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName    = 'nubasic' 
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$bits           = Get-ProcessorBits
-$url64          = "$toolsDir\nubasic-ide-"+$ENV:ChocolateyPackageVersion+"-setup_x64.zip"
-$ahkExe         = 'AutoHotKey'
-$ahkFile        = "$toolsDir\nubasic-install.ahk"
-$EXELocation    = "$toolsDir\setup-vs64.exe"
-
+$packageName = 'nubasic'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$bits        = Get-ProcessorBits
+$url64       = "$toolsDir\nubasic-ide-"+"$ENV:ChocolateyPackageVersion"+"-setup_x64.zip"
+$Installer   = "$toolsDir\setup-vs64.exe"
+ 
 Get-ChocolateyUnzip -FileFullPath $url64 -Destination $toolsDir
-
+ 
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'EXE'
-  file           = $EXELocation
+  file           = $Installer
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
   softwareName   = 'nuBASIC*'
 }
-
-Start-Process $ahkExe $ahkFile
-Write-Host "  ** Please wait up to 30 seconds to cancel Microsoft Visual C++ 2017 Redistributable install." -Foreground Magenta
-Write-Host "  ** Microsoft Visual C++ 2017 Redistributable should already be installed as a dependency." -Foreground Magenta
  
+Start-WaitandStop "vc_redist.x64"
 Install-ChocolateyInstallPackage @packageArgs
-
+ 
 Remove-Item $url64 -Force -ErrorAction SilentlyContinue
-Remove-Item $EXELocation -Force -ErrorAction SilentlyContinue
+Remove-Item $Installer -Force -ErrorAction SilentlyContinue
