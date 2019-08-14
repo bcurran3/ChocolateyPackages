c8c041637a49443c58f79d351a5f72ad07876ca3
bcurran3 <bcurran3@yahoo.com>
2019-08-13 22:18:00 -0700

    Orange C v6.0.42.1

---
+++ orangec/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$TodaysVersion = ($ENV:ChocolateyPackageVersion -replace '[.]','')
+$installer     = "$toolsDir\occ"+$TodaysVersion+".exe"
+
+$packageArgs = @{
+  packageName   = 'orangec'
+  fileType      = 'EXE'
+  file          = $installer
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  softwareName  = 'Orange C Version*' 
+  }
+  
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -Force | Out-Null
+Remove-Item $toolsDir\*.ignore -Force | Out-Null
