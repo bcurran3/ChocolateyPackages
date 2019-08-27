a7fd6a3213497e54081bb22898b078c73f9c188c
Bill Curran <bcurran@lacomputerbuddy.com>
2019-08-27 10:47:05 -0700

    v2.4.13

--- kmttg/tools/chocolateyinstall.ps1
+++ kmttg/tools/chocolateyinstall.ps1
@@ -1,22 +1,14 @@
-﻿$packageName = 'kmttg' 
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'kmttg' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://downloads.sourceforge.net/project/kmttg/kmttg_v2.4l.zip'
-$checksum    = '76B0C6BF7A2CE6163CD4379CD16503B53C6A9EA2D2DBA01C3811142A6256246B'
+$file        = "$toolsDir\kmttg_v2.4m.zip"
 
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'ZIP' 
-  url           = $url
-  checksum      = $checksum
-  checksumType  = 'sha256'
-}
+Get-ChocolateyUnzip -FileFullPath "$file" -Destination "$toolsDir"
 
-Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\kmttg.lnk" -targetPath "$toolsDir\kmttg.jar" -WorkingDirectory "$toolsDir" -IconLocation "$toolsDir\kmttg.ico"
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\kmttg.lnk" -targetPath "$toolsDir\kmttg.jar" -WorkingDirectory "$toolsDir" -IconLocation "$toolsDir\kmttg.ico"
 
-Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\kmttg.lnk" -targetPath "$toolsDir\kmttg.jar" -WorkingDirectory $toolsDir -IconLocation "$toolsDir\kmttg.ico"
-Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\kmttg.lnk" -targetPath "$toolsDir\kmttg.jar" -WorkingDirectory $toolsDir -IconLocation "$toolsDir\kmttg.ico"
+Remove-Item "$file" -Force | Out-Null
 
 $WhoAmI=whoami
-icacls.exe $toolsDir /grant $WhoAmI":"'(OI)(CI)'F /T | out-null
-
+icacls.exe $toolsDir /grant $WhoAmI":"'(OI)(CI)'F /T | Out-Null
