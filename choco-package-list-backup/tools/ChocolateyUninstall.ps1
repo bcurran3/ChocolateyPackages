d5122f60fd12bf762b48aa18ca3db9145efe5022
Bill Curran <bcurran3@yahoo.com>
2019-08-27 06:31:32 -0700

    v2019.08.27

--- choco-package-list-backup/tools/ChocolateyUninstall.ps1
+++ choco-package-list-backup/tools/ChocolateyUninstall.ps1
@@ -1,16 +1,17 @@
 ﻿#$ErrorActionPreference = 'Stop'
 $packageName     = 'choco-package-list-backup' 
-$script          = 'choco-package-list-backup.ps1'
-$xml             = 'choco-package-list-backup.xml'
+$scriptDir       = "$(Get-ToolsLocation)\BCURRAN3"
 $oldshortcutName = 'Choco Package List Backup.lnk'
 $shortcutName    = 'Chocolatey Package List Backup.lnk'
 $altshortcutName = 'Package List Backup.lnk'
 
 SchTasks /Delete /TN choco-package-list-backup /F
 
-Remove-Item "$env:ChocolateyInstall\bin\$script" -Force -ErrorAction SilentlyContinue
-Remove-Item "$env:ChocolateyInstall\bin\$script*.bak" -Force -ErrorAction SilentlyContinue
-Remove-Item "$env:ChocolateyInstall\bin\$xml" -Force -ErrorAction SilentlyContinue
-Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$oldshortcutName" -Force -ErrorAction SilentlyContinue
-Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
-Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Chocolatey\$altshortcutName" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$oldshortcutName" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\Chocolatey\$altshortcutName" -Force -ErrorAction SilentlyContinue
+Remove-Item "$scriptDir\choco-package-list-backup.*" -Force -ErrorAction SilentlyContinue | Out-Null
+if (!(Get-ChildItem -Path "$ENV:ChocolateyToolsLocation\BCURRAN3" | Measure-Object | %{$_.Count})) {
+    $ENV:Path.Replace("$ChocolateyToolsLocation\BCURRAN3","") | Out-Null
+    Remove-Item "$ENV:ChocolateyToolsLocation\BCURRAN3" | Out-Null
+   }
