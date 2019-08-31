afe14af6048db05370767263fea21e5867ddc957
bcurran3 <bcurran3@yahoo.com>
2019-08-31 13:15:10 -0700

    Add files via upload

--- jellyfin/tools/chocolateyInstall.ps1
+++ jellyfin/tools/chocolateyInstall.ps1
@@ -4,24 +4,22 @@ $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $ShortcutName = 'Jellyfin'
 $exe          = 'jellyfin.exe'
 
+Start-CheckandStop 'jellyfin'
+
 $packageArgs = @{
   packageName    = $packageName
   FileFullPath   = "$toolsDir\jellyfin_"+$ENV:ChocolateyPackageVersion+"_windows-x86.zip"
   FileFullPath64 = "$toolsDir\jellyfin_"+$ENV:ChocolateyPackageVersion+"_windows-x64.zip"
-  Destination    = "$toolsDir\jellyfin"
+  Destination    = "$ENV:ChocolateyToolsLocation\jellyfin"
 }
 
 Get-ChocolateyUnzip @packageArgs
 
-Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\$ShortcutName.lnk" -targetPath "$toolsDir\jellyfin\$exe" -WorkingDirectory "$toolsDir\jellyfin" -IconLocation "$toolsDir\jellyfin\jellyfin-web\src\favicon.ico"
-Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName.lnk" -targetPath "$toolsDir\jellyfin\$exe" -WorkingDirectory "$toolsDir\jellyfin" -IconLocation "$toolsDir\jellyfin\jellyfin-web\src\favicon.ico"
+if ($ProcessWasRunning -eq "True") {Start-Process $ProcessFullPath}
+
+Install-BinFile -Name 'jellyfin' -Path "$ENV:ChocolateyToolsLocation\jellyfin\$exe"
+
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\$ShortcutName.lnk" -targetPath "$ENV:ChocolateyToolsLocation\jellyfin\$exe" -WorkingDirectory "$ENV:ChocolateyToolsLocation\jellyfin" -IconLocation "$ENV:ChocolateyToolsLocation\jellyfin\jellyfin-web\src\favicon.ico"
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName.lnk" -targetPath "$ENV:ChocolateyToolsLocation\jellyfin\$exe" -WorkingDirectory "$ENV:ChocolateyToolsLocation\jellyfin" -IconLocation "$ENV:ChocolateyToolsLocation\jellyfin\jellyfin-web\src\favicon.ico"
 
 Remove-Item "$toolsDir\*.zip" -EA SilentlyContinue | Out-Null
-Write-Host "" | Out-File "$toolsDir\jellyfin\ffmpeg.exe.ignore"
-Write-Host "" | Out-File "$toolsDir\jellyfin\ffprobe.exe.ignore"
-Write-Host "" | Out-File "$toolsDir\jellyfin\nssm.exe.ignore"
-
-$WhoAmI=whoami
-icacls.exe $toolsDir /grant $WhoAmI":"'(OI)(CI)'F /T | Out-Null
-
-if ($ProcessWasRunning -eq "True") {&$ProcessFullPath}
--- jellyfin/tools/chocolateyUninstall.ps1
+++ jellyfin/tools/chocolateyUninstall.ps1
@@ -2,10 +2,14 @@
 $packageName  = 'jellyfin'
 $ShortcutName = 'Jellyfin'
 
+Start-CheckandStop "jellyfin"
+
+Remove-Item "$ENV:ChocolateyToolsLocation\jellyfin" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item "$ENV:Public\Desktop\$ShortcutName.lnk" -Force -ErrorAction SilentlyContinue
 Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName.lnk" -Force -ErrorAction SilentlyContinue
+Uninstall-BinFile 'jellyfin'
 
 if (Test-Path "$ENV:LOCALAPPDATA\jellyfin") {
     Write-Host "  ** Found $ENV:LOCALAPPDATA\jellyfin data directory." -Foreground Magenta
-	Write-Host "  ** Manually delete it if you no longer need the data." -Foreground Magenta
+	Write-Host "  ** Manually delete it if you no longer want the data." -Foreground Magenta
 	}
