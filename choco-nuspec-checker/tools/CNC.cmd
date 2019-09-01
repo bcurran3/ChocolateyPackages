479a5ae5c6b2da1d1956dcc60440444a48a3d5db
bcurran3 <bcurran3@yahoo.com>
2019-09-01 14:40:53 -0700

    Delete CNC.cmd

--- choco-nuspec-checker/tools/CNC.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "%ChocolateyToolsLocation%\BCURRAN3\CNC.PS1 %1 %2 %3 %4 %5 %6 %7 %8 %9"
