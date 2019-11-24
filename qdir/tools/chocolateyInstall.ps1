56ecfbe7ab8a1445b47a0fa9b0528dd2b489da15
bcurran3 <bcurran3@yahoo.com>
2019-11-24 11:21:58 -0800

    Add files via upload

--- qdir/tools/chocolateyInstall.ps1
+++ qdir/tools/chocolateyInstall.ps1
@@ -2,65 +2,29 @@
 $packageName    = 'qdir'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $bits           = Get-ProcessorBits
-$url            = "$toolsDir\Q-Dir_Installer.zip"
-$url64          = "$toolsDir\Q-Dir_Installer_x64.zip"
 
 $packageArgs = @{
   packageName    = $packageName
-  unzipLocation  = $toolsDir
-  fileType       = 'ZIP' 
-  url            = $url
-  url64          = $url64
+  Destination    = $toolsDir
+  FileFullPath   = "$toolsDir\Q-Dir_Installer.zip"
+  FileFullPath64 = "$toolsDir\Q-Dir_Installer_x64.zip"
 }
 
-Install-ChocolateyZipPackage @packageArgs
-
-if ($bits -eq 64)
-   {
-    $fileLocation = "$toolsDir\Q-Dir_Installer_x64.exe"
-   } else {
-    $fileLocation = "$toolsDir\Q-Dir_Installer_UC.exe"
-   }
-   
-$langid=(Get-Culture).LCID
-# Probably best to match .EnglishName to cover various countries with same language but different layout instead of culture ID
-################# or use .parent.name
-# GERMAN=0
-# ENGLISH=1 1033, 4105, 
-# FRENCH=2
-# THAI = 5
-# JAPANESE = 6
-# KOREAN = 7
-# POLISH = 8
-# CHINESE = 9
-# ITALIAN = 3
-# RUSSIAN = 10
-# DUTCH = 11
-# TURKISH = 12
-# GREEK = 13
-# SPANISH = 14
-# SLOVAK = 15
-# CZECH = 16
-# DANISH = 17
-# PROTUGUESE = 18
-# ROMANIAN = 19
-# CROATIAN - 23
-# SLOVENIAN = 24
-# FINISH = 21
-# ARABIC = 25 1
-# HUNGARIAN = 22
-# UKRAINIAN = 26
+Get-ChocolateyUnzip @packageArgs
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'EXE'
-  file          = $fileLocation
-  silentArgs    = '/S langid=1'
-  validExitCodes= @(0,1)
-  softwareName  = 'Q-Dir'
+  packageName    = $packageName
+  fileType       = 'EXE'
+  file           = "$toolsDir\Q-Dir_Installer_UC.exe"
+  file64         = "$toolsDir\Q-Dir_Installer_x64.exe"
+  silentArgs     = '/S langid=1'
+  validExitCodes = @(0,1)
+  softwareName   = 'Q-Dir'
 }
  
 Install-ChocolateyInstallPackage @packageArgs
 
 Install-BinFile -Name qdir -Path $env:ProgramFiles\Q-Dir\Q-Dir.exe
-Remove-Item "$toolsDir\q*.zip" | Out-Null
+Remove-Item "$toolsDir\*.zip" -ErrorAction SilentlyContinue | Out-Null
+Remove-Item "$toolsDir\*.exe" -ErrorAction SilentlyContinue | Out-Null
+Remove-Item "$toolsDir\*.ignore" -ErrorAction SilentlyContinue | Out-Null
--- windows-iso-downloader/tools/chocolateyinstall.ps1
+++ windows-iso-downloader/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $packageName   = 'windows-iso-downloader' 
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url           = 'https://www.heidoc.net/php/Windows%20ISO%20Downloader.exe'
-$checksum      = '0EDC160F366AD7C8D224B46634702AE5D09C9BF7593A658A9C76E461AECCD301'
+$checksum      = '5B7B7E392A3A24034F2C0ACEA00B2DD7BA816C7EDD26B1B66C6E04EF8A813CE1'
 $ExeFile       = 'Windows ISO Downloader.exe'
 $ShortcutName  = 'Microsoft Windows and Office ISO Download Tool.lnk'
 
