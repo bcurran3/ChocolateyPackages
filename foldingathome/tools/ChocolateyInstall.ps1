07d949e8200315bab2daff23a25b3c4e263a5ca6
bcurran3 <bcurran3@yahoo.com>
2020-03-29 21:23:25 -0700

    Add files via upload

---
+++ foldingathome/tools/ChocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName    = 'foldingathome'
+$url            = 'https://download.foldingathome.org/releases/public/release/fah-installer/windows-10-32bit/v7.5/fah-installer_7.5.1_x86.exe'
+$checksum       = 'E97AECCBF3692001DAFE16612B1CAE6A90CB6C22DBF57E947D9C13FF4E1B5715'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir  
+  fileType      = 'EXE'
+  url           = $url
+  validExitCodes= @(0,1)
+  silentArgs    = '/S'
+  softwareName  = 'folding@home'
+  checksum      = $checksum
+  checksumType  = 'sha256' 
+}
+
+Install-ChocolateyPackage @packageArgs
