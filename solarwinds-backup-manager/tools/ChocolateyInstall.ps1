50a5643a42fcf58818b50d77aabc91c174740a25
Bill Curran <bcurran3@yahoo.com>
2020-08-26 22:06:21 -0700

    v20.7.0.20181

--- solarwinds-backup-manager/tools/ChocolateyInstall.ps1
+++ solarwinds-backup-manager/tools/ChocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $packageName    = 'solarwinds-backup-manager'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://cdn.cloudbackup.management/maxdownloads/mxb-windows-x86_x64.exe'
-$checksum       = 'AF86E875643C02103B73AF3EBEB944B1B400E8FEBDFA32E0BB1CEE65303727B7'
+$checksum       = 'B5A2F73C6DFF982889F44ABFAA88EB1D4B2A3F010F8B1DEDDE6BD26554E42A1C'
 
 Write-Host "  ** Backup Manager will open its web console in your default browser after installing." -ForeGround Magenta
 
