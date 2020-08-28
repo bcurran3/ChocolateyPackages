862bdc870638baf5cf677e377e16e70269497afc
Bill Curran <bcurran3@yahoo.com>
2020-08-27 23:30:53 -0700

    v0.9.24

--- tux-paint/tools/ChocolateyInstall.ps1
+++ tux-paint/tools/ChocolateyInstall.ps1
@@ -1,19 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName    = 'tux-paint'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = "$toolsDir\tuxpaint-0.9.23-win32-installer.exe"
-$checksum       = '52C43AA4925458421A20506F64659FABAD5A8939947ADB4B4C26A8BDC90E08E5'
 
 $packageArgs = @{
-  packageName    = $packageName
+  packageName    = 'tux-paint'
   fileType       = 'EXE'
-  file           = $url
-  checksum       = $checksum
-  checksumType   = 'sha256'  
+  file           = "$toolsDir\tuxpaint-0.9.24-1-win32-installer.exe"
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0,1)
-  softwareName   = 'Tux Paint 0.9.23'
+  softwareName   = "Tux Paint*"
 }
  
 Install-ChocolateyInstallPackage @packageArgs
-Remove-Item $url | out-null
+Remove-Item $toolsDir\*.exe | Out-Null
