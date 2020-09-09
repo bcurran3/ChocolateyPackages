236e3a87a7796840725b025c6be5092e174834bc
Bill Curran <bcurran3@yahoo.com>
2020-09-09 15:19:16 -0700

    v0.23.1.20200909

--- hfsexplorer/tools/chocolateyinstall.ps1
+++ hfsexplorer/tools/chocolateyinstall.ps1
@@ -1,17 +1,15 @@
-﻿$packageName= 'hfsexplorer'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://downloads.sourceforge.net/project/catacombae/HFSExplorer/0.23.1/hfsexplorer-0.23.1-setup.exe' 
+﻿$ErrorActionPreference = 'Stop'
+$packageName= 'hfsexplorer'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE' 
-  url           = $url
-  checksum      = 'EE7AFAC9E0C2D1A31035FAB8F175CE07F95FE6EDD29D19532DD09BB1E353F4F8'
-  checksumType  = 'sha256'   
-  silentArgs    = '/S'  
-  validExitCodes= @(0) 
+  fileType      = 'EXE'
+  file          = "$toolsDir\hfsexplorer-0.23.1-setup.exe"
+  silentArgs    = '/S'
   softwareName  = 'HFSExplorer*'
-}
-
-Install-ChocolateyPackage @packageArgs
+  }
+  
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -Force | Out-Null
+Remove-Item $toolsDir\*.ignore -Force | Out-Null
