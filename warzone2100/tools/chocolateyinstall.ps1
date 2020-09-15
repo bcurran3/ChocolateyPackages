334a8476e18588a28abc4729ba6c551a13d1120f
Bill Curran <bcurran3@yahoo.com>
2020-09-14 21:15:04 -0700

    now with embedded binary

--- warzone2100/tools/chocolateyinstall.ps1
+++ warzone2100/tools/chocolateyinstall.ps1
@@ -1,18 +1,15 @@
-﻿$ErrorActionPreference = 'Stop'
-$packageName    = 'warzone2100'
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://downloads.sourceforge.net/project/warzone2100/releases/3.4.1/warzone2100_win_x86_installer.exe' 
-$checksum       = '379E7016AE282ACE7D672BE87C1AB447135E254BF00B0C71E979F64D8BEDFFAD'
+﻿# https://sourceforge.net/projects/warzone2100/files/latest/download
+$ErrorActionPreference = 'Stop'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName    = $packageName
-  fileType       = 'EXE'
-  url            = $url
-  validExitCodes = @(0)
-  silentArgs     = '/S'
-  softwareName   = 'Warzone 2100*'
-  checksum       = $checksum 
-  checksumType   = 'sha256' 
-}
-
-Install-ChocolateyPackage @packageArgs  
+  packageName  = 'warzone2100'
+  fileType     = 'EXE'
+  file         = "$toolsDir\warzone2100_win_x86_installer"
+  silentArgs   = '/S'
+  softwareName = 'Warzone 2100*'
+  }
+  
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -Force | Out-Null
+Remove-Item $toolsDir\*.ignore -Force | Out-Null
