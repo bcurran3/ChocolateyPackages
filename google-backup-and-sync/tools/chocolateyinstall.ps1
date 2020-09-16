cca302807b8244360cce918d9a67ff7235f8db3b
Bill Curran <bcurran3@yahoo.com>
2020-09-15 18:14:04 -0700

    start of using AU with this package

--- google-backup-and-sync/tools/chocolateyinstall.ps1
+++ google-backup-and-sync/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $packageName = 'google-backup-and-sync' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url         = 'https://dl.google.com/drive/gsync_enterprise.msi'
-$checksum    = 'FB5A3205F4D5D5243FDB16104C707521DC1BD8C36195575BAB61A64085DA8F67'
+$checksum32  = 'FB5A3205F4D5D5243FDB16104C707521DC1BD8C36195575BAB61A64085DA8F67'
 $url64       = 'https://dl.google.com/drive/gsync_enterprise64.msi'
 $checksum64  = '8C723ED84CF0B8EF69B041943D742F84C59710F524AF76919EE279ABDA795BB8'
 
@@ -14,7 +14,7 @@ $packageArgs = @{
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/quiet /norestart'
   softwareName   = 'Backup and Sync from Google'
-  checksum       = $checksum
+  checksum       = $checksum32
   checksumType   = 'sha256'
   checksum64     = $checksum64
   checksumType64 = 'sha256'  
