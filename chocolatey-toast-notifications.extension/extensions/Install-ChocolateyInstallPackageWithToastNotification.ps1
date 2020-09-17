17aa33c8c126af99d0c0eb799ac8c2d3e01da671
Bill Curran <bcurran3@yahoo.com>
2020-09-17 14:29:28 -0700

    v0.0.4-pre

--- chocolatey-toast-notifications.extension/extensions/Install-ChocolateyInstallPackageWithToastNotification.ps1
+++ chocolatey-toast-notifications.extension/extensions/Install-ChocolateyInstallPackageWithToastNotification.ps1
@@ -1,4 +1,4 @@
-﻿# chocolatey-toast-notifications.extension v0.0.3.1 by Bill Curran AKA BCURRAN3 - 2018-2020 Copyleft Bill Curran
+﻿# chocolatey-toast-notifications.extension v0.0.4 by Bill Curran AKA BCURRAN3 - 2018-2020 Copyleft Bill Curran
 # This function is aliased as Uninstall-ChocolateyPackage to intercept and run before
 # Uninstall-ChocolateyPackage solely to add a a toast notification after running Uninstall-ChocolateyPackage.
 # Got it? No? Read it again.
@@ -21,11 +21,9 @@ Install-ChocolateyInstallPackage @args
 $chocolateySoftwareName = Get-ChocolateySoftwareName
 
 # Show toast notification
-if ($env:USERNAME -eq "$env:COMPUTERNAME$"){
     If ((Get-Service WinRM).Status -eq 'Running') {
          Invoke-Command -ComputerName $env:COMPUTERNAME -ArgumentList $env:chocolateyPackageName,$chocolateySoftwareName,$env:chocolateyPackageVersion -ScriptBlock {param([string]$chocolateyPackageName, [string]$chocolateySoftwareName, [string]$chocolateyPackageVersion) New-BurntToastNotification -Text "Chocolatey ($chocolateyPackageName)", "$chocolateySoftwareName v$chocolateyPackageVersion", "Installed or updated." -Button (New-BTButton -Content 'Package Webpage' -Arguments "https://chocolatey.org/packages/$env:chocolateyPackageName") -AppLogo "$env:ChocolateyInstall\extensions\chocolatey-toast-notifications\choco.ico"}
-   }		 
- } else {
-   New-BurntToastNotification -Text "Chocolatey ($env:chocolateyPackageName)", "$chocolateySoftwareName v$env:chocolateyPackageVersion", "Installed or updated." -Button (New-BTButton -Content 'Package Webpage' -Arguments "https://chocolatey.org/packages/$env:chocolateyPackageName") -AppLogo "$env:ChocolateyInstall\extensions\chocolatey-toast-notifications\choco.ico"
-  }
-}
+   } else {
+     Write-Host "** Can't send global Toast because WinRM service is not running." -Foreground red
+	 }
+ }
