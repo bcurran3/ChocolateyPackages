bd4d7d555dc94c63092bc886a4d57d106c230748
Bill Curran <bcurran3@yahoo.com>
2020-09-18 23:22:13 -0700

    check file not dir

--- chocolateypowershell/tools/ChocolateyEnvironment.ps1
+++ chocolateypowershell/tools/ChocolateyEnvironment.ps1
@@ -18,7 +18,7 @@ function global:CPSHelp{
 Set-Location $env:ChocolateyInstall\helpers
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 Import-Module $env:ChocolateyInstall\helpers\chocolateyInstaller.psm1
-if (Test-Path $env:ChocolateyInstall\extensions\chocolatey-core) {
+if (Test-Path $env:ChocolateyInstall\extensions\chocolatey-core\chocolatey-core.psm1) {
    Set-Location $env:ChocolateyInstall\extensions\chocolatey-core
    Import-Module $env:ChocolateyInstall\extensions\chocolatey-core\chocolatey-core.psm1
    }
