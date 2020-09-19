39dd939ddc68cf4cfd710eb556612a29d176d484
Bill Curran <bcurran3@yahoo.com>
2020-09-19 00:50:38 -0700

    delete test file

--- chocolateypowershell/test.ps1
+++
@@ -1,4 +0,0 @@
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$ENV:Public\Desktop\ChocolateyPowerShell.lnk")
-$Shortcut.TargetPath = "$env:chocolateyinstall\choco.ico"
-$Shortcut.Save()
