29193ba63612c7c1a40e46e606fb6c1b4db095ac
Alex Lopez <info@ithinkvirtual.com>
2020-10-01 15:48:23 -0400

    Update ChocolateyInstall.ps1

--- vmware-tools/vmware-tools_11.1.5.16724464/tools/ChocolateyInstall.ps1
+++ vmware-tools/vmware-tools_11.1.5.16724464/tools/ChocolateyInstall.ps1
@@ -1,4 +1,4 @@
-﻿# https://packages.vmware.com/tools/releases
+# https://packages.vmware.com/tools/releases
 
 $ErrorActionPreference = 'Stop'
 $packageName       = 'vmware-tools'
@@ -7,19 +7,44 @@ $url               = 'https://packages.vmware.com/tools/releases/11.1.5/windows/
 $checksum          = '53443098A1927A3227AD1E1B59935732426CBF2E7E84D3C3FDD502AB3454ABBD'
 $url64             = 'https://packages.vmware.com/tools/releases/11.1.5/windows/x64/VMware-tools-11.1.5-16724464-x86_64.exe'
 $checksum64        = '7919592ED91712205969848F0B9D8C278ABAF3DF6F31CEA16731F17FCD7F12EA'
+$pp                = Get-PackageParameters
 
-$packageArgs = @{
-  packageName    = $packageName
-  fileType       = 'EXE'
-  url            = $url
-  url64bit       = $url64
-  validExitCodes = @(0)
-  silentArgs     = '/S /v /qn REBOOT=R ADDLOCAL=ALL'
-  softwareName   = 'VMware Tools'
-  checksum       = $checksum 
-  checksumType   = 'sha256' 
-  checksum64     = $checksum64
-  checksumType64 = 'sha256'
+if ( $pp.ALL ) {
+		Write-Host "`nPerforming a Complete installation of VMware Tools..." -ForegroundColor Yellow
+		Write-Host ""
+		
+		$packageArgs = @{
+		  packageName    = $packageName
+		  fileType       = 'EXE'
+		  url            = $url
+		  url64bit       = $url64
+		  validExitCodes = @(0, 3010)
+		  silentArgs     = '/S /v /qn REBOOT=R ADDLOCAL=ALL'
+		  softwareName   = 'VMware Tools'
+		  checksum       = $checksum 
+		  checksumType   = 'sha256' 
+		  checksum64     = $checksum64
+		  checksumType64 = 'sha256'
+		}
+
+		Install-ChocolateyPackage @packageArgs
+} else {
+		Write-Host "`nPerforming a Typical installation of VMware Tools..." -ForegroundColor Yellow
+		Write-Host ""
+		
+		$packageArgs = @{
+		  packageName    = $packageName
+		  fileType       = 'EXE'
+		  url            = $url
+		  url64bit       = $url64
+		  validExitCodes = @(0, 3010)
+		  silentArgs     = '/S /v /qn REBOOT=R'
+		  softwareName   = 'VMware Tools'
+		  checksum       = $checksum 
+		  checksumType   = 'sha256' 
+		  checksum64     = $checksum64
+		  checksumType64 = 'sha256'
+		}
+
+		Install-ChocolateyPackage @packageArgs
 }
-
-Install-ChocolateyPackage @packageArgs  
