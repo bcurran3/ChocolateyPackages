e67cb207efaeac738f96e2edb823e283783bdaca
Bill Curran <bcurran3@yahoo.com>
2020-10-02 18:19:44 -0700

    v27.20.100.8783

--- intel-graphics-driver/tools/ChocolateyInstall.ps1
+++ intel-graphics-driver/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = 'intel-graphics-driver' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = 'https://downloadmirror.intel.com/29855/a08/igfx_win10_100.8681.exe'
-$checksum64  = '90198DEC7D3210A317974A0757416E38E6E756C69CD33AAFC99FFD622DFDDED9'
+$url64       = 'https://downloadmirror.intel.com/29904/a08/igfx_win10_100.8783.exe'
+$checksum64  = '687343110FBD5E84B1987262495248F4099311B3CBF4BA1FECAA434F18C50BAF'
 
 if (!(Get-IsWin10)){
     Write-Warning "  ** This version is only for Windows 10."
