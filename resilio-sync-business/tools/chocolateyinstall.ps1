5170f78d1b04f77f503bf3f1ccd897b1df6e2344
Bill Curran <bcurran3@yahoo.com>
2020-11-15 23:52:46 -0800

    resilio-sync-business.2.7.2.1375

--- resilio-sync-business/tools/chocolateyinstall.ps1
+++ resilio-sync-business/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName    = 'resilio-sync-business'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://download-cdn.resilio.com/stable/windows/cc/2501/Resilio-Sync.exe'
-$checksum       = '6E9FB31BB1EB4D29BC06B27E2F137371B83C4C5FA4263C28CDAE31EE0BFD0ADE'
+$checksum       = '3E3D9BC73EC6F33B5002E2B4D96E7F4CC2B3E4081AE7A9F63A4EFD7C85008240'
 $url64          = 'https://download-cdn.resilio.com/stable/windows64/cc/2501/Resilio-Sync_x64.exe'
-$checksum64     = '4441E71C4390EB5F9F4D0E48483493CEC8E3B86BDEADBACA5ABEE70F803F5B25'
+$checksum64     = '56EB5832A5155D265653AAEF92354F5CDCFAE98F9E8A721F595265C5953E9530'
 
 $packageArgs = @{
   packageName    = $packageName
