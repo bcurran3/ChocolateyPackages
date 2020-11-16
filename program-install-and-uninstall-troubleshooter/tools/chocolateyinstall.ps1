36b0bd5085b7027e6b73c1656e5949ba80b24c99
Bill Curran <bcurran3@yahoo.com>
2020-11-16 01:13:30 -0800

    program-install-and-uninstall-troubleshooter.1.0.0.20201116

--- program-install-and-uninstall-troubleshooter/tools/chocolateyinstall.ps1
+++ program-install-and-uninstall-troubleshooter/tools/chocolateyinstall.ps1
@@ -1,14 +1,15 @@
-﻿$packageName= 'program-install-and-uninstall-troubleshooter' 
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/7/E/9/7E9188C0-2511-4B01-8B4E-0A641EC2F600/MicrosoftProgram_Install_and_Uninstall.meta.diagcab'
-$FileFullPath = Join-Path $toolsDir 'MicrosoftProgram_Install_and_Uninstall.meta.diagcab'
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'program-install-and-uninstall-troubleshooter' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'https://download.microsoft.com/download/7/E/9/7E9188C0-2511-4B01-8B4E-0A641EC2F600/MicrosoftProgram_Install_and_Uninstall.meta.diagcab'
+$checksum    = '8CAD66ADB36B1F4F64204A4328A063AE33695DBBD5386F761CFB56C2C0987471'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  FileFullPath  = $FileFullPath
+  FileFullPath  = "$toolsDir\MicrosoftProgram_Install_and_Uninstall.meta.diagcab"
   url           = $url
-  checksum      = 'B7712D302092E2B08A63E6B15A423EBB4E96E1804B9FEEF15F52979B37812E0C'
+  checksum      = $checksum
   checksumType  = 'sha256'
 }
 
--- program-install-and-uninstall-troubleshooter/tools/chocolateyuninstall.ps1
+++ program-install-and-uninstall-troubleshooter/tools/chocolateyuninstall.ps1
@@ -1,4 +1,5 @@
-﻿$packageName= 'program-install-and-uninstall-troubleshooter' 
+﻿$ErrorActionPreference = 'Stop'
+$packageName= 'program-install-and-uninstall-troubleshooter' 
 
 remove-item "$env:Public\Desktop\MS Install Uninstall Troubleshooter (Portable).lnk" -Force -ErrorAction 'SilentlyContinue'
 remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\MS Install Uninstall Troubleshooter (Portable).lnk" -Force -ErrorAction 'SilentlyContinue'
