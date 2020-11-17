08d0d937d69c121aa68104607bb671c9e265cc1c
Bill Curran <bcurran3@yahoo.com>
2020-11-16 19:15:34 -0800

    performancetest.10.0.1010.0

--- performancetest/tools/chocolateyinstall.ps1
+++ performancetest/tools/chocolateyinstall.ps1
@@ -2,18 +2,18 @@
 $packageName    = 'performancetest'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://www.passmark.com/ftp/petst.exe'
-$checksum       = 'F496E8DF1B3DEBA5EED500E045919BEBF6392FE530F4B1A4E42076407E054D93'
+$checksum       = '5C5B22ECC1305AD2CC1895294677DB703294467E56AAFB83D6DC9A96DF3AF492'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir  
-  fileType      = 'EXE'
-  url           = $url
-  validExitCodes= @(0)
-  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  softwareName  = 'PerformanceTest*'
-  checksum      = $checksum
-  checksumType  = 'sha256' 
+  packageName    = $packageName
+  unzipLocation  = $toolsDir  
+  fileType       = 'EXE'
+  url            = $url
+  validExitCodes = @(0)
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  softwareName   = 'PerformanceTest*'
+  checksum       = $checksum
+  checksumType   = 'sha256' 
 }
 
 Install-ChocolateyPackage @packageArgs
