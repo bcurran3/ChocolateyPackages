2e3eeb1d87ec0634c31f3a266bf1a8d3167fad34
Bill Curran <bcurran3@yahoo.com>
2020-11-16 16:03:01 -0800

    intel-rst-driver.18.0.0.633

--- intel-rst-driver/tools/ChocolateyInstall.ps1
+++ intel-rst-driver/tools/ChocolateyInstall.ps1
@@ -1,18 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = 'intel-rst-driver' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://downloadmirror.intel.com/29647/eng/SetupRST.exe'
-$checksum    = '1FD4057DD81C316C6F771C429C500F16F067678BEAA7E48952583ED17C6C20F8'
+$url         = 'https://downloadmirror.intel.com/29978/eng/SetupRST.exe'
+$checksum    = '4307EEF4789AA4977182150A6866310E4A76ACBB6D5D369C93E7E77FF0E3CC9A'
 
 if ((Get-IsWin10 -eq $false) -and (Get-IsWinServer2016 -eq $false)){
    Write-Warning "  ** This version is Windows 10 and Windows Server 2016 only."
    throw
 }
 
-if (Get-PendingReboot -eq $true){
-   Write-Warning "  ** There is a pending reboot. Please reboot and try again."
-   throw
-}
+#if (Get-PendingReboot -eq $true){
+#   Write-Warning "  ** There is a pending reboot. Please reboot and try again."
+#   throw
+#}
 
 $packageArgs = @{
   packageName    = $packageName
