995f989c8fe6f6ac49be6edb6bd9ecc21c49ce00
Bill Curran <bcurran3@yahoo.com>
2020-11-16 16:43:22 -0800

    intel-graphics-driver.27.20.100.8935

--- intel-graphics-driver/tools/ChocolateyInstall.ps1
+++ intel-graphics-driver/tools/ChocolateyInstall.ps1
@@ -1,18 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = 'intel-graphics-driver' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = 'https://downloadmirror.intel.com/29904/a08/igfx_win10_100.8783.exe'
-$checksum64  = '687343110FBD5E84B1987262495248F4099311B3CBF4BA1FECAA434F18C50BAF'
+$url64       = 'https://downloadmirror.intel.com/29988/a08/igfx_win10_100.8935.exe'
+$checksum64  = 'AB73EDDDA92D70F494D03F0372D6452BA47A0D1C5A98D89A51BE2CA4FA285F0C'
 
 if (!(Get-IsWin10)){
     Write-Warning "  ** This version is only for Windows 10."
     throw
    }
-
-if (!(Get-Is64)){
-   Write-Warning "  ** This version is only for 64 bit OSes."
-   throw
-  }
 
 if (!(Get-IsIntelVideo)){
     Write-Warning "  ** No Intel display adapter found."
@@ -26,7 +21,7 @@ $packageArgs = @{
   url64bit      = $url64
   checksum64    = $checksum64
   checksumType64= 'sha256'
-  silentArgs    = '-overwrite -s'
+  silentArgs    = "-overwrite -s -report $toolsDir\install.log"
   softwareName  = ''
   }
 
