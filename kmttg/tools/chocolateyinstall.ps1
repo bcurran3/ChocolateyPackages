ff49e655d7c6686afbeb91b590ef55d84efcae57
Bill Curran <bcurran3@yahoo.com>
2020-11-17 14:26:57 -0800

    kmttg.2.4.16

--- kmttg/tools/chocolateyinstall.ps1
+++ kmttg/tools/chocolateyinstall.ps1
@@ -1,14 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = 'kmttg' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$file        = "$toolsDir\kmttg_v2.4o.zip"
 
-Get-ChocolateyUnzip -FileFullPath "$file" -Destination "$toolsDir"
+Get-ChocolateyUnzip -FileFullPath "$toolsDir\kmttg_v2.4p.zip" -Destination "$toolsDir"
 
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\kmttg.lnk" -targetPath "$toolsDir\kmttg.jar" -WorkingDirectory "$toolsDir" -IconLocation "$toolsDir\kmttg.ico"
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\kmttg.lnk" -targetPath "$toolsDir\kmttg.jar" -WorkingDirectory "$toolsDir" -IconLocation "$toolsDir\kmttg.ico"
 
-Remove-Item "$file" -Force | Out-Null
+Remove-Item "$toolsDir\*.zip" -EA SilentlyContinue | Out-Null
 
 $WhoAmI=whoami
 icacls.exe $toolsDir /grant $WhoAmI":"'(OI)(CI)'F /T | Out-Null
