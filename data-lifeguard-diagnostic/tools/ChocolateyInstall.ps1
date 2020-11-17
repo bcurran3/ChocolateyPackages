044a79cd6cd0aad2eb5e4886c0cb8840c03130bf
Bill Curran <bcurran3@yahoo.com>
2020-11-16 18:24:28 -0800

    data-lifeguard-diagnostic.1.37

--- data-lifeguard-diagnostic/tools/ChocolateyInstall.ps1
+++ data-lifeguard-diagnostic/tools/ChocolateyInstall.ps1
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 ﻿$ErrorActionPreference = 'Stop'
-$packageName  = 'data-lifeguard-diagnostic'
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'http://downloads.wdc.com/windlg/WinDlg_v1_36.zip'
-$checksum     = 'CC6ADB8DDCADD7ECEEC9BBCAADA4ACD1921164C272D13E1AFE02F8E43AFB779B'
-$exeInstaller = "$toolsDir\WinDlg_v1_36.exe"
+$packageName   = 'data-lifeguard-diagnostic'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$TodaysVersion = ($env:ChocolateyPackageVersion -replace '[.]','_')
+$url           = "http://downloads.wdc.com/windlg/WinDlg_v"+$TodaysVersion+".zip"
+$checksum      = '931C8D9515F661B73DE8322A9C298403A90181F01DD6081C1B39F6F7B4E8CA4F'
  
 $packageArgs = @{
   packageName   = $packageName
@@ -19,7 +18,7 @@ Install-ChocolateyZipPackage @packageArgs
  
 $packageArgs = @{
     packageName    = $packageName
-    file           = $exeInstaller
+    file           = "$toolsDir\WinDlg_v"+$TodaysVersion+".exe"
     fileType       = 'EXE'
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
     softwareName   = 'Data Lifeguard Diagnostic*'
@@ -27,35 +26,4 @@ $packageArgs = @{
 }
 
 Install-ChocolateyInstallPackage @packageArgs
-Remove-Item $exeInstaller | Out-Null
-=======
-﻿$ErrorActionPreference = 'Stop'
-$packageName  = 'data-lifeguard-diagnostic'
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'http://downloads.wdc.com/windlg/WinDlg_v1_36.zip'
-$checksum     = 'CC6ADB8DDCADD7ECEEC9BBCAADA4ACD1921164C272D13E1AFE02F8E43AFB779B'
-$exeInstaller = "$toolsDir\WinDlg_v1_36.exe"
- 
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'ZIP'
-  url           = $url
-  checksum      = $checksum
-  checksumType  = 'sha256' 
-}
-  
-Install-ChocolateyZipPackage @packageArgs 
- 
-$packageArgs = @{
-    packageName    = $packageName
-    file           = $exeInstaller
-    fileType       = 'EXE'
-    silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-    softwareName   = 'Data Lifeguard Diagnostic*'
-    validExitCodes = @(0)   
-}
-
-Install-ChocolateyInstallPackage @packageArgs
-Remove-Item $exeInstaller | Out-Null
->>>>>>> 27f72bd1d91c65406d60ff1baab58075e1892b75
+Remove-Item $toolsDir\*.exe -EA SilentlyContinue | Out-Null
