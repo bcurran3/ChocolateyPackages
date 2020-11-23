6d40c005a31ea058b834b6fe24212b8fb7f389ba
Bill Curran <bcurran3@yahoo.com>
2020-11-23 03:22:39 -0800

    expandrive.7.6.5

--- expandrive/tools/chocolateyinstall.ps1
+++ expandrive/tools/chocolateyinstall.ps1
@@ -1,21 +1,20 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName   = 'expandrive' 
 $url           = 'https://secure.expandrive.com/expandrive/download_win' 
-$checksum      = '640EDD703AE800582661D44C2713D5278AFA262B167824CD33C86B3050E3602B'
+$checksum      = '2C360D02CD254EAFDD4D2C390242532D9BFC8A069E45FD1FA41075887977B486'
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'EXE'
-  url           = $url
-  validExitCodes= @(0, 3010, 1641)
-  silentArgs    = ''
-  softwareName  = 'Expandrive*'
-  checksum      = $checksum
-  checksumType  = 'sha256' 
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = ''
+  softwareName   = 'Expandrive*'
+  checksum       = $checksum
+  checksumType   = 'sha256' 
 }
 
 Start-CheckandStop "ExpanDrive"
 Install-ChocolateyPackage @packageArgs
-Sleep 5
-if ($GLOBAL:ProcessWasRunning -eq $False) {Start-CheckandStop "ExpanDrive"}
-# ^ doesn't necessary work in this case
+if ($ProcessWasRunning -eq $False) {Start-WaitandStop "ExpanDrive"}
+if ($ProcessWasRunning -eq $False) {Start-WaitandStop "ExpanDrive"}
