2f3a7f085b4beede30f25a259dce61bd8426621d
Bill Curran <bcurran3@yahoo.com>
2020-11-28 01:31:35 -0800

    gcompris.1.0.nupkg

---
+++ gcompris/tools/ChocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'gcompris'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  file           = "$toolsDir\gcompris-qt-1.0-win32-gcc.exe"
+  file64         = "$toolsDir\gcompris-qt-1.0-win64-gcc.exe"
+  silentArgs     = '/S'
+  validExitCodes = @(0,1)
+  softwareName   = 'GCompris Educational Software'
+}
+ 
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item "$toolsDir\*.exe" -EA SilentlyContinue | Out-Null
