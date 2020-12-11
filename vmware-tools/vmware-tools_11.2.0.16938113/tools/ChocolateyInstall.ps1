3b9a5a52c560e5e01530199cce424bad9b2ba9de
virtualex <info@ithinkvirtual.com>
2020-12-11 11:35:49 -0500

    Update ChocolateyInstall.ps1

--- vmware-tools/vmware-tools_11.2.0.16938113/tools/ChocolateyInstall.ps1
+++ vmware-tools/vmware-tools_11.2.0.16938113/tools/ChocolateyInstall.ps1
@@ -7,7 +7,6 @@ $url               = 'https://packages.vmware.com/tools/releases/11.2.0/windows/
 $checksum          = 'BAA86197B7B2E51D6EF60B6C1C8B4209D8496E8641D0940AAF0B907E6FCB5D37'
 $url64             = 'https://packages.vmware.com/tools/releases/11.2.0/windows/x64/VMware-tools-11.2.0-16938113-x86_64.exe'
 $checksum64        = '12E5190ABCD12C301A1BF04CDADAE471B590A349D5C45BDDD2CAE1B39F258DA5'
-
 $pp                = Get-PackageParameters
 
 if ( $pp.ALL ) {
@@ -48,20 +47,4 @@ if ( $pp.ALL ) {
 		}
 
 		Install-ChocolateyPackage @packageArgs
-}
-
-$packageArgs = @{
-  packageName    = $packageName
-  fileType       = 'EXE'
-  url            = $url
-  url64bit       = $url64
-  validExitCodes = @(0)
-  silentArgs     = '/S /v /qn REBOOT=R'
-  softwareName   = 'VMware Tools'
-  checksum       = $checksum 
-  checksumType   = 'sha256' 
-  checksum64     = $checksum64
-  checksumType64 = 'sha256'
-}
-
-Install-ChocolateyPackage @packageArgs  
+} 
