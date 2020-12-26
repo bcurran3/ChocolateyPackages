3063cac980e4c93325d96c0b8823828afcda53be
Bill Curran <bcurran3@yahoo.com>
2020-12-26 14:36:07 -0800

    advanced-codecs.14.4.7

--- advanced-codecs/tools/chocolateyinstall.ps1
+++ advanced-codecs/tools/chocolateyinstall.ps1
@@ -3,13 +3,15 @@ $ErrorActionPreference = 'Stop';
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $ahkExe         = 'AutoHotKey'
 $ahkFile        = "$toolsDir\advanced-codecs_install.ahk"
-$TodaysVersion  = ($env:ChocolateyPackageVersion -replace '[.]','')
+$culture        = Get-Culture
+$LCID           = $culture.LCID
+$TodaysVersion  = $env:ChocolateyPackageVersion -replace '[.]',''
 
 $packageArgs = @{
   packageName    = 'advanced-codecs'
   softwareName   = 'Shark007 ADVANCED*'
   fileType       = 'EXE'
-  silentArgs     = '/S /v/qn'
+  silentArgs     = "/S /v/qn /l$LCID"
   file           = "$toolsDir\ADVANCED_Codecs_v"+$TodaysVersion+".exe"
   validExitCodes = @(0, 3010, 1641)
   }
@@ -20,4 +22,4 @@ Start-Sleep -s 20
 Start-CheckandStop "Settings32"
 Start-CheckandStop "Settings64"
 Start-CheckandStop "AutoHotkey"
-Remove-Item $toolsDir\*.exe -force | Out-Null
+Remove-Item $toolsDir\*.exe -Force -EA SilentlyContinue | Out-Null
