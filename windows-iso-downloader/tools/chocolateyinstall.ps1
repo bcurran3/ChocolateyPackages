b3f2f88e25ca09244712b9e173845e7071d7bc64
Bill Curran <bcurran3@yahoo.com>
2021-01-05 16:00:37 -0800

    windows-iso-downloader.8.45.0.0152

--- windows-iso-downloader/tools/chocolateyinstall.ps1
+++ windows-iso-downloader/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $packageName   = 'windows-iso-downloader' 
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url           = 'https://www.heidoc.net/php/Windows%20ISO%20Downloader.exe'
-$checksum      = '3319A1826DCFE439399DB6AF81CB65ECB25D372089597C8C507F56DF95B30646'
+$checksum      = '0496B1C953F2F6FEF8B37541A40940987DB49844D018ABEB8E72C331F3AB4A75'
 $ExeFile       = 'Windows ISO Downloader.exe'
 $ShortcutName  = 'Microsoft Windows and Office ISO Download Tool.lnk'
 
