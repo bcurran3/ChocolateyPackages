9022264d6d4ccb87d79ff184d61d6297444ec887
Bill Curran <bcurran3@yahoo.com>
2021-01-12 14:59:17 -0800

    repo fix/update

--- vmware-powercli-psmodule/tools/chocolateyinstall.ps1
+++ vmware-powercli-psmodule/tools/chocolateyinstall.ps1
@@ -1,25 +1,28 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 $packageName = 'vmware-powercli-psmodule'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $shortcutName = 'VMware.PowerCLI.lnk'
 $exe          = "$ENV:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
+$moduleName   = 'VMware.PowerCLI'
+$moduleVers   = "$env:ChocolateyPackageVersion"
 
 $PSversion = $PSVersionTable.PSVersion.Major
-if ($PSversion -lt "5")
-   {
-    Write-Warning "  ** PowerShell < v5 detected."
-	Write-Warning "  ** $packageName installs via the PowerShell Gallery and thus requires PowerShell v5+."
-	Write-Warning "  ** If PowerShell v5 was installed as a dependency, you need to reboot and reinstall this package."
-	throw
+if ($PSversion -lt "5") {
+        Write-Warning "  ** PowerShell < v5 detected."
+        Write-Warning "  ** $packageName installs via the PowerShell Gallery and thus requires PowerShell v5+."
+        Write-Warning "  ** If PowerShell v5 was installed as a dependency, you need to reboot and reinstall this package."
+        throw
    }
 
-if (Get-Module -ListAvailable -Name VMware.PowerCLI -ErrorAction SilentlyContinue){
-# Will fail if packageVersion is a revised version not matching module version, i.e. x.x.x.0020180101
-     Update-Module "VMware.PowerCLI" -RequiredVersion "$ENV:packageVersion" -Force
-   } else {   
-     Get-PackageProvider -Name NuGet -Force
-     Install-Module -Name VMware.PowerCLI -Scope AllUsers -RequiredVersion $ENV:packageVersion -AllowClobber -Force
-   }
+if (Get-Module -ListAvailable -Name $moduleName -ErrorAction SilentlyContinue){
+        Write-Host "  ** Removing installed version, please be patient..." -ForegroundColor Yellow
+        Get-InstalledModule -Name VMware.* | Uninstall-Module -AllVersions -Force -ErrorAction "SilentlyContinue" -Verbose
+   }  
+ 
+# Will fail if package version is a revised version not matching the module version, i.e. x.x.x.0020180101
+Write-Host "`n  ** Installing $moduleName v$moduleVers..." -ForegroundColor Yellow
+Get-PackageProvider -Name NuGet -Force
+Install-Module -Name $moduleName -Scope AllUsers -RequiredVersion $moduleVers -AllowClobber -Force
 
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\$shortcutName" -targetPath $exe -Arguments '-noe -c "Import-Module VMware.PowerCLI"' -WorkingDirectory "$ENV:SystemDrive\" -Description 'VMware.PowerCLI' -IconLocation "$toolsDir\powercli.ico" -RunAsAdmin
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath $exe -Arguments '-noe -c "Import-Module VMware.PowerCLI"' -WorkingDirectory "$ENV:SystemDrive\" -Description 'VMware.PowerCLI' -IconLocation "$toolsDir\powercli.ico" -RunAsAdmin
