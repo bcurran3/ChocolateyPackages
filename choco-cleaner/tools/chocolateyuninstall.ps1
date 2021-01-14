3ed8298f9aa17b6967add73c2b593809f3a173ef
Bill Curran <bcurran3@yahoo.com>
2021-01-14 15:59:10 -0800

    del choco-cleaner.bat during uninstall

--- choco-cleaner/tools/chocolateyuninstall.ps1
+++ choco-cleaner/tools/chocolateyuninstall.ps1
@@ -9,6 +9,7 @@ SchTasks /Delete /TN “choco-cleaner” /F
 Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
 Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$altshortcutName" -Force -ErrorAction SilentlyContinue
 Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\Chocolatey\$shortcutName" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:ChocolateyInstall\bin\choco-cleaner.bat" -Force -ErrorAction SilentlyContinue
 Remove-Item "$scriptDir\choco-cleaner.*" -Force -ErrorAction SilentlyContinue | Out-Null
 if (!(Get-ChildItem -Path "$ENV:ChocolateyToolsLocation\BCURRAN3" | Measure-Object | %{$_.Count})) {
     $ENV:Path.Replace("$ChocolateyToolsLocation\BCURRAN3","") | Out-Null
