8ee1d25f596b4a604f7d57b50d415e6cae3bf282
Bill Curran <bcurran3@yahoo.com>
2021-01-30 12:37:12 -0800

    screenlogic-connect.5.2.738.0020210130

--- screenlogic-connect/tools/ChocolateyInstall.ps1
+++ screenlogic-connect/tools/ChocolateyInstall.ps1
@@ -1,24 +1,30 @@
-﻿$packageName    = 'screenlogic-connect'
-$installerType  = 'exe'
-$url            = 'https://pentairpool.com/~/media/websites/pool/software/screenlogic/build-5-2-736/step%202%20-%20install%20screenlogic%20connect%20software%20736.exe?la=en'
-$checksum       = 'A66F3C86BACF0F813976F12D8E4C38FE21D2A26A935D4C92E1FF4F5E8B6A12F3'
-$silentArgs     = '/quiet'
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'screenlogic-connect'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$validExitCodes = @(0,1)
-$softwareName   = 'Pentair ScreenLogic'
+$url            = 'https://www.pentair.com/content/dam/extranet/pentair-pool/residential/software-firmware/screenlogic/setup.exe'
+$checksum       = '6DEABE14ACD64F1ECEEA6677AB0E8AABFE99C80B2A1717B3F3A2E9BE583A70DA'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir  
-  fileType      = $installerType
-  url           = $url
-  validExitCodes= $validExitCodes
-  silentArgs    = $silentArgs
-  softwareName  = $softwareName
-  checksum      = $checksum
-  checksumType  = 'sha256' 
+  packageName  = $packageName
+  FileFullPath = "$toolsDir\pentairupdate.exe"
+  url          = 'https://www.pentair.com/content/dam/extranet/pentair-pool/residential/software-firmware/screenlogic/pentairupdate.exe'
+  checksum     = 'BC3A19B1B01A0D0AEEDFDC85584EF77B6A8ED783E0D0BEFEB4F3291761AFA3BA'
+  checksumType = 'sha256'
+}
+
+Write-Host "  ** Downloading Protocol Adapter firmware update program v5.2 Build 738.0 Rel. (03/26/19) for manual installation." -Foreground Magenta
+Get-ChocolateyWebFile @packageArgs
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir  
+  fileType       = 'EXE'
+  url            = $url
+  validExitCodes = @(0,1)
+  silentArgs     = '/quiet'
+  softwareName   = 'Pentair ScreenLogic'
+  checksum       = $checksum
+  checksumType   = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
-
-
