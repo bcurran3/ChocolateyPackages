76e85be8fd6c163c3f8f4890a8a37715207a7e4f
bcurran3 <bcurran3@yahoo.com>
2021-02-01 14:40:48 -0800

    allway-sync.20.2.1

--- allway-sync/tools/chocolateyinstall.ps1
+++ allway-sync/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url         = 'https://allwaysync.com/content/download/allwaysync.exe'
-$checksum    = 'D97CBDB54C10D008918E8C97E3EA7F506B99962D2373347C1D2CB1753CF30719'
+$checksum    = 'EFD10324D81669FA47ECE5CA98D1C043C4717CB81E97BEF926C21C110532A0F2'
 $url64       = 'https://allwaysync.com/content/download/allwaysync-x64.exe'
-$checksum64  = 'B6D72B4D9EBF64ABDD1443AFB22C619145AA66634FECC8195E81FA45AB372F75'
+$checksum64  = '6A2B489E07964975B666CC82492DF21EE9A5DB51B4827E39D0694F6FCAF08D89'
 
 $packageArgs = @{
   packageName    = 'allway-sync'
