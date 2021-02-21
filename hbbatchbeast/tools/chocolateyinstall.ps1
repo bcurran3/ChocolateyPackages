4e125ecf089306571bc978324b52376a56073766
Bill Curran <bcurran3@yahoo.com>
2021-02-21 04:57:54 -0800

    HBBatchBeast WIP

---
+++ hbbatchbeast/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$shortcutName = 'HBBatchBeast.lnk'
+$exe          = 'hbbatchbeast.exe'
+
+$packageArgs = @{
+  packageName    = 'hbbatchbeast'
+  unzipLocation  = $toolsDir
+  fileType       = 'ZIP' 
+  file           = "$toolsDir\hbbatchbeast-Windows-"+"$env:ChocolateyPackageVersion"+".7z"
+  }
+
+Install-ChocolateyZipPackage @packageArgs	
+
+Remove-Item $toolsDir\*.zip -EA SilentlyContinue | Out-Null
+
+Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath "$toolsDir\$exe" -WorkingDirectory "$toolsDir\"
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$toolsDir\$exe" -WorkingDirectory "$toolsDir\"
---
+++ hbbatchbeast/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'hbbatchbeast' 
+$shortcutName = 'HBBatchBeast.lnk'
+
+Remove-Item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
