41f96a95708b6c1ff6873f6caa62ac4ec1046477
Bill Curran <bcurran3@yahoo.com>
2021-03-10 15:13:14 -0800

    hbbatchbeast.2.1.5

--- hbbatchbeast/tools/chocolateyinstall.ps1
+++ hbbatchbeast/tools/chocolateyinstall.ps1
@@ -1,18 +1,31 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $shortcutName = 'HBBatchBeast.lnk'
-$exe          = 'hbbatchbeast.exe'
+$exe          = "hbbatchbeast-Windows-v"+"$env:ChocolateyPackageVersion"+"\hbbatchbeast.exe"
+
+Remove-Item "$toolsDir\hbbatchbeast-Windows-v*" -Exclude *.7z -Recurse -EA SilentlyContinue
 
 $packageArgs = @{
   packageName    = 'hbbatchbeast'
-  unzipLocation  = $toolsDir
+  UnzipLocation  = $toolsDir
   fileType       = 'ZIP' 
-  file           = "$toolsDir\hbbatchbeast-Windows-"+"$env:ChocolateyPackageVersion"+".7z"
+  file           = "$toolsDir\hbbatchbeast-Windows-v"+"$env:ChocolateyPackageVersion"+".7z"
   }
 
-Install-ChocolateyZipPackage @packageArgs	
+Install-ChocolateyZipPackage @packageArgs
 
-Remove-Item $toolsDir\*.zip -EA SilentlyContinue | Out-Null
+$IgnoreFile = "$toolsDir\hbbatchbeast-Windows-v"+"$env:ChocolateyPackageVersion"+'\HandBrakeCLI.exe.ignore'
+Echo "no shim" | Out-File -Encoding ASCII -FilePath $IgnoreFile
+$IgnoreFile = "$toolsDir\hbbatchbeast-Windows-v"+"$env:ChocolateyPackageVersion"+'\hbbatchbeast.exe.ignore'
+Echo "no shim" | Out-File -Encoding ASCII -FilePath $IgnoreFile
+$IgnoreFile = "$toolsDir\hbbatchbeast-Windows-v"+"$env:ChocolateyPackageVersion"+'\resources\app.asar.unpacked\node_modules\@ffmpeg-installer\win32-x64\ffmpeg.exe.ignore'
+Echo "no shim" | Out-File -Encoding ASCII -FilePath $IgnoreFile
+$IgnoreFile = "$toolsDir\hbbatchbeast-Windows-v"+"$env:ChocolateyPackageVersion"+'\resources\app.asar.unpacked\node_modules\ffprobe-static\bin\win32\ia32\ffprobe.exe.ignore'
+Echo "no shim" | Out-File -Encoding ASCII -FilePath $IgnoreFile
+$IgnoreFile = "$toolsDir\hbbatchbeast-Windows-v"+"$env:ChocolateyPackageVersion"+'\resources\app.asar.unpacked\node_modules\ffprobe-static\bin\win32\x64\ffprobe.exe.ignore'
+Echo "no shim" | Out-File -Encoding ASCII -FilePath $IgnoreFile
+
+Remove-Item $toolsDir\*.7z -EA SilentlyContinue | Out-Null
 
 Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\$shortcutName" -targetPath "$toolsDir\$exe" -WorkingDirectory "$toolsDir\"
 Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$toolsDir\$exe" -WorkingDirectory "$toolsDir\"
