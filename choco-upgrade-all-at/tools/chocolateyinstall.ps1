9353f03c57a6396fd7e158ec235c38df7fc977fe
bcurran3 <bcurran3@yahoo.com>
2021-03-13 01:42:48 -0800

    changed ABORTTIME default handling

--- choco-upgrade-all-at/tools/chocolateyinstall.ps1
+++ choco-upgrade-all-at/tools/chocolateyinstall.ps1
@@ -34,17 +34,20 @@ if ($GotTask -ne $null){
      &SchTasks /DELETE /TN "choco-upgrade-all-at" /F 
    }
 
-$GotTask = (&schtasks /QUERY /TN choco-upgrade-all) 2> $null
+# check if choco-ugrade-all-at task already exists and exit if so
+$GotTask = (&schtasks /QUERY /TN choco-upgrade-all-at-rev3) 2> $null
 if ($GotTask -ne $null){
-     Write-Host "  ** Existing choco-upgrade-all scheduled task found. Keeping existing scheduled task.`n     If you want to change the task runtime or abort time, uninstall and reinstall the package." -Foreground Magenta 
+     Write-Host "  ** Existing choco-upgrade-all-at scheduled task found. Keeping existing scheduled task.`n     If you want to change the task runtime or abort time, uninstall and reinstall the package." -Foreground Magenta 
      exit
    }
 
+# exit if a scheduled task is not wanted
 if ($pp["NOTASK"] -eq 'true' -or $pp["NOSCHEDULE"] -eq 'true'){
        Write-Host "  ** NOTASK or NOSCHEDULE specified, not installing scheduled task." -Foreground Magenta
 	   exit
    }
-   
+
+# begin setup of scheduled task if one doesn't exist
 Write-Host "" 
 Write-Host "CHOCO-UPGRADE-ALL-AT Summary:" -Foreground Magenta
 
@@ -61,25 +64,25 @@ if ($pp["USER"] -eq $null -or $pp["USER"] -eq ''){
 	}
 
 if ($pp["TIME"] -eq $null -or $pp["TIME"] -eq ''){
-       Write-Host "  ** TIME NOT specified, defaulting to 2 AM." -Foreground Magenta
 	   $RunTime = "02:00"
+       Write-Host "  ** TIME NOT specified, defaulting to $RunTime." -Foreground Magenta
      } else {
 	   $RunTime = $pp["TIME"]
 	   Write-Host "  ** TIME specified as $RunTime." -Foreground Magenta
       } 
 
 if ($pp["ABORTTIME"] -eq $null -or $pp["ABORTTIME"] -eq ''){
-      Write-Host "  ** ABORTTIME NOT specified, defaulting to 4 AM." -Foreground Magenta
-	  $AbortTime = "04:00"
+      $AbortTime = (Get-Date $RunTime).AddHours(2).ToString("HH:mm")
+      Write-Host "  ** ABORTTIME NOT specified, defaulting to $AbortTime." -Foreground Magenta
     } else {
 	  $AbortTime = $pp["ABORTTIME"]
 	  Write-Host "  ** ABORTTIME specified as $AbortTime." -Foreground Magenta
-    } 	  
+    }
 	  
 if (($pp["DAILY"] -eq $null -or $pp["DAILY"] -eq '') -and ($pp["WEEKLY"] -eq $null -or $pp["WEEKLY"] -eq '')){
       Write-Host "  ** DAILY or WEEKLY NOT specified, defaulting to DAILY." -Foreground Magenta
-      SchTasks /CREATE /SC DAILY /RU $RunAsUser /RL HIGHEST /TN choco-upgrade-all /TR "cmd /c powershell -NoProfile -ExecutionPolicy Bypass -Command %ChocolateyToolsLocation%\BCURRAN3\choco-upgrade-all.ps1" /ST $RunTime /F
-	  SchTasks /QUERY /TN "choco-upgrade-all"
+      SchTasks /CREATE /SC DAILY /RU $RunAsUser /RL HIGHEST /TN choco-upgrade-all-at-rev3 /TR "cmd /c powershell -NoProfile -ExecutionPolicy Bypass -Command %ChocolateyToolsLocation%\BCURRAN3\choco-upgrade-all.ps1" /ST $RunTime /F
+	  SchTasks /QUERY /TN "choco-upgrade-all-at-rev3"
 	  SchTasks /CREATE /SC DAILY /RU $RunAsUser /RL HIGHEST /TN choco-upgrade-all-at-abort /TR "taskkill /im choco.exe /f /t" /ST $AbortTime /F
       SchTasks /QUERY /TN "choco-upgrade-all-at-abort"
 	  Start-Sleep -s 10
@@ -90,8 +93,8 @@ if ($pp["DAILY"] -eq $null -or $pp["DAILY"] -eq ''){
        Write-Host "  ** DAILY NOT specified." -Foreground Magenta
      } else {
 	   Write-Host "  ** DAILY specified." -Foreground Magenta
-	   SchTasks /CREATE /SC DAILY /RU $RunAsUser /RL HIGHEST /TN choco-upgrade-all /TR "cmd /c powershell -NoProfile -ExecutionPolicy Bypass -Command %ChocolateyToolsLocation%\BCURRAN3\choco-upgrade-all.ps1" /ST $RunTime /F
-	   SchTasks /QUERY /TN "choco-upgrade-all"
+	   SchTasks /CREATE /SC DAILY /RU $RunAsUser /RL HIGHEST /TN choco-upgrade-all-at-rev3 /TR "cmd /c powershell -NoProfile -ExecutionPolicy Bypass -Command %ChocolateyToolsLocation%\BCURRAN3\choco-upgrade-all.ps1" /ST $RunTime /F
+	   SchTasks /QUERY /TN "choco-upgrade-all-at-rev3"
 	   SchTasks /CREATE /SC DAILY /RU $RunAsUser /RL HIGHEST /TN choco-upgrade-all-at-abort /TR "taskkill /im choco.exe /f /t" /ST $AbortTime /F
        SchTasks /QUERY /TN "choco-upgrade-all-at-abort"
 	   Start-Sleep -s 10
@@ -104,16 +107,16 @@ if ($pp["WEEKLY"] -eq $null -or $pp["WEEKLY"] -eq ''){
 	   Write-Host "  ** WEEKLY specified." -Foreground Magenta
        if ($pp["DAY"] -eq $null -or $pp["DAY"] -eq ''){
             Write-Host " * DAY NOT specified, defaulting to SUNDAY." -Foreground Magenta
-            SchTasks /CREATE /SC WEEKLY /D SUN /RU $RunAsUser /RL HIGHEST /TN choco-upgrade-all /TR "cmd /c powershell -NoProfile -ExecutionPolicy Bypass -Command %ChocolateyToolsLocation%\BCURRAN3\choco-upgrade-all.ps1" /ST $RunTime /F
-		    SchTasks /QUERY /TN "choco-upgrade-all"
+            SchTasks /CREATE /SC WEEKLY /D SUN /RU $RunAsUser /RL HIGHEST /TN choco-upgrade-all-at-rev3 /TR "cmd /c powershell -NoProfile -ExecutionPolicy Bypass -Command %ChocolateyToolsLocation%\BCURRAN3\choco-upgrade-all.ps1" /ST $RunTime /F
+		    SchTasks /QUERY /TN "choco-upgrade-all-at-rev3"
 			SchTasks /CREATE /SC WEEKLY /D SUN /RU $RunAsUser /RL HIGHEST /TN choco-upgrade-all-at-abort /TR "taskkill /im choco.exe /f /t" /ST $AbortTime /F
             SchTasks /QUERY /TN "choco-upgrade-all-at-abort"
           } else {
 		    $RunDay = $pp["DAY"]
             Write-Host "  ** DAY specified as $RunDay." -Foreground Magenta
-		    SchTasks /CREATE /SC WEEKLY /D $pp["DAY"] /RU $RunAsUser /RL HIGHEST /TN choco-upgrade-all /TR "cmd /c powershell -NoProfile -ExecutionPolicy Bypass -Command %ChocolateyToolsLocation%\BCURRAN3\choco-upgrade-all.ps1" /ST $RunTime /F
-		    SchTasks /QUERY /TN "choco-upgrade-all"
-			SchTasks /CREATE /SC WEEKLY /D $pp["DAY"] /RU $RunAsUser /RL HIGHEST /TN choco-upgrade-all-at-abort /TR "taskkill /im choco.exe /f /t" /ST $AbortTime /F
+		    SchTasks /CREATE /SC WEEKLY /D $pp["DAY"] /RU $RunAsUser /RL HIGHEST /TN choco-upgrade-all-at-rev3 /TR "cmd /c powershell -NoProfile -ExecutionPolicy Bypass -Command %ChocolateyToolsLocation%\BCURRAN3\choco-upgrade-all.ps1" /ST $RunTime /F
+		    SchTasks /QUERY /TN "choco-upgrade-all-at-rev3"
+            SchTasks /CREATE /SC WEEKLY /D $pp["DAY"] /RU $RunAsUser /RL HIGHEST /TN choco-upgrade-all-at-abort /TR "taskkill /im choco.exe /f /t" /ST $AbortTime /F
             SchTasks /QUERY /TN "choco-upgrade-all-at-abort"
 	      }
 		}
