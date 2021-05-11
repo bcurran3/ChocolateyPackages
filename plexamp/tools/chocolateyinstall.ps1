d16b715720d870e5960ffdc627e3995095a35e4f
bcurran3 <bcurran3@yahoo.com>
2021-05-11 00:44:37 -0700

    plexamp.3.4.5

--- plexamp/tools/chocolateyinstall.ps1
+++ plexamp/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $packageName    = 'plexamp'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64          = "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp%20Setup%20$env:ChocolateyPackageVersion.exe"
-$checksum64     = '4209AC596D861FBE2F5779B4A8B65B5B2B47005F77F60B8681A9B5975FEEE868'
+$checksum64     = '0C09E49A014740B58D7A3729581F5BFDF92D6E529A434A0C575D19952FA1F64C'
 
 $packageArgs = @{
   packageName    = $packageName
