687cdb646e79828078707a27ac1ac1f81b20ff59
Bill Curran <bcurran3@yahoo.com>
2021-05-13 09:11:11 -0700

    intel-dsa.21.2.13.9

--- intel-dsa/tools/chocolateyinstall.ps1
+++ intel-dsa/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = 'intel-dsa'
 $url         = 'https://downloadmirror.intel.com/28425/a08/Intel-Driver-and-Support-Assistant-Installer.exe'
-$checksum    = '86D6AC2C91D779393F353F35DAD40E8C084B13452373E17DC1C5DE38FDBC0B8B'
+$checksum    = '350C6A4ECB33EE2260865BA65A42EB18A2EA31D3F0C2EE6FF814954647BBD52E'
 
 $packageArgs = @{
   packageName    = $packageName
