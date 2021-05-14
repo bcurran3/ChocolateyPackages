91b5201c974f0a6c2b083a96e2522ce526e5dfd3
Bill Curran <bcurran3@yahoo.com>
2021-05-14 13:15:27 -0700

    hdhomerun-view.2021.03.01

--- hdhomerun-view/tools/chocolateyinstall.ps1
+++ hdhomerun-view/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName    = 'hdhomerun-view'
-$url            = 'http://download.silicondust.com/hdhomerun/hdhomerun_windows_20200907.exe' 
-$checksum       = '89BA8782090851A14EF9E94900993A65CAAB975DF72AB3AB104B0A8E3E2EB253'
+$url            = 'http://download.silicondust.com/hdhomerun/hdhomerun_windows_20210301.exe' 
+$checksum       = '9AE106C78E48AC2DBEBAA9374C03307BBA8620FFDB5ED060623F1402031F5C55'
 
 $packageArgs = @{
   packageName   = $packageName
