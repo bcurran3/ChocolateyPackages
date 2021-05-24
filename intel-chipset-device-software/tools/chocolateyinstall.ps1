2a6acbbde7fe89d516243d000898d957bf898af4
Bill Curran <bcurran3@yahoo.com>
2021-05-23 22:03:17 -0700

    intel-chipset-device-software.10.1.18716.8265

--- intel-chipset-device-software/tools/chocolateyinstall.ps1
+++ intel-chipset-device-software/tools/chocolateyinstall.ps1
@@ -1,28 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName    = 'intel-chipset-device-software'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://downloadmirror.intel.com/29634/eng/chipset-10.1.18383.8213-public-mup.zip'
-$checksum       = '8756C6BB1AF5A1B403F55938C6AEAC1B642DCFE3DC5E4B15A225992D7A16546D'
+$url            = 'https://downloadmirror.intel.com/30334/eng/SetupChipset.exe'
+$checksum       = 'D4A1891CD83E19FBB3D908F6DB3BFD516F65E06108FBE0C45FB7FC52F132D3B4'
 
 $packageArgs = @{
   packageName    = $packageName
-  unzipLocation  = "$toolsDir\unzipped"
-  fileType       = 'ZIP' 
+  fileType       = 'EXE'
   url            = $url
+  validExitCodes = @(0,1,3010)
+  silentArgs     = '-s -norestart -downgrade'
+  softwareName   = 'Intel(R) Chipset Device Software'
   checksum       = $checksum
-  checksumType   = 'sha256'
+  checksumType   = 'sha256' 
 }
 
-Install-ChocolateyZipPackage @packageArgs
-
-$packageArgs = @{
-  packageName    = $packageName
-  softwareName   = 'Intel(R) Chipset Device Software'
-  fileType       = 'EXE'
-  silentArgs     = '-s -norestart -log install.log'
-  file           = "$toolsDir\unzipped\SetupChipset.exe"
-  validExitCodes = @(0)
-  }
-
-Install-ChocolateyInstallPackage @packageArgs
-Remove-Item "$toolsDir\unzipped" –Recurse | Out-Null
+Install-ChocolateyPackage @packageArgs
