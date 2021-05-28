6a72e700ddd003e7de65ff9a388d5cd728742a14
Bill Curran <bcurran3@yahoo.com>
2021-05-28 10:32:54 -0700

    intel-rst-driver.18.1.6.1039

--- intel-rst-driver/tools/ChocolateyInstall.ps1
+++ intel-rst-driver/tools/ChocolateyInstall.ps1
@@ -1,8 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = 'intel-rst-driver' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://downloadmirror.intel.com/29978/eng/SetupRST.exe'
-$checksum    = '4307EEF4789AA4977182150A6866310E4A76ACBB6D5D369C93E7E77FF0E3CC9A'
+$url         = 'https://downloadmirror.intel.com/30314/eng/SetupRST.exe'
+#                                                ^^^^^ is what changes with the version
+$checksum    = 'E3C7BDAEA88511920CFD16B42DC7DF75F53476F4CC6CB11DAB2B73CCFF8481A9'
 
 if ((Get-IsWin10 -eq $false) -and (Get-IsWinServer2016 -eq $false)){
    Write-Warning "  ** This version is Windows 10 and Windows Server 2016 only."
