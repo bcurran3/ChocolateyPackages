92633b8b8b9d9a161fdd0c28ca59fc457b77d6c9
bcurran3 <bcurran3@yahoo.com>
2021-06-06 12:58:02 -0700

    dell-update.4.2.0

--- dell-update/tools/chocolateyinstall.ps1
+++ dell-update/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = 'dell-update' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://dl.dell.com/FOLDER06986434M/1/Dell-Update-Application-for-Windows-10_7CKK6_WIN_4.1.0_A00.EXE'
-$checksum    = 'b0d5c1735d0d771917d7cb76422d1c7391ce68cbc57baca4360848c14e9353f7'
+$url         = 'https://dl.dell.com/FOLDER07337369M/1/Dell-Update-Application-for-Windows10_0672W_WIN_4.2.0_A00.EXE'
+$checksum    = '06D930E205190E17ED879EE14A73E86223157C10F72CBCF09E79CEF8E9229919'
 
 $packageArgs = @{
   packageName   = $packageName
