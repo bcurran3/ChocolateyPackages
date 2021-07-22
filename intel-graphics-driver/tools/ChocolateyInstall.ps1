ea35add9d7b8c2fb8f05238b1ca5eb7364ba7889
bcurran3 <bcurran3@yahoo.com>
2021-07-22 11:09:25 -0700

    intel-graphics-driver.30.0.100.9684

--- intel-graphics-driver/tools/ChocolateyInstall.ps1
+++ intel-graphics-driver/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = 'intel-graphics-driver' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = 'https://downloadmirror.intel.com/30381/a08/igfx_win10_100.9466.exe'
-$checksum64  = '266C4921DFE3DAA1AC190522B75170558519ADF7D136F99A1D9E6269A39D7ECC'
+$url64       = 'https://downloadmirror.intel.com/30579/a08/igfx_win_100.9684.exe'
+$checksum64  = '91FA5A58175339C100FEFA05A97D4011ACE4DD9C9924661255BC0C90BEA04906'
 
 if (!(Get-IsWin10)){
     Write-Warning "  ** This version is only for Windows 10."
@@ -15,14 +15,15 @@ if (!(Get-IsIntelVideo)){
    }
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE' 
-  url64bit      = $url64
-  checksum64    = $checksum64
-  checksumType64= 'sha256'
-  silentArgs    = "-overwrite -s -report $toolsDir\install.log"
-  softwareName  = ''
-  }
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE' 
+  url64bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+  silentArgs     = "--overwrite --silent --report $toolsDir\install.log"
+  softwareName   = ''
+  validExitCodes = @(0, 3010, 1641, 14)
+   }
 
 Install-ChocolateyPackage @packageArgs
