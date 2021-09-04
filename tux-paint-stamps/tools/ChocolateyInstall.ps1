a300ba63532ab93cbaccb52fbf33e1ba0532b2ce
Bill Curran <bcurran3@yahoo.com>
2021-09-04 16:03:40 -0700

    tux-paint-stamps.2021.06.28

--- tux-paint-stamps/tools/ChocolateyInstall.ps1
+++ tux-paint-stamps/tools/ChocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName    = 'tux-paint-stamps'
   fileType       = 'EXE'
-  file           = "$toolsDir\tuxpaint-stamps-2020-05-29-win32-installer.exe"
+  file           = "$toolsDir\tuxpaint-stamps-2021-06-28-windows-installer.exe"
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0,1)
   softwareName   = 'Tux Paint Stamps*'
