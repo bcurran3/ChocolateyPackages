5d5c301f5326a52aa8e39220bcd06cd9a35fb53b
Bill Curran <bcurran3@yahoo.com>
2021-09-04 15:19:08 -0700

    utorrent.3.5.5.46074

--- utorrent/tools/chocolateyinstall.ps1
+++ utorrent/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $packageName  = 'utorrent'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url          = 'http://download-hr.utorrent.com/track/stable/endpoint/utorrent/os/windows'
-$checksum     = 'C1EC45E575687AD3F37BD7C8679EEFBE94A90A6873CD4050D581FA97AF8ED9BC'
+$checksum     = '41B0EBFE8BD3341B87E354C8CE536FEBD589C00019267F3D40D10E5F44FFC2D7'
 $extractDir   = "$toolsDir\extracted"
 $fileLocation = "$extractDir\Carrier.exe"
 
