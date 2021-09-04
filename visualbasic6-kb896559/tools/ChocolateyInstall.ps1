aafbe7a0eef5e3869479ebcc8dd0fe3149921b9d
Bill Curran <bcurran3@yahoo.com>
2021-09-04 15:33:44 -0700

    visualbasic6-kb896559.6.0.2800.1107

--- visualbasic6-kb896559/tools/ChocolateyInstall.ps1
+++ visualbasic6-kb896559/tools/ChocolateyInstall.ps1
@@ -3,14 +3,14 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $ahkExe         = 'AutoHotKey'
 $ahkFile        = "$toolsDir\visualbasic6-kb896559_install.ahk"
 $url            = 'https://download.microsoft.com/download/3/a/5/3a5925ac-e779-4b1c-bb01-af67dc2f96fc/VisualBasic6-KB896559-v1-ENU.exe'
-$checksum       = 'C4D43FE4AEA782D7D6C08C038778FE3635C7BAE1B93FB1A7740C6644AABF567E'
+$checksum       = '32857E7B99254630F292B8F268FD60FEB128B5DC5BD7E50F4C2C6BAA5EA04EB3'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir  
   fileType      = 'EXE'
   url           = $url
-  validExitCodes= @(0,1)
+  validExitCodes= @(0,1, 3010)
   silentArgs    = '/Q'
   softwareName  = ''
   checksum      = $checksum
