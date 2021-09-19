632aab9c1fe4879ee64e2cdb71e5e295d9a40c8e
Bill Curran <bcurran3@yahoo.com>
2021-09-18 20:05:59 -0700

    fix link

--- hexamail-pop3-downloader/tools/ChocolateyInstall.ps1
+++ hexamail-pop3-downloader/tools/ChocolateyInstall.ps1
@@ -2,7 +2,7 @@
 # https://www.hexamail.com/download.html?page=response&did=NzgwMTc7NjA3NzA7MTU7MTI
 $packageName = 'hexamail-pop3-downloader'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = 'https://www.hexamail.com/download/hexamailpop3downloadersetup5.9.38.003_64.exe'
+$url64       = 'https://www.hexamail.com/download/hexamailpop3downloadersetup5.9.60.003_64.exe'
 $checksum64  = 'FBDD05E8F5755B814986754B6DE62BECDCAE0F8BC1AEEF69A44B5AE5A3188F26'
 
 $packageArgs = @{
