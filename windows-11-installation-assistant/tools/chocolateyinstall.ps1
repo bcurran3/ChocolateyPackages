772f523929399f364798a6b28613e7475fbda929
bcurran3 <bcurran3@yahoo.com>
2021-10-05 23:34:49 -0700

    windows-11-installation-assistant.1.4.19041.1285

---
+++ windows-11-installation-assistant/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'windows-11-installation-assistant' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'https://go.microsoft.com/fwlink/?linkid=2171764'
+$checksum    = '9D04E6C0EEF448763AD50BF094C8076F4CE6673A83232FB200355F20F77F021A'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = '' 
+  softwareName  = 'Windows 11 Installation Assistant' 
+}
+
+Install-ChocolateyPackage @packageArgs
+Start-Sleep -Seconds 5
+Start-CheckandStop " Windows10UpgraderApp"
