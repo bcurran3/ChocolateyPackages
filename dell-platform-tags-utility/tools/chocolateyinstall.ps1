580250e2ca11304a04f354cbe28fdb18332c4172
bcurran3 <bcurran3@yahoo.com>
2021-10-27 00:30:58 -0700

    dell-platform-tags-utility.4.0.30.0

--- dell-platform-tags-utility/tools/chocolateyinstall.ps1
+++ dell-platform-tags-utility/tools/chocolateyinstall.ps1
@@ -1,7 +1,8 @@
-﻿$packageName  = 'dell-platform-tags-utility' 
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'dell-platform-tags-utility' 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://downloads.dell.com/FOLDER03706387M/1/Setup.exe'
-$checksum     = '6FFAA93612CC03A881CE9358C69A96EE10597D32D2082B54935B5546F5DEE765'
+$url          = 'https://downloads.dell.com/FOLDER06126915M/2/PlatTagsSWB40300_ZPE.exe'
+$checksum     = '8A0926B89834BAAA1AE9E4A9A985AD2C46975127F8A3BFAB99950C3DABD4C224'
 
 $packageArgs = @{
   packageName   = $packageName
