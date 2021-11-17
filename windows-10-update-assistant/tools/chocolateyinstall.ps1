817a3656c9e4432c536771eb6a3bd495f108615a
bcurran3 <bcurran3@yahoo.com>
2021-11-17 00:53:00 -0800

    windows-10-update-assistant.1.4.19041.1375

--- windows-10-update-assistant/tools/chocolateyinstall.ps1
+++ windows-10-update-assistant/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = 'windows-10-update-assistant' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://download.microsoft.com/download/3/0/1/301147e7-e3d3-467e-a9eb-003a72840887/Windows10Upgrade9252.exe'
-$checksum    = 'A83C1BA49475ED7DEAEF6E47766819317D9E6E50C7A5DB8175BDDF63BFBA5965'
+$url         = 'https://download.microsoft.com/download/f/1/9/f19ea9d5-1b5f-4a4d-9f50-04cbf63dc9c6/Windows10Upgrade9252.exe'
+$checksum    = 'D2447038C5DA0853F3F12F12B3589B4E3445889FB306D2DCFCC37FCA0561186C'
 
 $packageArgs = @{
   packageName   = $packageName
