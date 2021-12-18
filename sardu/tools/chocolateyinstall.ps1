40738fb113d258ca0dc639f437cfd165164ed867
Bill Curran <bcurran3@yahoo.com>
2021-12-18 15:31:54 -0800

    sardu.4.0.0

--- sardu/tools/chocolateyinstall.ps1
+++ sardu/tools/chocolateyinstall.ps1
@@ -3,10 +3,12 @@ $packageName   = 'sardu'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $TodaysVersion = ($ENV:ChocolateyPackageVersion -replace '[.]','')
 $url           = "https://www.sarducd.it/download/SARDU_"+$TodaysVersion+".zip"
-$checksum      = 'B0A4BCFBE2B3ECCB2E6421FAEA40676A5926CC4FA7BC1B827D4AB967430A8124'
+$checksum      = '38266E20A6CD589BAC04C119E6230D2C65FB0EF5567C4E48B64F470D1A5E447F'
 $shortcutName  = 'SARDU.lnk'
 $workingDir    = "SARDU_$TodaysVersion"
-$exe           = 'sardu_3.exe'
+$exe           = 'sardu_4.exe'
+
+Remove-Item "$toolsDir\SARDU_*" -recurse -force # remove old versions but will break old version if new install fails
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,8 +17,10 @@ $packageArgs = @{
   url           = $url
   checksum      = $checksum
   checksumType  = 'sha256'
-}
+ }
 
 Install-ChocolateyZipPackage @packageArgs
+New-Item "$toolsDir\SARDU_$TodaysVersion\7z.exe.ignore" -type file
+New-Item "$toolsDir\SARDU_$TodaysVersion\sardu_4.exe.ignore" -type file
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\$shortcutName" -targetPath "$toolsDir\$workingDir\$exe" -WorkingDirectory "$toolsDir\$workingDir"
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$toolsDir\$workingDir\$exe" -WorkingDirectory "$toolsDir\$workingDir"
