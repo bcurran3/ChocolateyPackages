9bf316e19f14dafea92d724ad6ca894dbc0ea0aa
bcurran3 <bcurran3@yahoo.com>
2021-12-23 23:21:19 -0800

    log4jscanwin.1.2.18

--- log4jscanwin/tools/chocolateyinstall.ps1
+++ log4jscanwin/tools/chocolateyinstall.ps1
@@ -4,6 +4,6 @@ $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $bits        = Get-ProcessorBits
 
 Get-ChocolateyUnzip -FileFullPath "$toolsDir\Log4jScanner-$ENV:ChocolateyPackageVersion.zip" -Destination $toolsDir
-if ($bits -eq 64){New-Item "$toolsDir\Log4jScanner\x86\Log4jScanner.exe.ignore" -type file | Out-Null}
-if ($bits -eq 32){New-Item "$toolsDir\Log4jScanner\x64\Log4jScanner.exe.ignore" -type file | Out-Null}
+if ($bits -eq 64){New-Item "$toolsDir\Log4jScanner\x86\Log4jScanner.exe.ignore" -type file -force | Out-Null}
+if ($bits -eq 32){New-Item "$toolsDir\Log4jScanner\x64\Log4jScanner.exe.ignore" -type file -force | Out-Null}
 Remove-Item "$toolsDir\*.zip" -EA SilentlyContinue | Out-Null
