d86f21f6a1f06f9360414c389750c9506e9a1f12
Bob Tanner <tanner@real-time.com>
2021-12-30 20:09:00 -0600

    Updated sha256 checksum Added .gitignore

--- arq/tools/chocolateyinstall.ps1
+++ arq/tools/chocolateyinstall.ps1
@@ -1,18 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName   = 'arq' 
-$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url           = 'https://www.arqbackup.com/download/arqbackup/Arq7.msi' 
-$checksum      = '359F1A23EAAD22D3542A4A38BF5E8E8EA6146F2415BA863C3AF8728C96972F05'
+$packageName = 'arq'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://www.arqbackup.com/download/arqbackup/Arq7.msi'
+$checksum = '2eaba8af024a3c238946a268504dc55ca5986b6021461fbc58be70c4cde2ba23'
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'MSI'
-  url           = $url
-  validExitCodes= @(0, 3010, 1641)
-  silentArgs    = '/quiet /norestart'
-  softwareName  = ''
-  checksum      = $checksum
-  checksumType  = 'sha256' 
+    packageName    = $packageName
+    fileType       = 'MSI'
+    url            = $url
+    validExitCodes = @(0, 3010, 1641)
+    silentArgs     = '/quiet /norestart'
+    softwareName   = ''
+    checksum       = $checksum
+    checksumType   = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
