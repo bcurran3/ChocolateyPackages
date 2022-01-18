26ee70991d4082484763d13f701a4e5d6452a755
Bill Curran <bcurran3@yahoo.com>
2022-01-18 11:59:15 -0800

    solarwinds-recovery-console.21.10.0.21332

--- solarwinds-recovery-console/tools/ChocolateyInstall.ps1
+++ solarwinds-recovery-console/tools/ChocolateyInstall.ps1
@@ -2,9 +2,9 @@
 $packageName    = 'solarwinds-recovery-console'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://install.mob.system-monitor.com/backup-and-recovery-rc-windows-x86.exe'
-$checksum       = '89ED88CE6834005D0EF41D14C2E2C86A4796DC6BE79382BCBA2A76BC5170A0E5'
+$checksum       = 'A5687B01ADA98623EABF01B63F858769AA77AC1BE4B63F2FEE728C7104973E7C'
 $url64          = 'https://install.mob.system-monitor.com/backup-and-recovery-rc-windows-x64.exe'
-$checksum64     = '4ADC867826C9A60888D7697E08534F12D86CDEA27BAF945B363AE2B15F524A75'
+$checksum64     = '138E1C519B9018FF9B36A6D47A9014571AB7D8C46AA54F5568612318A6670383'
 
 $packageArgs = @{
   packageName    = $packageName
