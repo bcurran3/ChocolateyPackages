b66ab3bf7675df25006de50b920c4cf30ef603a2
Bill Curran <bcurran3@yahoo.com>
2022-04-02 17:09:28 -0700

    intel-dsa.22.2.14.5

--- intel-dsa/tools/chocolateyinstall.ps1
+++ intel-dsa/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = 'intel-dsa'
 $url         = 'https://downloadmirror.intel.com/28425/Intel-Driver-and-Support-Assistant-Installer.exe'
-#               ^^^^^^^^^ consistent dl link
-$checksum    = '8F70F116ED8B19358DA8D3100C78731F55E8DCBA55EF30E85AAF441826C835DA'
+#               ^^^^^^^^^ consistent dl link (only checksum of file changes)
+$checksum    = 'A28FC79FCF0E11CF78C67B8948B26E17A7FA3A24E38AA178176C450B7E5DFA54'
 
 $packageArgs = @{
   packageName    = $packageName
