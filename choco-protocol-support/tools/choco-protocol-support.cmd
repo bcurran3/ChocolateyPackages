5d9810ff76f38bb8584e3930c368324a229f2309
bcurran3 <bcurran3@yahoo.com>
2022-04-08 13:02:28 -0700

    choco-protocol-support.0.0.1.1

--- choco-protocol-support/tools/choco-protocol-support.cmd
+++ choco-protocol-support/tools/choco-protocol-support.cmd
@@ -2,7 +2,7 @@
 setlocal enableDelayedExpansion
 mode con: cols=120 lines=8
 color 2f
-echo choco-protocol-support v0.0.1 (12/26/2018)
+echo choco-protocol-support v0.0.1.1 (04/08/2022)
 echo choco:// protocol implementation by Bill Curran (bcurran3@yahoo.com)
 echo.
 set chocoprotocolURL=%1
