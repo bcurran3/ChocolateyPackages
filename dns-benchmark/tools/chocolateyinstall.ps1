7ac26d7f5e12814c77df8e8abce68df76460f43f
bcurran3 <bcurran3@yahoo.com>
2022-05-14 14:09:46 -0700

    auto-create .ignore files

--- dns-benchmark/tools/chocolateyinstall.ps1
+++ dns-benchmark/tools/chocolateyinstall.ps1
@@ -4,3 +4,8 @@ $toolsDir    = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\DNS Benchmark.lnk" -targetPath "$toolsDir\DNSBench.exe"
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\DNS Benchmark.lnk" -targetPath "$toolsDir\DNSBench.exe"
+Get-ChildItem -Path $toolsDir -Recurse | Where {
+ $_.Extension -eq '.exe'} | % {
+ New-Item $($_.FullName + '.ignore') -Force -ItemType file
+} | Out-Null
+
