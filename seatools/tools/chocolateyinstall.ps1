c734e408b3380a5998dff56626ee9f7fe0a9c50d
bcurran3 <bcurran3@yahoo.com>
2022-06-25 15:11:03 -0700

    seatools.5.0.140

--- seatools/tools/chocolateyinstall.ps1
+++ seatools/tools/chocolateyinstall.ps1
@@ -1,18 +1,15 @@
-﻿$packageName   = 'seatools'
-$installerType = 'exe'
-$url           = 'http://www.seagate.com/files/www-content/support-content/downloads/seatools/_shared/downloads/SeaToolsforWindowsSetup.exe'
-$checksum      = 'C181762A2F4DAA32479FE2F85613E71E92D32A1026ADAB03DB85B337FCA47A45'
-$silentArgs    = '/S'
+﻿$ErrorActionPreference = 'Stop'
+$url           = 'https://www.seagate.com/files/www-content/support-content/downloads/seatools/_shared/downloads/SeaToolsWindowsInstaller.exe'
+$checksum      = '2D98BAD583599491E782CF60319DDBB6C437874F5E03E07A7ECBE3022CF5D105'
 
 $packageArgs = @{
-  packageName   = $packageName
+  packageName   = 'seatools'
   unzipLocation = $toolsDir
-  fileType      = $installerType
+  fileType      = 'exe'
   url           = $url
-  silentArgs    = $silentArgs
+  silentArgs    = '--mode unattended'
   softwareName  = 'SeaTools*' 
   checksum      = $checksum
   checksumType  = 'sha256'  
   }
 Install-ChocolateyPackage @packageArgs
-
