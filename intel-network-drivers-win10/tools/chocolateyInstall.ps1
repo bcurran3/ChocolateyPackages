44f5342e286a10ddc6823b6b81c7b986d6f51510
bcurran3 <bcurran3@yahoo.com>
2022-06-25 18:21:01 -0700

    intel-network-drivers-win10.27.4

--- intel-network-drivers-win10/tools/chocolateyInstall.ps1
+++ intel-network-drivers-win10/tools/chocolateyInstall.ps1
@@ -4,11 +4,13 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $unzipLocation  = "$toolsDir\unzipped"
 $bits           = Get-ProcessorBits
 
+Confirm-Win10
+
 # Wired Drivers
-$url            = 'https://downloadmirror.intel.com/721689/Wired_driver_27.0_32.zip'
-$checksum       = 'FA3F7AFD60151B5B18152395DD5A96A9B1CBCD83C55E903103CE3BC4C75D7248'
-$url64          = 'https://downloadmirror.intel.com/721689/Wired_driver_27.0_x64.zip'
-$checksum64     = '1840255E3CCABF68EAF3E9D4F3E015950C60C3E0DC94248ED0379601D6B69732'
+$url            = "https://downloadmirror.intel.com/735339/Wired_driver_"+$ENV:ChocolateyPackageVersion+"_32.zip"
+$checksum       = '0073F808DB8D47D937EF9265CDFD6F40151F24295D1C8E0B90C4A5FA3903444E'
+$url64          = "https://downloadmirror.intel.com/735339/Wired_driver_"+$ENV:ChocolateyPackageVersion+"_x64.zip"
+$checksum64     = 'B1D945C00FCB851B52B6DEE3A188B8F914212A3F9B0D070050A9D348CDA990CA'
 
 $packageArgs = @{
   packageName    = $packageName
@@ -27,8 +29,8 @@ Install-ChocolateyZipPackage @packageArgs
 $packageArgs = @{
   packageName    = $packageName
   Destination    = $unzipLocation
-  FileFullPath   = "$unzipLocation\Wired_driver_27.0_32.exe"
-  FileFullPath64 = "$unzipLocation\Wired_driver_27.0_x64.exe"
+  FileFullPath   = "$unzipLocation\Wired_driver_"+$ENV:ChocolateyPackageVersion+"_32.exe"
+  FileFullPath64 = "$unzipLocation\Wired_driver_"+$ENV:ChocolateyPackageVersion+"_x64.exe"
 }
 
 Get-ChocolateyUnzip @packageArgs
@@ -37,10 +39,10 @@ Get-ChocolateyUnzip @packageArgs
 pnputil /add-driver $unzipLocation\*.inf /install /subdirs
 
 # Wired PROSet drivers
-$url            = 'https://downloadmirror.intel.com/721689/Wired_PROSet_27.0_32.zip'
-$checksum       = '568BFD88D3A0F98FE656664A8B71A805627E0CA5A41F7EF11A5002E2AD67DE83'
-$url64          = 'https://downloadmirror.intel.com/721689/Wired_PROSet_27.0_x64.zip'
-$checksum64     = '2561C7CC258EB231B73443FB6FE3250621FF3558B306697972C41D1826F54CFC'
+$url            = "https://downloadmirror.intel.com/735339/Wired_PROSet_"+$ENV:ChocolateyPackageVersion+"_32.zip"
+$checksum       = '8E676AFCFD8509AEC0EADD183078EB9CCADA6AE550BCC0A493957E731DF96445'
+$url64          = "https://downloadmirror.intel.com/735339/Wired_PROSet_"+$ENV:ChocolateyPackageVersion+"_x64.zip"
+$checksum64     = 'EBEF61433978AC18646267CACF1B419B15FA97D301BB237ED70845B3BBAD35D5'
 
 $packageArgs = @{
   packageName    = $packageName
@@ -59,24 +61,29 @@ Install-ChocolateyZipPackage @packageArgs
 $packageArgs = @{
   packageName    = $packageName
   Destination    = $unzipLocation
-  FileFullPath   = "$unzipLocation\Wired_PROSet_27.0_32.exe"
-  FileFullPath64 = "$unzipLocation\Wired_PROSet_27.0_x64.exe"
+  FileFullPath   = "$unzipLocation\Wired_PROSet_"+$ENV:ChocolateyPackageVersion+"_32.exe"
+  FileFullPath64 = "$unzipLocation\Wired_PROSet_"+$ENV:ChocolateyPackageVersion+"_x64.exe"
 }
 
 Get-ChocolateyUnzip @packageArgs
 
-
+# replace this with Get-IsWin10 from chocolatey-fastanswers.extension.0.0.3
+$OSBuild=[Environment]::OSVersion.Version.Build
+if ($OSBuild -lt 22000)
+{
+# Win10 only
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'EXE'
   file           = "$unzipLocation\APPS\PROSETDX\Win32\DxSetup.exe"
   file64         = "$unzipLocation\APPS\PROSETDX\Winx64\DxSetup.exe"
   silentArgs     = '/quiet /qn /norestart'
-  validExitCodes = @(0)
+  validExitCodes = @(0,1150)
   softwareName   = 'Intel(R) Network Connections*'
 }
  
 Install-ChocolateyInstallPackage @packageArgs
+}
 
 Start-CheckandStop msiexec
 Remove-Item $unzipLocation -Recurse -EA SilentlyContinue | Out-Null
