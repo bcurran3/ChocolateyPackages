ef00451093b283d68fccc6a871959f7c1244352c
Bill Curran <bcurran3@yahoo.com>
2022-07-22 12:39:41 -0700

    advancedrun.1.51

--- advancedrun/tools/chocolateyinstall.ps1
+++ advancedrun/tools/chocolateyinstall.ps1
@@ -1,10 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName  = 'advancedrun' 
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'http://www.nirsoft.net/utils/advancedrun.zip'
-$checksum     = '1826158A41EDE17BC4A0736A22EC68C2F18FAED3839CE04BAFDC7B19D5E853EE'
-$url64        = 'http://www.nirsoft.net/utils/advancedrun-x64.zip'
-$checksum64   = '34743C6E9E940B2CD7A65447E2B0A9A033CF3BA0027BAD1929948A427CE4FB83'
 $shortcutName = 'AdvancedRun.lnk'
 $exe          = 'AdvancedRun.exe'
 
@@ -16,8 +12,7 @@ $packageArgs = @{
 }
 
 Get-ChocolateyUnzip @packageArgs
+Remove-Item "$toolsDir\*.zip" -EA SilentlyContinue | Out-Null
 
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\$shortcutName" -targetPath "$toolsDir\$exe" -WorkingDirectory "$toolsDir"
-Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$toolsDir\$exe"
-Remove-Item "$toolsDir\*.zip" -ErrorAction SilentlyContinue | Out-Null
-Remove-Item "$toolsDir\*.ignore" -ErrorAction SilentlyContinue | Out-Null
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$toolsDir\$exe"    
