bd0f40d4f7388e4f811ede0cdbed2796864f99ce
Jeroen Wiert Pluimers <jeroen.gitlab.com@pluimers.com>
2022-10-06 18:36:52 +0200

    Upgrade SetACL from 3.0.6 to 3.1.2 fixing #306
    Updates .nuspect (version) and install script (download URL and sha256)

--- setacl/tools/chocolateyInstall.ps1
+++ setacl/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName    = 'setacl' 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://helgeklein.com/downloads/SetACL/current/SetACL%20(executable%20version).zip'
-$checksum       = '09E34E294C95BA7967C5463ECA1ACB571B745620CC2F58527DD1C1F154436EE0'
+$url            = 'https://helgeklein.com/downloads/SetACL/current/SetACL%203.1.2%20(executable%20version).zip'
+$checksum       = 'BA74399A70963C156580180455FBFC0FA68EA673A64EB89010A46273C7D478CC'
 
 $packageArgs = @{
   packageName   = $packageName
