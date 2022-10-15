fecddc3b11464a42b1ae2748243acfd1cceaf365
bcurran3 <bcurran3@yahoo.com>
2022-10-15 12:47:36 -0700

    intel-processor-identification-utility.6.8.26.0630

--- intel-processor-identification-utility/tools/chocolateyinstall.ps1
+++ intel-processor-identification-utility/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $packageName    = 'intel-processor-identification-utility'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://downloadmirror.intel.com/28539/Intel%20Processor%20Identification%20Utility.exe'
-$checksum       = 'ACF990CDB2C394610581D5E731FE546DA2451A0FBC0A482C566372BBDCE97A13'
+$checksum       = 'FFA943557F13D717B286D5CCB6797654E49A889719CEF2ED5845EB76953EB5B9'
 
 $packageArgs = @{
   packageName    = $packageName
