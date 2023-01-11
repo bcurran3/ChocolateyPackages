c3739cbee88e2c35303d4cfb3cf0ce61344d59d3
bcurran3 <bcurran3@yahoo.com>
2023-01-11 10:24:15 -0800

    intel-proset-drivers.22.190.0

--- intel-proset-drivers/tools/ChocolateyInstall.ps1
+++ intel-proset-drivers/tools/ChocolateyInstall.ps1
@@ -2,12 +2,12 @@
 $packageName    = 'intel-proset-drivers' 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $unzipLocation  = "$toolsDir\unzipped"
-$url            = "https://downloadmirror.intel.com/736240/WiFi-"+$ENV:ChocolateyPackageVersion+"-Driver32_Win10.zip"
+$url            = "https://downloadmirror.intel.com/763754/WiFi-"+$ENV:ChocolateyPackageVersion+"-Driver32_Win10.exe"
 #                                                   ^^^^^^ changes
-$checksum       = 'EC9C10FBB0C9978AFC4210BCE4A87F71E5645DD1F6822EA41FABBA5CB0A9D74F'
-$url64          = "https://downloadmirror.intel.com/736240/WiFi-"+$ENV:ChocolateyPackageVersion+"-Driver64-Win10-Win11.zip"
+$checksum       = '64F1B31AFD8F842B887C99575494EBE16BB21AFC582688008344A4FA0A3A94DE'
+$url64          = "https://downloadmirror.intel.com/763754/WiFi-"+$ENV:ChocolateyPackageVersion+"-Driver64-Win10-Win11.exe"
 #                                                   ^^^^^^ changes
-$checksum64     = '3E8B762BD324355906EA8D9841789DBFFEE6491DEB1FF1591E565EE56CA51C56'
+$checksum64     = '9A6B36994D3FD2B2A869FEFD0DAFBB41A3D99E06BAE23392DBD0AF05D844AF81'
 
 # Last Windows 7+8 version was 21.40.5
 # No need to check for hardware, drivers install even if an Intel PROSet/Wireless card is not found
@@ -16,18 +16,16 @@ Confirm-Win10
 
 $packageArgs = @{
   packageName    = $packageName
-  unzipLocation  = $unzipLocation
-  fileType       = 'ZIP' 
+  fileType       = 'EXE'
   url            = $url
   checksum       = $checksum
   checksumType   = 'sha256'
-  url64          = $url64    
-  checksum64     = $checksum64  
-  checksumType64 = 'sha256'  
+  url64          = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '-silent'
+  softwareName   = ''  
 }
 
-Install-ChocolateyZipPackage @packageArgs
-
-pnputil /add-driver $unzipLocation\*.inf /install
-
-Remove-Item $unzipLocation -Recurse -Force | Out-Null
+Install-ChocolateyPackage @packageArgs
