acd2a1c7c94b75bf5fd9c9b391e8124e1f680566
Bill Curran <bcurran3@yahoo.com>
2023-02-06 14:25:15 -0800

    seatools.5.1.176

--- seatools/tools/chocolateyinstall.ps1
+++ seatools/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $url           = 'https://www.seagate.com/files/www-content/support-content/downloads/seatools/_shared/downloads/SeaToolsWindowsInstaller.exe'
                # ^ persistent
-$checksum      = '4F5EF85BD8FC9A5B53B72A448122E0F8E266CAE1FA0BA8AF50644231B943F15F'
+$checksum      = '92194258048C2C033245931D5E8388083E48301498CD774C1CD2EEA3F278A0A5'
 
 $packageArgs = @{
   packageName   = 'seatools'
