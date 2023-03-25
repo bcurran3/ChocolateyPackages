f3e4c1e82219d435fe115a1b44f6cdf6cdfb92d4
bcurran3 <bcurran3@yahoo.com>
2023-03-25 11:33:08 -0700

    dell-update.4.8.0

--- dell-update/tools/chocolateyinstall.ps1
+++ dell-update/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = 'dell-update' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://dl.dell.com/FOLDER09268420M/1/Dell-Update-Windows-Universal-Application_0991N_WIN_4.7.1_A00.EXE?uid=351dc1fb-478e-4ac6-96c1-60aaa7f8012c&fn=Dell-Update-Windows-Universal-Application_0991N_WIN_4.7.1_A00.EXE'
-$checksum    = '532b68372ce7d6ae80ac17fca2ab5d4d2c435276eaff270269075361894faab5'
+$url         = 'https://dl.dell.com/FOLDER09523896M/1/Dell-Update-Windows-Universal-Application_0RCG0_WIN_4.8.0_A00.EXE?uid=8d6690be-eddc-4b75-e9ab-fb7670b153c3&fn=Dell-Update-Windows-Universal-Application_0RCG0_WIN_4.8.0_A00.EXE'
+$checksum    = '4B09CD47E12745743F24F8878C800D1E106550B69113801CFAF1E2B78D9AB1E4'
 
 Confirm-Win10
 
