17f26593a4e63e03ee5be1935ab4aee2e81043d3
Luke Morey <lukemorey@gmail.com>
2023-03-25 14:58:47 +1100

    intel-bluetooth-drivers v22.210.0.3 + SHA1, readme

--- intel-bluetooth-drivers/tools/ChocolateyInstall.ps1
+++ intel-bluetooth-drivers/tools/ChocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = 'intel-bluetooth-drivers' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = 'https://downloadmirror.intel.com/737033/BT-22.150.0-32-64UWD-Win10-Win11.exe'
+$url64       = 'https://downloadmirror.intel.com/774106/BT-22.210.0-64UWD-Win10-Win11.exe'
 #                                                ^^^^^^ changes  ^^^^^^^^
-$checksum64  = '01BAAE8CD6CBDA906FD1A9933A7518F141BEDDD0628EFC7E986EE3A7136C7291'
+$checksum64  = 'E6B19394C1AFE787AD2347AA842C5E2CACEF6508'
 
 Confirm-Win10
 
@@ -13,7 +13,7 @@ $packageArgs = @{
   fileType       = 'EXE' 
   url64bit       = $url64
   checksum64     = $checksum64
-  checksumType64 = 'sha256'
+  checksumType64 = 'sha1'
   silentArgs     = "/quiet /norestart"
   softwareName   = ''
   validExitCodes = @(0, 3010, 1641, 14)
