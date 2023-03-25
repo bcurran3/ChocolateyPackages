f8bb2b4cea78776a37a45b382ac00b1a9234fdfa
bcurran3 <bcurran3@yahoo.com>
2023-03-25 11:45:13 -0700

    intel-bluetooth-drivers.22.210.0

--- intel-bluetooth-drivers/tools/ChocolateyInstall.ps1
+++ intel-bluetooth-drivers/tools/ChocolateyInstall.ps1
@@ -1,17 +1,22 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = 'intel-bluetooth-drivers' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = "https://downloadmirror.intel.com/768309/BT-"+$env:ChocolateyPackageVersion+"-32-64UWD-Win10-Win11.exe"
+$url         = "https://downloadmirror.intel.com/774106/BT-"+$env:ChocolateyPackageVersion+"-32UWD-Win10.exe"
 #                                                ^^^^^^ changes
-$checksum64  = 'B14A37738DFBDC34D2F1FAAD497EB8BC58D37E05A3690067336D8F9BE0B8F1AE'
+$url64       = "https://downloadmirror.intel.com/774106/BT-"+$env:ChocolateyPackageVersion+"-64UWD-Win10-Win11.exe"
+#                                                ^^^^^^ changes
+$checksum    = 'B14A37738DFBDC34D2F1FAAD497EB8BC58D37E05A3690067336D8F9BE0B8F1AE'
+$checksum64  = 'E83F7BC394B8071C8CB5B3D17631C523DD57FB38D8C9A1C4909DE1DCECDBC06E'
 
 Confirm-Win10
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
-  fileType       = 'EXE' 
+  fileType       = 'EXE'
+  url            = $url  
   url64bit       = $url64
+  checksum       = $checksum
   checksum64     = $checksum64
   checksumType64 = 'sha256'
   silentArgs     = "/quiet /norestart"
