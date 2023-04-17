a5cfe2503f01cd90800bc0e7bd899ef66564b7ee
bcurran3 <bcurran3@yahoo.com>
2023-04-17 10:45:53 -0700

    sumo.5.17.4.536

--- sumo/tools/ChocolateyInstall.ps1
+++ sumo/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $ahkExe      = 'AutoHotKey'
 $ahkFile     = "$toolsDir\SUMo_Install.ahk"
 $url         = 'https://kcsoftwares.com/files/sumo_lite.exe'
-$checksum    = '1EF6E1714C9715766548334F7E69044AC256DDED26C08082DE89EC895BD9ADFE'
+$checksum    = 'F1805EC769607B9C8F565614B92025930E87024ED048EA799275243A48267F1E'
 
 $packageArgs = @{
     packageName    = $packageName
@@ -20,3 +20,6 @@ $packageArgs = @{
 Install-ChocolateyPackage @packageArgs
 Start-Sleep -s 10
 Start-CheckandStop "SUMo"  
+
+# UPDATE INSTRUCTIONS:
+# Update checksum
