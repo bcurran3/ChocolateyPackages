83f68e0fa2805f4f7c7cee016c3d892642369566
bcurran3 <bcurran3@yahoo.com>
2023-04-17 12:46:54 -0700

    tux-paint.0.9.29

--- tux-paint/tools/ChocolateyInstall.ps1
+++ tux-paint/tools/ChocolateyInstall.ps1
@@ -4,10 +4,8 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = 'tux-paint'
   fileType       = 'EXE'
-#  file           = "$toolsDir\tuxpaint-"+"$ENV:ChocolateyPackageVersion"+"-windows-i686-installer.exe"
-#  file64         = "$toolsDir\tuxpaint-"+"$ENV:ChocolateyPackageVersion"+"-windows-x86_64-installer.exe"
-  file           = "$toolsDir\tuxpaint-0.9.28-3-windows-sdl2.0-i686-installer.exe"
-  file64         = "$toolsDir\tuxpaint-0.9.28-3-windows-sdl2.0-x86_64-installer.exe"
+  file           = "$toolsDir\tuxpaint-"+"$ENV:ChocolateyPackageVersion"+"-windows-i686-installer.exe"
+  file64         = "$toolsDir\tuxpaint-"+"$ENV:ChocolateyPackageVersion"+"-windows-x86_64-installer.exe"
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0,1)
   softwareName   = "Tux Paint*"
@@ -15,3 +13,7 @@ $packageArgs = @{
  
 Install-ChocolateyInstallPackage @packageArgs
 Remove-Item $toolsDir\*.exe -EA SilentlyContinue | Out-Null
+
+# UPDATE INSTRUCTIONS:
+# https://tuxpaint.org/download/windows/
+# Replace EXE installer files with new ones
