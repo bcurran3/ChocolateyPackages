0ec808f1eda2c8d8540f3405cd24464ad185c9ad
bcurran3 <bcurran3@yahoo.com>
2023-04-17 14:29:47 -0700

    ultimate-settings-panel.6.7

--- ultimate-settings-panel/tools/chocolateyInstall.ps1
+++ ultimate-settings-panel/tools/chocolateyInstall.ps1
@@ -5,15 +5,15 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName  = $packageName
   Destination  = "$toolsDir\extracted"
-  FileFullPath = "$toolsDir\Ultimate-Settings-Panel.zip"
+  FileFullPath = "$toolsDir\UltimateSettingsPanel.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
 
 $packageArgs = @{
   packageName    = $packageName
-  fileType       = 'MSI'
-  file           = "$toolsDir\extracted\Ultimate-Settings-Panel\Installer\Ultimate Settings Panel.msi"
+  fileType       = 'EXE'
+  file           = "$toolsDir\extracted\setup.exe"
   silentArgs     = '/quiet /qn /norestart'
   validExitCodes = @(0)
   softwareName   = 'Ultimate Settings Panel*'   
@@ -23,3 +23,6 @@ Install-ChocolateyInstallPackage @packageArgs
 
 Remove-Item "$toolsDir\extracted" -Recurse -Force -EA SilentlyContinue | Out-Null
 Remove-Item "$toolsDir\Ultimate-Settings-Panel.zip" -Force -EA SilentlyContinue | Out-Null
+
+# UPDATE INSTRUCTIONS:
+# Every versoin of the ZIP file seems to have different pathing, check it and change this script accordingly.
