48e0dc05b2af5d4991e60f87d0a619c176397439
bcurran3 <bcurran3@yahoo.com>
2023-04-30 18:25:31 -0700

    chocolatey-fastanswers.extension.0.0.2.2

--- chocolatey-fastanswers.extension/extensions/Get-IsInDomain.ps1
+++ chocolatey-fastanswers.extension/extensions/Get-IsInDomain.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
-# chocolatey-fastanswers.extension v0.0.2 by Bill Curran AKA BCURRAN3 - public domain
+# chocolatey-fastanswers.extension v0.0.2.2 by Bill Curran AKA BCURRAN3 - public domain
 
 function Get-IsInDomain{
-if ($env:LOGONSERVER) {return $true} else {return $false}
+if ($env:LOGONSERVER -ne "\\$env:COMPUTERNAME") {return $true} else {return $false}
 }
