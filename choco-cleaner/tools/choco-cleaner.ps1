ab8c1953ffa4dab1565b0ff1ce06eb062a6bdaac
bcurran3 <bcurran3@yahoo.com>
2023-05-03 19:55:15 -0700

    choco-cleaner.0.0.9.0-pre2

--- choco-cleaner/tools/choco-cleaner.ps1
+++ choco-cleaner/tools/choco-cleaner.ps1
@@ -4,7 +4,7 @@
 # LICENSE: GNU GPL v3 - https://www.gnu.org/licenses/gpl.html
 # Open a GitHub issue at https://github.com/bcurran3/ChocolateyPackages/issues if you have suggestions for improvement.
 
-Write-Host "Choco-Cleaner.ps1 v0.0.9.0 (05/03/2023) - deletes unnecessary residual Chocolatey files to free up disk space" -Foreground White
+Write-Host "Choco-Cleaner.ps1 v0.0.9.0-pre2 (05/03/2023) - deletes unnecessary residual Chocolatey files to free up disk space" -Foreground White
 Write-Host "Copyleft 2017-2023 Bill Curran (bcurran3@yahoo.com) - free for personal and commercial use`n" -Foreground White
 
 # Verify ChocolateyToolsLocation was created by Get-ToolsLocation during install and is in the environment
