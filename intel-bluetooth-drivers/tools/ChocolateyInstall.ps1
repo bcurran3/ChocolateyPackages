22972f964c2350a04c931a3d52f4a88b492f19fe
bcurran3 <bcurran3@yahoo.com>
2023-05-06 09:10:40 -0700

    intel-bluetooth-drivers.22.220.1

--- intel-bluetooth-drivers/tools/ChocolateyInstall.ps1
+++ intel-bluetooth-drivers/tools/ChocolateyInstall.ps1
@@ -1,12 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName = 'intel-bluetooth-drivers' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = "https://downloadmirror.intel.com/774106/BT-"+$env:ChocolateyPackageVersion+"-32UWD-Win10.exe"
-#                                                ^^^^^^ changes
-$url64       = "https://downloadmirror.intel.com/774106/BT-"+$env:ChocolateyPackageVersion+"-64UWD-Win10-Win11.exe"
-#                                                ^^^^^^ changes
+$IntelPackageNumber = '777669'
+$url         = "https://downloadmirror.intel.com/$IntelPackageNumber/BT-"+$env:ChocolateyPackageVersion+"-32UWD-Win10.exe"
 $checksum    = 'B14A37738DFBDC34D2F1FAAD497EB8BC58D37E05A3690067336D8F9BE0B8F1AE'
-$checksum64  = 'E83F7BC394B8071C8CB5B3D17631C523DD57FB38D8C9A1C4909DE1DCECDBC06E'
+$url64       = "https://downloadmirror.intel.com/$IntelPackageNumber/BT-"+$env:ChocolateyPackageVersion+"-64UWD-Win10-Win11.exe"
+$checksum64  = 'CADE1C5298DDCF3D65F2C10AC309D6C54B8AE40A1C5FC9D088BF5CEC60C1EB01'
 
 Confirm-Win10
 
@@ -24,4 +23,8 @@ $packageArgs = @{
   validExitCodes = @(0, 3010, 1641, 14)
    }
 
+Write-Warning "During instalation of the drivers, Bluetooth peripherals will temporarily be unavailable."
 Install-ChocolateyPackage @packageArgs
+
+# UPDATE INSTRUCTIONS:
+# Update the IntelPackageNumber and checksum variables
