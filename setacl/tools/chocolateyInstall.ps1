ffb308cc3453e4ff079909abb9d7bf98f5a78195
bcurran3 <bcurran3@yahoo.com>
2023-05-08 09:52:26 -0700

    setacl.3.1.2.86

--- setacl/tools/chocolateyInstall.ps1
+++ setacl/tools/chocolateyInstall.ps1
@@ -1,4 +1,5 @@
-﻿$packageName    = 'setacl' 
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'setacl' 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url            = 'https://helgeklein.com/downloads/SetACL/current/SetACL%203.1.2%20(executable%20version).zip'
 $checksum       = 'BA74399A70963C156580180455FBFC0FA68EA673A64EB89010A46273C7D478CC'
