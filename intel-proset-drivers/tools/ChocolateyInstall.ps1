8860cecbfe427f2c3ffeffc2c74332c880eb6638
bcurran3 <bcurran3@yahoo.com>
2023-05-09 11:12:22 -0700

    intel-proset-drivers.22.220.0

--- intel-proset-drivers/tools/ChocolateyInstall.ps1
+++ intel-proset-drivers/tools/ChocolateyInstall.ps1
@@ -2,11 +2,11 @@
 $packageName    = 'intel-proset-drivers'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $unzipLocation  = "$toolsDir\unzipped"
-$IntelPackageNumber ="774319"
+$IntelPackageNumber ="777183"
 $url            = "https://downloadmirror.intel.com/$IntelPackageNumber/WiFi-"+$ENV:ChocolateyPackageVersion+"-Driver32_Win10.exe"
 $checksum       = '64F1B31AFD8F842B887C99575494EBE16BB21AFC582688008344A4FA0A3A94DE'
 $url64          = "https://downloadmirror.intel.com/$IntelPackageNumber/WiFi-"+$ENV:ChocolateyPackageVersion+"-Driver64-Win10-Win11.exe"
-$checksum64     = 'FE5BF9207C8D57E59B6BB48059824F97AB583F878F06A9B175A55A54A3E353AC'
+$checksum64     = '51629E45887752F6EEB4C22A9BDC0229230789ECD37D987EA09C9DEDE689B3CB'
 
 # Last Windows 7+8 version was 21.40.5
 # No need to check for hardware, drivers install even if an Intel PROSet/Wireless card is not found
