4d15741538c0706acb3ad40a50a243b5f6490974
bcurran3 <bcurran3@yahoo.com>
2023-05-10 12:42:39 -0700

    xmedia-recode.3.5.7.8

--- xmedia-recode/tools/ChocolateyInstall.ps1
+++ xmedia-recode/tools/ChocolateyInstall.ps1
@@ -3,7 +3,7 @@ $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $TodaysVersion = ($ENV:ChocolateyPackageVersion -replace '[.]','')
  
 $packageArgs = @{
-  packageName    = 'xmedia-recode'
+  packageName    = "$ENV:ChocolateyPackageName"
   fileType       = 'EXE'
   file           = "$toolsDir\XMediaRecode"+$TodaysVersion+"_setup.exe"
   file64         = "$toolsDir\XMediaRecode"+$TodaysVersion+"_x64_setup.exe"
@@ -14,3 +14,6 @@ $packageArgs = @{
 Install-ChocolateyInstallPackage @packageArgs
 
 Remove-Item "$toolsDir\*.exe" -ErrorAction SilentlyContinue | Out-Null
+
+# UPDATE INSTRUCTIONS:
+# Replace the .EXE files
