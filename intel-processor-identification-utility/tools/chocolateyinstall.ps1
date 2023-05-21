2bb827bf3d0fccaef2f9cd828ea04351074456cd
bcurran3 <bcurran3@yahoo.com>
2023-05-21 11:34:56 -0700

    intel-processor-identification-utility.7.0.3

--- intel-processor-identification-utility/tools/chocolateyinstall.ps1
+++ intel-processor-identification-utility/tools/chocolateyinstall.ps1
@@ -1,8 +1,39 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName    = 'intel-processor-identification-utility'
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://downloadmirror.intel.com/28539/Intel%20Processor%20Identification%20Utility.exe' # url does not change
-$checksum       = 'F46830E9F41421CE2AE531F2FCC710E297691761C4BC4CC41ACD0A24CB10F8F5'
+$packageName   = 'intel-processor-identification-utility'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$TodaysVersion = $env:ChocolateyPackageVersion
+
+function Check-IntelProcessorGeneration {
+    $processor = (Get-WmiObject Win32_Processor).Name
+    if ($processor -match "12th") {
+        return 12
+    }
+    elseif ($processor -match "13th") {
+        return 13
+    }
+    elseif ($processor -match "14th") {
+        return 14
+    }
+    elseif ($processor -match "15th") {
+        return 15
+    }
+    else {
+        return 0
+    }
+}
+
+if (Check-IntelProcessorGeneration) {
+	# Intel 12th Generation processors and later
+	$url            = "https://downloadmirror.intel.com/28539/Intel%20Processor%20Identification%20Utility%20v"+$TodaysVersion+".exe"
+    $checksum       = '30E2B8B7333A6598F7B3F60E93A41FDF6021D58B86FD8951280AD84CC138DF3A'
+} else {
+	# Intel 11th Generation processors and earlier
+	$url            = 'https://downloadmirror.intel.com/28539/Intel%20Processor%20Identification%20Utility-Legacy.exe'
+    $checksum       = '1916BAABF0E2530C997BFB82449D0F1F14E6D036FBE86494A027A2AC553D7E9C'
+	Write-Host "  **  Installing Intel Processor Identification Utility LEGACY v6.10.28.0323" -Foreground Magenta
+}
+
+
 
 $packageArgs = @{
   packageName    = $packageName
@@ -16,3 +47,7 @@ $packageArgs = @{
 }
 
 Install-ChocolateyPackage @packageArgs  
+
+# UPDATE INSTRUCTIONS:
+# Update checksum(s)
+# Update Write-Host LEGACY version statement if changed.
