4fc9be50f4ceb0bcc00cafdf76729476034e5c04
Bill Curran <bcurran3@yahoo.com>
2023-06-06 12:00:46 -0700

    intel-processor-identification-utility.7.0.4

--- intel-processor-identification-utility/tools/chocolateyinstall.ps1
+++ intel-processor-identification-utility/tools/chocolateyinstall.ps1
@@ -25,11 +25,11 @@ function Check-IntelProcessorGeneration {
 if (Check-IntelProcessorGeneration) {
 	# Intel 12th Generation processors and later
 	$url            = "https://downloadmirror.intel.com/28539/Intel%20Processor%20Identification%20Utility%20v"+$TodaysVersion+".exe"
-    $checksum       = '30E2B8B7333A6598F7B3F60E93A41FDF6021D58B86FD8951280AD84CC138DF3A'
+    $checksum       = '711C3E19323E4BCEB845D6B3691FE2AC75AF7DCD15167ADAE7047E55D0B52D53'
 } else {
 	# Intel 11th Generation processors and earlier
 	$url            = 'https://downloadmirror.intel.com/28539/Intel%20Processor%20Identification%20Utility-Legacy.exe'
-    $checksum       = '1916BAABF0E2530C997BFB82449D0F1F14E6D036FBE86494A027A2AC553D7E9C'
+    $checksum       = 'B406D7EAAA288B066127AA0EAC296654CED904E478ADBF8D2CE8CD62E97AFB7F'
 	Write-Host "  **  Installing Intel Processor Identification Utility LEGACY v6.10.28.0323" -Foreground Magenta
 }
 
