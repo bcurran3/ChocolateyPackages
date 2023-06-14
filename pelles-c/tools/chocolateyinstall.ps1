b3c95d5981a22b1041b5dba63fd5f45a4a7b8e24
Bill Curran <bcurran3@yahoo.com>
2023-06-14 09:36:19 -0700

    pelles-c.10.0.6

--- pelles-c/tools/chocolateyinstall.ps1
+++ pelles-c/tools/chocolateyinstall.ps1
@@ -2,19 +2,16 @@
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $silentArgs     = ''
 $validExitCodes = @(0)
-$ahkExe         = 'AutoHotKey'
-$ahkFile        = "$toolsDir\PellesCInstall.ahk"
 
 $packageArgs = @{
   packageName    = 'pelles-c'
   softwareName   = 'Pelles C for Windows'
   fileType       = 'EXE'
-  silentArgs     = ''
+  silentArgs     = '/S'
   file           = "$toolsDir\setup.exe"
   validExitCodes = @(0, 3010, 1641)
   }
 
-Start-Process $ahkExe $ahkFile  
 Install-ChocolateyInstallPackage @packageArgs
 
 Remove-Item $toolsDir\*.exe -Force | Out-Null
--- pelles-c/tools/chocolateyuninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'
-$packageName    = 'pelles-c'
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$ahkExe         = 'AutoHotKey'
-$ahkFile        = "$toolsDir\PellesCUninstall.ahk"
-
-Start-Process $ahkExe $ahkFile
