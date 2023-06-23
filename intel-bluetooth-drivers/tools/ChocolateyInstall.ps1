2e7fb5f284be7fd0e87e47af7f0506f601dda858
Bill Curran <bcurran3@yahoo.com>
2023-06-23 11:09:45 -0700

    intel-bluetooth-drivers.22.230.0

--- intel-bluetooth-drivers/tools/ChocolateyInstall.ps1
+++ intel-bluetooth-drivers/tools/ChocolateyInstall.ps1
@@ -2,10 +2,10 @@
 $packageName = 'intel-bluetooth-drivers' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $IntelPackageNumber = '777669'
-$url         = "https://downloadmirror.intel.com/$IntelPackageNumber/BT-"+$env:ChocolateyPackageVersion+"-32UWD-Win10.exe"
+$url         = "https://downloadmirror.intel.com/781640/BT-22.200.0-32UWD-Win10.exe"
 $checksum    = 'B14A37738DFBDC34D2F1FAAD497EB8BC58D37E05A3690067336D8F9BE0B8F1AE'
 $url64       = "https://downloadmirror.intel.com/$IntelPackageNumber/BT-"+$env:ChocolateyPackageVersion+"-64UWD-Win10-Win11.exe"
-$checksum64  = 'CADE1C5298DDCF3D65F2C10AC309D6C54B8AE40A1C5FC9D088BF5CEC60C1EB01'
+$checksum64  = 'AC4B34354F80417929C9BBDA13515ABE75309AAFA50334218A598214374BE404'
 
 Confirm-Win10
 
