0e43768cdd62e68b64588dfcf922edb2efd09f35
Bill Curran <bcurran3@yahoo.com>
2023-06-23 14:18:37 -0700

    gpu-z.2.54.0

--- gpu-z/tools/chocolateyinstall.ps1
+++ gpu-z/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $PortableEXE = "$toolsDir\GPU-Z."+$ENV:ChocolateyPackageVersion+".exe"
 Remove-Item $ENV:ChocolateyInstall\bin\gpu-z*.exe # delete old shims
 Remove-Item $toolsDir\*.exe -Exclude "GPU-Z.$ENV:ChocolateyPackageVersion.exe" # delete old versions kept by upgrade
 Remove-Item $toolsDir\*.ignore  # delete old .ignore files (2.18.0+)
-Write-Host "" | Out-File "$toolsDir\GPU-Z.$ENV:ChocolateyPackageVersion.ignore" # create .ignore file so shim isn't created
+Write-Host "" | Out-File "$toolsDir\GPU-Z.$ENV:ChocolateyPackageVersion.exe.ignore" # create .ignore file so shim isn't created
 
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\GPU-Z.lnk" -targetPath $PortableEXE
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\GPU-Z.lnk" -targetPath $PortableEXE
