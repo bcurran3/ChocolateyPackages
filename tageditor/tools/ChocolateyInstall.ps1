2767d9f7aa36cee5381621ca45667497a16a8dc6
Bill Curran <bcurran3@yahoo.com>
2023-06-23 14:12:48 -0700

    tageditor.3.8.1

--- tageditor/tools/ChocolateyInstall.ps1
+++ tageditor/tools/ChocolateyInstall.ps1
@@ -3,7 +3,6 @@ $packageName  = 'tageditor'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $bits         = Get-ProcessorBits
 $shortcutName = 'Tag Editor.lnk'
-$exeTarget    = "tageditor.exe"
 
 Remove-Item "$toolsDir\*.exe" -Force -ErrorAction SilentlyContinue | Out-Null
 
@@ -11,17 +10,19 @@ Get-ChocolateyUnzip -FileFullPath "$toolsDir\*.zip" -Destination $toolsDir
 
 if ($bits -eq 64)
    {
-    Rename-Item "$toolsDir\tageditor-$ENV:ChocolateyPackageVersion-i686-w64-mingw32.exe" 'tageditor.exe'
+	$exeTarget = "tageditor-$ENV:ChocolateyPackageVersion-i686-w64-mingw32.exe"
+	Install-BinFile -Name tageditor -Path "$toolsDir\tageditor-$ENV:ChocolateyPackageVersion-i686-w64-mingw32-cli.exe"
 	Remove-Item "$toolsDir\tageditor-$ENV:ChocolateyPackageVersion-x86_64-w64-mingw32.exe"
    } else {
-    Rename-Item "$toolsDir\tageditor-$ENV:ChocolateyPackageVersion-x86_64-w64-mingw32.exe" 'tageditor.exe'
-	Rename-Item "$toolsDir\tageditor-$ENV:ChocolateyPackageVersion-i686-w64-mingw32.exe"
+	$exeTarget = "tageditor-$ENV:ChocolateyPackageVersion-x86_64-w64-mingw32.exe"
+	Install-BinFile -Name tageditor -Path "$toolsDir\tageditor-$ENV:ChocolateyPackageVersion-x86_64-w64-mingw32-cli.exe"
+	Remove-Item "$toolsDir\tageditor-$ENV:ChocolateyPackageVersion-i686-w64-mingw32.exe"
    }
 
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\$shortcutName" -targetPath "$toolsDir\$exeTarget" -WorkingDirectory "$toolsDir\"
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -targetPath "$toolsDir\$exeTarget" -WorkingDirectory "$toolsDir\"
 
-Remove-Item "$toolsDir\*.zip" -Force -ErrorAction SilentlyContinue
+Remove-Item $toolsDir\*.zip -Force | Out-Null
 
 # UPDATE INSTRUCTIONS:
 # https://github.com/Martchus/tageditor/releases
--- tageditor/tools/ChocolateyUninstall.ps1
+++ tageditor/tools/ChocolateyUninstall.ps1
@@ -2,5 +2,6 @@
 $packageName  = 'tageditor' 
 $shortcutName = 'Tag Editor.lnk'
 
+Uninstall-BinFile tageditor
 Remove-Item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
 Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
