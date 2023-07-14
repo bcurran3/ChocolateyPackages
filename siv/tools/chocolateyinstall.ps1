ca2a59c96793a85932967dd5d1e9a9e53648a9c1
Bill Curran <bcurran3@yahoo.com>
2023-07-14 09:46:11 -0700

    siv.5.71.0

--- siv/tools/chocolateyinstall.ps1
+++ siv/tools/chocolateyinstall.ps1
@@ -2,15 +2,17 @@
 $packageName = 'siv' 
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $bits        = Get-ProcessorBits
+$file        = Get-Item $toolsDir\siv*.zip
+
+
 
 $packageArgs = @{
   packageName    = $packageName
-  unzipLocation  = $toolsDir
-  fileType       = 'ZIP' 
-  url            = "$toolsDir\siv_v$env:ChocolateyPackageVersion.zip"
-  }
+  Destination    = $toolsDir
+  FileFullPath   = "$file"
+}
 
-Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyUnzip @packageArgs
 
 if ($bits -eq 64)
    {
@@ -20,7 +22,9 @@ if ($bits -eq 64)
     Install-ChocolateyShortcut -shortcutFilePath "$env:Public\Desktop\SIV.lnk" -targetPath "$toolsDir\SIV32X.exe" -WorkingDirectory "$toolsDir"
     Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\SIV.lnk" -targetPath "$toolsDir\SIV32X.exe"       
    }
+   
 Remove-Item "$toolsDir\*.zip" | Out-Null
+
 Get-ChildItem -Path $toolsDir -Recurse | Where {
  $_.Extension -eq '.exe'} | % {
  New-Item $($_.FullName + '.ignore') -Force -ItemType file
