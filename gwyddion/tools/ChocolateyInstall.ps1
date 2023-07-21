bd9dee5307f92bc6f9a5ba5dfd97f6e2c92825c0
Bill Curran <bcurran3@yahoo.com>
2023-07-21 11:18:20 -0700

    gwyddion.2.63.0

---
+++ gwyddion/tools/ChocolateyInstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'gwyddion'
+$bits        = Get-ProcessorBits
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$file        = Get-Item $toolsDir\Gwyddion*.exe
+
+if ($bits -eq 64){
+	$installer=$(Get-Item $toolsDir\Gwyddion-*win64.exe).name
+} else {
+	$installer=$(Get-Item $toolsDir\Gwyddion-*win32.exe).name
+}
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  file           = "$toolsDir\$installer"
+  validExitCodes = @(0)
+  silentArgs     = '/S'
+  softwareName   = 'Gwyddion'
+}
+
+Install-ChocolateyInstallPackage @packageArgs 	
+
+Remove-Item $toolsDir\*.exe -EA SilentlyContinue | Out-Null
+
+# UPDATE INSTRUCTIONS
+# Replace EXE files
