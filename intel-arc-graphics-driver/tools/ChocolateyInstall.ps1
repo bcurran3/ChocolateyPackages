aebc4e2b8b199d2dcd4e52b195d1edde0cf321dc
Bill Curran <bcurran3@yahoo.com>
2023-07-21 09:45:27 -0700

    intel-arc-graphics-driver.31.0.101.4575

--- intel-arc-graphics-driver/tools/ChocolateyInstall.ps1
+++ intel-arc-graphics-driver/tools/ChocolateyInstall.ps1
@@ -2,9 +2,9 @@
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $TodaysArray   = ($env:ChocolateyPackageVersion.split('.'))
 $TodaysVersion = $TodaysArray[2] + "." + $TodaysArray[3]
-$IntelPackageNumber = '781902'
+$IntelPackageNumber = '784083'
 $url64       = "https://downloadmirror.intel.com/$IntelPackageNumber/gfx_win_"+"$TodaysVersion"+".exe"
-$checksum64  = '89D4D5C7E6EADD82518CEDF827ED3FF08493483F7A7F5665E7C3E3A3BE86C705'
+$checksum64  = 'EE12EBA7AB15561FBF5559EFA5502C34AAF1E06378F92FCB2C1FB39F43C13D13'
 
 Confirm-WinMinimumBuild 19042
 if (!(Get-IsIntelVideo)){
@@ -27,4 +27,4 @@ $packageArgs = @{
 Install-ChocolateyPackage @packageArgs
 
 # UPDATE INSTRUCTIONS:
-# To updtae this package update the IntelPackageNumber and checksum64 variables
+# To update this package update the IntelPackageNumber and checksum64 variables
