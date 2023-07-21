79b1f87d21f2c3fceae151f5d41a09c6314c8aa2
Bill Curran <bcurran3@yahoo.com>
2023-07-21 10:38:59 -0700

    gnu-units.2.22.0

---
+++ gnu-units/tools/ChocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName    = 'gnu-units'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  file           = "$toolsDir\units-2.22-setup.exe"
+  validExitCodes = @(0)
+  silentArgs     = '/VERYSILENT /NOCANCEL /CLOSEAPPLICATIONS /RESTART APPLICATIONS /NORESTART'
+  softwareName   = 'units version 2.*'
+}
+
+Install-ChocolateyInstallPackage @packageArgs 	
+
+Remove-Item $toolsDir\*.exe -EA SilentlyContinue | Out-Null
+Remove-Item $toolsDir\*.sig -EA SilentlyContinue | Out-Null
+
+# UPDATE INSTRUCTIONS
+# Replace EXE and SIG files
