c8b19d618e0809ee23fd0979811c92403c108205
Bill Curran <bcurran3@yahoo.com>
2023-08-12 17:16:40 -0700

    intel-killer-performance-suite.34.23.7168

--- intel-killer-performance-suite/tools/chocolateyInstall.ps1
+++ intel-killer-performance-suite/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName    = 'intel-killer-performance-suite' 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$IntelPackageNumber = '781426'
+$IntelPackageNumber = '785179'
 $url64          = "https://downloadmirror.intel.com/$IntelPackageNumber/KillerPerformanceSuite_"+$env:ChocolateyPackageVersion+"_Win10-11x64.exe"
-$checksum64     = '39D4E3E8ECCC50C94461C3B4264ACD46F87E95F1D35A8357B25375736252DB0C'
+$checksum64     = 'E63DBF406BA085D539966BDD3083AC0CFA7766BE57246543C77A74A1E6438F7E'
 
 Confirm-Win10
 
