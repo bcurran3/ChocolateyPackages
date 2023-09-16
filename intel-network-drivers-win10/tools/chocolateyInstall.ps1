9a6fab1cce659b11d8f72f28f61d8ed91e330218
Bill Curran <bcurran3@yahoo.com>
2023-09-16 14:20:39 -0700

    intel-network-drivers-win10.28.2.0

--- intel-network-drivers-win10/tools/chocolateyInstall.ps1
+++ intel-network-drivers-win10/tools/chocolateyInstall.ps1
@@ -1,16 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName    = 'intel-network-drivers-win10' 
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$unzipLocation  = "$toolsDir\unzipped"
-$bits           = Get-ProcessorBits
+$packageName   = 'intel-network-drivers-win10' 
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$unzipLocation = "$toolsDir\unzipped"
+$bits          = Get-ProcessorBits
+$TodaysVersion = $ENV:ChocolateyPackageVersion.trim(".0,")
 $IntelPackageNumber = "785800"
 
 Confirm-Win10
 
 # Drivers
-$url            = "https://downloadmirror.intel.com/$IntelPackageNumber/Wired_driver_"+$ENV:ChocolateyPackageVersion+"_32.zip"
+$url            = "https://downloadmirror.intel.com/$IntelPackageNumber/Wired_driver_"+$TodaysVersion+"_32.zip"
 $checksum       = 'C9E02CDC17E9739B3AF0458CD16535E64A15C218FCD459835D2425B3D2449CB8'
-$url64          = "https://downloadmirror.intel.com/$IntelPackageNumber/Wired_driver_"+$ENV:ChocolateyPackageVersion+"_x64.zip"
+$url64          = "https://downloadmirror.intel.com/$IntelPackageNumber/Wired_driver_"+$TodaysVersion+"_x64.zip"
 $checksum64     = '0D10E40853836E94F3CDFFD566CAF324259F776258A7E9757D159BE62C706BAD'
 
 $packageArgs = @{
@@ -30,8 +31,8 @@ Install-ChocolateyZipPackage @packageArgs
 $packageArgs = @{
   packageName    = $packageName
   Destination    = $unzipLocation
-  FileFullPath   = "$unzipLocation\Wired_driver_"+$ENV:ChocolateyPackageVersion+"_32.exe"
-  FileFullPath64 = "$unzipLocation\Wired_driver_"+$ENV:ChocolateyPackageVersion+"_x64.exe"
+  FileFullPath   = "$unzipLocation\Wired_driver_"+$TodaysVersion+"_32.exe"
+  FileFullPath64 = "$unzipLocation\Wired_driver_"+$TodaysVersion+"_x64.exe"
 }
 
 Get-ChocolateyUnzip @packageArgs
@@ -40,9 +41,9 @@ Get-ChocolateyUnzip @packageArgs
 pnputil /add-driver $unzipLocation\*.inf /install /subdirs
 
 # PROSet software
-$url            = "https://downloadmirror.intel.com/$IntelPackageNumber/Wired_PROSet_"+$ENV:ChocolateyPackageVersion+"_32.zip"
+$url            = "https://downloadmirror.intel.com/$IntelPackageNumber/Wired_PROSet_"+$TodaysVersion+"_32.zip"
 $checksum       = '00300C2934305A6438A985131346899FE264E74E338E6E1996E9F230D3E4E57D'
-$url64          = "https://downloadmirror.intel.com/$IntelPackageNumber/Wired_PROSet_"+$ENV:ChocolateyPackageVersion+"_x64.zip"
+$url64          = "https://downloadmirror.intel.com/$IntelPackageNumber/Wired_PROSet_"+$TodaysVersion+"_x64.zip"
 $checksum64     = 'F7DB9FC5A499BBFFEEA85C4B5C2876DEC8AAD84A439F0EC9AAE16B5130CF8194'
 
 $packageArgs = @{
@@ -62,8 +63,8 @@ Install-ChocolateyZipPackage @packageArgs
 $packageArgs = @{
   packageName    = $packageName
   Destination    = $unzipLocation
-  FileFullPath   = "$unzipLocation\Wired_PROSet_"+$ENV:ChocolateyPackageVersion+"_32.exe"
-  FileFullPath64 = "$unzipLocation\Wired_PROSet_"+$ENV:ChocolateyPackageVersion+"_x64.exe"
+  FileFullPath   = "$unzipLocation\Wired_PROSet_"+$TodaysVersion+"_32.exe"
+  FileFullPath64 = "$unzipLocation\Wired_PROSet_"+$TodaysVersion+"_x64.exe"
 }
 
 Get-ChocolateyUnzip @packageArgs
