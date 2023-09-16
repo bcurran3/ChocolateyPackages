35f318de8f78be8a1fc9a6ad705d3a914d57e06c
Bill Curran <bcurran3@yahoo.com>
2023-09-16 14:51:21 -0700

    flicflac.1.11.0

--- flicflac/tools/chocolateyinstall.ps1
+++ flicflac/tools/chocolateyinstall.ps1
@@ -1,4 +1,21 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName  = 'flicflac' 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName = 'flicflac'
 
+if (Test-Path "$toolsDir\FlicFlac.exe") {Remove-Item "$toolsDir\FlicFlac.exe"}
+
+$packageArgs = @{
+  packageName    = $packageName
+  Destination    = $toolsDir
+  FileFullPath   = "$toolsDir\flicflac.zip"
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+New-Item "$toolsDir\FlicFlac.exe.ignore" -Force -ItemType file
+Remove-Item "$toolsDir\*.zip" -EA SilentlyContinue | Out-Null
 Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\FlicFlac.lnk" -targetPath "$ENV:ChocolateyInstall\lib\flicflac\tools\FlicFlac.exe"
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\FlicFlac.lnk" -targetPath "$toolsDir\FlicFlac.exe"    
+
+# UPDATE INSTRUCTIONS
+# Replace ZIP file
--- flicflac/tools/chocolateyuninstall.ps1
+++ flicflac/tools/chocolateyuninstall.ps1
@@ -2,3 +2,4 @@
 $packageName = 'flicflac'
 
 Remove-Item "$ENV:Public\Desktop\FlicFlac.lnk" -Force -ErrorAction SilentlyContinue
+Remove-Item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\FlicFlac.lnk" -Force -ErrorAction SilentlyContinue
