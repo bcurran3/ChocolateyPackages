337a9e3cb0ee4269f0841861778ee560d5218359
Bill Curran <bcurran3@yahoo.com>
2023-09-16 13:59:03 -0700

    intel-proset-drivers.22.240.0

--- intel-proset-drivers/tools/ChocolateyInstall.ps1
+++ intel-proset-drivers/tools/ChocolateyInstall.ps1
@@ -2,13 +2,14 @@
 $packageName    = 'intel-proset-drivers'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $unzipLocation  = "$toolsDir\unzipped"
-$IntelPackageNumber ="777183"
-$url            = "https://downloadmirror.intel.com/$IntelPackageNumber/WiFi-"+$ENV:ChocolateyPackageVersion+"-Driver32_Win10.exe"
+$IntelPackageNumber ="784628"
+$url            = "https://downloadmirror.intel.com/784628/WiFi-22.160.0-Driver32-Win10.exe"
 $checksum       = '64F1B31AFD8F842B887C99575494EBE16BB21AFC582688008344A4FA0A3A94DE'
 $url64          = "https://downloadmirror.intel.com/$IntelPackageNumber/WiFi-"+$ENV:ChocolateyPackageVersion+"-Driver64-Win10-Win11.exe"
-$checksum64     = '51629E45887752F6EEB4C22A9BDC0229230789ECD37D987EA09C9DEDE689B3CB'
+$checksum64     = '565FFF61AA1D3A3357778779ED4D21763DC4536CE9240AC3E876A7E086927E5D'
 
 # Last Windows 7+8 version was 21.40.5
+# Last Windows 10 32-bit version was 22.160.0
 # No need to check for hardware, drivers install even if an Intel PROSet/Wireless card is not found
 
 Confirm-Win10
