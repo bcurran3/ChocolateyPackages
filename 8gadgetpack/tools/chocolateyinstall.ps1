dade9b9cbb63288899efca7ec8f9d25e91b07dd4
Yakov Lipkovich <github@yakov.ca>
2023-09-20 19:42:17 -0600

    Bump 8gadgetpack to 37.0

--- 8gadgetpack/tools/chocolateyinstall.ps1
+++ 8gadgetpack/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName    = '8gadgetpack'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://8gadgetpack.net/dl_340/8GadgetPackSetup.msi'
-$checksum       = 'DFD69B596DFC48A4CC1260FDCA354B048DC0614801E2057B95A135CE3EF8BF12'
+$url            = 'https://8gadgetpack.net/dl_370/8GadgetPackSetup.msi'
+$checksum       = '821E33CF757BD01BEC6703796C01726E6674B8DE3BC1E7EA834318039E46909E'
 
 $packageArgs = @{
   packageName   = $packageName
