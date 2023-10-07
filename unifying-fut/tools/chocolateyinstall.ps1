44af184cf87c8d485dfe0e640e540f726e51e050
Bill Curran <bcurran3@yahoo.com>
2023-10-07 12:59:35 -0700

    unifying-fut.3.2.276959

--- unifying-fut/tools/chocolateyinstall.ps1
+++ unifying-fut/tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url      = 'https://download01.logi.com/web/ftp/pub/techsupport/keyboards/FirmwareUpdateTool_2.6.201219_x86.exe'
-$checksum = '38BCA469FF6418ADDD22A4AFFAB9F15BF96C2CE704CFC9D0A2AB51307A29F888'
-$exe      = 'FirmwareUpdateTool_2.6.201219_x86.exe'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64        = 'https://download01.logi.com/web/ftp/pub/techsupport/keyboards/FirmwareUpdateTool_3.2.276959_x64.exe'
+$checksum64   = 'C02429E3B3CF9F53233B85CA6971A54BEA20830C3BC32DF788A142813E02FC38'
+$exe          = 'FirmwareUpdateTool_2.6.201219_x86.exe'
 $shortcutName = 'Logitech Firmware Update Tool.lnk'
 
 
 $packageArgs = @{
     packageName    = 'unifying-fut'
-    url            = $url
-    checkum        = $checksum
-    checksumType   = 'sha256'
+    url64          = $url64
+    checkum64      = $checksum64
+    checksumType64 = 'sha256'
     fileType       = 'EXE'
     FileFullPath   = "$toolsDir\$exe"
 }
