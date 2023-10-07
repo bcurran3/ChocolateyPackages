467d39aeaf260fd6c6af6c4fee8f9b272a122fe5
Bill Curran <bcurran3@yahoo.com>
2023-10-07 13:59:35 -0700

    intel-processor-identification-utility.7.0.4.231007

--- intel-processor-identification-utility/tools/chocolateyinstall.ps1
+++ intel-processor-identification-utility/tools/chocolateyinstall.ps1
@@ -1,7 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName   = 'intel-processor-identification-utility'
-$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName = 'intel-processor-identification-utility'
 $TodaysVersion = $env:ChocolateyPackageVersion
+#$url         = "https://downloadmirror.intel.com/28539/Intel%20Processor%20Identification%20Utility%20v"+$TodaysVersion+".exe"
+$url         = 'https://downloadmirror.intel.com/28539/Intel%20Processor%20Identification%20Utility%20v7.0.4.exe'
+$checksum    = '711C3E19323E4BCEB845D6B3691FE2AC75AF7DCD15167ADAE7047E55D0B52D53'
 
 function Check-IntelProcessorGeneration {
     $processor = (Get-WmiObject Win32_Processor).Name
@@ -22,17 +25,12 @@ function Check-IntelProcessorGeneration {
     }
 }
 
-if (Check-IntelProcessorGeneration) {
-	# Intel 12th Generation processors and later
-	$url            = "https://downloadmirror.intel.com/28539/Intel%20Processor%20Identification%20Utility%20v"+$TodaysVersion+".exe"
-    $checksum       = '711C3E19323E4BCEB845D6B3691FE2AC75AF7DCD15167ADAE7047E55D0B52D53'
-} else {
+if (!(Check-IntelProcessorGeneration)) {
 	# Intel 11th Generation processors and earlier
-	$url            = 'https://downloadmirror.intel.com/28539/Intel%20Processor%20Identification%20Utility-Legacy.exe'
-    $checksum       = 'B406D7EAAA288B066127AA0EAC296654CED904E478ADBF8D2CE8CD62E97AFB7F'
-	Write-Host "  **  Installing Intel Processor Identification Utility LEGACY v6.10.28.0323" -Foreground Magenta
+	Write-Host "  **  intel-processor-identification-utility is for 12th Gen and newer processors." -Foreground Yellow
+	Write-Host "  **  Use intel-processor-identification-utility-legacy for 11th Gen and older processors."  -Foreground Yellow
+	throw
 }
-
 
 
 $packageArgs = @{
@@ -41,7 +39,7 @@ $packageArgs = @{
   url            = $url
   validExitCodes = @(0, 3010, 1641)
   silentArgs     = '/quiet /norestart'
-  softwareName   = 'Intel(R) Processor Identification Utility'
+  softwareName   = 'Intel® Processor Identification Utility'
   checksum       = $checksum
   checksumType   = 'sha256' 
 }
@@ -50,4 +48,4 @@ Install-ChocolateyPackage @packageArgs
 
 # UPDATE INSTRUCTIONS:
 # Update checksum(s)
-# Update Write-Host LEGACY version statement if changed.
+
