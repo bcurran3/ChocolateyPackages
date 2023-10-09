cac1b837242a7c678c47b92615425b645fef5bfc
Bill Curran <bcurran3@yahoo.com>
2023-10-09 14:44:02 -0700

    intel-proset-drivers.22.250.1

--- intel-proset-drivers/tools/ChocolateyInstall.ps1
+++ intel-proset-drivers/tools/ChocolateyInstall.ps1
@@ -2,11 +2,11 @@
 $packageName    = 'intel-proset-drivers'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $unzipLocation  = "$toolsDir\unzipped"
-$IntelPackageNumber ="784628"
+$IntelPackageNumber = "788770"
 $url            = "https://downloadmirror.intel.com/784628/WiFi-22.160.0-Driver32-Win10.exe"
 $checksum       = '64F1B31AFD8F842B887C99575494EBE16BB21AFC582688008344A4FA0A3A94DE'
 $url64          = "https://downloadmirror.intel.com/$IntelPackageNumber/WiFi-"+$ENV:ChocolateyPackageVersion+"-Driver64-Win10-Win11.exe"
-$checksum64     = '565FFF61AA1D3A3357778779ED4D21763DC4536CE9240AC3E876A7E086927E5D'
+$checksum64     = '0540CAF5723053CC31B4BB6781551A5150598BFF5C7C906254C7ECB0A365E0A9'
 
 # Last Windows 7+8 version was 21.40.5
 # Last Windows 10 32-bit version was 22.160.0
