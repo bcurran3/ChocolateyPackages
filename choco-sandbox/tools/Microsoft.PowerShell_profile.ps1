c393dd1d77d46140a35d8771fcc08746806716e4
Bill Curran <bcurran3@yahoo.com>
2023-10-09 15:48:22 -0700

    choco-sandbox.1.0.0

--- choco-sandbox/tools/Microsoft.PowerShell_profile.ps1
+++ choco-sandbox/tools/Microsoft.PowerShell_profile.ps1
@@ -3,10 +3,10 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
-function cinst {choco install}
-function cuninst {choco uninstall}
-function cup {choco upgrade}
-function clist {choco list}
+function cinst {choco install $args}
+function cuninst {choco uninstall $args}
+function cup {choco upgrade $args}
+function clist {choco list $args}
 function cweb {param ([string]$package ) 
 if (($package -eq '') -or ($package -eq '.')) {$package=(Split-Path -Leaf $(Get-Location).Path)}
 start https://community.chocolatey.org/packages/$package
