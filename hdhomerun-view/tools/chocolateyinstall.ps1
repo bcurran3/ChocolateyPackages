8a08a96fddeae75414e4f37a6f4c607559bcee9a
Bill Curran <bcurran3@yahoo.com>
2023-10-29 12:51:50 -0700

    hdhomerun-view.2023.7.19

--- hdhomerun-view/tools/chocolateyinstall.ps1
+++ hdhomerun-view/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url            = 'https://download.silicondust.com/hdhomerun/hdhomerun_windows.exe' 
-$checksum       = '0F19D183921CFF3D7F7DCDF74D658EC6971D11C78DA08D40180CA2F83156D9DA'
+$url            = 'https://download.silicondust.com/hdhomerun/hdhomerun_windows.exe'
+$checksum       = '24A1573A42766E2250BED77538E327B28C087634C85A853A787A52230D4DCD5F'
 
 $packageArgs = @{
   packageName   = $env:chocolateyPackageName
