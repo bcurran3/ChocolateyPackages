3667ecc6173f6466783bc11af6dd4dbf3169aa5f
Bill Curran <bcurran3@yahoo.com>
2024-01-23 15:56:58 -0800

    intel-arc-graphics-driver.31.0.101.5123

--- intel-arc-graphics-driver/tools/ChocolateyInstall.ps1
+++ intel-arc-graphics-driver/tools/ChocolateyInstall.ps1
@@ -2,10 +2,10 @@
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $TodaysArray   = ($env:ChocolateyPackageVersion.split('.'))
 $TodaysVersion = $TodaysArray[2] + "." + $TodaysArray[3]
-$IntelPackageNumber = '812114'
+$IntelPackageNumber = '813048'
 #$url64       = "https://downloadmirror.intel.com/$IntelPackageNumber/gfx_win_"+"$TodaysVersion"+".exe"
-$url64       = "https://downloadmirror.intel.com/812114/gfx_win_101.5084_101.5122.exe"
-$checksum64  = '414390B649D9DEF62E5F7C6DDEB2C7AD3B504171B1D7133D812D6578668120BE'
+$url64       = "https://downloadmirror.intel.com/813048/gfx_win_101.5085_101.5122.exe"
+$checksum64  = '185D1F9DC2B49676A0FA7617C68873CF80DF8F77959E64F3230A0C6B304803F0'
 
 Confirm-WinMinimumBuild 19042
 if (!(Get-IsIntelVideo)){
@@ -13,8 +13,8 @@ if (!(Get-IsIntelVideo)){
     throw
    }
    
-#Write-Host "  ** These drivers are for Intel 11th Gen processors and higher." -Foreground Yellow
-#Write-Host "  ** Use intel-graphics-driver for Intel 6th thru 10th Gen processors." -Foreground Yellow
+Write-Host "  ** These drivers are for Intel 11th Gen processors and higher." -Foreground Yellow
+Write-Host "  ** Use intel-graphics-driver for Intel 6th thru 10th Gen processors." -Foreground Yellow
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
