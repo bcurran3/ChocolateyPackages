931219bd03d9e418d7f9fd6d06db4a3049ac032c
Bill Curran <bcurran3@yahoo.com>
2024-08-25 19:18:09 -0700

    .basename

--- choco-sandbox/tools/choco-sandbox.ps1
+++ choco-sandbox/tools/choco-sandbox.ps1
@@ -1,10 +1,10 @@
-﻿# Choco-Sandbox.ps1 Copyleft 2023 by Bill Curran AKA BCURRAN3
+﻿# Choco-Sandbox.ps1 Copyleft 2023-2024 by Bill Curran AKA BCURRAN3
 # LICENSE: GNU GPL v3 - https://www.gnu.org/licenses/gpl.html
 # Suggestions? Problems? Open a GitHub issue at https://github.com/bcurran3/ChocolateyPackages/issues
 
 Clear-Host
-Write-Host "Choco-Sandbox.ps1 v1.0.0 (2023-10-02) - Sets up Windows Sandbox with Chocolatey Installed" -Foreground White
-Write-Host "Copyleft 2023 Bill Curran (bcurran3@yahoo.com) - free for personal and commercial use`n" -Foreground White
+Write-Host "Choco-Sandbox.ps1 v1.0.1 (2024-08-25) - Sets up Windows Sandbox with Chocolatey Installed" -Foreground White
+Write-Host "Copyleft 2023-2024 Bill Curran (bcurran3@yahoo.com) - free for personal and commercial use`n" -Foreground White
 
 $InstalledFromWeb=$False
 
@@ -18,33 +18,35 @@ Copy-Item "C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\Microsoft.PowerShel
 # Install Chocolatey from previously downloadeded file so we don't skew the download count too much for Rob and the boys
 if (Test-Path C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\chocolatey.zip){
 	Write-Host "  ** Installing Chocolatey from cached download." -Foreground Magenta
-	Expand-Archive C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\chocolatey.zip -DestinationPath C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\chocolatey -Force | Out-Null
-	C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\chocolatey\tools\chocolateyInstall.ps1  | Out-Null
+	Expand-Archive C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\chocolatey.zip -DestinationPath C:\Windows\Temp\chocolatey -Force | Out-Null
+	C:\Windows\Temp\chocolatey\tools\chocolateyInstall.ps1  | Out-Null
 	Copy-Item C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\chocolatey.zip C:\ProgramData\chocolatey\lib\chocolatey\chocolatey.nupkg
-	Copy-Item C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\chocolatey\chocolatey.nuspec C:\ProgramData\chocolatey\lib\chocolatey
-	Remove-Item C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\chocolatey -Recurse -Force
+	Copy-Item C:\Windows\Temp\chocolatey\chocolatey.nuspec C:\ProgramData\chocolatey\lib\chocolatey
+#	Remove-Item C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\chocolatey -Recurse -Force
 } else {
 # Install Chocolatey from the web
     Write-Host "  ** Installing Chocolatey from the web." -Foreground Magenta
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) | Out-Null
 # Save chocolatey.zip for future use so we don't have to continuously download it
-	Move-Item C:\Users\WDAGUtilityAccount\AppData\Local\Temp\chocolatey\chocoInstall\chocolatey.zip C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox
+    Copy-Item C:\ProgramData\chocolatey\lib\chocolatey\chocolatey.nupkg C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\chocolatey.zip -Force
 	$InstalledFromWeb=$True
 }
 
 # Upgrade Chocolatey if outdated
 if (!($InstalledFromWeb)){
 	Write-Host "  ** Making sure Chocolatey is up-to-date..." -Foreground Magenta
-    choco upgrade chocolatey
-    if (Test-Path C:\Users\WDAGUtilityAccount\AppData\Local\Temp\chocolatey\chocoInstall\chocolatey.zip){
-		Move-Item C:\Users\WDAGUtilityAccount\AppData\Local\Temp\chocolatey\chocoInstall\chocolatey.zip C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox -Force
-		}
+    choco upgrade chocolatey -y
+    Copy-Item C:\ProgramData\chocolatey\lib\chocolatey\chocolatey.nupkg C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\chocolatey.zip -Force
 }
 
 # Install packages
 if (Test-Path C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\packages.config){
 	Write-Host "  ** Installing packages from packages.config..." -Foreground Magenta
-	choco install C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\packages.config
+	choco install C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\packages.config -y
+} else {
+	if (Test-Path C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\packages.config.example){
+		Write-Host "  ** HINT: Rename packages.config.example to packages.config and edit to have packages automatically installed." -Foreground Cyan
+	}
 }
 
 # Load Chocolatey PowerShell profile
@@ -54,6 +56,10 @@ if (Test-Path C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\packages.config)
 if (Test-Path C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\choco-sandbox-customize.ps1){
 	Write-Host "  ** Running choco-sandbox-customize.ps1..." -Foreground Magenta
 	& C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\choco-sandbox-customize.ps1 -ErrorActtion SilentlyContinue
+} else {
+	if (Test-Path C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox\choco-sandbox-customize.ps1.example){
+		Write-Host "  ** HINT: Rename choco-sandbox-customize.ps1.example to choco-sandbox-customize.ps1 and edit to script anything else you want automated." -Foreground Cyan
+	}
 }
 
 Set-Location "C:\Users\WDAGUtilityAccount\Desktop\choco-sandbox"
